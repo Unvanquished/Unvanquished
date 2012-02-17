9265ede44370fe1355e73ce72121026dcf586efa
Sixthly <contact@sixthly.net>
2012-02-17 13:51:26 +1000

    Revert "Revert b77be509fe9f853c377328fd0c8c56ce3aa0b9ef^..HEAD"
    This reverts commit c92f09e905f9c9e59f74aad5f4bb6d282e83f43a.

--- src/gamelogic/gpp/src/game/g_admin.c
+++ src/gamelogic/gpp/src/game/g_admin.c
@@ -120,6 +120,11 @@ g_admin_cmd_t g_admin_cmds[ ] =
       ""
     },
 
+    {"listrotation", G_admin_listrotation, qfalse, "listrotation",
+      "display the active map rotation",
+      ""
+    },
+
     {"lock", G_admin_lock, qfalse, "lock",
       "lock a team to prevent anyone from joining it",
       "[^3a|h^7]"
@@ -2365,6 +2370,11 @@ qboolean G_admin_listplayers( gentity_t *ent )
   return qtrue;
 }
 
+qboolean G_admin_listrotation( gentity_t *ent )
+{
+    G_PrintCurrentRotation( ent, "listrotation" );
+}
+
 static qboolean ban_matchip( void *ban, const void *ip )
 {
   return G_AddressCompare( &((g_admin_ban_t *)ban)->ip, (addr_t *)ip ) ||
@@ -2468,7 +2478,7 @@ qboolean G_admin_adminhelp( gentity_t *ent )
     {
       if( G_admin_permission( ent, g_admin_cmds[ i ].flag ) )
       {
-        ADMBP( va( "^3%-12s", g_admin_cmds[ i ].keyword ) );
+        ADMBP( va( "^3%-13s", g_admin_cmds[ i ].keyword ) );
         count++;
         // show 6 commands per line
         if( count % 6 == 0 )
@@ -2479,7 +2489,7 @@ qboolean G_admin_adminhelp( gentity_t *ent )
     {
       if( !G_admin_permission( ent, c->flag ) )
         continue;
-      ADMBP( va( "^3%-12s", c->command ) );
+      ADMBP( va( "^3%-13s", c->command ) );
       count++;
       // show 6 commands per line
       if( count % 6 == 0 )
