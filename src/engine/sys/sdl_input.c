f40df7eaa72a18e132c82ff7dac6ac4a529691e2
Sixthly <contact@sixthly.net>
2012-02-22 01:12:54 +1000

    Removed annoying MACOS_X_ACCELERATION_HACK debugging output.
    This annoying code has been in backported clients, GPP and now
    Unvanquished. Even though the debugging output is only shown when
    "in_disablemacosxmouseaccel" is set to "1" (default), playing with it
    disabled is disgusting because of unpredictable mouse movement. The
    code works fine and falls back fine.

--- src/engine/sys/sdl_input.c
+++ src/engine/sys/sdl_input.c
@@ -446,23 +446,19 @@ static void IN_ActivateMouse( void )
 			{
 				if(IOHIDGetAccelerationWithKey(mouseDev, CFSTR(kIOHIDMouseAccelerationType), &originalMouseSpeed) == kIOReturnSuccess)
 				{
-					Com_Printf("previous mouse acceleration: %f\n", originalMouseSpeed);
 					if(IOHIDSetAccelerationWithKey(mouseDev, CFSTR(kIOHIDMouseAccelerationType), -1.0) != kIOReturnSuccess)
 					{
-						Com_Printf("Could not disable mouse acceleration (failed at IOHIDSetAccelerationWithKey).\n");
 						Cvar_Set ("in_disablemacosxmouseaccel", 0);
 					}
 				}
 				else
 				{
-					Com_Printf("Could not disable mouse acceleration (failed at IOHIDGetAccelerationWithKey).\n");
 					Cvar_Set ("in_disablemacosxmouseaccel", 0);
 				}
 				IOServiceClose(mouseDev);
 			}
 			else
 			{
-				Com_Printf("Could not disable mouse acceleration (failed at IO_GetIOHandle).\n");
 				Cvar_Set ("in_disablemacosxmouseaccel", 0);
 			}
 		}
@@ -526,13 +522,9 @@ static void IN_DeactivateMouse( void )
 			io_connect_t mouseDev = IN_GetIOHandle();
 			if(mouseDev != 0)
 			{
-				Com_Printf("restoring mouse acceleration to: %f\n", originalMouseSpeed);
-				if(IOHIDSetAccelerationWithKey(mouseDev, CFSTR(kIOHIDMouseAccelerationType), originalMouseSpeed) != kIOReturnSuccess)
-					Com_Printf("Could not re-enable mouse acceleration (failed at IOHIDSetAccelerationWithKey).\n");
+				IOHIDSetAccelerationWithKey(mouseDev, CFSTR(kIOHIDMouseAccelerationType), originalMouseSpeed);
 				IOServiceClose(mouseDev);
 			}
-			else
-				Com_Printf("Could not re-enable mouse acceleration (failed at IO_GetIOHandle).\n");
 		}
 	}
 #endif
