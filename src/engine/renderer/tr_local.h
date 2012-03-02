095b7b9e0649b3ffc60cafec0bd12ced2aab9b04
Darren Salt <linux@youmustbejoking.demon.co.uk>
2012-03-02 23:06:20 +0000

    Dummy LoadWEBP() for the "old" renderer. Avoids build failure with -Werror=implicit-function-declaration.

--- src/engine/renderer/tr_local.h
+++ src/engine/renderer/tr_local.h
@@ -2428,6 +2428,10 @@ int             SaveJPGToBuffer(byte * buffer, size_t bufSize, int quality, int 
 void            SaveJPG(char *filename, int quality, int image_width, int image_height, unsigned char *image_buffer);
 void            SavePNG(const char *name, const byte * pic, int width, int height, int numBytes, qboolean flip);
 
+#ifdef USE_WEBP
+#define	LoadWEBP(n,p,w,h) /* nothing doing */
+#endif
+
 // video stuff
 const void     *RB_TakeVideoFrameCmd(const void *data);
 void            RE_TakeVideoFrame(int width, int height, byte * captureBuffer, byte * encodeBuffer, qboolean motionJpeg);
