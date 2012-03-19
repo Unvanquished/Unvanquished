7240c7ad2237fcbacd2d906579ab9ad224e494db
dolcetriade <vcelestialragev@gmail.com>
2012-03-19 16:07:21 -0700

    Halved jump requirement. Can perform ~8 jumps

--- src/gamelogic/gpp/src/game/tremulous.h
+++ src/gamelogic/gpp/src/game/tremulous.h
@@ -631,7 +631,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define STAMINA_WALK_RESTORE          15
 #define STAMINA_MEDISTAT_RESTORE      30 // stacked on STOP or WALK
 #define STAMINA_SPRINT_TAKE           6
-#define STAMINA_JUMP_TAKE             250 // Doubled jump requirement. Can perform ~6 jumps.
+#define STAMINA_JUMP_TAKE             250 // Doubled jump requirement. Can perform ~8 jumps.
 #define STAMINA_DODGE_TAKE            750 // Tripled dodge stamina requirement.
 #define STAMINA_MAX                   2000 // Doubled maximum stamina.
 #define STAMINA_BREATHING_LEVEL       0
