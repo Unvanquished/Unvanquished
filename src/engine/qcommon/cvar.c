749d37ce60d76605d9d593586f7ebb56e1b13279
dolcetriade <vcelestialragev@gmail.com>
2012-03-31 00:51:34 -0700

    Add CVAR_SHADER to make the engine recompile GLSL shaders

--- src/engine/qcommon/cvar.c
+++ src/engine/qcommon/cvar.c
@@ -574,6 +574,12 @@ cvar_t         *Cvar_Set2( const char *var_name, const char *value, qboolean for
 			var->modificationCount++;
 			return var;
 		}
+
+		if ( var->flags & CVAR_SHADER )
+		{
+			Com_Printf( "%s will be changed upon recompiling shaders.\n", var_name );
+			Cvar_Set( "r_recompileShaders", "1" );
+		}
 	}
 	else
 	{
