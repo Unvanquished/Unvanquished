8056f6b3d1da3079ddbef10da648e8cf7276f718
dolcetriade <vcelestialragev@gmail.com>
2012-04-01 01:50:12 -0700

    Update LLVM code to work with Clang 3 and update CMakeLists to build with USE_LLVM

--- src/engine/qcommon/vm_llvm.cpp
+++ src/engine/qcommon/vm_llvm.cpp
@@ -1,26 +1,24 @@
 /*
 ===========================================================================
+Copyright (C) 2011-2012 Unvanquished Development
 
-Daemon GPL Source Code
-Copyright (C) 2011 Dusan Jocic <dusanjocic@msn.com>
+This file is part of Daemon.
 
-Daemon is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
+Daemon is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
 
-Daemon is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
+Daemon is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+along with Daemon; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
-
-#ifdef USE_LLVM
 
 #ifdef __cplusplus
 extern "C" {
@@ -30,24 +28,23 @@ extern "C" {
 }
 #endif
 
+#ifdef USE_LLVM
+
 #include "vm_llvm.h"
-
-#define INT64_C(C)  ((int64_t) C ## LL )
-#define UINT64_C(C) ((uint64_t) C ## ULL )
-
-#define __STDC_CONSTANT_MACROS
-#define __STDC_LIMIT_MACROS
+#include <stdint.h>
+#include <ctype.h>
 #include <llvm/Module.h>
 #include <llvm/LLVMContext.h>
 #include <llvm/Support/MemoryBuffer.h>
 #include <llvm/Bitcode/ReaderWriter.h>
 #include <llvm/PassManager.h>
-#include <llvm/Target/TargetSelect.h>
+#include <llvm/Support/TargetSelect.h>
 #include <llvm/Target/TargetData.h>
 #include <llvm/Transforms/IPO.h>
 #include <llvm/ExecutionEngine/ExecutionEngine.h>
 #include <llvm/ExecutionEngine/JIT.h>
-#include <llvm/Support/StandardPasses.h>
+#include <llvm/Transforms/IPO/PassManagerBuilder.h>
+
 
 using namespace llvm;
 
@@ -55,119 +52,108 @@ using namespace llvm;
 extern "C" {
 #endif
 
-	static ExecutionEngine *engine = NULL;
+static ExecutionEngine *engine = NULL;
 
-	void *VM_LoadLLVM( vm_t *vm, intptr_t ( *systemcalls )( intptr_t, ... ) )
-	{
-		char        name[ MAX_QPATH ];
-		char        filename[ MAX_QPATH ];
-		char        *bytes;
-		std::string error;
+void *VM_LoadLLVM( vm_t *vm, intptr_t (*systemcalls)(intptr_t, ...) ) {
+	char name[MAX_QPATH];
+	char filename[MAX_QPATH];
+	char *bytes;
+	std::string error;
 
-		COM_StripExtension3( vm->name, name, sizeof( name ) );
-		Com_sprintf( filename, sizeof( filename ), "%sllvm.bc", name );
-		int len = FS_ReadFile( filename, ( void ** ) &bytes );
+	COM_StripExtension( vm->name, name );
 
-		if ( !bytes )
-		{
-			Com_Printf( "Couldn't load llvm file: %s\n", filename );
-			return NULL;
-		}
+#if defined(__x86_64__) || defined( __WIN64__ )
+	Com_sprintf( filename, sizeof(filename), "vm/%s_64.bc", name );
+#elif defined(__i386__) || defined(_WIN32)
+	Com_sprintf( filename, sizeof(filename), "vm/%s_32.bc", name );
+#endif
 
-		MemoryBuffer *buffer = MemoryBuffer::getMemBuffer( StringRef( bytes, len ) );
-		Module       *module = ParseBitcodeFile( buffer, getGlobalContext(), &error );
-		delete buffer;
-		FS_FreeFile( bytes );
+	int len = FS_ReadFile( filename, (void **)&bytes );
 
-		if ( !module )
-		{
-			Com_Printf( "Couldn't parse llvm file: %s: %s\n", filename, error.c_str() );
-			return NULL;
-		}
-
-		PassManager p;
-		llvm::Pass  *InliningPass = createFunctionInliningPass( 275 );
-		p.add( new TargetData( module ) );
-		createStandardModulePasses( &p, 3, false, true, true, true, false, InliningPass );
-		p.run( *module );
-
-		if ( !engine )
-		{
-			InitializeNativeTarget();
-
-			std::string str;
-
-			/* LLVM has gone through some churn since the initial version of the patch was
-			 * written in 2009. For some unknown reason, the last argument of create() has
-			 * to be false, otherwise LLVM will seg fault when JIT compiling vmMain in the
-			 * call to engine->getPointerToFunction below. This is OK though, since the
-			 * parameter is only there for backwards compatibility and they plan to reverse
-			 * its default to false at some point in the future.  */
-			engine = ExecutionEngine::create( module, false, &str, CodeGenOpt::Default, false );
-
-			if ( !engine )
-			{
-				Com_Printf( "Couldn't create ExecutionEngine: %s\n", str.c_str() );
-				return NULL;
-			}
-		}
-		else
-		{
-			engine->addModule( module );
-		}
-
-		Function *dllEntry_ptr = module->getFunction( std::string( "dllEntry" ) );
-
-		if ( !dllEntry_ptr )
-		{
-			Com_Printf( "Couldn't get function address of dllEntry\n" );
-			return NULL;
-		}
-
-		void ( *dllEntry )( intptr_t ( *syscallptr )( intptr_t, ... ) ) = ( void ( * )( intptr_t ( *syscallptr )( intptr_t, ... ) ) ) engine->getPointerToFunction( dllEntry_ptr );
-		dllEntry( systemcalls );
-
-		Function *vmMain_ptr = module->getFunction( std::string( "vmMain" ) );
-
-		if ( !vmMain_ptr )
-		{
-			Com_Printf( "Couldn't get function address of vmMain\n" );
-			return NULL;
-		}
-
-		intptr_t ( *fp )( int, ... ) = ( intptr_t ( * )( int, ... ) ) engine->getPointerToFunction( vmMain_ptr );
-
-		vm->entryPoint = fp;
-
-		if ( com_developer->integer )
-		{
-			Com_Printf( "Loaded LLVM %s with module==%p\n", name, module );
-		}
-
-		return module;
+	if ( !bytes ) {
+		Com_Printf( "Couldn't load llvm file: %s\n", filename );
+		return NULL;
 	}
 
-	void VM_UnloadLLVM( void *llvmModule )
-	{
-		if ( !llvmModule )
-		{
-			Com_Printf( "VM_UnloadLLVM called with NULL pointer\n" );
-			return;
-		}
+	MemoryBuffer *buffer = MemoryBuffer::getMemBuffer(StringRef(bytes, len));
+	Module *module = ParseBitcodeFile( buffer, getGlobalContext(), &error );
+	delete buffer;
+	FS_FreeFile( bytes );
 
-		if ( com_developer->integer )
-		{
-			Com_Printf( "Unloading LLVM with module==%p\n", llvmModule );
-		}
-
-		if ( !engine->removeModule( ( Module * ) llvmModule ) )
-		{
-			Com_Printf( "Couldn't remove llvm\n" );
-			return;
-		}
-
-		delete( Module * ) llvmModule;
+	if ( !module ) {
+		Com_Printf( "Couldn't parse llvm file: %s: %s\n", filename, error.c_str() );
+		return NULL;
 	}
+
+	PassManagerBuilder PMBuilder;
+	PMBuilder.OptLevel = 3;
+	PMBuilder.SizeLevel = false;
+	PMBuilder.DisableSimplifyLibCalls = false;
+	PMBuilder.DisableUnitAtATime = false;
+	PMBuilder.DisableUnrollLoops = false;
+
+	PMBuilder.Inliner = createFunctionInliningPass(275);
+
+	PassManager * PerModulePasses = new PassManager();
+	PerModulePasses->add(new TargetData(module));
+
+	PassManager *MPM = PerModulePasses;
+	PMBuilder.populateModulePassManager(*MPM);
+
+	if ( !engine ) {
+		InitializeNativeTarget();
+
+		std::string str;
+		/* LLVM has gone through some churn since the initial version of the patch was
+		 * written in 2009. For some unknown reason, the last argument of create() has
+		 * to be false, otherwise LLVM will seg fault when JIT compiling vmMain in the
+		 * call to engine->getPointerToFunction below. This is OK though, since the
+		 * parameter is only there for backwards compatibility and they plan to reverse
+		 * its default to false at some point in the future.  */
+		engine = ExecutionEngine::create( module, false, &str, CodeGenOpt::Default, false );
+		if ( !engine ) {
+			Com_Printf("Couldn't create ExecutionEngine: %s\n", str.c_str());
+			return NULL;
+		}
+	} else {
+		engine->addModule( module );
+	}
+
+	Function *func = module->getFunction("dllEntry");
+	void (*dllEntry) (intptr_t(*syscallptr) (intptr_t, ...)) =
+		(void (*)(intptr_t(*syscallptr) (intptr_t,...)))engine->getPointerToFunction(func);
+
+
+	dllEntry(systemcalls);
+
+	func = module->getFunction(std::string("vmMain"));
+	intptr_t(*fp) (int,...) = (intptr_t(*)(int,...))engine->getPointerToFunction(func);
+
+	vm->entryPoint = fp;
+
+	if ( com_developer->integer ) {
+		Com_Printf("Loaded LLVM %s with module==%p\n", name, module);
+	}
+
+	return module;
+}
+
+void VM_UnloadLLVM( void *llvmModule ) {
+	if ( !llvmModule ) {
+		Com_Printf( "VM_UnloadLLVM called with NULL pointer\n" );
+		return;
+	}
+
+	if ( com_developer->integer ) {
+		Com_Printf( "Unloading LLVM with module==%p\n", llvmModule );
+	}
+
+	if ( !engine->removeModule( (Module *)llvmModule ) ) {
+		Com_Printf( "Couldn't remove llvm\n" );
+		return;
+	}
+	delete (Module *)llvmModule;
+}
 
 #ifdef __cplusplus
 }
--- src/engine/qcommon/vm_llvm.h
+++ src/engine/qcommon/vm_llvm.h
@@ -1,22 +1,22 @@
 /*
 ===========================================================================
+Copyright (C) 2011-2012 Unvanquished Development
 
-Daemon GPL Source Code
-Copyright (C) 2011 Dusan Jocic <dusanjocic@msn.com>
+This file is part of Daemon.
 
-Daemon is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
+Daemon is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
 
-Daemon is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
+Daemon is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+along with Daemon; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
@@ -25,10 +25,8 @@ extern "C" {
 #endif
 
 #ifdef USE_LLVM
-
-	void *VM_LoadLLVM( vm_t *vm, intptr_t ( *systemcalls )( intptr_t, ... ) );
-	void VM_UnloadLLVM( void *llvmModuleProvider );
-
+void *VM_LoadLLVM( vm_t *vm, intptr_t (*systemcalls)(intptr_t, ...) );
+void VM_UnloadLLVM( void *llvmModuleProvider );
 #endif
 
 #ifdef __cplusplus
