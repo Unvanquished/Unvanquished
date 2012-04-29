82a1706b8aad6d49f30fcf65657b4bc64d4e5c4b
Darren Salt <linux@youmustbejoking.demon.co.uk>
2012-04-29 01:15:47 +0100

    Report if the server sent pubkey_decrypt but with nothing to decrypt.

--- src/engine/client/cl_cgame.c
+++ src/engine/client/cl_cgame.c
@@ -499,11 +499,18 @@ rescan:
 		return qfalse;
 	}
 
-	if ( cl_pubkeyID->integer && !strcmp( cmd, "pubkey_decrypt" ) && argc > 1 )
+	if ( cl_pubkeyID->integer && !strcmp( cmd, "pubkey_decrypt" ) )
 	{
 		char         buffer[ MAX_STRING_CHARS ] = "pubkey_identify ";
 		unsigned int msg_len = MAX_STRING_CHARS - 16;
 		mpz_t        message;
+
+		if ( argc == 1 )
+		{
+			Com_Printf( "^3Server sent a pubkey_decrypt command, but sent nothing to decrypt!\n" );
+			return qfalse;
+		}
+
 		mpz_init_set_str( message, Cmd_Argv( 1 ), 16 );
 
 		if ( rsa_decrypt( &private_key, &msg_len, ( unsigned char * ) buffer + 16, message ) )
