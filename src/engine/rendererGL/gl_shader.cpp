d5d4f8d24a21e10aff6842ceb6a1b165f58d4504
Darren Salt <linux@youmustbejoking.demon.co.uk>
2012-05-02 22:56:36 +0100

    Ensure that the optimised output has an appropriate #version header.
    This prevents compilation failures with Mesa.

--- src/engine/rendererGL/gl_shader.cpp
+++ src/engine/rendererGL/gl_shader.cpp
@@ -959,10 +959,12 @@ void GLShader::CompileAndLinkGPUShaderProgram( shaderProgram_t *program,
 			int                 length = 0;
 			int                 i;
 
+			const std::string version = ( glConfig.driverType == GLDRV_OPENGL3 ) ? "#version 130\n" : "#version 120\n";
+
 			glslopt_shader *shaderOptimized = glslopt_optimize( s_glslOptimizer, kGlslOptShaderVertex, vertexShaderTextWithMacros.c_str(), 0 );
 			if( glslopt_get_status( shaderOptimized ) )
 			{
-				vertexShaderTextWithMacros = glslopt_get_output( shaderOptimized );
+				vertexShaderTextWithMacros = version + glslopt_get_output( shaderOptimized );
 
 				ri.Printf( PRINT_DEVELOPER, "----------------------------------------------------------\n" );
 				ri.Printf( PRINT_DEVELOPER, "OPTIMIZED VERTEX shader '%s' ----------\n", programName );
@@ -998,7 +1000,7 @@ void GLShader::CompileAndLinkGPUShaderProgram( shaderProgram_t *program,
 			glslopt_shader *shaderOptimized1 = glslopt_optimize( s_glslOptimizer, kGlslOptShaderFragment, fragmentShaderTextWithMacros.c_str(), 0 );
 			if( glslopt_get_status( shaderOptimized1 ) )
 			{
-				fragmentShaderTextWithMacros = glslopt_get_output( shaderOptimized1 );
+				fragmentShaderTextWithMacros = version + glslopt_get_output( shaderOptimized1 );
 
 				ri.Printf( PRINT_DEVELOPER, "----------------------------------------------------------\n" );
 				ri.Printf( PRINT_DEVELOPER, "OPTIMIZED FRAGMENT shader '%s' ----------\n", programName );
