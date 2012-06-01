d9b5550970d2055e92ced308e7a6c3480c0bad8c
Darren Salt <devspam@moreofthesa.me.uk>
2012-06-01 17:28:14 +0100

    Move downloaded data to /var/games/unvanquished.

--- debian/unvanquished-download-paks.sh
+++ debian/unvanquished-download-paks.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-exec /usr/lib/games/unvanquished/download-pk3.sh /var/lib/games/unvanquished/main /var/lib/games/unvanquished
+exec /usr/lib/games/unvanquished/download-pk3.sh /var/games/unvanquished/main /var/games/unvanquished
