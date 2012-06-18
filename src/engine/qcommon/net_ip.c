58b473a8938473514442bc8bf8124016919afd76
/dev/humancontroller <devhc@example.com>
2012-06-18 17:03:06 +0100

    fix wrong socket ID comparison

--- src/engine/qcommon/net_ip.c
+++ src/engine/qcommon/net_ip.c
@@ -1898,7 +1898,7 @@ void NET_Config( qboolean enableNetworking )
 			ip_socket = INVALID_SOCKET;
 		}
 
-		if ( multicast6_socket )
+		if ( multicast6_socket != INVALID_SOCKET )
 		{
 			if ( multicast6_socket != ip6_socket )
 			{
