a226575bbc8ce46245f632fd364691364137808c
Daniel Maloney <daniel.r.maloney@gmail.com>
2012-06-19 12:43:45 +1000

    Copy over needed libraries for VS
    Copy over any needed shared libraries for VS. Also fixed a bug with the
    renderer being given the wrong name. Also changed Glew so that it's
    supported under win32/win64

--- Visual_Studio32.cmake
+++ Visual_Studio32.cmake
@@ -4,8 +4,6 @@ set( PNG_PNG_INCLUDE_DIR ${LIB_DIR}/libpng/ CACHE STRING "PNG include directory"
 set( PNG_LIBRARY ${LIB_DIR}/libpng/libs/win32/libpng.lib CACHE STRING "PNG library" FORCE )
 set( FREETYPE_INCLUDE_DIRS ${LIB_DIR}/freetype/include/freetype2 ${LIB_DIR}/freetype/include CACHE STRING "Freetupe include directory" FORCE )
 set( FREETYPE_LIBRARY ${LIB_DIR}/freetype/lib/freetype.lib CACHE STRING "Freetype library" FORCE )
-set( GLEW_INCLUDE_DIR ${LIB_DIR}/glew/include CACHE STRING "GLEW include directory" FORCE )
-set( GLEW_LIBRARY optimized ${LIB_DIR}/glew/lib/glew32.lib debug ${LIB_DIR}/glew/lib/glew32d.lib CACHE STRING "GLEW library" FORCE )
 set( ZLIB_INCLUDE_DIR ${LIB_DIR}/zlibwapi/include CACHE STRING "ZLIB include directory" FORCE )
 set( ZLIB_LIBRARY ${LIB_DIR}/zlibwapi/lib/x32/zlibwapi.lib CACHE STRING "ZLIB library" FORCE )
 set( GMP_INCLUDE_DIR ${LIB_DIR}/gmp/include CACHE STRING "GMP include directory" FORCE )
@@ -24,9 +22,35 @@ set( THEORA_LIBRARY optimized ${LIB_DIR}/libtheora/libs/win32/release/libtheora.
 set( XVID_INCLUDE_DIR ${LIB_DIR}/xvidcore/include CACHE STRING "Xvid include directory" FORCE )
 set( XVID_LIBRARY ${LIB_DIR}/xvidcore/lib/xvidcore.lib CACHE STRING "Xvid library" FORCE )
 set( BZIP2_INCLUDE_DIR ${LIB_DIR}/libbz2/include CACHE STRING "Bunzip2 include directory" FORCE )
-set( BZIP2_LIBRARY ${LIB_DIR}/libbz2/libbz2.lib CACHE STRING "Bunzip2 library" FORCE )
+set( BZIP2_LIBRARY ${LIB_DIR}/libbz2/libs/x32/libbz2.lib CACHE STRING "Bunzip2 library" FORCE )
 set( GLIB_LIBRARY ${LIB_DIR}/glib/lib/x32/glib-2.0.lib CACHE STRING "GLIB library" FORCE )
 set( GLIB_INCLUDE_DIR ${LIB_DIR}/glib/include/glib-2.0 CACHE STRING "GLIB include directory" FORCE )
 set( GLIBCONFIG_INCLUDE_DIR ${LIB_DIR}/glib/lib/x32/glib-2.0/include CACHE STRING "GLIB config include directory" FORCE )
 set( OPENAL_LIBRARY ${LIB_DIR}/openal/libs/win32/OpenAL32.lib CACHE STRING "OpenAL library" FORCE )
 set( OPENAL_INCLUDE_DIR ${LIB_DIR}/openal/include CACHE STRING "OpenAL include directory" FORCE )
+
+# Release
+file( COPY
+  ${LIB_DIR}/libpng/libs/win32/libpng14-14.dll
+  ${LIB_DIR}/zlibwapi/lib/x32/zlibwapi.dll
+  ${LIB_DIR}/curl-7.21.6/lib/win32/release/libcurl.dll
+  ${LIB_DIR}/libvorbis/libs/Win32/Release/libvorbis.dll
+  ${LIB_DIR}/libvorbis/libs/Win32/Release/libvorbisfile.dll
+  ${LIB_DIR}/libogg/libs/win32/release/libogg.dll
+  ${LIB_DIR}/libtheora/libs/win32/release/libtheora.dll
+  ${LIB_DIR}/libbz2/libs/x32/libbz2.dll
+  DESTINATION Release
+)
+  
+# Debug
+file( COPY
+  ${LIB_DIR}/libpng/libs/win32/libpng14-14.dll
+  ${LIB_DIR}/zlibwapi/lib/x32/zlibwapi.dll
+  ${LIB_DIR}/curl-7.21.6/lib/win32/debug/libcurld.dll
+  ${LIB_DIR}/libvorbis/libs/Win32/Debug/libvorbis.dll
+  ${LIB_DIR}/libvorbis/libs/Win32/Debug/libvorbisfile.dll
+  ${LIB_DIR}/libogg/libs/win32/debug/libogg.dll
+  ${LIB_DIR}/libtheora/libs/win32/debug/libtheora.dll
+  ${LIB_DIR}/libbz2/libs/x32/libbz2.dll
+  DESTINATION Debug
+)
