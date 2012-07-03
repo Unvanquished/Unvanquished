fb25449f3d7456c262ef935eecb15f3c0f63b33b
cmf028 <cmf1292@hotmail.com>
2012-07-03 01:11:56 -0500

    Explicitly set timer resolution on Windows From ioquake3

--- src/engine/sys/sys_win32.c
+++ src/engine/sys/sys_win32.c
@@ -59,6 +59,10 @@ Maryland 20850 USA.
 // Used to determine where to store user-specific files
 static char homePath[ MAX_OSPATH ] = { 0 };
 
+#ifndef DEDICATED
+static UINT timerResolution = 0;
+#endif
+
 /*
 ================
 Sys_SetFPUCW
@@ -839,6 +843,8 @@ Windows specific initialisation
 void Sys_PlatformInit( void )
 {
 #ifndef DEDICATED
+	TIMECAPS ptc;
+	UINT res;
 	const char *SDL_VIDEODRIVER = getenv( "SDL_VIDEODRIVER" );
 #endif
 
@@ -857,6 +863,34 @@ void Sys_PlatformInit( void )
 		SDL_VIDEODRIVER_externallySet = qfalse;
 	}
 
+	
+	if(timeGetDevCaps(&ptc, sizeof(ptc)) == MMSYSERR_NOERROR)
+	{
+		timerResolution = ptc.wPeriodMin;
+		if(timerResolution > 1)
+		{
+			Com_Printf("Warning: Minimum supported timer resolution is %ums "
+					"on this system, recommended resolution 1ms\n", timerResolution);
+		}
+
+		timeBeginPeriod(timerResolution);
+	}
+	else
+		timerResolution = 0;
+#endif
+}
+/*
+==============
+Sys_PlatformExit
+
+Windows specific deinitialisation
+==============
+*/
+void Sys_PlatformExit( void )
+{
+#ifndef DEDICATED
+	if(timerResolution)
+		timeEndPeriod(timerResolution);
 #endif
 }
 
