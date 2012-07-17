084b7657e694274552ba3a835088a1e2cb454f96
Darren Salt <devspam@moreofthesa.me.uk>
2012-07-17 15:26:44 +0100

    Kill a few signed/unsigned comparison warnings.

--- src/engine/rendererGL/tr_world.cpp
+++ src/engine/rendererGL/tr_world.cpp
@@ -423,7 +423,7 @@ void R_AddBSPModelSurfaces( trRefEntity_t *ent )
 {
 	bspModel_t *bspModel;
 	model_t    *pModel;
-	int        i;
+	unsigned int i;
 	vec3_t     v;
 	vec3_t     transformed;
 	vec3_t     boundsCenter;
@@ -478,7 +478,6 @@ void R_AddBSPModelSurfaces( trRefEntity_t *ent )
 
 	if ( r_vboModels->integer && bspModel->numVBOSurfaces )
 	{
-		int          i;
 		srfVBOMesh_t *vboSurface;
 
 		for ( i = 0; i < bspModel->numVBOSurfaces; i++ )
