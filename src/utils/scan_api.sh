9259a3c6ee9dfb8b83a24046ceb3667da5ee8a3b
Darren Salt <devspam@moreofthesa.me.uk>
2012-07-20 21:29:43 +0100

    Allow for spaces etc. in the API scan script; mark as executable.

--- src/utils/scan_api.sh
+++ src/utils/scan_api.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-SELF="$(dirname $0)"
+SELF="$(dirname "$0")"
 cd "$SELF/../.."
 echo 'Generating UI API asm file...'
 perl "$SELF/scan_api.pl" src/gamelogic/ui/ui_api.c    src/engine/client/ui_api.h >src/gamelogic/ui/ui_api.asm
