76a2eb1098a923d8e101c1b7a19ba49b29983864
Darren Salt <devspam@moreofthesa.me.uk>
2012-07-20 14:45:21 +0100

    Fix up tarball and QVM trap call handle generation.

--- src/utils/scan_api.sh
+++ src/utils/scan_api.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
-cd "$(dirname $0)"/..
+SELF="$(dirname $0)"
+cd "$SELF/../.."
 echo 'Generating UI API asm file...'
-perl utils/scan_api.pl src/gamelogic/gpp/src/ui/ui_api.c    src/engine/client/ui_api.h >src/gamelogic/gpp/src/ui/ui_api.asm
+perl "$SELF/scan_api.pl" src/gamelogic/ui/ui_api.c    src/engine/client/ui_api.h >src/gamelogic/ui/ui_api.asm
 echo 'Generating client game API asm file...'
-perl utils/scan_api.pl src/gamelogic/gpp/src/cgame/cg_api.c src/engine/client/cg_api.h >src/gamelogic/gpp/src/cgame/cg_api.asm
+perl "$SELF/scan_api.pl" src/gamelogic/cgame/cg_api.c src/engine/client/cg_api.h >src/gamelogic/cgame/cg_api.asm
 echo 'Generating server game API asm file...'
-perl utils/scan_api.pl src/gamelogic/gpp/src/game/g_api.c   src/engine/server/g_api.h  >src/gamelogic/gpp/src/game/g_api.asm
+perl "$SELF/scan_api.pl" src/gamelogic/game/g_api.c   src/engine/server/g_api.h  >src/gamelogic/game/g_api.asm
 echo 'Done.'
