8bb3c7e5028fffd1a4572aa38da72901870875c5
Sixthly <contact@sixthly.net>
2012-07-23 01:16:26 +1000

    Make the default values for "r_ignorehwgamma" and "r_gamma" consistent across renderers.

--- src/engine/rendererGL/tr_init.cpp
+++ src/engine/rendererGL/tr_init.cpp
@@ -1488,7 +1488,7 @@ extern "C" {
 		r_finish = ri.Cvar_Get( "r_finish", "0", CVAR_CHEAT );
 		r_textureMode = ri.Cvar_Get( "r_textureMode", "GL_LINEAR_MIPMAP_NEAREST", CVAR_ARCHIVE );
 		r_swapInterval = ri.Cvar_Get( "r_swapInterval", "0", CVAR_ARCHIVE );
-		r_gamma = ri.Cvar_Get( "r_gamma", "1", CVAR_ARCHIVE );
+		r_gamma = ri.Cvar_Get( "r_gamma", "1.3", CVAR_ARCHIVE );
 		r_facePlaneCull = ri.Cvar_Get( "r_facePlaneCull", "1", CVAR_ARCHIVE );
 
 		r_railWidth = ri.Cvar_Get( "r_railWidth", "96", CVAR_ARCHIVE );
