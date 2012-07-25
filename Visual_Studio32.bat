a177ee7fb4268cf427801f35308c7a223d622a3d
Daniel Maloney <daniel.r.maloney@gmail.com>
2012-07-25 14:18:06 +1000

    Bat files now handle spaces in directory
    Batch files used to generate VS solutions can now handle spaces in the
    directory path.

--- Visual_Studio32.bat
+++ Visual_Studio32.bat
@@ -2,6 +2,6 @@
 mkdir build-32
 cd build-32
 REM Get value for %args%
-call %~dp0/cmake/Visual_Studio_arguments.bat
+call "%~dp0/cmake/Visual_Studio_arguments.bat"
 cmake -C../cmake/Visual_Studio32.cmake %args%  -G "Visual Studio 10" ..
 pause
--- Visual_Studio64.bat
+++ Visual_Studio64.bat
@@ -2,6 +2,6 @@
 mkdir build-64
 cd build-64
 REM Get value for %args%
-call %~dp0/cmake/Visual_Studio_arguments.bat
+call "%~dp0/cmake/Visual_Studio_arguments.bat"
 cmake -C../cmake/Visual_Studio64.cmake %args% -G "Visual Studio 10 Win64" ..
 pause
