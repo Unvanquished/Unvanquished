Unvanquished/Unvanquished

commit 3298e94fe7d343f207c65de2c0bd4b489cd5e476

Refactor duplicate MD5 animation code into reusable functions

src/gamelogic/cgame/cg_players.c
@@ -1604,37 +1604,6 @@ PLAYER ANIMATION
 =============================================================================
 */
 
-// TODO: choose proper values and use blending speed from character.cfg
-// blending is slow for testing issues
-static void CG_BlendPlayerLerpFrame( lerpFrame_t *lf )
-{
-	if ( cg_animBlend.value <= 0.0f )
-	{
-		lf->blendlerp = 0.0f;
-		return;
-	}
-
-	if ( ( lf->blendlerp > 0.0f ) && ( cg.time > lf->blendtime ) )
-	{
-		//exp blending
-		lf->blendlerp -= lf->blendlerp / cg_animBlend.value;
-
-		if ( lf->blendlerp <= 0.0f )
-		{
-			lf->blendlerp = 0.0f;
-		}
-
-		if ( lf->blendlerp >= 1.0f )
-		{
-			lf->blendlerp = 1.0f;
-		}
-
-		lf->blendtime = cg.time + 10;
-
-		debug_anim_blend = lf->blendlerp;
-	}
-}
-
 static void CG_CombineLegSkeleton( refSkeleton_t *dest, refSkeleton_t *legs, int *legBones, int numBones, float frac )
 {
 	int i;
@@ -1759,30 +1728,12 @@ static void CG_RunPlayerLerpFrame( clientInfo_t *ci, lerpFrame_t *lf, int newAni
 		CG_RunMD5LerpFrame( lf, speedScale, animChanged );
 
 		// blend old and current animation
-		CG_BlendPlayerLerpFrame( lf );
+		CG_BlendLerpFrame( lf );
 
-		if ( lf->animation && lf->animation->handle && ci->team != TEAM_NONE )
-		{
-			if ( !trap_R_BuildSkeleton( skel, lf->animation->handle, lf->oldFrame, lf->frame, lf->backlerp, lf->animation->clearOrigin ) )
-			{
-				CG_Printf( "%s", _( "Can't build lf->skeleton\n" ));
+		if( ci->team != TEAM_NONE )
+			CG_BuildAnimSkeleton( lf, skel, &oldSkeleton );
 			}
-
-			// lerp between old and new animation if possible
-			if ( lf->blendlerp >= 0.0f )
-			{
-				if ( skel->type != SK_INVALID && oldSkeleton.type != SK_INVALID && skel->numBones == oldSkeleton.numBones )
-				{
-					if ( !trap_R_BlendSkeleton( skel, &oldSkeleton, lf->blendlerp ) )
-					{
-						CG_Printf( "%s", _( "Can't blend\n" ));
-						return;
 					}
-				}
-			}
-		}
-	}
-}
 
 static void CG_RunCorpseLerpFrame( clientInfo_t *ci, lerpFrame_t *lf, int newAnimation, float speedScale )
 {
@@ -1821,10 +1772,8 @@ static void CG_RunCorpseLerpFrame( clientInfo_t *ci, lerpFrame_t *lf, int newAni
 		return; // shouldn't happen
 	}
 
-
-
 	// blend old and current animation
-	CG_BlendPlayerLerpFrame( lf );
+	CG_BlendLerpFrame( lf );
 
 	if ( lf->animation )
 	{

src/gamelogic/cgame/cg_animation.c
@@ -161,7 +161,6 @@ void CG_RunMD5LerpFrame( lerpFrame_t *lf, float scale, qboolean animChanged )
 			lf->oldFrameTime = cg.time;
 		}
 		else
-
 		{
 			lf->oldFrame = lf->frame;
 			lf->oldFrameTime = lf->frameTime;
@@ -249,5 +248,75 @@ void CG_RunMD5LerpFrame( lerpFrame_t *lf, float scale, qboolean animChanged )
 	else
 	{
 		lf->backlerp = 1.0 - ( float )( cg.time - lf->oldFrameTime ) / ( lf->frameTime - lf->oldFrameTime );
+	}
+}
+
+/*
+===============
+CG_BlendLerpFrame
+
+Sets lf->blendlerp and lf->blendtime
+===============
+*/
+void CG_BlendLerpFrame( lerpFrame_t *lf )
+{
+	if ( cg_animBlend.value <= 0.0f )
+	{
+		lf->blendlerp = 0.0f;
+		return;
+	}
+
+	if ( ( lf->blendlerp > 0.0f ) && ( cg.time > lf->blendtime ) )
+	{
+		//exp blending
+		lf->blendlerp -= lf->blendlerp / cg_animBlend.value;
+
+		if ( lf->blendlerp <= 0.0f )
+		{
+			lf->blendlerp = 0.0f;
+		}
+
+		if ( lf->blendlerp >= 1.0f )
+		{
+			lf->blendlerp = 1.0f;
+		}
+
+		lf->blendtime = cg.time + 10;
+
+		debug_anim_blend = lf->blendlerp;
+	}
+}
+
+/*
+===============
+CG_BuildAnimSkeleton
+
+Builds the skeleton for the current animation
+Also blends between the old and new skeletons if necessary
+===============
+*/
+void CG_BuildAnimSkeleton( const lerpFrame_t *lf, refSkeleton_t *newSkeleton, const refSkeleton_t *oldSkeleton )
+{
+	if( !lf->animation || !lf->animation->handle )
+	{
+		return;
+	}
+
+	if ( !trap_R_BuildSkeleton( newSkeleton, lf->animation->handle, lf->oldFrame, lf->frame, lf->backlerp, lf->animation->clearOrigin ) )
+	{
+		CG_Printf( "%s", _( "CG_BuildAnimSkeleton: Can't build skeleton\n" ));
+	}
+
+	// lerp between old and new animation if possible
+	if ( lf->blendlerp >= 0.0f )
+	{
+		if ( newSkeleton->type != SK_INVALID && oldSkeleton->type != SK_INVALID && newSkeleton->numBones == oldSkeleton->numBones )
+		{
+			if ( !trap_R_BlendSkeleton( newSkeleton, oldSkeleton, lf->blendlerp ) )
+			{
+				CG_Printf( "%s", _( "CG_BuildAnimSkeleton: Can't blend skeletons\n" ));
+				return;
+			}
+		}
 	}
 }

src/gamelogic/cgame/cg_buildable.c
@@ -880,55 +880,9 @@ static void CG_BuildableAnimation( centity_t *cent, int *old, int *now, float *b
 
 	if ( cg_buildables[ BG_Buildable( cent->currentState.modelindex )->number ].md5 )
 	{
-		// blend old and current animation
-		if ( cg_animBlend.value <= 0.0f )
-		{
-			lf->blendlerp = 0.0f;
-		}
+		CG_BlendLerpFrame( lf );
 
-		if ( ( lf->blendlerp > 0.0f ) && ( cg.time > lf->blendtime ) )
-		{
-#if 0
-			// linear blending
-			lf->blendlerp -= 0.025f;
-#else
-			// exp blending
-			lf->blendlerp -= lf->blendlerp / cg_animBlend.value;
-#endif
-
-			if ( lf->blendlerp <= 0.0f )
-			{
-				lf->blendlerp = 0.0f;
-			}
-
-			if ( lf->blendlerp >= 1.0f )
-			{
-				lf->blendlerp = 1.0f;
-			}
-
-			lf->blendtime = cg.time + 10;
-		}
-
-		if ( lf->animation && lf->animation->handle )
-		{
-			if ( !trap_R_BuildSkeleton( &bSkeleton, lf->animation->handle, lf->oldFrame, lf->frame, 1.0 - lf->backlerp, lf->animation->clearOrigin ) )
-			{
-				CG_Printf( "%s", _( "CG_RunBuildableLerpFrame: Can't build lf->bSkeleton\n" ));
-			}
-
-			if ( oldbSkeleton.type != SK_INVALID && oldbSkeleton.numBones == bSkeleton.numBones )
-			{
-				// lerp between old and new animation if possible
-				if ( lf->blendlerp > 0.0f && oldbSkeleton.numBones == bSkeleton.numBones )
-				{
-					if ( !trap_R_BlendSkeleton( &bSkeleton, &oldbSkeleton, lf->blendlerp ) )
-					{
-						CG_Printf( "%s", _( "CG_RunBuildableLerpFrame: Can't blend lf->bSkeleton\n" ));
-						return;
-					}
-				}
-			}
-		}
+		CG_BuildAnimSkeleton( lf, &bSkeleton, &oldbSkeleton );
 	}
 }
 

src/gamelogic/cgame/cg_local.h
@@ -1782,6 +1782,8 @@ qboolean CG_GetBuildableRangeMarkerProperties( buildable_t bType, rangeMarkerTyp
 //
 void CG_RunLerpFrame( lerpFrame_t *lf, float scale );
 void CG_RunMD5LerpFrame( lerpFrame_t *lf, float scale, qboolean animChanged );
+void CG_BlendLerpFrame( lerpFrame_t *lf );
+void CG_BuildAnimSkeleton( const lerpFrame_t *lf, refSkeleton_t *newSkeleton, const refSkeleton_t *oldSkeleton );
 
 //
 // cg_animmapobj.c

src/gamelogic/cgame/cg_weapons.c
@@ -1294,49 +1294,9 @@ static void CG_WeaponAnimation( centity_t *cent, int *old, int *now, float *back
 
 	if ( cg_weapons[ cent->currentState.weapon ].md5 )
 	{
-		// blend old and current animation
-		if ( cg_animBlend.value <= 0.0f )
-		{
-			lf->blendlerp = 0.0f;
-		}
+		CG_BlendLerpFrame( lf );
 
-		if ( ( lf->blendlerp > 0.0f ) && ( cg.time > lf->blendtime ) )
-		{
-#if 0
-			// linear blending
-			lf->blendlerp -= 0.025f;
-#else
-			// exp blending
-			lf->blendlerp -= lf->blendlerp / cg_animBlend.value;
-#endif
-
-			if ( lf->blendlerp <= 0.0f )
-			{
-				lf->blendlerp = 0.0f;
-			}
-
-			if ( lf->blendlerp >= 1.0f )
-			{
-				lf->blendlerp = 1.0f;
-			}
-
-			lf->blendtime = cg.time + 10;
-		}
-
-		if ( !trap_R_BuildSkeleton( &gunSkeleton, lf->animation->handle, lf->oldFrame, lf->frame, 1.0 - lf->backlerp, lf->animation->clearOrigin ) )
-		{
-			CG_Printf( "%s", _( "CG_RunWeaponLerpFrame: Can't build lf->gunSkeleton\n" ));
-		}
-
-		// lerp between old and new animation if possible
-		if ( lf->blendlerp > 0.0f && gunSkeleton.numBones == oldGunSkeleton.numBones )
-		{
-			if ( !trap_R_BlendSkeleton( &gunSkeleton, &oldGunSkeleton, lf->blendlerp ) )
-			{
-				CG_Printf( "%s", _( "CG_RunWeaponLerpFrame: Can't blend lf->gunSkeleton\n" ));
-				return;
-			}
-		}
+		CG_BuildAnimSkeleton( lf, &gunSkeleton, &oldGunSkeleton );
 	}
 }
 

commit 2f0f7388d64d5090c112f55b325a960ced372642

Fix bad formatting caused by commit 3298e94f

src/gamelogic/cgame/cg_players.c
@@ -1732,8 +1732,8 @@ static void CG_RunPlayerLerpFrame( clientInfo_t *ci, lerpFrame_t *lf, int newAni
 
 		if( ci->team != TEAM_NONE )
 			CG_BuildAnimSkeleton( lf, skel, &oldSkeleton );
-			}
-					}
+	}
+}
 
 static void CG_RunCorpseLerpFrame( clientInfo_t *ci, lerpFrame_t *lf, int newAnimation, float speedScale )
 {