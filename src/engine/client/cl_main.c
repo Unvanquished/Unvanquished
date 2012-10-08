Unvanquished/Unvanquished

commit 29a44f38a33252485523bac8dbbcd2f0b3b7d306

Load the renderer lib from basepath, not from current dir

src/engine/client/cl_main.c
@@ -2164,12 +2164,12 @@ static void CL_GenerateRSAKey( void )
 	fileHandle_t         f;
 	void                 *buf;
 	struct nettle_buffer key_buffer;
-	
+
 	int                  key_buffer_len = 0;
-	
+
 	rsa_public_key_init( &public_key );
 	rsa_private_key_init( &private_key );
-	
+
 	if( cl_profile->string[ 0 ] )
 	{
 		len = FS_FOpenFileRead( va( "profiles/%s/%s", cl_profile->string, RSAKEY_FILE ), &f, qtrue );
@@ -2184,32 +2184,32 @@ static void CL_GenerateRSAKey( void )
 		Com_Printf( "%s", _( "Daemon RSA public-key file not found, regenerating\n" ) );
 		goto new_key;
 	}
-	
+
 	buf = Z_TagMalloc( len, TAG_CRYPTO );
 	FS_Read( buf, len, f );
 	FS_FCloseFile( f );
-	
+
 	if ( !rsa_keypair_from_sexp( &public_key, &private_key, 0, len, buf ) )
 	{
 		Com_Printf( "%s", _( "Invalid RSA keypair in RSAKey, regenerating\n" ) );
 		Z_Free( buf );
 		goto new_key;
 	}
-	
+
 	Z_Free( buf );
 	Com_Printf( "%s", _( "Daemon RSA public-key found.\n" ) );
 	return;
-	
+
 	new_key:
 	mpz_set_ui( public_key.e, RSA_PUBLIC_EXPONENT );
-	
+
 	if ( !rsa_generate_keypair( &public_key, &private_key, NULL, qnettle_random, NULL, NULL, RSA_KEY_LENGTH, 0 ) )
 	{
 		goto keygen_error;
 	}
-	
+
 	qnettle_buffer_init( &key_buffer, &key_buffer_len );
-	
+
 	if ( !rsa_keypair_to_sexp( &key_buffer, NULL, &public_key, &private_key ) )
 	{
 		goto keygen_error;
@@ -2223,19 +2223,19 @@ static void CL_GenerateRSAKey( void )
 	{
 		f = FS_FOpenFileWrite( RSAKEY_FILE );
 	}
-	
+
 	if ( !f )
 	{
 		Com_Error( ERR_FATAL, _( "Daemon RSA public-key could not open %s for write, RSA support will be disabled" ), RSAKEY_FILE );
 		return;
 	}
-	
+
 	FS_Write( key_buffer.contents, key_buffer.size, f );
 	nettle_buffer_clear( &key_buffer );
 	FS_FCloseFile( f );
 	Com_Printf( "%s", _( "Daemon RSA public-key generated\n" ) );
 	return;
-	
+
 	keygen_error:
 	Com_Error( ERR_FATAL, _( "Error generating RSA keypair, RSA support will be disabled" ) );
 	Crypto_Shutdown();
@@ -2317,7 +2317,7 @@ void CL_Vid_Restart_f( void )
 
 	if( Cvar_VariableIntegerValue( "cl_newProfile" ) )
 	{
-		CL_GenerateRSAKey();		
+		CL_GenerateRSAKey();
 		Cvar_Set( "cl_newProfile", "0" );
 	}
 #ifdef _WIN32
@@ -4139,14 +4139,11 @@ void CL_InitRef( const char *renderer )
 	char        dllName[ MAX_OSPATH ];
 #endif
 
-	char        fn[ 1024 ];
-	Q_strncpyz( fn, Sys_Cwd(), sizeof( fn ) );
-
 	Com_Printf("%s", _( "----- Initializing Renderer ----\n" ));
 
 #if !defined( REF_HARD_LINKED )
 
-	Com_sprintf( dllName, sizeof( dllName ), "%s/" DLL_PREFIX "renderer%s" ARCH_STRING DLL_EXT, fn, renderer );
+	Com_sprintf( dllName, sizeof( dllName ), DLL_PREFIX "renderer%s" ARCH_STRING DLL_EXT, renderer );
 
 	Com_Printf(_( "Loading \"%s\"…"), dllName );
 
@@ -4155,9 +4152,9 @@ void CL_InitRef( const char *renderer )
 		Com_Printf(_( "failed:\n\"%s\"\n"), Sys_LibraryError() );
 
 		//fall back to default
-		Com_sprintf( dllName, sizeof( dllName ), "%s/" DLL_PREFIX "rendererGL" ARCH_STRING DLL_EXT, fn );
+		Com_sprintf( dllName, sizeof( dllName ), "%s/" DLL_PREFIX "rendererGL" ARCH_STRING DLL_EXT, Cvar_VariableString( "fs_basepath" ) );
 
-		Com_Printf(_( "Loading \"%s\"…"), dllName );
+		Com_Printf(_	( "Loading \"%s\"…"), dllName );
 
 		if ( ( rendererLib = Sys_LoadLibrary( dllName ) ) == 0 )
 		{

commit 071ad19ac9c869c96fa42bd67ebd98c308f7635e

Check in fs_libpath as well as fs_basepath for the renderer code.
29a44f3 broke things for my .debs.
This puts that right while retaining the intent.

src/engine/client/cl_main.c
@@ -4035,6 +4035,8 @@ void CL_StartHunkUsers( void )
 		char renderers[ MAX_QPATH ];
 		char *from, *to;
 
+		Com_Printf("%s", _( "----- Initializing Renderer ----\n" ));
+
 		Q_strncpyz( renderers, cl_renderer->string, sizeof( renderers ) );
 		from = renderers;
 
@@ -4047,9 +4049,7 @@ void CL_StartHunkUsers( void )
 				*to++ = '\0';
 			}
 
-			CL_InitRef( from );
-
-			if ( CL_InitRenderer() )
+			if ( CL_InitRef( from ) && CL_InitRenderer() )
 			{
 				cls.rendererStarted = qtrue;
 				break;
@@ -4058,6 +4058,19 @@ void CL_StartHunkUsers( void )
 			CL_ShutdownRef();
 			from = to;
 		}
+
+		if ( !cls.rendererStarted && CL_InitRef( "GL" ) && CL_InitRenderer() )
+		{
+			cls.rendererStarted = qtrue;
+		}
+
+		if ( !cls.rendererStarted )
+		{
+			CL_ShutdownRef();
+			Com_Error( ERR_FATAL, "Couldn't load a renderer" );
+		}
+
+		Com_Printf( "-------------------------------\n" );
 	}
 	else if ( !cls.rendererStarted )
 	{
@@ -4132,48 +4145,51 @@ CL_InitRef
 RB: changed to load the renderer from a .dll
 ============
 */
-void CL_InitRef( const char *renderer )
+qboolean CL_InitRef( const char *renderer )
 {
 	refimport_t ri;
 	refexport_t *ret;
+	void        *lib = NULL;
 
 #if !defined( REF_HARD_LINKED )
 	GetRefAPI_t GetRefAPI;
 	char        dllName[ MAX_OSPATH ];
+	const char  varName[][16] = { "fs_libpath", "fs_basepath" };
+	int         i;
 #endif
 
-	Com_Printf("%s", _( "----- Initializing Renderer ----\n" ));
-
 #if !defined( REF_HARD_LINKED )
-
-	Com_sprintf( dllName, sizeof( dllName ), "%s/" DLL_PREFIX "renderer%s" ARCH_STRING DLL_EXT, Cvar_VariableString( "fs_basepath" ), renderer );
-
-	Com_Printf(_( "Loading \"%s\"…"), dllName );
-
-	if ( ( rendererLib = Sys_LoadLibrary( dllName ) ) == 0 )
+	for ( i = 0; i < ARRAY_LEN( varName ); ++i )
 	{
-		Com_Printf(_( "failed:\n\"%s\"\n"), Sys_LibraryError() );
-
-		//fall back to default
-		Com_sprintf( dllName, sizeof( dllName ), "%s/" DLL_PREFIX "rendererGL" ARCH_STRING DLL_EXT, Cvar_VariableString( "fs_basepath" ) );
+		Com_sprintf( dllName, sizeof( dllName ), "%s/" DLL_PREFIX "renderer%s" ARCH_STRING DLL_EXT, Cvar_VariableString( varName[ i ] ), renderer );
 
 		Com_Printf(_( "Loading \"%s\"…"), dllName );
 
-		if ( ( rendererLib = Sys_LoadLibrary( dllName ) ) == 0 )
+		lib = Sys_LoadLibrary( dllName );
+
+		if ( lib )
 		{
-			Com_Error( ERR_FATAL, "failed:\n\"%s\"", Sys_LibraryError() );
+			break;
 		}
+
+		Com_Printf(_( "failed:\n\"%s\"\n"), Sys_LibraryError() );
+	}
+
+	if ( !lib )
+	{
+		return qfalse;
 	}
 
 	Com_Printf("%s", _( "done\n" ));
 
-	GetRefAPI = Sys_LoadFunction( rendererLib, "GetRefAPI" );
+	GetRefAPI = Sys_LoadFunction( lib, "GetRefAPI" );
 
 	if ( !GetRefAPI )
 	{
-		Com_Error( ERR_FATAL, "Can't load symbol GetRefAPI: '%s'",  Sys_LibraryError() );
+		Com_Printf( "Can't load symbol GetRefAPI: '%s'",  Sys_LibraryError() );
+		Sys_UnloadDll( lib );
+		return qfalse;
 	}
-
 #endif
 
 	ri.Cmd_AddCommand = Cmd_AddCommand;
@@ -4252,17 +4268,20 @@ void CL_InitRef( const char *renderer )
 	Com_Printf("%s", _( "Calling GetRefAPI…\n" ));
 	ret = GetRefAPI( REF_API_VERSION, &ri );
 
-	Com_Printf( "-------------------------------\n" );
-
 	if ( !ret )
 	{
-		Com_Error( ERR_FATAL, "Couldn't initialize refresh" );
+		Com_Printf( "Couldn't initialize refresh\n" );
+		Sys_UnloadDll( lib );
+		return qfalse;
 	}
 
 	re = *ret;
 
 	// unpause so the cgame definitely gets a snapshot and renders a frame
 	Cvar_Set( "cl_paused", "0" );
+
+	rendererLib = lib; // at this point, we pass on unloading responsibility
+	return qtrue;
 }
 
 /*

src/engine/client/client.h
@@ -564,7 +564,7 @@ void        CL_ClearPing( int n );
 int         CL_GetPingQueueCount( void );
 
 void        CL_ShutdownRef( void );
-void        CL_InitRef( const char *renderer );
+qboolean    CL_InitRef( const char *renderer );
 
 int         CL_ServerStatus( char *serverAddress, char *serverStatusString, int maxLen );
 