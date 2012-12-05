1c0a1fd2787afd212934be68e939cc4aa85c09de
Matthias Bentrup <matthias.bentrup@googlemail.com>
2012-12-05 20:13:45 +0100

    Fix range markers for blueprints.
    Also refactor a bit to use the same code for buildables and blueprints.

--- src/gamelogic/cgame/cg_buildable.c
+++ src/gamelogic/cgame/cg_buildable.c
@@ -600,7 +600,7 @@ void CG_InitBuildables( void )
 CG_BuildableRangeMarkerProperties
 ================
 */
-qboolean CG_GetBuildableRangeMarkerProperties( buildable_t bType, rangeMarker_t *rmType, float *range, vec3_t rgb )
+qboolean CG_GetBuildableRangeMarkerProperties( buildable_t bType, rangeMarker_t *rmType, float *range, vec4_t rgba )
 {
 	shaderColorEnum_t shc;
 
@@ -673,7 +673,8 @@ qboolean CG_GetBuildableRangeMarkerProperties( buildable_t bType, rangeMarker_t 
 		*rmType = RM_SPHERE;
 	}
 
-	VectorCopy( cg_shaderColors[ shc ], rgb );
+	VectorCopy( cg_shaderColors[ shc ], rgba );
+	rgba[3] = 1.0f;
 
 	return qtrue;
 }
@@ -940,18 +941,20 @@ static void CG_PositionAndOrientateBuildable( const vec3_t angles, const vec3_t 
 
 /*
 ================
-CG_GhostBuildableRangeMarker
+CG_DrawBuildableRangeMarker
 ================
 */
-static void CG_GhostBuildableRangeMarker( buildable_t buildable, const vec3_t origin, const vec3_t normal )
+static void CG_DrawBuildableRangeMarker( buildable_t buildable, const vec3_t origin, const vec3_t normal, float opacity )
 {
 	rangeMarker_t rmType;
 	float    range;
-	vec3_t   rgb;
+	vec4_t   rgba;
 
-	if ( CG_GetBuildableRangeMarkerProperties( buildable, &rmType, &range, rgb ) )
+	if ( CG_GetBuildableRangeMarkerProperties( buildable, &rmType, &range, rgba ) )
 	{
 		vec3_t localOrigin;
+
+		rgba[3] *= opacity;
 
 		if ( buildable == BA_A_HIVE || buildable == BA_H_TESLAGEN )
 		{
@@ -964,13 +967,13 @@ static void CG_GhostBuildableRangeMarker( buildable_t buildable, const vec3_t or
 
 		if ( rmType == RM_SPHERE )
 		{
-			CG_DrawRangeMarker( rmType, localOrigin, range, NULL, rgb );
+			CG_DrawRangeMarker( rmType, localOrigin, range, NULL, rgba );
 		}
 		else
 		{
 			vec3_t angles;
 			vectoangles( normal, angles );
-			CG_DrawRangeMarker( rmType, localOrigin, range, angles, rgb );
+			CG_DrawRangeMarker( rmType, localOrigin, range, angles, rgba );
 		}
 	}
 }
@@ -999,7 +1002,7 @@ void CG_GhostBuildable( buildable_t buildable )
 
 	if ( cg_rangeMarkerForBlueprint.integer && tr.entityNum != ENTITYNUM_NONE )
 	{
-		CG_GhostBuildableRangeMarker( buildable, entity_origin, tr.plane.normal );
+		CG_DrawBuildableRangeMarker( buildable, entity_origin, tr.plane.normal, 1.0f );
 	}
 
 	CG_PositionAndOrientateBuildable( ps->viewangles, entity_origin, tr.plane.normal, ps->clientNum,
@@ -2108,5 +2111,32 @@ void CG_Buildable( centity_t *cent )
 	//smoke etc for damaged buildables
 	CG_BuildableParticleEffects( cent );
 
-	CG_RangeMarker( cent );
+	// draw range marker if enabled
+	if( team == cg.predictedPlayerState.stats[ STAT_TEAM ] ) {
+		qboolean drawRange;
+		float dist, maxDist = MAX( HELMET_RANGE, ALIENSENSE_RANGE );
+
+		if ( team == TEAM_HUMANS ) {
+			drawRange = BG_InventoryContainsWeapon( WP_HBUILD, cg.predictedPlayerState.stats );
+		} else if ( team == TEAM_ALIENS ) {
+			drawRange = cg.predictedPlayerState.weapon == WP_ABUILD ||
+			            cg.predictedPlayerState.weapon == WP_ABUILD2;
+		} else {
+			drawRange = !!(cg_buildableRangeMarkerMask.integer & (1 << BA_NONE));
+		}
+
+		drawRange &= !!(cg_buildableRangeMarkerMask.integer & (1 << buildable->number));
+
+		dist = Distance( cent->lerpOrigin, cg.refdef.vieworg );
+
+		if( drawRange && dist <= maxDist ) {
+			float opacity = 1.0f;
+
+			if( dist >= 0.9f * maxDist ) {
+				opacity = 10.0f - 10.0f * dist / maxDist;
+			}
+
+			CG_DrawBuildableRangeMarker( buildable->number, cent->lerpOrigin, cent->currentState.origin2, opacity );
+		}
+	}
 }
