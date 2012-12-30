ec48fa1d4485efb892efc4e50bea984521611e1c
Matthias Bentrup <matthias.bentrup@googlemail.com>
2012-12-30 12:57:09 +0100

    Bloom adjustments for non-deferred rendering.

--- pkg/unvanquished_src.dpkdir/glsl/contrast_fp.glsl
+++ pkg/unvanquished_src.dpkdir/glsl/contrast_fp.glsl
@@ -26,8 +26,17 @@ uniform sampler2D	u_ColorMap;
 
 const vec4			LUMINANCE_VECTOR = vec4(0.2125, 0.7154, 0.0721, 0.0);
 
+// contrast adjustment function
+vec4 f(vec4 color) {
+	float L = dot(LUMINANCE_VECTOR, color);
+	L = max(L - 0.71, 0.0) * (1.0 / (1.0 - 0.71));
+	//L = pow(L, 8.0);
+	return color * L;
+}
+
 void	main()
 {
+	vec2 scale = r_FBufScale * r_NPOTScale;
 	vec2 st = gl_FragCoord.st;
 
 	// calculate the screen texcoord in the 0.0 to 1.0 range
@@ -39,26 +48,12 @@ void	main()
 	// scale by the screen non-power-of-two-adjust
 	st *= r_NPOTScale;
 
-	// calculate contrast color
-#if 0
 	// perform a box filter for the downsample
-	vec3 color = texture2D(u_ColorMap, st + vec2(-1.0, 1.0) * r_FBufScale).rgb;
-	color += texture2D(u_ColorMap, st + vec2(1.0, 1.0) * r_FBufScale).rgb;
-	color += texture2D(u_ColorMap, st + vec2(1.0, -1.0) * r_FBufScale).rgb;
-	color += texture2D(u_ColorMap, st + vec2(-1.0, -1.0) * r_FBufScale).rgb;
+	vec4 color = f(texture2D(u_ColorMap, st + vec2(-1.0, -1.0) * scale));
+	color += f(texture2D(u_ColorMap, st + vec2(-1.0, 1.0) * scale));
+	color += f(texture2D(u_ColorMap, st + vec2(1.0, -1.0) * scale));
+	color += f(texture2D(u_ColorMap, st + vec2(1.0, 1.0) * scale));
 	color *= 0.25;
-#else
-	vec4 color = texture2D(u_ColorMap, st);
-#endif
-
-	float L = dot(LUMINANCE_VECTOR, color);
-
-	// adjust contrast
-	L = pow(L, 1.32);
-
-	float T = clamp(L - 0.71, 0.0, 1.0);
-
-	color.rgb *= T;
 
 	gl_FragColor = color;
 }
