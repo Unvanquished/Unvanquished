e707e0d47b144c408b9147a9306905a64304d763
Gilles Bedel <gillou.ray@free.fr>
2013-01-06 00:40:20 +0100

    Fixes for the pot generation script.
    Include ui menu files that are in any subfolders of main/ (previously,
    the script was missing AlienCentered/ and khaos/).
    
    Also, fix the file references in the generated pot file. Previously, these
    were written with the gettext code comment tag (i.e. line starting with #.).
    Using the proper file reference tag instead (line starting with #:) allows
    gettext tools (like msgmerge) to merge and replace these lines when someone
    updates a po file from an updated pot file, instead of duplicating them.

--- src/utils/generate_pot.sh
+++ src/utils/generate_pot.sh
@@ -6,10 +6,15 @@ cd ../..
 
 find src/engine/server/ src/engine/client/ src/engine/qcommon/ | grep .c[p]*$ | xgettext --from-code=UTF-8 -o messages_client.pot -k_ -kN_ -kC_:2 -f -
 touch messages_game.pot
-for i in main/ui/*.menu
+find main/ -name '*.menu' | sort | while read i
 do
 	src/utils/generate_menu_pot.pl $i | xgettext --from-code=UTF-8 -a --no-location -C -c -j -o messages_game.pot -
 done
+
+# generate_menu_pot.pl writes file references as code comments (#. file:line)
+# but we want them as gettext file references (#: file:line)
+sed 's/^#\. \(.\+:[0-9]\+\)$/#: \1/' < messages_game.pot > messages_game.pot.tmp
+mv messages_game.pot.tmp messages_game.pot
 
 src/utils/gender_context.pl src/gamelogic/cgame/cg_event.c >> messages_game.pot
 
