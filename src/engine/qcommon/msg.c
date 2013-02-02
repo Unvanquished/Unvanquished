971f841fe76e5af56e61720140364290f1aac340
Zack Middleton <zturtleman@gmail.com>
2013-02-01 22:07:52 -0600

    Fix MSG_ReadDeltaKey setting bit 1<<bits often
    MSG_ReadDeltaKey would often set 1<<bits, it should never be set. It exceeds the size of what was being read.
    Worked okay for weapons/forward/right/up as they stored as chars (value would wrap around to correct value, lucky).
    Angles had the wrong value, not sure if it was causing issues.

--- src/engine/qcommon/msg.c
+++ src/engine/qcommon/msg.c
@@ -764,7 +764,7 @@ int MSG_ReadDeltaKey( msg_t *msg, int key, int oldV, int bits )
 {
 	if ( MSG_ReadBits( msg, 1 ) )
 	{
-		return MSG_ReadBits( msg, bits ) ^ ( key & kbitmask[ bits ] );
+		return MSG_ReadBits( msg, bits ) ^ ( key & kbitmask[ bits - 1 ] );
 	}
 
 	return oldV;
