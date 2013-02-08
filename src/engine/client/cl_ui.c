4a76dadb661724381fd00348b41314e8523b7928
Zack Middleton <zturtleman@gmail.com>
2013-02-08 22:47:27 +0000

    Fix resolving IPv6 favorites in Team Arena UI

--- src/engine/client/cl_ui.c
+++ src/engine/client/cl_ui.c
@@ -229,7 +229,7 @@ static int LAN_AddServer( int source, const char *name, const char *address )
 
 	if ( servers && *count < max )
 	{
-		NET_StringToAdr( address, &adr, NA_IP );
+		NET_StringToAdr( address, &adr, NA_UNSPEC );
 
 		for ( i = 0; i < *count; i++ )
 		{
@@ -286,7 +286,7 @@ static void LAN_RemoveServer( int source, const char *addr )
 	if ( servers )
 	{
 		netadr_t comp;
-		NET_StringToAdr( addr, &comp, NA_IP );
+		NET_StringToAdr( addr, &comp, NA_UNSPEC );
 
 		for ( i = 0; i < *count; i++ )
 		{
