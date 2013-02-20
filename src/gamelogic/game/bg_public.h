Unvanquished/Unvanquished

commit 9a288e82fb77e859a60cb126d0414f0be389a3d5

Revert unneeded misc changes made for bot code

src/gamelogic/game/bg_public.h
@@ -1154,21 +1154,21 @@ void                        BG_InitBuildableConfigs( void );
 
 const classAttributes_t     *BG_ClassByName( const char *name );
 
-const classAttributes_t     *BG_Class( class_t pClass );
-qboolean                    BG_ClassAllowedInStage( class_t pClass,
+const classAttributes_t     *BG_Class( class_t class );
+qboolean                    BG_ClassAllowedInStage( class_t class,
     stage_t stage );
 
-classConfig_t               *BG_ClassConfig( class_t pClass );
+classConfig_t               *BG_ClassConfig( class_t class );
 
-void                        BG_ClassBoundingBox( class_t pClass, vec3_t mins,
+void                        BG_ClassBoundingBox( class_t class, vec3_t mins,
     vec3_t maxs, vec3_t cmaxs,
     vec3_t dmins, vec3_t dmaxs );
-qboolean                    BG_ClassHasAbility( class_t pClass, int ability );
+qboolean                    BG_ClassHasAbility( class_t class, int ability );
 int                         BG_ClassCanEvolveFromTo( class_t fclass,
     class_t tclass,
     int credits, int alienStage, int num );
 
-qboolean                  BG_AlienCanEvolve( class_t pClass, int credits, int alienStage );
+qboolean                  BG_AlienCanEvolve( class_t class, int credits, int alienStage );
 
 void                      BG_InitClassConfigs( void );
 
@@ -1222,7 +1222,7 @@ void     BG_InitAllowedGameElements( void );
 qboolean BG_WeaponIsAllowed( weapon_t weapon );
 qboolean BG_UpgradeIsAllowed( upgrade_t upgrade );
 
-qboolean BG_ClassIsAllowed( class_t pClass );
+qboolean BG_ClassIsAllowed( class_t class );
 qboolean BG_BuildableIsAllowed( buildable_t buildable );
 
 weapon_t BG_PrimaryWeapon( int stats[] );
@@ -1256,7 +1256,7 @@ typedef struct voiceTrack_s
 	char                *text;
 	int                 enthusiasm;
 	int                 team;
-	int                 pClass;
+	int                    class;
 	int                 weapon;
 	struct voiceTrack_s *next;
 } voiceTrack_t;
@@ -1284,7 +1284,7 @@ voiceCmd_t   *BG_VoiceCmdByNum( voiceCmd_t *head, int num );
 voiceTrack_t *BG_VoiceTrackByNum( voiceTrack_t *head, int num );
 
 voiceTrack_t *BG_VoiceTrackFind( voiceTrack_t *head, team_t team,
-                                 class_t pClass, weapon_t weapon,
+                                 class_t class, weapon_t weapon,
                                  int enthusiasm, int *trackNum );
 
 int  BG_LoadEmoticons( emoticon_t *emoticons, int num );

src/gamelogic/game/bg_voice.c
@@ -207,12 +207,12 @@ static qboolean BG_VoiceParseTrack( int handle, voiceTrack_t *voiceTrack )
 			{
 				found = qtrue;
 
-				if ( voiceTrack->pClass < 0 )
+				if ( voiceTrack->class < 0 )
 				{
-					voiceTrack->pClass = 0;
+					voiceTrack->class = 0;
 				}
 
-				voiceTrack->pClass |= ( 1 << token.intvalue );
+				voiceTrack->class |= ( 1 << token.intvalue );
 				foundToken = trap_Parse_ReadToken( handle, &token );
 			}
 
@@ -345,7 +345,7 @@ static voiceTrack_t *BG_VoiceParseCommand( int handle )
 		}
 
 		voiceTracks->team = -1;
-		voiceTracks->pClass = -1;
+		voiceTracks->class = -1;
 		voiceTracks->weapon = -1;
 		voiceTracks->enthusiasm = 0;
 		voiceTracks->text = NULL;
@@ -503,7 +503,7 @@ void BG_PrintVoices( voice_t *voices, int debugLevel )
 				if ( debugLevel > 2 )
 				{
 					Com_Printf( "    team -> %d\n", voiceTrack->team );
-					Com_Printf( "    class -> %d\n", voiceTrack->pClass );
+					Com_Printf( "    class -> %d\n", voiceTrack->class );
 					Com_Printf( "    weapon -> %d\n", voiceTrack->weapon );
 					Com_Printf( "    enthusiasm -> %d\n", voiceTrack->enthusiasm );
 #ifdef CGAME
@@ -651,7 +651,7 @@ voiceTrack_t *BG_VoiceTrackFind( voiceTrack_t *head, team_t team,
 	while ( vt )
 	{
 		if ( ( vt->team >= 0 && !( vt->team  & ( 1 << team ) ) ) ||
-		     ( vt->pClass >= 0 && !( vt->pClass & ( 1 << class ) ) ) ||
+		     ( vt->class >= 0 && !( vt->class & ( 1 << class ) ) ) ||
 		     vt->enthusiasm > enthusiasm )
 		{
 			vt = vt->next;
@@ -688,7 +688,7 @@ voiceTrack_t *BG_VoiceTrackFind( voiceTrack_t *head, team_t team,
 		j++;
 
 		if ( ( vt->team >= 0 && !( vt->team  & ( 1 << team ) ) ) ||
-		     ( vt->pClass >= 0 && !( vt->pClass & ( 1 << class ) ) ) ||
+		     ( vt->class >= 0 && !( vt->class & ( 1 << class ) ) ) ||
 		     vt->enthusiasm != highestMatch )
 		{
 			vt = vt->next;

CMakeLists.txt
@@ -690,7 +690,6 @@ set( CGAMEGPP_BASE
 set( CGAMEGPP_SO
   ${CGAMEGPP_BASE}
   ${GPP_DIR}/cgame/cg_api.c
-#   ${GPP_DIR}/cgame/cg_nav.c
   ${GPP_DIR}/cgame/cgame.def
 )
 
@@ -1236,9 +1235,8 @@ GAMELIB( LIB_NAME gppgame
   OUTPUT_DIR main
   COMPILE_DEF GAME GAMEDLL GPP USE_REFENTITY_ANIMATIONSYSTEM
   LIB_FILES ${GAMEGPP_SO} ${GPP_DIR}
-# temporarily disabled due to build failure with bots code
-   QVM_FILES ${GAMEGPP_BASE}
-   QVM_ASM ${GAMEGPP_QVM}
+  QVM_FILES ${GAMEGPP_BASE}
+  QVM_ASM ${GAMEGPP_QVM}
 )
 
 GAMELIB( LIB_NAME gpptrem

src/engine/qcommon/q_shared.h
@@ -295,11 +295,7 @@ extern int memcmp( void *, void *, size_t );
 #define PADP(base, alignment)   ((void *) PAD((intptr_t) ( base ), ( alignment )))
 
 #ifndef NULL
-#ifdef __cplusplus
-#define NULL ( 0L )
-#else
 #define NULL ( (void *)0 )
-#endif
 #endif
 
 #define STRING(s)  #s

src/gamelogic/game/bg_lib.h
@@ -29,11 +29,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define BG_LIB_H
 
 #ifndef NULL
-#ifdef __cplusplus
-#define NULL ( 0L )
-#else
-#define NULL ( (void *)0 )
-#endif
+#define NULL ((void *)0 )
 #endif
 
 

commit 365bb2bfc4efc29a61a774d7d0ed583ef2f1b5dc

Fix compile error caused by commit 9a288e8

src/gamelogic/game/bg_public.h
@@ -1154,21 +1154,21 @@ void                        BG_InitBuildableConfigs( void );
 
 const classAttributes_t     *BG_ClassByName( const char *name );
 
-const classAttributes_t     *BG_Class( class_t class );
-qboolean                    BG_ClassAllowedInStage( class_t class,
+const classAttributes_t     *BG_Class( class_t pClass );
+qboolean                    BG_ClassAllowedInStage( class_t pClass,
     stage_t stage );
 
-classConfig_t               *BG_ClassConfig( class_t class );
+classConfig_t               *BG_ClassConfig( class_t pClass );
 
-void                        BG_ClassBoundingBox( class_t class, vec3_t mins,
+void                        BG_ClassBoundingBox( class_t pClass, vec3_t mins,
     vec3_t maxs, vec3_t cmaxs,
     vec3_t dmins, vec3_t dmaxs );
-qboolean                    BG_ClassHasAbility( class_t class, int ability );
+qboolean                    BG_ClassHasAbility( class_t pClass, int ability );
 int                         BG_ClassCanEvolveFromTo( class_t fclass,
     class_t tclass,
     int credits, int alienStage, int num );
 
-qboolean                  BG_AlienCanEvolve( class_t class, int credits, int alienStage );
+qboolean                  BG_AlienCanEvolve( class_t pClass, int credits, int alienStage );
 
 void                      BG_InitClassConfigs( void );
 
@@ -1222,7 +1222,7 @@ void     BG_InitAllowedGameElements( void );
 qboolean BG_WeaponIsAllowed( weapon_t weapon );
 qboolean BG_UpgradeIsAllowed( upgrade_t upgrade );
 
-qboolean BG_ClassIsAllowed( class_t class );
+qboolean BG_ClassIsAllowed( class_t pClass );
 qboolean BG_BuildableIsAllowed( buildable_t buildable );
 
 weapon_t BG_PrimaryWeapon( int stats[] );
@@ -1256,7 +1256,7 @@ typedef struct voiceTrack_s
 	char                *text;
 	int                 enthusiasm;
 	int                 team;
-	int                    class;
+	int                 pClass;
 	int                 weapon;
 	struct voiceTrack_s *next;
 } voiceTrack_t;
@@ -1284,7 +1284,7 @@ voiceCmd_t   *BG_VoiceCmdByNum( voiceCmd_t *head, int num );
 voiceTrack_t *BG_VoiceTrackByNum( voiceTrack_t *head, int num );
 
 voiceTrack_t *BG_VoiceTrackFind( voiceTrack_t *head, team_t team,
-                                 class_t class, weapon_t weapon,
+                                 class_t pClass, weapon_t weapon,
                                  int enthusiasm, int *trackNum );
 
 int  BG_LoadEmoticons( emoticon_t *emoticons, int num );

src/gamelogic/game/bg_voice.c
@@ -207,12 +207,12 @@ static qboolean BG_VoiceParseTrack( int handle, voiceTrack_t *voiceTrack )
 			{
 				found = qtrue;
 
-				if ( voiceTrack->class < 0 )
+				if ( voiceTrack->pClass < 0 )
 				{
-					voiceTrack->class = 0;
+					voiceTrack->pClass = 0;
 				}
 
-				voiceTrack->class |= ( 1 << token.intvalue );
+				voiceTrack->pClass |= ( 1 << token.intvalue );
 				foundToken = trap_Parse_ReadToken( handle, &token );
 			}
 
@@ -345,7 +345,7 @@ static voiceTrack_t *BG_VoiceParseCommand( int handle )
 		}
 
 		voiceTracks->team = -1;
-		voiceTracks->class = -1;
+		voiceTracks->pClass = -1;
 		voiceTracks->weapon = -1;
 		voiceTracks->enthusiasm = 0;
 		voiceTracks->text = NULL;
@@ -503,7 +503,7 @@ void BG_PrintVoices( voice_t *voices, int debugLevel )
 				if ( debugLevel > 2 )
 				{
 					Com_Printf( "    team -> %d\n", voiceTrack->team );
-					Com_Printf( "    class -> %d\n", voiceTrack->class );
+					Com_Printf( "    class -> %d\n", voiceTrack->pClass );
 					Com_Printf( "    weapon -> %d\n", voiceTrack->weapon );
 					Com_Printf( "    enthusiasm -> %d\n", voiceTrack->enthusiasm );
 #ifdef CGAME
@@ -651,7 +651,7 @@ voiceTrack_t *BG_VoiceTrackFind( voiceTrack_t *head, team_t team,
 	while ( vt )
 	{
 		if ( ( vt->team >= 0 && !( vt->team  & ( 1 << team ) ) ) ||
-		     ( vt->class >= 0 && !( vt->class & ( 1 << class ) ) ) ||
+		     ( vt->pClass >= 0 && !( vt->pClass & ( 1 << class ) ) ) ||
 		     vt->enthusiasm > enthusiasm )
 		{
 			vt = vt->next;
@@ -688,7 +688,7 @@ voiceTrack_t *BG_VoiceTrackFind( voiceTrack_t *head, team_t team,
 		j++;
 
 		if ( ( vt->team >= 0 && !( vt->team  & ( 1 << team ) ) ) ||
-		     ( vt->class >= 0 && !( vt->class & ( 1 << class ) ) ) ||
+		     ( vt->pClass >= 0 && !( vt->pClass & ( 1 << class ) ) ) ||
 		     vt->enthusiasm != highestMatch )
 		{
 			vt = vt->next;