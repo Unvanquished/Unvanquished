207c91f7f260ab35f27c52e97303fbe9fed61d88
Tim <idn.inbox@yahoo.com>
2013-02-24 07:08:59 +0100

    let G_Find ignore disabled entities
    this has the immediate effect of us being able to disable spawnpoints and intermissions,
    but also allows us to use G_Find in other places now

--- src/gamelogic/game/g_utils.c
+++ src/gamelogic/game/g_utils.c
@@ -182,7 +182,7 @@ gentity_t *G_Find( gentity_t *from, int fieldofs, const char *match )
 
 	for ( ; from < &g_entities[ level.num_entities ]; from++ )
 	{
-		if ( !from->inuse )
+		if ( !from->inuse || !from->enabled )
 		{
 			continue;
 		}
