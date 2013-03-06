9b1c5fac4954ac140c3745e41cab420a65352866
Darren Salt <devspam@moreofthesa.me.uk>
2013-03-06 18:54:11 +0000

    Bump version no.

--- src/engine/qcommon/q_shared.h
+++ src/engine/qcommon/q_shared.h
@@ -45,10 +45,10 @@ extern "C" {
 #define PRODUCT_NAME            "Unvanquished"
 #define PRODUCT_NAME_UPPER      "UNVANQUISHED" // Case, No spaces
 #define PRODUCT_NAME_LOWER      "unvanquished" // No case, No spaces
-#define PRODUCT_VERSION         "0.13.0"
+#define PRODUCT_VERSION         "0.13.1"
 
 #define ENGINE_NAME             "Daemon Engine"
-#define ENGINE_VERSION          "0.13.0"
+#define ENGINE_VERSION          "0.13.1"
 
 #ifdef REVISION
 # define Q3_VERSION             PRODUCT_NAME " " PRODUCT_VERSION " " REVISION
