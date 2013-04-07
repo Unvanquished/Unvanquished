b84af672122c542938e260491e41f2c74bd219bb
Tim <idn.inbox@yahoo.com>
2013-04-07 21:37:09 +0200

    fix preceeding blackouts of loading screen and main menu

--- src/engine/rendererGL/tr_init.cpp
+++ src/engine/rendererGL/tr_init.cpp
@@ -1175,9 +1175,6 @@ extern "C" {
 		glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
 		glClearDepth( 1.0 );
 
-		glDrawBuffer( GL_FRONT );
-		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT );
-
 		glDrawBuffer( GL_BACK );
 		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT );
 
