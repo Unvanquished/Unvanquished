125fe4269a5a8271c31d6e373c35c44abe6b092c
Viech <viech@example.com>
2013-04-19 17:32:18 +0200

    Let aliens hear the luci overcharge sound.
    Cheating physics is no valid way of balancing stuff, seriously.

--- src/gamelogic/cgame/cg_weapons.c
+++ src/gamelogic/cgame/cg_weapons.c
@@ -1663,9 +1663,7 @@ void CG_AddPlayerWeapon( refEntity_t *parent, playerState_t *ps, centity_t *cent
 	}
 
 	// Lucifer cannon charge warning beep
-	if ( weaponNum == WP_LUCIFER_CANNON &&
-	     ( cent->currentState.eFlags & EF_WARN_CHARGE ) &&
-	     cg.snap->ps.stats[ STAT_TEAM ] != TEAM_ALIENS )
+	if ( weaponNum == WP_LUCIFER_CANNON && ( cent->currentState.eFlags & EF_WARN_CHARGE ) )
 	{
 		trap_S_AddLoopingSound( cent->currentState.number, cent->lerpOrigin,
 		                        vec3_origin, ps ? cgs.media.lCannonWarningSound :
