18b3c1a5dac03d54f8594657a86dcbf05f24bfba
dolcetriade <vcelestialragev@gmail.com>
2013-04-26 01:13:22 -0700

    Fix too many arguments for format warning.
    Thanks kangz.

--- src/engine/rendererGL/gl_shader.h
+++ src/engine/rendererGL/gl_shader.h
@@ -468,7 +468,7 @@ protected:
 			GLimp_LogComment( va( "GLSL_SetUniformMatrix4f( %s, shader: %s, transpose: %d, [ %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f ] ) ---\n",
 				this->GetName(), _shader->GetName().c_str(), transpose,
 				m[ 0 ], m[ 1 ], m[ 2 ], m[ 3 ], m[ 4 ], m[ 5 ], m[ 6 ], m[ 7 ], m[ 8 ], m[ 9 ], m[ 10 ], m[ 11 ], m[ 12 ],
-				m[ 13 ], m[ 14 ], m[ 15 ], m[ 16 ] ) );
+				m[ 13 ], m[ 14 ], m[ 15 ] ) );
 		}
 #endif
 #if defined( USE_UNIFORM_FIREWALL )
