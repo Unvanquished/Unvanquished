Unvanquished/Unvanquished

commit c4d60b8503894766fdb20b80ae6717b020421a4a

coneptually clarify and and set states accordingly for act(), delay and the active state in relation to each other

src/gamelogic/game/g_buildable.c
@@ -2751,7 +2751,7 @@ void HMGTurret_Think( gentity_t *self )
 	if ( !HMGTurret_CheckTarget( self, self->target, qtrue ) )
 	{
 		self->active = qfalse;
-		self->activeAtTime = -1;
+		self->nextAct = 0;
 		HMGTurret_FindEnemy( self );
 	}
 
@@ -2767,24 +2767,24 @@ void HMGTurret_Think( gentity_t *self )
 	if ( !HMGTurret_TrackEnemy( self ) )
 	{
 		self->active = qfalse;
-		self->activeAtTime = -1;
+		self->nextAct = 0;
 		return;
 	}
 
 	// Update spin state
 	if ( !self->active && self->timestamp < level.time )
 	{
-		self->active = qtrue;
-
-		self->activeAtTime = level.time + MGTURRET_SPINUP_TIME;
+		self->nextAct = level.time + MGTURRET_SPINUP_TIME;
 		G_AddEvent( self, EV_MGTURRET_SPINUP, 0 );
 	}
 
 	// Not firing or haven't spun up yet
-	if ( !self->active || self->activeAtTime > level.time )
+	if ( !self->nextAct || self->nextAct > level.time )
 	{
 		return;
 	}
+
+	self->active = qtrue;
 
 	// Fire repeat delay
 	if ( self->timestamp > level.time )

src/gamelogic/game/g_entities.c
@@ -797,7 +797,11 @@ void G_CallEntity(gentity_t *targetedEntity, gentityCall_t *call)
 			break;
 		case ECA_ACT:
 			if (targetedEntity->act)
+			{
+				targetedEntity->active = qtrue;
 				targetedEntity->act(targetedEntity, call->caller, call->activator);
+				targetedEntity->active = qfalse;
+			}
 			break;
 
 		default:

src/gamelogic/game/g_local.h
@@ -104,7 +104,8 @@ typedef struct
 	int damage;
 
 	/**
-	 * how long delay firing an event
+	 * how long to wait before fullfilling the maintask act()
+	 * (e.g. how long to delay sensing as sensor or relaying as relay)
 	 */
 	variatingTime_t delay;
 	/**
@@ -163,13 +164,25 @@ struct gentity_s
 	int          creationTime;
 
 	char         *names[ MAX_ENTITY_ALIASES + 1 ];
-	/*
+
+	/**
 	 * is the entity considered active?
 	 * as in 'currently doing something'
 	 * e.g. used for buildables (e.g. medi-stations or hives can be in an active state or being inactive)
+	 * or during executing act() in general
 	 */
 	qboolean     active;
-	int          activeAtTime; /*< delay being really active until this time, e.g for spinup for norfenturrets */
+	/**
+	 * delay being really active until this time, e.g for act() delaying or for spinup for norfenturrets
+	 * this will most probably be set by think() before act()ing, probably by using the config.delay time
+	 */
+	int          nextAct;
+	/**
+	 * Fulfill the main task of this entity.
+	 * act, therefore also become active,
+	 * but only if enabled
+	 */
+	void ( *act )( gentity_t *self, gentity_t *caller, gentity_t *activator );
 
 	/**
 	 * is the entity able to become active?
@@ -302,12 +315,12 @@ struct gentity_s
 
 	int       nextthink;
 	void ( *think )( gentity_t *self );
+
 	void ( *reset )( gentity_t *self );
 	void ( *reached )( gentity_t *self );       // movers call this when hitting endpoint
 	void ( *blocked )( gentity_t *self, gentity_t *other );
 	void ( *touch )( gentity_t *self, gentity_t *other, trace_t *trace );
 	void ( *use )( gentity_t *self, gentity_t *other, gentity_t *activator );
-	void ( *act )( gentity_t *self, gentity_t *caller, gentity_t *activator );
 	void ( *pain )( gentity_t *self, gentity_t *attacker, int damage );
 	void ( *die )( gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int damage, int mod );
 

commit 6f9034c4dd0e7e053baf49faf43d6a7883742906

Turrets never stopped spinning up. Fix this.
Broken in commit c4d60b8503894766fdb20b80ae6717b020421a4a.

src/gamelogic/game/g_buildable.c
@@ -2774,6 +2774,7 @@ void HMGTurret_Think( gentity_t *self )
 	// Update spin state
 	if ( !self->active && self->timestamp < level.time )
 	{
+		self->active = qtrue;
 		self->nextAct = level.time + MGTURRET_SPINUP_TIME;
 		G_AddEvent( self, EV_MGTURRET_SPINUP, 0 );
 	}
@@ -2783,8 +2784,6 @@ void HMGTurret_Think( gentity_t *self )
 	{
 		return;
 	}
-
-	self->active = qtrue;
 
 	// Fire repeat delay
 	if ( self->timestamp > level.time )