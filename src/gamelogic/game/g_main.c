5c9b2263c16e50eeaf925017b45335653458cc4b
Darren Salt <devspam@moreofthesa.me.uk>
2013-06-16 03:07:16 +0100

    If g_nextMap is set and valid, the next (in-rotation) map would be skipped. Fix this.

--- src/gamelogic/game/g_main.c
+++ src/gamelogic/game/g_main.c
@@ -1914,11 +1914,6 @@ void ExitLevel( void )
 	if ( G_MapExists( g_nextMap.string ) )
 	{
 		trap_SendConsoleCommand( EXEC_APPEND, va( "map %s %s\n", Quote( g_nextMap.string ), Quote( g_nextMapLayouts.string ) ) );
-
-		if ( G_MapRotationActive() )
-		{
-			G_AdvanceMapRotation( 0 );
-		}
 	}
 	else if ( G_MapRotationActive() )
 	{
