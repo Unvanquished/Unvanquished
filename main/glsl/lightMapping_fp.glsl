Unvanquished/Unvanquished

commit 483b1f2127fc65291ab008dc8ee2162b1414cb1b

Simplify gl_FrontFacing glsl branches
This allows AMD's glsl compiler to optimize away an expensive JUMP + associated instructions

main/glsl/lightMapping_fp.glsl
@@ -52,17 +52,13 @@ void	main()
 	vec2 texSpecular = var_TexSpecular.st;
 
 	// invert tangent space for two sided surfaces
-	mat3 tangentToWorldMatrix;
+	mat3 tangentToWorldMatrix = mat3(var_Tangent.xyz, var_Binormal.xyz, var_Normal.xyz);
 #if defined(TWOSIDED)
 	if(gl_FrontFacing)
 	{
-		tangentToWorldMatrix = mat3(-var_Tangent.xyz, -var_Binormal.xyz, -var_Normal.xyz);
+		tangentToWorldMatrix = -tangentToWorldMatrix;
 	}
-	else
 #endif
-	{
-		tangentToWorldMatrix = mat3(var_Tangent.xyz, var_Binormal.xyz, var_Normal.xyz);
-	}
 
 	// compute view direction in world space
 	vec3 I = normalize(u_ViewOrigin - var_Position);
@@ -167,18 +163,14 @@ void	main()
 		return;
 	}
 
-	vec3 N;
+	vec3 N = normalize(var_Normal);
 
 #if defined(TWOSIDED)
 	if(gl_FrontFacing)
 	{
-		N = -normalize(var_Normal);
+		N = -N
 	}
-	else
 #endif
-	{
-		N = normalize(var_Normal);
-	}
 
 	vec3 specular = vec3(0.0, 0.0, 0.0);
 

main/glsl/vertexLighting_DBS_world_fp.glsl
@@ -50,22 +50,16 @@ void	main()
 #if defined(USE_NORMAL_MAPPING)
 
 	// construct object-space-to-tangent-space 3x3 matrix
-	mat3 objectToTangentMatrix;
+	mat3 objectToTangentMatrix = objectToTangentMatrix = mat3(	var_Tangent.x, var_Binormal.x, var_Normal.x,
+							var_Tangent.y, var_Binormal.y, var_Normal.y,
+							var_Tangent.z, var_Binormal.z, var_Normal.z	);
 
 #if defined(TWOSIDED)
 	if(gl_FrontFacing)
 	{
-		objectToTangentMatrix = mat3( -var_Tangent.x, -var_Binormal.x, -var_Normal.x,
-							-var_Tangent.y, -var_Binormal.y, -var_Normal.y,
-							-var_Tangent.z, -var_Binormal.z, -var_Normal.z	);
+		objectToTangentMatrix = -objectToTangentMatrix;
 	}
-	else
 #endif
-	{
-		objectToTangentMatrix = mat3(	var_Tangent.x, var_Binormal.x, var_Normal.x,
-							var_Tangent.y, var_Binormal.y, var_Normal.y,
-							var_Tangent.z, var_Binormal.z, var_Normal.z	);
-	}
 
 	// compute view direction in tangent space
 	vec3 V = normalize(objectToTangentMatrix * (u_ViewOrigin - var_Position));
@@ -150,18 +144,14 @@ void	main()
 
 #elif defined(COMPAT_Q3A)
 
-	vec3 N;
+	vec3 N = normalize(var_Normal);
 
 #if defined(TWOSIDED)
 	if(gl_FrontFacing)
 	{
-		N = -normalize(var_Normal);
+		N = -N;
 	}
-	else
 #endif
-	{
-		N = normalize(var_Normal);
-	}
 
 	// compute the diffuse term
 	vec4 diffuse = texture2D(u_DiffuseMap, var_TexDiffuseNormal.st);

main/glsl/forwardLighting_fp.glsl
@@ -942,17 +942,13 @@ void	main()
 #if defined(USE_NORMAL_MAPPING)
 
 	// invert tangent space for twosided surfaces
-	mat3 tangentToWorldMatrix;
+	mat3 tangentToWorldMatrix = mat3(var_Tangent.xyz, var_Binormal.xyz, var_Normal.xyz);
 #if defined(TWOSIDED)
 	if(gl_FrontFacing)
 	{
-		tangentToWorldMatrix = mat3(-var_Tangent.xyz, -var_Binormal.xyz, -var_Normal.xyz);
+		tangentToWorldMatrix = -tangentToWorldMatrix;
 	}
-	else
 #endif
-	{
-		tangentToWorldMatrix = mat3(var_Tangent.xyz, var_Binormal.xyz, var_Normal.xyz);
-	}
 
 
 	vec2 texNormal = var_TexNormal.st;
@@ -1007,17 +1003,13 @@ void	main()
 
 #else // USE_NORMAL_MAPPING
 
-	vec3 N;
+	vec3 N = normalize(var_Normal.xyz);
 #if defined(TWOSIDED)
 	if(gl_FrontFacing)
 	{
-		N = -normalize(var_Normal.xyz);
+		N = -N;
 	}
-	else
 #endif
-	{
-		N = normalize(var_Normal.xyz);
-	}
 
 #endif // USE_NORMAL_MAPPING
 

main/glsl/geometricFill_fp.glsl
@@ -63,18 +63,14 @@ void	main()
 
 #if defined(USE_NORMAL_MAPPING)
 	// invert tangent space for two sided surfaces
-	mat3 tangentToWorldMatrix;
+	mat3 tangentToWorldMatrix = mat3(var_Tangent.xyz, var_Binormal.xyz, var_Normal.xyz);
 
 #if defined(TWOSIDED)
 	if(gl_FrontFacing)
 	{
-		tangentToWorldMatrix = mat3(-var_Tangent.xyz, -var_Binormal.xyz, -var_Normal.xyz);
+		tangentToWorldMatrix = -tangentToWorldMatrix;
 	}
-	else
 #endif
-	{
-		tangentToWorldMatrix = mat3(var_Tangent.xyz, var_Binormal.xyz, var_Normal.xyz);
-	}
 
 	vec2 texNormal = var_TexNormal.st;
 #if !defined(r_DeferredLighting)
@@ -154,25 +150,18 @@ void	main()
 
 #else // USE_NORMAL_MAPPING
 
-	vec3 N;
+	vec3 N = normalize(var_Normal);
 
 #if defined(TWOSIDED)
 	if(gl_FrontFacing)
 	{
-		N = -normalize(var_Normal);
-		// gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-		// return;
+		N = -N;
 	}
-	else
 #endif
-	{
-		N = normalize(var_Normal);
-	}
 
 	vec3 specular = vec3(0.0);
 
 #endif // USE_NORMAL_MAPPING
-
 
 	// compute the diffuse term
 	vec4 diffuse = texture2D(u_DiffuseMap, texDiffuse);

main/glsl/liquid_fp.glsl
@@ -105,11 +105,11 @@ void	main()
 	// compute incident ray
 	vec3 I = normalize(u_ViewOrigin - var_Position);
 
-	mat3 tangentToWorldMatrix;
+	mat3 tangentToWorldMatrix = mat3(var_Tangent.xyz, var_Binormal.xyz, var_Normal.xyz);
 	if(gl_FrontFacing)
-		tangentToWorldMatrix = mat3(-var_Tangent.xyz, -var_Binormal.xyz, -var_Normal.xyz);
-	else
-		tangentToWorldMatrix = mat3(var_Tangent.xyz, var_Binormal.xyz, var_Normal.xyz);
+	{
+		tangentToWorldMatrix = -tangentToWorldMatrix;
+	}
 
 	mat3 worldToTangentMatrix;
 #if defined(GLHW_ATI) || defined(GLHW_ATI_DX10) || defined(GLDRV_MESA)

main/glsl/reflection_CB_fp.glsl
@@ -48,13 +48,13 @@ void	main()
 	#endif
 
 	// invert tangent space for twosided surfaces
-	mat3 tangentToWorldMatrix;
+	mat3 tangentToWorldMatrix = mat3(var_Tangent.xyz, var_Binormal.xyz, var_Normal.xyz);
 #if defined(TWOSIDED)
 	if(gl_FrontFacing)
-		tangentToWorldMatrix = mat3(-var_Tangent.xyz, -var_Binormal.xyz, -var_Normal.xyz);
-	else
+	{
+		tangentToWorldMatrix = -tangentToWorldMatrix;
+	}
 #endif
-		tangentToWorldMatrix = mat3(var_Tangent.xyz, var_Binormal.xyz, var_Normal.xyz);
 
 	// transform normal into world space
 	N = normalize(tangentToWorldMatrix * N);

main/glsl/vertexLighting_DBS_entity_fp.glsl
@@ -62,18 +62,14 @@ void	main()
 
 #if defined(USE_NORMAL_MAPPING)
 	// invert tangent space for two sided surfaces
-	mat3 tangentToWorldMatrix;
+	mat3 tangentToWorldMatrix = mat3(var_Tangent.xyz, var_Binormal.xyz, var_Normal.xyz);
 
 #if defined(TWOSIDED)
 	if(gl_FrontFacing)
 	{
-		tangentToWorldMatrix = mat3(-var_Tangent.xyz, -var_Binormal.xyz, -var_Normal.xyz);
+		tangentToWorldMatrix = -tangentToWorldMatrix;
 	}
-	else
 #endif
-	{
-		tangentToWorldMatrix = mat3(var_Tangent.xyz, var_Binormal.xyz, var_Normal.xyz);
-	}
 
 	vec2 texNormal = var_TexNormal.st;
 	vec2 texSpecular = var_TexSpecular.st;
@@ -156,20 +152,14 @@ void	main()
 
 #else // USE_NORMAL_MAPPING
 
-	vec3 N;
+	vec3 N = normalize(var_Normal);
 
 #if defined(TWOSIDED)
 	if(gl_FrontFacing)
 	{
-		N = -normalize(var_Normal);
-		// gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-		// return;
+		N = -N;
 	}
-	else
 #endif
-	{
-		N = normalize(var_Normal);
-	}
 
 	vec3 specBase = vec3(0.0);
 	vec3 specMult = vec3(0.0);

commit 4e26120179289fd081048d73b3edba10bfd2fdfe

Fix typos introduced from commit 483b1f2127fc65291ab008dc8ee2162b1414cb1b

main/glsl/lightMapping_fp.glsl
@@ -168,7 +168,7 @@ void	main()
 #if defined(TWOSIDED)
 	if(gl_FrontFacing)
 	{
-		N = -N
+		N = -N;
 	}
 #endif
 

main/glsl/vertexLighting_DBS_world_fp.glsl
@@ -50,7 +50,7 @@ void	main()
 #if defined(USE_NORMAL_MAPPING)
 
 	// construct object-space-to-tangent-space 3x3 matrix
-	mat3 objectToTangentMatrix = objectToTangentMatrix = mat3(	var_Tangent.x, var_Binormal.x, var_Normal.x,
+	mat3 objectToTangentMatrix = mat3(	var_Tangent.x, var_Binormal.x, var_Normal.x,
 							var_Tangent.y, var_Binormal.y, var_Normal.y,
 							var_Tangent.z, var_Binormal.z, var_Normal.z	);
 