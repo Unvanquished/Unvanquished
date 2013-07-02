48c42c31d9a801e73cf01bf12d4334bbcdaf6126
dolcetriade <vcelestialragev@gmail.com>
2013-07-02 01:07:23 -0700

    Rewrite progressbar to use daemon renderer functions. Another side effect is your eyes will bleed less when you read the code

--- src/engine/client/rocketProgressBar.h
+++ src/engine/client/rocketProgressBar.h
@@ -65,13 +65,9 @@ typedef enum
 class RocketProgressBar : public Rocket::Core::Element
 {
 public:
-	RocketProgressBar( const Rocket::Core::String &tag ) : Rocket::Core::Element( tag ), dirty_geometry( true ), orientation( LEFT ), value( 0.0f ), renderFilter( 0 ), loadFilter( 0 ), color( Rocket::Core::Colourb( 255, 255, 255 ) )
+	RocketProgressBar( const Rocket::Core::String &tag ) : Rocket::Core::Element( tag ), dirty_geometry( true ), orientation( LEFT ), value( 0.0f ), shader( 0 )
 	{
-		for ( int i = START; i < NUM_GEOMETRIES; ++i )
-		{
-			geometry[ i ].SetHostElement( this );
-			size[ i ] = Rocket::Core::Vector2f( 0, 0 );
-		}
+		Com_Memset( color, 1, sizeof( color ) );
 	}
 
 	~RocketProgressBar() {}
@@ -107,18 +103,59 @@ public:
 
 	void OnRender( void )
 	{
-		OnUpdate(); // When loading maps, Render() is called multiple times without updating.
 
-		if ( dirty_geometry )
+		if ( !shader )
 		{
-			GenerateGeometry();
+			return;
 		}
 
-		for ( int i = 0; i < NUM_GEOMETRIES; ++i )
+		OnUpdate(); // When loading maps, Render() is called multiple times without updating.
+		Rocket::Core::Vector2f position = GetAbsoluteOffset();
+
+		// Vertical meter
+		if( orientation >= UP )
 		{
-			if ( renderFilter & 1 << i )
+			float height;
+			height = dimensions.y * value;
+
+			// Meter decreases down
+			if ( orientation == DOWN )
 			{
-				geometry[ i ].Render( GetAbsoluteOffset( Rocket::Core::Box::CONTENT ) );
+				re.SetColor( color );
+				re.DrawStretchPic( position.x, position.y, dimensions.x, height,
+						       0.0f, 0.0f, 1.0f, value, shader );
+				re.SetColor( NULL );
+			}
+			else
+			{
+				re.SetColor( color );
+				re.DrawStretchPic( position.x, position.y - height + dimensions.y, dimensions.x,
+						       height, 0.0f, 1.0f - value, 1.0f, 1.0f, shader );
+				re.SetColor( NULL );
+			}
+		}
+
+		// Horizontal meter
+		else
+		{
+			float width;
+
+			width = dimensions.x * value;
+
+			// Meter decreases to the left
+			if ( orientation == LEFT )
+			{
+				re.SetColor( color );
+				re.DrawStretchPic( position.x, position.y, width,
+						       dimensions.y, 0.0f, 0.0f, value, 1.0f, shader );
+				re.SetColor( NULL );
+			}
+			else
+			{
+				re.SetColor( color );
+				re.DrawStretchPic( position.x - width + dimensions.x, position.y, width,
+						       dimensions.y, 1.0f - value, 0.0f, 1.0f, 1.0f, shader );
+				re.SetColor( NULL );
 			}
 		}
 	}
@@ -129,36 +166,34 @@ public:
 
 		if ( changed_properties.find( "color" ) != changed_properties.end() )
 		{
-			color = GetProperty( "color" )->Get<Rocket::Core::Colourb>();
+			Rocket::Core::Colourb tempColor = GetProperty( "color" )->Get<Rocket::Core::Colourb>();
+
+			color[ 0 ] = tempColor.red / 255.0f;
+			color[ 1 ] = tempColor.blue / 255.0f;
+			color[ 2 ] = tempColor.green / 255.0f;
+			color[ 3 ] = tempColor.alpha / 255.0f;
 		}
 
-		if ( changed_properties.find( "start-image" ) != changed_properties.end() )
+		if ( changed_properties.find( "image" ) != changed_properties.end() )
 		{
-			LoadTexture( START );
-		}
+			const char *image = GetProperty( "image" )->Get<Rocket::Core::String>().CString();
 
-		if ( changed_properties.find( "progress-image" ) != changed_properties.end() )
-		{
-			LoadTexture( PROGRESS );
-		}
+			// skip the leading slash
+			if ( image && *image == '/' )
+			{
+				image++;
+			}
 
-		if ( changed_properties.find( "decoration-image" ) != changed_properties.end() )
-		{
-			LoadTexture( DECORATION );
-		}
-
-		if ( changed_properties.find( "end-image" ) != changed_properties.end() )
-		{
-			LoadTexture( END );
+			shader = re.RegisterShader( image, RSF_NOMIP );
 		}
 
 		if ( changed_properties.find( "orientation" ) != changed_properties.end() )
 		{
 			Rocket::Core::String  orientation_string = GetProperty<Rocket::Core::String>( "orientation" );
 
-			if ( orientation_string == "right" )
+			if ( orientation_string == "left" )
 			{
-				orientation = RIGHT;
+				orientation = LEFT;
 			}
 			else if ( orientation_string == "up" )
 			{
@@ -170,7 +205,7 @@ public:
 			}
 			else
 			{
-				orientation = LEFT;
+				orientation = RIGHT;
 			}
 
 			dirty_geometry = true;
@@ -189,287 +224,65 @@ public:
 		}
 	}
 
-	bool GetIntrinsicDimensions( Rocket::Core::Vector2f &dimensions )
+	bool GetIntrinsicDimensions( Rocket::Core::Vector2f &dimension )
 	{
-		// Calculate the x dimension.
-		if ( ( ( dimensions.x = GetProperty< float >( "width" ) ) == 0 ) )
+		const Rocket::Core::Property *property;
+		property = GetProperty( "width" );
+
+		// Absolute unit. We can use it as is
+		if ( property->unit & Rocket::Core::Property::ABSOLUTE_UNIT )
 		{
-			dimensions.x = ( float ) GetParentNode()->GetBox( Rocket::Core::Box::CONTENT ).GetSize().x;
+			dimensions.x = property->value.Get<float>();
+		}
+		else
+		{
+			float base_size = 0;
+			Rocket::Core::Element *parent = this;
+
+			while ( parent = parent->GetParentNode() )
+			{
+				if ( ( base_size = parent->GetOffsetWidth() ) != 0 )
+				{
+					dimensions.x = ResolveProperty( "width", base_size );
+					break;
+				}
+			}
 		}
 
-		// Calculate the y dimension.
-		if ( ( ( dimensions.y = GetProperty< float >( "height" ) ) == 0 ) )
+		property = GetProperty( "height" );
+		if ( property->unit & Rocket::Core::Property::ABSOLUTE_UNIT )
 		{
-			dimensions.y = ( float ) GetParentNode()->GetBox( Rocket::Core::Box::CONTENT ).GetSize().y;
+			dimensions.y = property->value.Get<float>();
 		}
+		else
+		{
+			float base_size = 0;
+			Rocket::Core::Element *parent = this;
 
+			while ( parent = parent->GetParentNode() )
+			{
+				if ( ( base_size = parent->GetOffsetHeight() ) != 0 )
+				{
+					dimensions.y = ResolveProperty( "height", base_size );
+					break;
+				}
+			}
+		}
 		// Return the calculated dimensions. If this changes the size of the element, it will result in
 		// a 'resize' event which is caught below and will regenerate the geometry.
+
+		dimension = dimensions;
+
 		return true;
 	}
 
 private:
-	bool LoadTexture( Rocket::Core::URL &source_url, int index, const char *property_name, Rocket::Core::Geometry &geometry, Rocket::Core::Vector2f &size )
-	{
-		Rocket::Core::RenderInterface *render_interface = GetRenderInterface();
-		Rocket::Core::StringList words;
-		Rocket::Core::String source = GetProperty< Rocket::Core::String >( property_name );
-		Rocket::Core::StringUtilities::ExpandString( words, source, ' ' );
-		bool it_uses_tex_coords = false;
-		Rocket::Core::Texture &texture = textures[index];
-
-		if ( words.size() == 5 )
-		{
-			it_uses_tex_coords = true;
-			source = words[0];
-			Rocket::Core::TypeConverter< Rocket::Core::String, float >::Convert( words[1], texcoords[index][0].x );
-			Rocket::Core::TypeConverter< Rocket::Core::String, float >::Convert( words[2], texcoords[index][0].y );
-			Rocket::Core::TypeConverter< Rocket::Core::String, float >::Convert( words[3], texcoords[index][1].x );
-			Rocket::Core::TypeConverter< Rocket::Core::String, float >::Convert( words[4], texcoords[index][1].y );
-		}
-
-		if ( !texture.Load( source, source_url.GetPath() ) )
-		{
-			geometry.SetTexture( NULL );
-			return false;
-		}
-
-		if ( it_uses_tex_coords )
-		{
-			Rocket::Core::Vector2i texture_dimensions = texture.GetDimensions( render_interface );
-
-			size.x = texcoords[index][1].x - texcoords[index][0].x;
-			size.y = texcoords[index][1].y - texcoords[index][0].y;
-
-			for ( int i = 0; i < 2; i++ )
-			{
-				texcoords[index][i].x /= texture_dimensions.x;
-				texcoords[index][i].y /= texture_dimensions.y;
-			}
-		}
-		else
-		{
-			texcoords[index][0].x = 0.0f;
-			texcoords[index][0].y = 0.0f;
-			texcoords[index][1].x = 1.0f;
-			texcoords[index][1].y = 1.0f;
-			size.x = float( texture.GetDimensions( render_interface ).x );
-			size.y = float( texture.GetDimensions( render_interface ).y );
-		}
-
-		geometry.SetTexture( &texture );
-		return true;
-	}
-
-	void LoadTexture( int in )
-	{
-		Rocket::Core::ElementDocument *document = GetOwnerDocument();
-		Rocket::Core::URL source_url( document == NULL ? "" : document->GetSourceURL() );
-		Rocket::Core::String property_name;
-
-		switch ( in )
-		{
-			case START:
-				property_name = "start-image";
-				break;
-
-			case PROGRESS:
-				property_name = "progress-image";
-				break;
-
-			case DECORATION:
-				property_name = "decoration-image";
-				break;
-
-			case END:
-				property_name = "end-image";
-				break;
-		}
-
-		if ( LoadTexture( source_url, in, property_name.CString(), geometry[ in ], size[ in ] ) )
-		{
-			renderFilter |= 1 << in;
-			loadFilter |= 1 << in;
-		}
-		else
-		{
-			renderFilter &= ~( 1 << in );
-			loadFilter &= ~( 1 << in );
-		}
-	}
-
-	void GenerateGeometry( void )
-	{
-		Rocket::Core::Vector2f geometryPosition[ NUM_GEOMETRIES ] = { Rocket::Core::Vector2f( 0, 0 ) };
-		Rocket::Core::Vector2f geometrySize[ NUM_GEOMETRIES ];
-
-		for ( int i = 0; i < NUM_GEOMETRIES; i++ )
-		{
-			geometrySize[ i ] = size[ i ];
-		}
-
-		Rocket::Core::Vector2f elementSize = GetBox().GetSize( Rocket::Core::Box::CONTENT );
-
-		Rocket::Core::Vector2f totalSize( 0, 0 );
-
-		float progress_size;
-
-		for ( int i = START; i < NUM_GEOMETRIES; ++i )
-		{
-			geometry[ i ].Release();
-		}
-
-		if ( value == 0 )
-		{
-			return;
-		}
-
-		switch ( orientation )
-		{
-			case LEFT:
-				progress_size = elementSize.x * value;
-				geometrySize[ PROGRESS ].x = ( elementSize.x - geometrySize[ START ].x - geometrySize[ END ].x ) * value;
-
-				for ( int i = START; i < NUM_GEOMETRIES; ++i )
-				{
-					if ( i == DECORATION || !( loadFilter & 1<<i ) )
-					{
-						continue;
-					}
-
-					if ( i == START )
-					{
-						geometryPosition[0].x = geometryPosition[0].y = 0;
-					}
-					else
-					{
-						geometryPosition[i].x = geometryPosition[i-1].x + geometrySize[i-1].x;
-					}
-
-					if ( progress_size > geometrySize[i].x )
-					{
-						progress_size -= geometrySize[i].x;
-						texcoords[i][0] = Rocket::Core::Vector2f( 0, 0 );
-						texcoords[i][1] = Rocket::Core::Vector2f( 1, 1 );
-
-						renderFilter |= 1 << i;
-					}
-					else
-					{
-						texcoords[i][0] = Rocket::Core::Vector2f( 0, 0 );
-						texcoords[i][1] = Rocket::Core::Vector2f( progress_size / geometrySize[i].x, 1 );
-
-						renderFilter |= 1 << i;
-
-						for ( int j = i + 1; j <= END ; ++j )
-						{
-							renderFilter &= ~( 1 << j );
-						}
-
-						break;
-					}
-				}
-
-				break;
-
-			case RIGHT:
-				progress_size = elementSize.x * value;
-				geometrySize[ PROGRESS ].x = ( elementSize.x - geometrySize[ START ].x - geometrySize[ END ].x ) * value;
-
-				for ( int i = START; i < NUM_GEOMETRIES; ++i )
-				{
-					if ( i == DECORATION || !( loadFilter & 1<<i ) )
-					{
-						continue;
-					}
-
-					if ( i == START )
-					{
-						geometryPosition[0].x = elementSize.x + geometrySize[i].x;
-						geometryPosition[0].y = 0;
-					}
-					else
-					{
-						geometryPosition[i].x = geometryPosition[i-1].x - geometrySize[i].x;
-					}
-
-					if ( progress_size > geometrySize[i].x )
-					{
-						progress_size -= geometrySize[i].x;
-						texcoords[i][0] = Rocket::Core::Vector2f( 0, 0 );
-						texcoords[i][1] = Rocket::Core::Vector2f( 1, 1 );
-
-						renderFilter |= 1 << i;
-					}
-					else
-					{
-						texcoords[i][0] = Rocket::Core::Vector2f( 1 - ( progress_size / geometrySize[i].x ), 0 );
-						texcoords[i][1] = Rocket::Core::Vector2f( 1, 1 );
-
-						renderFilter |= 1 << i;
-
-						for ( int j = i + 1; j <= END ; ++j )
-						{
-							renderFilter &= ~( 1 << j );
-						}
-
-						break;
-					}
-				}
-				break;
-
-			case UP:
-				break;
-
-			case DOWN:
-				break;
-		}
-
-		for ( int i = START; i < NUM_GEOMETRIES; ++i )
-		{
-			if ( renderFilter & ( 1 << i ) )
-			{
-				Rocket::Core::Container::vector< Rocket::Core::Vertex >::Type &verticies = geometry[ i ].GetVertices();
-				Rocket::Core::Container::vector< int >::Type &indicies = geometry[ i ].GetIndices();
-
-				verticies.resize( 4 );
-				indicies.resize( 6 );
-
-
-				Rocket::Core::GeometryUtilities::GenerateQuad( &verticies[0],
-											 &indicies[0],
-											 geometryPosition[i],
-											 geometrySize[i],
-											 color,
-											 texcoords[i][0],
-											 texcoords[i][1]
-										   );
-			}
-		}
-
-		dirty_geometry = false;
-	}
-
 	bool dirty_geometry; // Rebuild geometry
 	progressBarOrientation_t orientation; // Direction progressbar grows
 	float value; // current value
-	int renderFilter; // Filter components to draw
-	int loadFilter; // Images loaded
-
-	Rocket::Core::Colourb color;
-
-	Rocket::Core::Geometry geometry[ NUM_GEOMETRIES ]; // 1: start image, 2: straight section, 3: straight section decoration, 4: end image
-
-	Rocket::Core::Vector2f texcoords[ NUM_GEOMETRIES ][ 2 ]; // texture coords
-	Rocket::Core::Vector2f size[ NUM_GEOMETRIES ]; // size of images
-	Rocket::Core::Vector2f dimensions; // dimensions of this element
-
-	Rocket::Core::Texture textures[ NUM_GEOMETRIES ];
-
-
-
-
-
+	qhandle_t shader;
+	vec4_t color;
+	Rocket::Core::Vector2f dimensions;
 };
 
 #endif
