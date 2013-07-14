279987d91efdf361a4e3d86accbca1cb8f12b29b
Viech <viech@example.com>
2013-07-14 02:24:23 +0200

    Decrease dretch pounce range 400 → 300, increase cooldown 1600 → 2000.

--- src/gamelogic/game/unvanquished.h
+++ src/gamelogic/game/unvanquished.h
@@ -49,9 +49,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define LEVEL0_BITE_RANGE             64.0f
 #define LEVEL0_BITE_WIDTH             6.0f
 #define LEVEL0_BITE_REPEAT            500
-#define LEVEL0_POUNCE_DISTANCE        400 // pitch between LEVEL0_POUNCE_MINPITCH and pi/4 result in this distance
+#define LEVEL0_POUNCE_DISTANCE        300 // pitch between LEVEL0_POUNCE_MINPITCH and pi/4 results in this distance
 #define LEVEL0_POUNCE_MINPITCH        M_PI / 12.0f // 15°, minimum pitch that will result in full pounce distance
-#define LEVEL0_POUNCE_COOLDOWN        1600
+#define LEVEL0_POUNCE_COOLDOWN        2000
 #define LEVEL0_WALLPOUNCE_MAGNITUDE   600
 #define LEVEL0_WALLPOUNCE_COOLDOWN    750
 #define LEVEL0_SIDEPOUNCE_MAGNITUDE   400
