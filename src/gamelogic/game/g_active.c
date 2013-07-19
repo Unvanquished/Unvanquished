ab6023d3dba067ca2777fc5c6e744e8059a49464
Darren Salt <devspam@moreofthesa.me.uk>
2013-07-20 00:58:49 +0100

    If no auth challenge response is received from a client, retry after a short time.
    We've seen quite a few instances of a challenge being sent by the server but
    no response being received. Since this is sent via UDP and therefore not
    guaranteed to be delivered, assume that either the challenge or the response
    got lost and retry.

--- src/gamelogic/game/g_active.c
+++ src/gamelogic/game/g_active.c
@@ -1584,6 +1584,9 @@ void ClientThink_real( gentity_t *ent )
 		//  return;
 	}
 
+	// client is admin but program hasn't responded to challenge? Resend
+	ClientAdminChallenge( ent - g_entities );
+
 	//
 	// check for exiting intermission
 	//
