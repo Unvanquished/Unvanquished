Unvanquished/Unvanquished

commit 6dd3d81ea9b43ab17fb91b653011c2c67e2b9e74

Build oldSkeleton before resetting frames

src/gamelogic/cgame/cg_buildable.c
@@ -705,6 +705,23 @@ static void CG_SetBuildableLerpFrameAnimation( buildable_t buildable, lerpFrame_
 		CG_Error( "Bad animation number: %i", newAnimation );
 	}
 
+	if ( cg_buildables[ buildable ].md5 )
+	{
+		if ( bSkeleton.type != SK_INVALID )
+		{
+			oldbSkeleton = bSkeleton;
+
+			if ( lf->old_animation != NULL )
+			{
+				if ( !trap_R_BuildSkeleton( &oldbSkeleton, lf->old_animation->handle, lf->oldFrame, lf->frame, lf->blendlerp, lf->old_animation->clearOrigin ) )
+				{
+					CG_Printf( "Can't build old buildable bSkeleton\n" );
+					return;
+				}
+			}
+		}
+	}
+
 	anim = &cg_buildables[ buildable ].animations[ newAnimation ];
 
 	//this item has just spawned so lf->frameTime will be zero
@@ -737,23 +754,6 @@ static void CG_SetBuildableLerpFrameAnimation( buildable_t buildable, lerpFrame_
 	else
 	{
 		lf->blendlerp = 1.0f - lf->blendlerp; //use old blending for smooth blending between two blended animations
-	}
-
-	if ( cg_buildables[ buildable ].md5 )
-	{
-		if ( bSkeleton.type != SK_INVALID )
-		{
-			oldbSkeleton = bSkeleton;
-
-			if ( lf->old_animation != NULL )
-			{
-				if ( !trap_R_BuildSkeleton( &oldbSkeleton, lf->old_animation->handle, lf->oldFrame, lf->frame, lf->blendlerp, lf->old_animation->clearOrigin ) )
-				{
-					CG_Printf( "Can't build old buildable bSkeleton\n" );
-					return;
-				}
-			}
-		}
 	}
 }
 

src/gamelogic/cgame/cg_players.c
@@ -1664,15 +1664,6 @@ static void CG_SetLerpFrameAnimation( clientInfo_t *ci, lerpFrame_t *lf, int new
 	anim = &ci->animations[ newAnimation ];
 
 	lf->animation = anim;
-	lf->animationTime = cg.time + anim->initialLerp;
-
-	lf->oldFrame = lf->frame = 0;
-	lf->oldFrameTime = lf->frameTime = 0;
-
-	if ( cg_debugAnim.integer )
-	{
-		CG_Printf( "Anim: %i\n", newAnimation );
-	}
 
 	if ( ci->md5 )
 	{
@@ -1717,6 +1708,17 @@ static void CG_SetLerpFrameAnimation( clientInfo_t *ci, lerpFrame_t *lf, int new
 			}
 		}
 	}
+
+	lf->animationTime = cg.time + anim->initialLerp;
+
+	lf->oldFrame = lf->frame = 0;
+	lf->oldFrameTime = lf->frameTime = 0;
+
+	if ( cg_debugAnim.integer )
+	{
+		CG_Printf( "Anim: %i\n", newAnimation );
+	}
+
 }
 
 /*

commit d333fab3c69229312c592810bdfd89bc837e5c7d

Fix MD3 animations (broken in 6dd3d81e/b202fa0d).

src/gamelogic/cgame/cg_buildable.c
@@ -731,10 +731,18 @@ static void CG_SetBuildableLerpFrameAnimation( buildable_t buildable, lerpFrame_
 	}
 
 	lf->animation = anim;
-	lf->animationTime = cg.time + anim->initialLerp;
 
-	lf->oldFrame = lf->frame = 0;
-	lf->oldFrameTime = lf->frameTime = 0;
+	if ( cg_buildables[ buildable ].md5 )
+	{
+		lf->animationTime = cg.time + anim->initialLerp;
+
+		lf->oldFrame = lf->frame = 0;
+		lf->oldFrameTime = lf->frameTime = 0;
+	}
+	else
+	{
+		lf->animationTime = lf->frameTime + anim->initialLerp;
+	}
 
 	if ( cg_debugAnim.integer )
 	{

src/gamelogic/cgame/cg_players.c
@@ -1707,12 +1707,16 @@ static void CG_SetLerpFrameAnimation( clientInfo_t *ci, lerpFrame_t *lf, int new
 				return;
 			}
 		}
+
+		lf->animationTime = cg.time + anim->initialLerp;
+
+		lf->oldFrame = lf->frame = 0;
+		lf->oldFrameTime = lf->frameTime = 0;
 	}
-
-	lf->animationTime = cg.time + anim->initialLerp;
-
-	lf->oldFrame = lf->frame = 0;
-	lf->oldFrameTime = lf->frameTime = 0;
+	else
+	{
+		lf->animationTime = lf->frameTime + anim->initialLerp;
+	}
 
 	if ( cg_debugAnim.integer )
 	{