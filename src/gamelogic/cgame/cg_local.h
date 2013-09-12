Unvanquished/Unvanquished

commit 46d1dd843dd1a5733676574f37c3d38613c5c8c9

Introduce g_unlockableMinTime to control lock thresholds.

src/gamelogic/cgame/cg_local.h
@@ -1388,6 +1388,8 @@ typedef struct
 	qboolean markDeconstruct; // Whether or not buildables are marked
 	int      powerReactorRange;
 	int      powerRepeaterRange;
+	int      confidenceHalfLife; // used for confidence bar (un)lock markers
+	int      unlockableMinTime;  // used for confidence bar (un)lock markers
 
 	int      voteTime[ NUM_TEAMS ];
 	int      voteYes[ NUM_TEAMS ];

src/gamelogic/cgame/cg_servercmds.c
@@ -132,11 +132,15 @@ void CG_ParseServerinfo( void )
 	const char *info;
 
 	info = CG_ConfigString( CS_SERVERINFO );
-	cgs.timelimit = atoi( Info_ValueForKey( info, "timelimit" ) );
-	cgs.maxclients = atoi( Info_ValueForKey( info, "sv_maxclients" ) );
-	cgs.markDeconstruct = atoi( Info_ValueForKey( info, "g_markDeconstruct" ) );
-	cgs.powerReactorRange = atoi( Info_ValueForKey( info, "g_powerReactorRange" ) );
+
+	cgs.timelimit          = atoi( Info_ValueForKey( info, "timelimit" ) );
+	cgs.maxclients         = atoi( Info_ValueForKey( info, "sv_maxclients" ) );
+	cgs.markDeconstruct    = atoi( Info_ValueForKey( info, "g_markDeconstruct" ) );
+	cgs.powerReactorRange  = atoi( Info_ValueForKey( info, "g_powerReactorRange" ) );
 	cgs.powerRepeaterRange = atoi( Info_ValueForKey( info, "g_powerRepeaterRange" ) );
+	cgs.confidenceHalfLife = atoi( Info_ValueForKey( info, "g_confidenceHalfLife" ) );
+	cgs.unlockableMinTime  = atoi( Info_ValueForKey( info, "g_unlockableMinTime" ) );
+
 	Q_strncpyz( cgs.mapname, Info_ValueForKey( info, "mapname" ), sizeof(cgs.mapname) );
 }
 

src/gamelogic/game/bg_teamprogress.c
@@ -29,7 +29,7 @@ along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #ifdef CGAME
-#include "../../engine/client/cg_api.h"
+#include "../cgame/cg_local.h"
 #endif
 
 #ifdef UI
@@ -39,9 +39,6 @@ along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 // -----------
 // definitions
 // -----------
-
-// TODO: Make LOCK_RATIO a (synchronized) cvar
-#define LOCK_RATIO      0.8f
 
 #define NUM_UNLOCKABLES WP_NUM_WEAPONS + UP_NUM_UPGRADES + BA_NUM_BUILDABLES + PCL_NUM_CLASSES
 
@@ -175,6 +172,53 @@ static INLINE void CheckStatusKnowledge( unlockableType_t type, int itemNum )
 	}
 }
 
+static float UnlockToLockThreshold( float unlockThreshold )
+{
+	float confidenceHalfLife = 0.0f;
+	float unlockableMinTime  = 0.0f;
+
+	// maintain cache
+	static float lastConfidenceHalfLife = 0.0f;
+	static float lastunlockableMinTime  = 0.0f;
+	static float lastMod                = 0.0f;
+
+	// retrieve relevant settings
+#ifdef GAME
+	confidenceHalfLife = g_confidenceHalfLife.value;
+	unlockableMinTime  = g_unlockableMinTime.value;
+#endif
+#ifdef CGAME
+	confidenceHalfLife = cgs.confidenceHalfLife;
+	unlockableMinTime  = cgs.unlockableMinTime;
+#endif
+#ifdef UI
+	// NOT IMPLEMENTED
+	return unlockThreshold;
+#endif
+
+	// a half life time of 0 means there is no decrease, so we don't need to alter thresholds
+	if ( confidenceHalfLife <= 0.0f )
+	{
+		return unlockThreshold;
+	}
+
+	// do cache lookup
+	if ( lastConfidenceHalfLife == confidenceHalfLife &&
+	     lastunlockableMinTime  == unlockableMinTime  &&
+	     lastMod > 0.0f )
+	{
+		return lastMod * unlockThreshold;
+	}
+
+	lastConfidenceHalfLife = confidenceHalfLife;
+	lastunlockableMinTime  = unlockableMinTime;
+
+	// ln(2) ~= 0.6931472
+	lastMod = exp( -0.6931472f * ( unlockableMinTime / ( confidenceHalfLife * 60.0f ) ) );
+
+	return lastMod * unlockThreshold;
+}
+
 // ----------
 // BG methods
 // ----------
@@ -267,7 +311,7 @@ void BG_ImportUnlockablesFromMask( team_t team, int mask )
 		unlockable->num             = itemNum;
 		unlockable->team            = currentTeam;
 		unlockable->unlockThreshold = unlockThreshold;
-		unlockable->lockThreshold   = unlockThreshold * LOCK_RATIO;
+		unlockable->lockThreshold   = UnlockToLockThreshold( unlockThreshold );
 
 		// retrieve the item's locking state
 		if ( !unlockThreshold )
@@ -493,7 +537,7 @@ void G_UpdateUnlockables( void )
 		unlockable->team            = team;
 		unlockable->statusKnown     = qtrue;
 		unlockable->unlockThreshold = unlockThreshold;
-		unlockable->lockThreshold   = unlockThreshold * LOCK_RATIO;
+		unlockable->lockThreshold   = UnlockToLockThreshold( unlockThreshold );
 
 		// calculate the item's locking state
 		unlockable->unlocked = (

src/gamelogic/game/g_local.h
@@ -1271,7 +1271,6 @@ extern  vmCvar_t g_knockback;
 extern  vmCvar_t g_inactivity;
 extern  vmCvar_t g_debugMove;
 extern  vmCvar_t g_debugDamage;
-extern  vmCvar_t g_debugConfidence;
 extern  vmCvar_t g_synchronousClients;
 extern  vmCvar_t g_motd;
 extern  vmCvar_t g_warmup;
@@ -1302,7 +1301,9 @@ extern  vmCvar_t g_initialBuildPoints;
 extern  vmCvar_t g_mineRateHalfLife;
 extern  vmCvar_t g_minimumMineRate;
 
+extern  vmCvar_t g_debugConfidence;
 extern  vmCvar_t g_confidenceHalfLife;
+extern  vmCvar_t g_unlockableMinTime;
 
 extern  vmCvar_t g_humanAllowBuilding;
 extern  vmCvar_t g_alienAllowBuilding;

src/gamelogic/game/g_main.c
@@ -62,7 +62,6 @@ vmCvar_t           g_knockback;
 vmCvar_t           g_inactivity;
 vmCvar_t           g_debugMove;
 vmCvar_t           g_debugDamage;
-vmCvar_t           g_debugConfidence;
 vmCvar_t           g_motd;
 vmCvar_t           g_synchronousClients;
 vmCvar_t           g_warmup;
@@ -101,7 +100,9 @@ vmCvar_t           g_initialMineRate;
 vmCvar_t           g_mineRateHalfLife;
 vmCvar_t           g_minimumMineRate;
 
+vmCvar_t           g_debugConfidence;
 vmCvar_t           g_confidenceHalfLife;
+vmCvar_t           g_unlockableMinTime;
 
 vmCvar_t           g_humanAllowBuilding;
 vmCvar_t           g_alienAllowBuilding;
@@ -237,7 +238,6 @@ static cvarTable_t gameCvarTable[] =
 	{ &g_inactivity,                  "g_inactivity",                  "0",                                0,                                               0, qtrue            },
 	{ &g_debugMove,                   "g_debugMove",                   "0",                                0,                                               0, qfalse           },
 	{ &g_debugDamage,                 "g_debugDamage",                 "0",                                0,                                               0, qfalse           },
-	{ &g_debugConfidence,             "g_debugConfidence",             "0",                                0,                                               0, qfalse           },
 	{ &g_motd,                        "g_motd",                        "",                                 0,                                               0, qfalse           },
 
 	{ &g_allowVote,                   "g_allowVote",                   "1",                                CVAR_ARCHIVE,                                    0, qfalse           },
@@ -268,7 +268,9 @@ static cvarTable_t gameCvarTable[] =
 	{ &g_mineRateHalfLife,            "g_mineRateHalfLife",            DEFAULT_MINE_RATE_HALF_LIFE,        CVAR_ARCHIVE,                                    0, qfalse           },
 	{ &g_minimumMineRate,             "g_minimumMineRate",             DEFAULT_MINIMUM_MINE_RATE,          CVAR_ARCHIVE,                                    0, qfalse           },
 
-	{ &g_confidenceHalfLife,          "g_confidenceHalfLife",          DEFAULT_CONFIDENCE_HALF_LIFE,       CVAR_ARCHIVE,                                    0, qfalse           },
+	{ &g_debugConfidence,             "g_debugConfidence",             "0",                                0,                                               0, qfalse           },
+	{ &g_confidenceHalfLife,          "g_confidenceHalfLife",          DEFAULT_CONFIDENCE_HALF_LIFE,       CVAR_SERVERINFO | CVAR_ARCHIVE,                  0, qfalse           },
+	{ &g_unlockableMinTime,           "g_unlockableMinTime",           DEFAULT_UNLOCKABLE_MIN_TIME,        CVAR_SERVERINFO | CVAR_ARCHIVE,                  0, qfalse           },
 
 	{ &g_humanAllowBuilding,          "g_humanAllowBuilding",          "1",                                0,                                               0, qfalse           },
 	{ &g_alienAllowBuilding,          "g_alienAllowBuilding",          "1",                                0,                                               0, qfalse           },

src/gamelogic/game/unvanquished.h
@@ -493,6 +493,7 @@ extern int   REACTOR_ATTACK_DCC_DAMAGE;
 
 // confidence
 #define DEFAULT_CONFIDENCE_HALF_LIFE       "5"   // in min
+#define DEFAULT_UNLOCKABLE_MIN_TIME        "60"  // in s
 #define CONFIDENCE_PER_CREDIT              0.01f // used to award confidence based on credit rewards
 #define CONFIDENCE_BUILDING_BASEMOD        0.6f  // building award in relation to destruction
 

commit 6d99e19d16663be4a2b9e33c0c9729303bd541cc

Fix a crash introduced in 46d1dd8.

src/gamelogic/cgame/cg_local.h
@@ -1388,8 +1388,8 @@ typedef struct
 	qboolean markDeconstruct; // Whether or not buildables are marked
 	int      powerReactorRange;
 	int      powerRepeaterRange;
-	int      confidenceHalfLife; // used for confidence bar (un)lock markers
-	int      unlockableMinTime;  // used for confidence bar (un)lock markers
+	float    confidenceHalfLife; // used for confidence bar (un)lock markers
+	float    unlockableMinTime;  // used for confidence bar (un)lock markers
 
 	int      voteTime[ NUM_TEAMS ];
 	int      voteYes[ NUM_TEAMS ];

src/gamelogic/cgame/cg_main.c
@@ -246,6 +246,8 @@ vmCvar_t        ui_voteActive;
 vmCvar_t        ui_alienTeamVoteActive;
 vmCvar_t        ui_humanTeamVoteActive;
 vmCvar_t        ui_unlockables;
+vmCvar_t        ui_confidenceHalfLife;
+vmCvar_t        ui_unlockablesMinTime;
 
 vmCvar_t        cg_debugRandom;
 
@@ -408,7 +410,9 @@ static const cvarTable_t cvarTable[] =
 	{ &ui_voteActive,                  "ui_voteActive",                  "0",            CVAR_ROM                     },
 	{ &ui_humanTeamVoteActive,         "ui_humanTeamVoteActive",         "0",            CVAR_ROM                     },
 	{ &ui_alienTeamVoteActive,         "ui_alienTeamVoteActive",         "0",            CVAR_ROM                     },
-    { &ui_unlockables,                 "ui_unlockables",                 "0 0",          CVAR_ROM                     },
+	{ &ui_unlockables,                 "ui_unlockables",                 "0 0",          CVAR_ROM                     },
+	{ &ui_confidenceHalfLife,          "ui_confidenceHalfLife",          "0",            CVAR_ROM                     },
+	{ &ui_unlockablesMinTime,          "ui_unlockablesMinTime",          "0",            CVAR_ROM                     },
 
 	{ &cg_debugRandom,                 "cg_debugRandom",                 "0",            0                            },
 

src/gamelogic/cgame/cg_servercmds.c
@@ -138,10 +138,14 @@ void CG_ParseServerinfo( void )
 	cgs.markDeconstruct    = atoi( Info_ValueForKey( info, "g_markDeconstruct" ) );
 	cgs.powerReactorRange  = atoi( Info_ValueForKey( info, "g_powerReactorRange" ) );
 	cgs.powerRepeaterRange = atoi( Info_ValueForKey( info, "g_powerRepeaterRange" ) );
-	cgs.confidenceHalfLife = atoi( Info_ValueForKey( info, "g_confidenceHalfLife" ) );
-	cgs.unlockableMinTime  = atoi( Info_ValueForKey( info, "g_unlockableMinTime" ) );
+	cgs.confidenceHalfLife = atof( Info_ValueForKey( info, "g_confidenceHalfLife" ) );
+	cgs.unlockableMinTime  = atof( Info_ValueForKey( info, "g_unlockableMinTime" ) );
 
 	Q_strncpyz( cgs.mapname, Info_ValueForKey( info, "mapname" ), sizeof(cgs.mapname) );
+
+	// pass some of these to UI
+	trap_Cvar_Set( "ui_confidenceHalfLife", va( "%f", cgs.confidenceHalfLife ) );
+	trap_Cvar_Set( "ui_unlockableMinTime",  va( "%f", cgs.unlockableMinTime ) );
 }
 
 /*

src/gamelogic/game/bg_teamprogress.c
@@ -176,6 +176,7 @@ static float UnlockToLockThreshold( float unlockThreshold )
 {
 	float confidenceHalfLife = 0.0f;
 	float unlockableMinTime  = 0.0f;
+	char  buffer[ MAX_TOKEN_CHARS ];
 
 	// maintain cache
 	static float lastConfidenceHalfLife = 0.0f;
@@ -192,9 +193,10 @@ static float UnlockToLockThreshold( float unlockThreshold )
 	unlockableMinTime  = cgs.unlockableMinTime;
 #endif
 #ifdef UI
-	// NOT IMPLEMENTED
-	Com_Error( ERR_FATAL, "UnlockToLockThreshold: Called from within UI subsystem, "
-	                      "where access to necessary data is not implemented" );
+	trap_Cvar_VariableStringBuffer( "ui_confidenceHalfLife", buffer, sizeof( buffer ) );
+	sscanf( buffer, "%f", &confidenceHalfLife );
+	trap_Cvar_VariableStringBuffer( "ui_unlockableMinTime",  buffer, sizeof( buffer ) );
+	sscanf( buffer, "%f", &unlockableMinTime );
 #endif
 
 	// a half life time of 0 means there is no decrease, so we don't need to alter thresholds