e0ffb1435efdd357499fb1c82b1530fed86cfc2c
cmf028 <cmf1292@hotmail.com>
2013-09-16 22:53:07 -0500

    Revert "Increase the value of s_mixahead"
    This reverts commit d45f30476ae17bc07097d9c90e75687c35baff47.

--- src/engine/client/snd_dma.c
+++ src/engine/client/snd_dma.c
@@ -1832,7 +1832,7 @@ qboolean S_Base_Init( soundInterface_t *si )
 		return qfalse;
 	}
 
-	s_mixahead = Cvar_Get( "s_mixahead", "1.0", CVAR_ARCHIVE );
+	s_mixahead = Cvar_Get( "s_mixahead", "0.2", CVAR_ARCHIVE );
 	s_mixPreStep = Cvar_Get( "s_mixPreStep", "0.05", CVAR_ARCHIVE );
 	s_show = Cvar_Get( "s_show", "0", CVAR_CHEAT );
 	s_testsound = Cvar_Get( "s_testsound", "0", CVAR_CHEAT );
