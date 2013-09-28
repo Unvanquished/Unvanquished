Unvanquished/Unvanquished

commit 5aec25339c1d718ed7dd46cc559e0bce45b92814

Use constant sized arrays for weight info in md5Vertex_t
We will error on load if the number of weights exceed MAX_WEIGHTS.
This change should fix SSE skinning related crashes from unaligned accesses

src/engine/rendererGL/tr_surface.c
@@ -1310,7 +1310,6 @@ static void Tess_SurfaceMD5( md5Surface_t *srf )
 	md5Vertex_t     *v;
 	srfTriangle_t   *tri;
 	static ALIGNED( 16, boneMatrix_t boneMatrices[ MAX_BONES ] );
-	md5Weight_t *w;
 	boneMatrix_t tmpMat;
 
 	GLimp_LogComment( "--- Tess_SurfaceMD5 ---\n" );
@@ -1358,22 +1357,20 @@ static void Tess_SurfaceMD5( md5Surface_t *srf )
 		{
 #if id386_sse
 			__m128 a, b, c;
-			w = v->weights[ 0 ];
-			BoneMatrixMulSSE( &a, &b, &c, w->boneWeight, boneMatrices[ w->boneIndex ] );
+			BoneMatrixMulSSE( &a, &b, &c, v->boneWeights[ 0 ], boneMatrices[ v->boneIndexes[ 0 ] ] );
 
-			for ( k = 1, w = v->weights[ 1 ]; k < v->numWeights; k++, w++ )
+			for ( k = 1; k < v->numWeights; k++ )
 			{
-				BoneMatrixMadSSE( &a, &b, &c, w->boneWeight, boneMatrices[ w->boneIndex ] );
+				BoneMatrixMadSSE( &a, &b, &c, v->boneWeights[ k ], boneMatrices[ v->boneIndexes[ k ] ] );
 			}
 
 			BoneMatrixTransformPointSSE( a, b, c, v->position, tess.xyz[ tess.numVertexes + j ] );
 #else
-			w = v->weights[ 0 ];
-			BoneMatrixMul( tmpMat, w->boneWeight, boneMatrices[ w->boneIndex ] );
+			BoneMatrixMul( tmpMat, v->boneWeights[ 0 ], boneMatrices[ v->boneIndexes[ 0 ] ] );
 
-			for ( k = 1, w = v->weights[ 1 ]; k < v->numWeights; k++, w++ )
+			for ( k = 1; k < v->numWeights; k++ )
 			{
-				BoneMatrixMad( tmpMat, w->boneWeight, boneMatrices[ w->boneIndex ] );
+				BoneMatrixMad( tmpMat, v->boneWeights[ k ], boneMatrices[ v->boneIndexes[ k ] ] );
 			}
 
 			BoneMatrixTransformPoint( tmpMat, v->position, tess.xyz[ tess.numVertexes + j ] );
@@ -1414,12 +1411,11 @@ static void Tess_SurfaceMD5( md5Surface_t *srf )
 #if id386_sse
 			__m128 a, b, c;
 
-			w = v->weights[ 0 ];
-			BoneMatrixMulSSE( &a, &b, &c, w->boneWeight, boneMatrices[ w->boneIndex ] );
+			BoneMatrixMulSSE( &a, &b, &c, v->boneWeights[ 0 ], boneMatrices[ v->boneIndexes[ 0 ] ] );
 
-			for ( k = 1, w = v->weights[ 1 ]; k < v->numWeights; k++, w++ )
+			for ( k = 1; k < v->numWeights; k++ )
 			{
-				BoneMatrixMadSSE( &a, &b, &c, w->boneWeight, boneMatrices[ w->boneIndex ] );
+				BoneMatrixMadSSE( &a, &b, &c, v->boneWeights[ k ], boneMatrices[ v->boneIndexes[ k ] ] );
 			}
 
 			BoneMatrixTransformPointSSE( a, b, c, v->position, tess.xyz[ tess.numVertexes + j ] );
@@ -1428,12 +1424,11 @@ static void Tess_SurfaceMD5( md5Surface_t *srf )
 			BoneMatrixTransformNormalSSE( a, b, c, v->binormal, tess.binormals[ tess.numVertexes + j ] );
 			BoneMatrixTransformNormalSSE( a, b, c, v->tangent, tess.tangents[ tess.numVertexes + j ] );
 #else
-			w = v->weights[ 0 ];
-			BoneMatrixMul( tmpMat, w->boneWeight, boneMatrices[ w->boneIndex ] );
+			BoneMatrixMul( tmpMat, v->boneWeights[ k ], boneMatrices[ v->boneIndexes[ k ] ] );
 
-			for ( k = 1, w = v->weights[ 1 ]; k < v->numWeights; k++, w++ )
+			for ( k = 1; k < v->numWeights; k++ )
 			{
-				BoneMatrixMad( tmpMat, w->boneWeight, boneMatrices[ w->boneIndex ] );
+				BoneMatrixMad( tmpMat, v->boneWeights[ k ], boneMatrices[ v->boneIndexes[ k ] ] );
 			}
 
 			BoneMatrixTransformPoint( tmpMat, v->position, tess.xyz[ tess.numVertexes + j ] );

src/engine/renderer/tr_surface.c
@@ -1415,7 +1415,6 @@ static void RB_SurfaceMD5( md5Surface_t *srf )
 	md5Triangle_t   *tri;
 	static ALIGNED( 16, boneMatrix_t boneMatrices[ MAX_BONES ] );
 	boneMatrix_t    tmpMat;
-	md5Weight_t     *w;
 
 	GLimp_LogComment( "--- Tess_SurfaceMD5 ---\n" );
 
@@ -1460,23 +1459,21 @@ static void RB_SurfaceMD5( md5Surface_t *srf )
 #if id386_sse
 		__m128 a, b, c;
 
-		w = v->weights[ 0 ];
-		BoneMatrixMulSSE( &a, &b, &c, w->boneWeight, boneMatrices[ w->boneIndex ] );
+		BoneMatrixMulSSE( &a, &b, &c, v->boneWeights[ 0 ], boneMatrices[ v->boneIndexes[ 0 ] ] );
 
-		for ( k = 1, w = v->weights[ 1 ]; k < v->numWeights; k++, w++ )
+		for ( k = 1; k < v->numWeights; k++ )
 		{
-			BoneMatrixMadSSE( &a, &b, &c, w->boneWeight, boneMatrices[ w->boneIndex ] );
+			BoneMatrixMadSSE( &a, &b, &c, v->boneWeights[ k ], boneMatrices[ v->boneIndexes[ k ] ] );
 		}
 
 		BoneMatrixTransformPointSSE( a, b, c, v->position, tess.xyz[ tess.numVertexes + j ].v );
 		BoneMatrixTransformNormalNSSE( a, b, c, v->normal, tess.normal[ tess.numVertexes + j ].v );
 #else
-		w = v->weights[ 0 ];
-		BoneMatrixMul( tmpMat, w->boneWeight, boneMatrices[ w->boneIndex ] );
+		BoneMatrixMul( tmpMat, v->boneWeights[ 0 ], boneMatrices[ v->boneIndexes[ 0 ] ] );
 
-		for ( k = 1, w = v->weights[ 1 ]; k < v->numWeights; k++, w++ )
+		for ( k = 1; k < v->numWeights; k++ )
 		{
-			BoneMatrixMad( tmpMat, w->boneWeight, boneMatrices[ w->boneIndex ] );
+			BoneMatrixMad( tmpMat, v->boneWeights[ k ], boneMatrices[ v->boneIndexes[ k ] ] );
 		}
 
 		BoneMatrixTransformPoint( tmpMat, v->position, tess.xyz[ tess.numVertexes + j ].v );

src/engine/renderer/tr_local.h
@@ -1193,9 +1193,10 @@ typedef ALIGNED( 16, struct
 	vec2_t      texCoords;
 
 	uint32_t    firstWeight;
-	uint16_t    numWeights;
-	md5Weight_t **weights;
-} md5Vertex_t );
+	uint32_t    numWeights;
+	uint32_t    boneIndexes[ MAX_WEIGHTS ];
+	float       boneWeights[ MAX_WEIGHTS ];
+} ) md5Vertex_t;
 
 typedef struct
 {

src/engine/renderer/tr_model_md5.c
@@ -460,14 +460,25 @@ qboolean R_LoadMD5( model_t *mod, void *buffer, int bufferSize, const char *modN
 			return qfalse;
 		}
 
-		// loop trough all vertices and set up the vertex weights
+		// loop through all vertices and set up the vertex weights and base positions
 		for ( j = 0, v = surf->verts; j < surf->numVerts; j++, v++ )
 		{
-			v->weights = ri.Hunk_Alloc( sizeof( *v->weights ) * v->numWeights, h_low );
+			md5Weight_t *w = surf->weights + v->firstWeight;
+			Vector4Set( v->position, 0, 0, 0, 1 );
 
-			for ( k = 0; k < v->numWeights; k++ )
+			for ( k = 0; k < v->numWeights; k++, w++ )
 			{
-				v->weights[ k ] = surf->weights + ( v->firstWeight + k );
+				vec3_t offsetVec;
+
+				v->boneIndexes[ k ] = w->boneIndex;
+				v->boneWeights[ k ] = w->boneWeight;
+
+				bone = &md5->bones[ w->boneIndex ];
+
+				QuatTransformVector( bone->rotation, w->offset, offsetVec );
+				VectorAdd( bone->origin, offsetVec, offsetVec );
+
+				VectorMA( v->position, w->boneWeight, offsetVec, v->position );
 			}
 		}
 	}
@@ -479,26 +490,7 @@ qboolean R_LoadMD5( model_t *mod, void *buffer, int bufferSize, const char *modN
 	{
 		for ( j = 0, v = surf->verts; j < surf->numVerts; j++, v++ )
 		{
-			vec3_t      tmpVert;
-			md5Weight_t *w;
-
-			VectorClear( tmpVert );
-
-			for ( k = 0, w = v->weights[ 0 ]; k < v->numWeights; k++, w++ )
-			{
-				vec3_t offsetVec;
-
-				bone = &md5->bones[ w->boneIndex ];
-
-				QuatTransformVector( bone->rotation, w->offset, offsetVec );
-				VectorAdd( bone->origin, offsetVec, offsetVec );
-
-				VectorMA( tmpVert, w->boneWeight, offsetVec, tmpVert );
-			}
-
-			VectorCopy( tmpVert, v->position );
-			v->position[ 3 ] = 1;
-			AddPointToBounds( tmpVert, md5->bounds[ 0 ], md5->bounds[ 1 ] );
+			AddPointToBounds( v->position, md5->bounds[ 0 ], md5->bounds[ 1 ] );
 		}
 
 		// calc normals

src/engine/rendererGL/tr_local.h
@@ -2236,10 +2236,11 @@ extern "C" {
 		vec4_t      normal;
 		vec2_t      texCoords;
 
-		uint16_t    firstWeight;
-		uint16_t    numWeights;
-		md5Weight_t **weights;
-	} md5Vertex_t );
+		uint32_t    firstWeight;
+		uint32_t    numWeights;
+		uint32_t    boneIndexes[ MAX_WEIGHTS ];
+		float       boneWeights[ MAX_WEIGHTS ];
+	} ) md5Vertex_t;
 
 	/*
 	typedef struct

src/engine/rendererGL/tr_model_md5.c
@@ -467,14 +467,25 @@ qboolean R_LoadMD5( model_t *mod, void *buffer, int bufferSize, const char *modN
 			return qfalse;
 		}
 
-		// loop trough all vertices and set up the vertex weights
+		// loop through all vertices and set up the vertex weights and base positions
 		for ( j = 0, v = surf->verts; j < surf->numVerts; j++, v++ )
 		{
-			v->weights = ri.Hunk_Alloc( sizeof( *v->weights ) * v->numWeights, h_low );
+			md5Weight_t *w = surf->weights + v->firstWeight;
+			Vector4Set( v->position, 0, 0, 0, 1 );
 
-			for ( k = 0; k < v->numWeights; k++ )
+			for ( k = 0; k < v->numWeights; k++, w++ )
 			{
-				v->weights[ k ] = surf->weights + ( v->firstWeight + k );
+				vec3_t offsetVec;
+
+				v->boneIndexes[ k ] = w->boneIndex;
+				v->boneWeights[ k ] = w->boneWeight;
+
+				bone = &md5->bones[ w->boneIndex ];
+
+				QuatTransformVector( bone->rotation, w->offset, offsetVec );
+				VectorAdd( bone->origin, offsetVec, offsetVec );
+
+				VectorMA( v->position, w->boneWeight, offsetVec, v->position );
 			}
 		}
 	}
@@ -486,26 +497,7 @@ qboolean R_LoadMD5( model_t *mod, void *buffer, int bufferSize, const char *modN
 	{
 		for ( j = 0, v = surf->verts; j < surf->numVerts; j++, v++ )
 		{
-			vec3_t      tmpVert;
-			md5Weight_t *w;
-
-			VectorClear( tmpVert );
-
-			for ( k = 0, w = v->weights[ 0 ]; k < v->numWeights; k++, w++ )
-			{
-				vec3_t offsetVec;
-
-				bone = &md5->bones[ w->boneIndex ];
-
-				QuatTransformVector( bone->rotation, w->offset, offsetVec );
-				VectorAdd( bone->origin, offsetVec, offsetVec );
-
-				VectorMA( tmpVert, w->boneWeight, offsetVec, tmpVert );
-			}
-
-			VectorCopy( tmpVert, v->position );
-			v->position[ 3 ] = 1;
-			AddPointToBounds( tmpVert, md5->bounds[ 0 ], md5->bounds[ 1 ] );
+			AddPointToBounds( v->position, md5->bounds[ 0 ], md5->bounds[ 1 ] );
 		}
 
 		// calc tangent spaces

src/engine/rendererGL/tr_model_psk.c
@@ -129,7 +129,6 @@ qboolean R_LoadPSK( model_t *mod, void *buffer, int bufferSize, const char *modN
 
 	md5Model_t        *md5;
 	md5Bone_t         *md5Bone;
-	md5Weight_t       *weight;
 
 	vec3_t            boneOrigin;
 	quat_t            boneQuat;
@@ -635,23 +634,13 @@ qboolean R_LoadPSK( model_t *mod, void *buffer, int bufferSize, const char *modN
 			//ri.Printf(PRINT_WARNING, "R_LoadPSK: vertex %i requires more weights %i than the maximum of %i in model '%s'\n", i, vboVert->numWeights, MAX_WEIGHTS, modName);
 		}
 
-		vboVert->weights = ri.Hunk_Alloc( sizeof( *vboVert->weights ) * vboVert->numWeights, h_low );
-
 		for ( j = 0, axWeight = axWeights, k = 0; j < numWeights; j++, axWeight++ )
 		{
 			if ( axWeight->pointIndex == vertex->pointIndex && axWeight->weight > 0.0f )
 			{
-				weight = ri.Hunk_Alloc( sizeof( *weight ), h_low );
-
-				weight->boneIndex = axWeight->boneIndex;
-				weight->boneWeight = axWeight->weight;
-
-				// FIXME?
-				weight->offset[ 0 ] = refBones[ axWeight->boneIndex ].bone.xSize;
-				weight->offset[ 1 ] = refBones[ axWeight->boneIndex ].bone.ySize;
-				weight->offset[ 2 ] = refBones[ axWeight->boneIndex ].bone.zSize;
-
-				vboVert->weights[ k++ ] = weight;
+				vboVert->boneIndexes[ k ] = axWeight->boneIndex;
+				vboVert->boneWeights[ k ] = axWeight->weight;
+				k++;
 			}
 		}
 

src/engine/rendererGL/tr_model_skel.c
@@ -126,7 +126,7 @@ qboolean AddTriangleToVBOTriangleList( growList_t *vboTriangles, skelTriangle_t 
 		{
 			if ( j < v->numWeights )
 			{
-				boneIndex = v->weights[ j ]->boneIndex;
+				boneIndex = v->boneIndexes[ j ];
 				hasWeights = qtrue;
 
 				// is the bone already referenced?
@@ -286,8 +286,8 @@ void AddSurfaceToVBOSurfacesList( growList_t *vboSurfaces, growList_t *vboTriang
 		{
 			if ( k < surf->verts[ j ].numWeights )
 			{
-				data.boneIndexes[ j ][ k ] = vboSurf->boneRemap[ surf->verts[ j ].weights[ k ]->boneIndex ];
-				data.boneWeights[ j ][ k ] = surf->verts[ j ].weights[ k ]->boneWeight;
+				data.boneIndexes[ j ][ k ] = vboSurf->boneRemap[ surf->verts[ j ].boneIndexes[ k ] ];
+				data.boneWeights[ j ][ k ] = surf->verts[ j ].boneWeights[ k ];
 			}
 			else
 			{
@@ -427,8 +427,8 @@ void AddSurfaceToVBOSurfacesList2( growList_t *vboSurfaces, growList_t *vboTrian
 		{
 			if ( k < v->numWeights )
 			{
-				data.boneIndexes[ j ][ k ] = vboSurf->boneRemap[ v->weights[ k ]->boneIndex ];
-				data.boneWeights[ j ][ k ] = v->weights[ k ]->boneWeight;
+				data.boneIndexes[ j ][ k ] = vboSurf->boneRemap[ v->boneIndexes[ k ] ];
+				data.boneWeights[ j ][ k ] = v->boneWeights[ k ];
 			}
 			else
 			{

commit 05fbb962bb9febe2590b81cc332b028ee4b2c673

Fix a segfault in bone matrix calculation.
This crash was accidentally introduced in commit 5aec2533.

src/engine/rendererGL/tr_surface.c
@@ -1424,7 +1424,7 @@ static void Tess_SurfaceMD5( md5Surface_t *srf )
 			BoneMatrixTransformNormalSSE( a, b, c, v->binormal, tess.binormals[ tess.numVertexes + j ] );
 			BoneMatrixTransformNormalSSE( a, b, c, v->tangent, tess.tangents[ tess.numVertexes + j ] );
 #else
-			BoneMatrixMul( tmpMat, v->boneWeights[ k ], boneMatrices[ v->boneIndexes[ k ] ] );
+			BoneMatrixMul( tmpMat, v->boneWeights[ 0 ], boneMatrices[ v->boneIndexes[ 0 ] ] );
 
 			for ( k = 1; k < v->numWeights; k++ )
 			{