Unvanquished/Unvanquished

commit 785e866f9f0e97016d18949d9509215cc322f906

Fix some compiler warnings in cgame and game

src/gamelogic/game/bg_teamprogress.c
@@ -176,7 +176,6 @@ static float UnlockToLockThreshold( float unlockThreshold )
 {
 	float confidenceHalfLife = 0.0f;
 	float unlockableMinTime  = 0.0f;
-	char  buffer[ MAX_TOKEN_CHARS ];
 
 	// maintain cache
 	static float lastConfidenceHalfLife = 0.0f;
@@ -251,7 +250,7 @@ void BG_ImportUnlockablesFromMask( team_t team, int mask )
 	unlockableType_t unlockableType = 0;
 	team_t           currentTeam;
 	qboolean         newStatus;
-	int              statusChanges[ NUM_UNLOCKABLES ], statusChangeCount = 0;
+	int              statusChanges[ NUM_UNLOCKABLES ];
 
 	// maintain a cache to prevent redundant imports
 	static int    lastMask = 0;

src/gamelogic/cgame/cg_weapons.c
@@ -63,7 +63,8 @@ void CG_RegisterUpgrade( int upgradeNum )
 
 	upgradeInfo->humanName = BG_Upgrade( upgradeNum )->humanName;
 
-	if ( icon = BG_Upgrade( upgradeNum )->icon )
+	icon = BG_Upgrade( upgradeNum )->icon;
+	if ( icon )
 	{
 		upgradeInfo->upgradeIcon = trap_R_RegisterShader( icon, RSF_DEFAULT );
 	}
@@ -81,7 +82,7 @@ static void CG_LoadCustomCrosshairs( void )
 {
 	char         *text_p, *token;
 	char         text[ 20000 ];
-	int          len, size;
+	int          len;
 	fileHandle_t f;
 	weapon_t     weapon;
 

src/gamelogic/game/g_buildable.c
@@ -3797,7 +3797,6 @@ G_Deconstruct
 */
 void G_Deconstruct( gentity_t *self, gentity_t *deconner, meansOfDeath_t deconType )
 {
-	float confidence;
 	int   refund;
 	const buildableAttributes_t *attr;
 
@@ -3969,11 +3968,9 @@ static qboolean PredictBuildablePower( buildable_t buildable, vec3_t origin )
 	gentity_t       *neighbor, *buddy;
 	float           distance, ownPrediction, neighborPrediction;
 
-	switch ( buildable )
+	if ( buildable == BA_H_REPEATER || buildable == BA_H_REACTOR )
 	{
-		case BA_H_REPEATER:
-		case BA_H_REACTOR:
-			return qtrue;
+		return qtrue;
 	}
 
 	ownPrediction = g_powerBaseSupply.integer - BG_Buildable( buildable )->powerConsumption;

src/gamelogic/game/g_client.c
@@ -1167,7 +1167,6 @@ char *ClientConnect( int clientNum, qboolean firstTime )
 	gentity_t       *ent;
 	char            reason[ MAX_STRING_CHARS ] = { "" };
 	int             i;
-	g_admin_admin_t *admin;
 	const char      *country;
 
 	ent = &g_entities[ clientNum ];
@@ -1318,13 +1317,10 @@ Doesn't do things not relevant to bots (which are local GUIDless clients).
 */
 char *ClientBotConnect( int clientNum, qboolean firstTime, team_t team )
 {
-	char            *value;
 	char            *userInfoError;
 	gclient_t       *client;
 	char            userinfo[ MAX_INFO_STRING ];
 	gentity_t       *ent;
-	char            reason[ MAX_STRING_CHARS ] = { "" };
-	int             i;
 
 	ent = &g_entities[ clientNum ];
 	client = &level.clients[ clientNum ];

src/gamelogic/game/g_weapon.c
@@ -674,7 +674,7 @@ static void FirebombMissileThink( gentity_t *self )
 
 	// ignite alien buildables in range
 	neighbor = NULL;
-	while ( neighbor = G_IterateEntitiesWithinRadius( neighbor, self->s.origin, FIREBOMB_IGNITE_RANGE ) )
+	while ( ( neighbor = G_IterateEntitiesWithinRadius( neighbor, self->s.origin, FIREBOMB_IGNITE_RANGE ) ) )
 	{
 		if ( neighbor->s.eType == ET_BUILDABLE && neighbor->buildableTeam == TEAM_ALIENS &&
 		     G_LineOfSight( self, neighbor ) )
@@ -1924,9 +1924,10 @@ void G_FireUpgrade( gentity_t *self, upgrade_t upgrade )
 		case UP_GRENADE:
 			FireGrenade( self );
 			break;
-
 		case UP_FIREBOMB:
 			FireFirebomb( self );
+			break;
+		default:
 			break;
 	}
 }

commit a556558a46893c6a480c820e46eb41b11bf7d4c6

Fix breakage caused by 785e866f.

src/gamelogic/game/bg_teamprogress.c
@@ -176,6 +176,9 @@ static float UnlockToLockThreshold( float unlockThreshold )
 {
 	float confidenceHalfLife = 0.0f;
 	float unlockableMinTime  = 0.0f;
+#ifdef UI
+	char  buffer[ MAX_TOKEN_CHARS ];
+#endif
 
 	// maintain cache
 	static float lastConfidenceHalfLife = 0.0f;
@@ -251,6 +254,9 @@ void BG_ImportUnlockablesFromMask( team_t team, int mask )
 	team_t           currentTeam;
 	qboolean         newStatus;
 	int              statusChanges[ NUM_UNLOCKABLES ];
+#ifdef CGAME
+	int statusChangeCount = 0;
+#endif
 
 	// maintain a cache to prevent redundant imports
 	static int    lastMask = 0;