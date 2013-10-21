Unvanquished/Unvanquished

commit 04d54d75375aba9c4ea234f0308124a56f5efbc0

Implement support for GL_ARB_debug_output

src/engine/sys/sdl_glimp.c
@@ -958,15 +958,111 @@ static qboolean GLimp_StartDriverAndSetMode( int mode, qboolean fullscreen, qboo
 	return qtrue;
 }
 
+#if defined USE_XREAL_RENDERER
+
+static GLenum debugTypes[] =
+{
+	0, 
+	GL_DEBUG_TYPE_ERROR_ARB,
+	GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR_ARB,
+	GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR_ARB,
+	GL_DEBUG_TYPE_PORTABILITY_ARB,
+	GL_DEBUG_TYPE_PERFORMANCE_ARB,
+	GL_DEBUG_TYPE_OTHER_ARB               
+};
+
+static void APIENTRY GLimp_DebugCallback( GLenum source, GLenum type, GLuint id,
+                                          GLenum severity, GLsizei length, const GLchar *message, void *userParam )
+{
+	const char *debugTypeName;
+	const char *debugSeverity;
+
+	if ( r_glDebugMode->integer <= GLDEBUG_NONE )
+	{
+		return;
+	}
+
+	if ( r_glDebugMode->integer < GLDEBUG_ALL )
+	{
+		if ( debugTypes[ r_glDebugMode->integer ] != type )
+		{
+			return;
+		}
+	}
+
+	switch ( type )
+	{
+		case GL_DEBUG_TYPE_ERROR_ARB:
+			debugTypeName = "DEBUG_TYPE_ERROR";
+			break;
+		case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR_ARB:
+			debugTypeName = "DEBUG_TYPE_DEPRECATED_BEHAVIOR";
+			break;
+		case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR_ARB:
+			debugTypeName = "DEBUG_TYPE_UNDEFINED_BEHAVIOR";
+			break;
+		case GL_DEBUG_TYPE_PORTABILITY_ARB:
+			debugTypeName = "DEBUG_TYPE_PORTABILITY";
+			break;
+		case GL_DEBUG_TYPE_PERFORMANCE_ARB:
+			debugTypeName = "DEBUG_TYPE_PERFORMANCE";
+			break;
+		case GL_DEBUG_TYPE_OTHER_ARB:
+			debugTypeName = "DEBUG_TYPE_OTHER";
+			break;
+		default:
+			debugTypeName = "DEBUG_TYPE_UNKNOWN";
+			break;
+	}
+
+	switch ( severity )
+	{
+		case GL_DEBUG_SEVERITY_HIGH_ARB:
+			debugSeverity = "high";
+			break;
+		case GL_DEBUG_SEVERITY_MEDIUM_ARB:
+			debugSeverity = "med";
+			break;
+		case GL_DEBUG_SEVERITY_LOW_ARB:
+			debugSeverity = "low";
+			break;
+		case GL_DEBUG_SEVERITY_NOTIFICATION:
+			debugSeverity = "notify";
+			break;
+		default:
+			debugSeverity = "none";
+			break;
+	}
+
+	ri.Printf( PRINT_ALL, "%s: severity: %s msg: %s\n", debugTypeName, debugSeverity, message );
+}
+
 /*
 ===============
 GLimp_XreaLInitExtensions
 ===============
 */
-#if defined USE_XREAL_RENDERER
 static void GLimp_XreaLInitExtensions( void )
 {
 	ri.Printf( PRINT_ALL, "Initializing OpenGL extensions\n" );
+
+	if ( GLEW_ARB_debug_output )
+	{
+		if ( r_glDebugProfile->integer )
+		{
+			glDebugMessageCallbackARB( GLimp_DebugCallback, NULL );
+			glEnable( GL_DEBUG_OUTPUT_SYNCHRONOUS_ARB );
+			ri.Printf( PRINT_ALL, "...using GL_ARB_debug_output\n" );
+		}
+		else
+		{
+			ri.Printf( PRINT_ALL, "...ignoring GL_ARB_debug_output\n" );
+		}
+	}
+	else
+	{
+		ri.Printf( PRINT_ALL, "...GL_ARB_debug_output not found\n" );
+	}
 
 	// GL_ARB_multitexture
 	if ( glConfig.driverType != GLDRV_OPENGL3 )

src/engine/rendererGL/tr_init.cpp
@@ -40,6 +40,7 @@ extern "C" {
 	cvar_t      *r_glMinorVersion;
 	cvar_t      *r_glCoreProfile;
 	cvar_t      *r_glDebugProfile;
+	cvar_t      *r_glDebugMode;
 	cvar_t      *r_glAllowSoftware;
 
 	cvar_t      *r_flares;
@@ -1370,6 +1371,7 @@ extern "C" {
 		r_glMinorVersion = ri.Cvar_Get( "r_glMinorVersion", "", CVAR_LATCH );
 		r_glCoreProfile = ri.Cvar_Get( "r_glCoreProfile", "", CVAR_LATCH );
 		r_glDebugProfile = ri.Cvar_Get( "r_glDebugProfile", "", CVAR_LATCH );
+		r_glDebugMode = ri.Cvar_Get( "r_glDebugMode", "0", CVAR_CHEAT ); 
 		r_glAllowSoftware = ri.Cvar_Get( "r_glAllowSoftware", "0", CVAR_LATCH );
 
 		// latched and archived variables
@@ -1505,7 +1507,8 @@ extern "C" {
 		r_vboSmoothNormals = ri.Cvar_Get( "r_vboSmoothNormals", "1", CVAR_ARCHIVE | CVAR_LATCH );
 
 		r_mergeLeafSurfaces = ri.Cvar_Get( "r_mergeLeafSurfaces", "0", CVAR_ARCHIVE | CVAR_LATCH );
-		r_dynamicBspOcclusionCulling = ri.Cvar_Get( "r_dynamicBspOcclusionCulling", "0", CVAR_ARCHIVE );		r_dynamicEntityOcclusionCulling = ri.Cvar_Get( "r_dynamicEntityOcclusionCulling", "0", CVAR_CHEAT );
+		r_dynamicBspOcclusionCulling = ri.Cvar_Get( "r_dynamicBspOcclusionCulling", "0", CVAR_ARCHIVE );
+		r_dynamicEntityOcclusionCulling = ri.Cvar_Get( "r_dynamicEntityOcclusionCulling", "0", CVAR_CHEAT );
 		r_dynamicLightOcclusionCulling = ri.Cvar_Get( "r_dynamicLightOcclusionCulling", "0", CVAR_CHEAT );
 		r_chcMaxPrevInvisNodesBatchSize = ri.Cvar_Get( "r_chcMaxPrevInvisNodesBatchSize", "50", CVAR_CHEAT );
 		r_chcMaxVisibleFrames = ri.Cvar_Get( "r_chcMaxVisibleFrames", "10", CVAR_CHEAT );

src/engine/rendererGL/tr_local.h
@@ -86,6 +86,18 @@ extern "C" {
 	  RSPEEDS_DECALS
 	} renderSpeeds_t;
 
+	typedef enum
+	{
+		GLDEBUG_NONE,
+		GLDEBUG_ERROR,
+		GLDEBUG_DEPRECIATED,
+		GLDEBUG_UNDEFINED,
+		GLDEBUG_PORTABILITY,
+		GLDEBUG_PERFORMANCE,
+		GLDEBUG_OTHER,
+		GLDEBUG_ALL
+	} glDebugModes_t;
+
 #define HDR_ENABLED()         (( r_hdrRendering->integer && glConfig2.textureFloatAvailable && glConfig2.framebufferObjectAvailable && glConfig2.framebufferBlitAvailable && glConfig.driverType != GLDRV_MESA ))
 
 #define REF_CUBEMAP_SIZE       32
@@ -2883,6 +2895,7 @@ extern "C" {
 	extern cvar_t *r_glMinorVersion;
 	extern cvar_t *r_glCoreProfile;
 	extern cvar_t *r_glDebugProfile;
+	extern cvar_t *r_glDebugMode;
 	extern cvar_t *r_glAllowSoftware;
 
 	extern cvar_t *r_flares; // light flares

commit 13cc3eb0ce184e8375daaadfbdb47e8ee062b465

Fix *nix builds
Broken by commit 04d54d7

src/engine/sys/sdl_glimp.c
@@ -968,11 +968,11 @@ static GLenum debugTypes[] =
 	GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR_ARB,
 	GL_DEBUG_TYPE_PORTABILITY_ARB,
 	GL_DEBUG_TYPE_PERFORMANCE_ARB,
-	GL_DEBUG_TYPE_OTHER_ARB               
+	GL_DEBUG_TYPE_OTHER_ARB
 };
 
-static void APIENTRY GLimp_DebugCallback( GLenum source, GLenum type, GLuint id,
-                                          GLenum severity, GLsizei length, const GLchar *message, void *userParam )
+static void QCALL GLimp_DebugCallback( GLenum source, GLenum type, GLuint id,
+                                       GLenum severity, GLsizei length, const GLchar *message, void *userParam )
 {
 	const char *debugTypeName;
 	const char *debugSeverity;