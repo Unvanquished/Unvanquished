8f5343184dd5e4b975ef8fc6122153e9e097bd77
Amanieu d'Antras <amanieu@gmail.com>
2013-11-21 23:39:58 +0000

    Simplify architecture detection

--- cmake/cross-toolchain-linux32.cmake
+++ cmake/cross-toolchain-linux32.cmake
@@ -2,6 +2,10 @@
 set( CMAKE_SYSTEM_NAME Linux )
 set( CMAKE_SYSTEM_PROCESSOR x86 )
 
+# C/C++ compilers
+set( CMAKE_C_COMPILER gcc -m32 )
+set( CMAKE_CXX_COMPILER g++ -m32 )
+
 # Only look for libraries in lib32
 set( CMAKE_SYSTEM_LIBRARY_PATH /lib32 /usr/lib32 /usr/local/lib32 )
 set_property( GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS OFF )
