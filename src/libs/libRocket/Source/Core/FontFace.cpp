0688bed0a8faa8bae07421130afc96af99260aa9
dolcetriade <vcelestialragev@gmail.com>
2013-12-25 17:29:01 -0800

    Don't use a different font handle per charset
    One handle can handle all the charsets.

--- src/libs/libRocket/Source/Core/FontFace.cpp
+++ src/libs/libRocket/Source/Core/FontFace.cpp
@@ -14,7 +14,7 @@
  *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -46,9 +46,7 @@ FontFace::~FontFace()
 {
 	for (HandleMap::iterator iterator = handles.begin(); iterator != handles.end(); ++iterator)
 	{
-		HandleList& handle_list = (*iterator).second;
-		for (size_t i = 0; i < handle_list.size(); ++i)
-			handle_list[i]->RemoveReference();
+		iterator->second->RemoveReference();
 	}
 
 	ReleaseFace();
@@ -74,44 +72,9 @@ FontFaceHandle* FontFace::GetHandle(const String& _raw_charset, int size)
 	HandleMap::iterator iterator = handles.find(size);
 	if (iterator != handles.end())
 	{
-		const HandleList& handles = (*iterator).second;
+		iterator->second->AddReference();
+		return iterator->second;
 
-		// Check all the handles if their charsets match the requested one exactly (ie, were specified by the same
-		// string).
-		String raw_charset(_raw_charset);
-		for (size_t i = 0; i < handles.size(); ++i)
-		{
-			if (handles[i]->GetRawCharset() == _raw_charset)
-			{
-				handles[i]->AddReference();
-				return handles[i];
-			}
-		}
-
-		// Check all the handles if their charsets contain the requested charset.
-		if (!UnicodeRange::BuildList(charset, raw_charset))
-		{
-			Log::Message(Log::LT_ERROR, "Invalid font charset '%s'.", _raw_charset.CString());
-			return NULL;
-		}
-
-		for (size_t i = 0; i < handles.size(); ++i)
-		{
-			bool range_contained = true;
-
-			const UnicodeRangeList& handle_charset = handles[i]->GetCharset();
-			for (size_t j = 0; j < charset.size() && range_contained; ++j)
-			{
-				if (!charset[j].IsContained(handle_charset))
-					range_contained = false;
-			}
-
-			if (range_contained)
-			{
-				handles[i]->AddReference();
-				return handles[i];
-			}
-		}
 	}
 
 	// See if this face has been released.
@@ -131,10 +94,7 @@ FontFaceHandle* FontFace::GetHandle(const String& _raw_charset, int size)
 
 	// Save the handle, and add a reference for the callee. The initial reference will be removed when the font face
 	// releases it.
-	if (iterator != handles.end())
-		(*iterator).second.push_back(handle);
-	else
-		handles[size] = HandleList(1, handle);
+	handles[size] = handle;
 
 	handle->AddReference();
 
