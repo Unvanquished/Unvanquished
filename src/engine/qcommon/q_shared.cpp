9a08611ec17589bf7e19ea191954d0fc0e9ebdbf
Amanieu d'Antras <amanieu@gmail.com>
2014-01-02 12:16:57 +0000

    Fix lcc build

--- src/engine/qcommon/q_shared.cpp
+++ src/engine/qcommon/q_shared.cpp
@@ -2001,6 +2001,10 @@ void Q_strncpyzDebug( char *dest, const char *src, int destsize, const char *fil
 void Q_strncpyz( char *dest, const char *src, int destsize )
 #endif
 {
+	char *d;
+	const char *s;
+	size_t n;
+
 #ifndef NDEBUG
 
 	if ( !dest )
@@ -2037,25 +2041,25 @@ void Q_strncpyz( char *dest, const char *src, int destsize )
 
 #endif
 
-/*
- * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
+	/*
+	 * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
+	 *
+	 * Permission to use, copy, modify, and distribute this software for any
+	 * purpose with or without fee is hereby granted, provided that the above
+	 * copyright notice and this permission notice appear in all copies.
+	 *
+	 * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+	 * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+	 * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+	 * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+	 * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+	 * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+	 * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+	 */
 
-	char *d = dest;
-	const char *s = src;
-	size_t n = destsize;
+	d = dest;
+	s = src;
+	n = destsize;
 
 	/* Copy as many bytes as will fit */
 	if (n != 0) {
