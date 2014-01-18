a429f973f30b37b62482a0aef35fd961b21a9e33
Corentin Wallez <corentin@wallez.net>
2014-01-18 11:59:50 +0100

    Fix the completion of /testReverb

--- src/engine/audio/Emitter.cpp
+++ src/engine/audio/Emitter.cpp
@@ -371,8 +371,13 @@ namespace Audio {
 
                 Cmd::CompletionResult res;
 
-                res.push_back({"stop", "stops the test"});
-                res.push_back({"none", "no effect at all"});
+                if (Str::IsPrefix(prefix, "stop")) {
+                    res.push_back({"stop", "stops the test"});
+                }
+
+                if (Str::IsPrefix(prefix, "none")) {
+                    res.push_back({"none", "no effect at all"});
+                }
 
                 for (auto& name: presets) {
                     if (Str::IsPrefix(prefix, name)) {
