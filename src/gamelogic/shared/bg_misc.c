780287ce1b627b27e7be4911dadd17caf83d58a0
dolcetriade <vcelestialragev@gmail.com>
2014-02-01 19:21:11 -0800

    Revert "Load emoticons as crn"
    This reverts commit d71df12cef6f357d0e36f123ec788c04e83b605e.

--- src/gamelogic/shared/bg_misc.c
+++ src/gamelogic/shared/bg_misc.c
@@ -2595,7 +2595,7 @@ int BG_LoadEmoticons( emoticon_t *emoticons, int num )
 	int  fileLen;
 	int  count;
 
-	numFiles = trap_FS_GetFileList( "emoticons", "x1.crn", fileList,
+	numFiles = trap_FS_GetFileList( "emoticons", "x1", fileList,
 	                                sizeof( fileList ) );
 
 	if ( numFiles < 1 )
