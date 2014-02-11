ac2ff204b179a6132f8e8a7a950247d828ad2ea6
Amanieu d'Antras <amanieu@gmail.com>
2014-02-11 17:42:52 +0100

    Initial work on new IPC framework

---
+++ src/common/IPC.cpp
@@ -0,0 +1,488 @@
+/*
+===========================================================================
+Daemon BSD Source Code
+Copyright (c) 2013-2014, Daemon Developers
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+===========================================================================
+*/
+
+#include "IPC.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#endif
+
+#undef INLINE
+#undef DLLEXPORT
+#undef NORETURN
+#undef NORETURN_PTR
+#include "../libs/nacl/native_client/src/shared/imc/nacl_imc_c.h"
+#include "../libs/nacl/native_client/src/public/imc_types.h"
+#include "../libs/nacl/native_client/src/trusted/service_runtime/nacl_config.h"
+#include "../libs/nacl/native_client/src/trusted/service_runtime/include/sys/fcntl.h"
+
+// Make sure if we notice if any of our assumptions about NaCl are violated
+static_assert(std::is_same<IPC::OSHandleType, NaClHandle>::value, "NaClHandle");
+static_assert(IPC::INVALID_HANDLE == NACL_INVALID_HANDLE, "NACL_INVALID_HANDLE");
+static_assert(IPC::MSG_MAX_BYTES == NACL_ABI_IMC_USER_BYTES_MAX, "NACL_ABI_IMC_USER_BYTES_MAX");
+static_assert(IPC::MSG_MAX_HANDLES == NACL_ABI_IMC_DESC_MAX, "NACL_ABI_IMC_DESC_MAX");
+
+// Definitions taken from nacl_desc_base.h
+enum NaClDescTypeTag {
+  NACL_DESC_INVALID,
+  NACL_DESC_DIR,
+  NACL_DESC_HOST_IO,
+  NACL_DESC_CONN_CAP,
+  NACL_DESC_CONN_CAP_FD,
+  NACL_DESC_BOUND_SOCKET,
+  NACL_DESC_CONNECTED_SOCKET,
+  NACL_DESC_SHM,
+  NACL_DESC_SYSV_SHM,
+  NACL_DESC_MUTEX,
+  NACL_DESC_CONDVAR,
+  NACL_DESC_SEMAPHORE,
+  NACL_DESC_SYNC_SOCKET,
+  NACL_DESC_TRANSFERABLE_DATA_SOCKET,
+  NACL_DESC_IMC_SOCKET,
+  NACL_DESC_QUOTA,
+  NACL_DESC_DEVICE_RNG,
+  NACL_DESC_DEVICE_POSTMESSAGE,
+  NACL_DESC_CUSTOM,
+  NACL_DESC_NULL
+};
+#define NACL_DESC_TYPE_MAX      (NACL_DESC_NULL + 1)
+#define NACL_DESC_TYPE_END_TAG  ((char)0xff)
+
+struct NaClInternalRealHeader {
+  uint32_t  xfer_protocol_version;
+  uint32_t  descriptor_data_bytes;
+};
+
+struct NaClInternalHeader {
+  struct NaClInternalRealHeader h;
+  char      pad[((sizeof(struct NaClInternalRealHeader) + 0x10) & ~0xf)
+                - sizeof(struct NaClInternalRealHeader)];
+};
+
+#define NACL_HANDLE_TRANSFER_PROTOCOL 0xd3c0de01
+// End of imported definitions
+
+namespace IPC {
+
+void CloseDesc(const Desc& desc)
+{
+#ifdef __native_client__
+	NaClClose(desc);
+#else
+	NaClClose(desc.handle);
+#endif
+}
+
+Desc DescFromFile(OSHandleType handle, FileOpenMode mode)
+{
+#ifdef __native_client__
+	return handle;
+#else
+	Desc out;
+	out.type = NACL_DESC_HOST_IO;
+	out.handle = handle;
+	switch (mode) {
+	case MODE_READ:
+		out.flags = NACL_ABI_O_RDONLY;
+		break;
+	case MODE_WRITE:
+		out.flags = NACL_ABI_O_WRONLY;
+		break;
+	case MODE_RW:
+		out.flags = NACL_ABI_O_RDWR;
+		break;
+	case MODE_WRITE_APPEND:
+		out.flags = NACL_ABI_O_WRONLY | NACL_ABI_O_APPEND;
+		break;
+	case MODE_RW_APPEND:
+		out.flags = NACL_ABI_O_RDWR | NACL_ABI_O_APPEND;
+		break;
+	}
+	return out;
+#endif
+}
+OSHandleType DescToHandle(const Desc& desc)
+{
+#ifdef __native_client__
+	return desc;
+#else
+	return desc.handle;
+#endif
+}
+
+void Socket::Close()
+{
+	if (handle != INVALID_HANDLE)
+		NaClClose(handle);
+	handle = INVALID_HANDLE;
+}
+
+Desc Socket::GetDesc() const
+{
+#ifdef __native_client__
+	return handle;
+#else
+	Desc out;
+	out.type = NACL_DESC_TRANSFERABLE_DATA_SOCKET;
+	out.handle = handle;
+	return out;
+#endif
+}
+Socket Socket::FromDesc(const Desc& desc)
+{
+	Socket out;
+#ifdef __native_client__
+	out.handle = desc;
+#else
+	out.handle = desc.handle;
+#endif
+	return out;
+}
+
+void Socket::SendMsg(const Writer& writer) const
+{
+	NaClMessageHeader hdr;
+	NaClIOVec iov[3];
+	NaClHandle h[NACL_ABI_IMC_DESC_MAX];
+	const Desc* handles = writer.GetHandles().data();
+	size_t num_handles = writer.GetHandles().size();
+	const void* data = writer.GetHandles().data();
+	size_t len = writer.GetHandles().size();
+
+	if (num_handles > NACL_ABI_IMC_DESC_MAX)
+		Com_Error(ERR_DROP, "IPC: Message contains more handles than maximum allowed: %lu\n", num_handles);
+	if (len > NACL_ABI_IMC_USER_BYTES_MAX)
+		Com_Error(ERR_DROP, "IPC: Message above maximum size: %lu\n", len);
+	for (size_t i = 0; i < num_handles; i++)
+		h[i] = DescToHandle(handles[i]);
+
+#ifdef __native_client__
+	hdr.iov = iov;
+	hdr.iov_length = 1;
+	hdr.handles = h;
+	hdr.handle_count = num_handles;
+	hdr.flags = 0;
+	iov[0].base = const_cast<void*>(data);
+	iov[0].length = len;
+	if (NaClSendDatagram(handle, &hdr, 0) == -1) {
+		char error[256];
+		NaClGetLastErrorString(error, sizeof(error));
+		Com_Error(ERR_DROP, "IPC: Failed to send message: %s", error);
+	}
+#else
+	size_t desc_bytes = 0;
+	std::unique_ptr<char[]> desc_buffer;
+	if (num_handles != 0) {
+		for (size_t i = 0; i < num_handles; i++) {
+			// tag: 1 byte
+			// flags: 4 bytes
+			// size: 8 bytes (only for SHM)
+			desc_bytes++;
+			desc_bytes += sizeof(uint32_t);
+			if (handles[i].type == NACL_DESC_SHM)
+				desc_bytes += sizeof(uint64_t);
+			else if (handles[i].type == NACL_DESC_HOST_IO)
+				desc_bytes += sizeof(int32_t);
+		}
+		// Add 1 byte end tag and round to 16 bytes
+		desc_bytes = (desc_bytes + 1 + 0xf) & ~0xf;
+
+		desc_buffer.reset(new char[desc_bytes]);
+		char* desc_buffer_ptr = &desc_buffer[0];
+		for (size_t i = 0; i < num_handles; i++) {
+			*desc_buffer_ptr++ = handles[i].type;
+			memset(desc_buffer_ptr, 0, sizeof(uint32_t));
+			desc_buffer_ptr += sizeof(uint32_t);
+			if (handles[i].type == NACL_DESC_SHM) {
+				memcpy(desc_buffer_ptr, &handles[i].size, sizeof(uint64_t));
+				desc_buffer_ptr += sizeof(uint64_t);
+			} else if (handles[i].type == NACL_DESC_HOST_IO) {
+				memcpy(desc_buffer_ptr, &handles[i].flags, sizeof(int32_t));
+				desc_buffer_ptr += sizeof(int32_t);
+			}
+		}
+		*desc_buffer_ptr++ = NACL_DESC_TYPE_END_TAG;
+
+		// Clear any padding bytes to avoid information leak
+		std::fill(desc_buffer_ptr, &desc_buffer[desc_bytes], 0);
+	}
+
+	NaClInternalHeader internal_hdr = {{NACL_HANDLE_TRANSFER_PROTOCOL, static_cast<uint32_t>(desc_bytes)}, {}};
+	hdr.iov = iov;
+	hdr.iov_length = 3;
+	hdr.handles = h;
+	hdr.handle_count = num_handles;
+	hdr.flags = 0;
+	iov[0].base = &internal_hdr;
+	iov[0].length = sizeof(NaClInternalHeader);
+	iov[1].base = desc_buffer.get();
+	iov[1].length = desc_bytes;
+	iov[2].base = const_cast<void*>(data);
+	iov[2].length = len;
+
+	if (NaClSendDatagram(handle, &hdr, 0) == -1) {
+		char error[256];
+		NaClGetLastErrorString(error, sizeof(error));
+		Com_Error(ERR_DROP, "IPC: Failed to send message: %s", error);
+	}
+#endif
+}
+
+#ifndef __native_client__
+// Clean up handles on error so that they don't leak
+static void FreeHandles(const NaClHandle* h)
+{
+	for (size_t i = 0; i < NACL_ABI_IMC_DESC_MAX; i++) {
+		if (h[i] != NACL_INVALID_HANDLE)
+			NaClClose(h[i]);
+	}
+}
+#endif
+
+Reader Socket::RecvMsg() const
+{
+	NaClMessageHeader hdr;
+	NaClIOVec iov[2];
+	NaClHandle h[NACL_ABI_IMC_DESC_MAX];
+	std::unique_ptr<char[]> recv_buffer(new char[NACL_ABI_IMC_BYTES_MAX]);
+	Reader out;
+
+	for (size_t i = 0; i < NACL_ABI_IMC_DESC_MAX; i++)
+		h[i] = NACL_INVALID_HANDLE;
+
+#ifdef __native_client__
+	hdr.iov = iov;
+	hdr.iov_length = 1;
+	hdr.handles = h;
+	hdr.handle_count = NACL_ABI_IMC_DESC_MAX;
+	hdr.flags = 0;
+	iov[0].base = recv_buffer.get();
+	iov[0].length = NACL_ABI_IMC_BYTES_MAX;
+
+	int result = NaClRecieveDatagram(handle, &hdr, 0);
+	if (result == -1) {
+		char error[256];
+		NaClGetLastErrorString(error, sizeof(error));
+		Com_Error(ERR_DROP, "IPC: Failed to receive message: %s", error);
+	}
+
+	for (size_t i = 0; i < NACL_ABI_IMC_DESC_MAX; i++) {
+		if (h[i] != NACL_INVALID_HANDLE)
+			out.GetHandles().push_back(h[i]);
+	}
+	out.GetData().assign(&recv_buffer[0], &recv_buffer[result]);
+	return out;
+#else
+	NaClInternalHeader internal_hdr;
+	hdr.iov = iov;
+	hdr.iov_length = 2;
+	hdr.handles = h;
+	hdr.handle_count = NACL_ABI_IMC_DESC_MAX;
+	hdr.flags = 0;
+	iov[0].base = &internal_hdr;
+	iov[0].length = sizeof(NaClInternalHeader);
+	iov[1].base = &recv_buffer[0];
+	iov[1].length = NACL_ABI_IMC_BYTES_MAX;
+
+	int result = NaClReceiveDatagram(handle, &hdr, 0);
+	if (result == -1) {
+		char error[256];
+		NaClGetLastErrorString(error, sizeof(error));
+		Com_Error(ERR_DROP, "IPC: Failed to receive message: %s", error);
+	}
+
+	if (result < (int)sizeof(NaClInternalHeader)) {
+		FreeHandles(h);
+		Com_Error(ERR_DROP, "IPC: Message with invalid header");
+	}
+	result -= sizeof(NaClInternalHeader);
+
+	if (internal_hdr.h.xfer_protocol_version != NACL_HANDLE_TRANSFER_PROTOCOL || result < (int)internal_hdr.h.descriptor_data_bytes) {
+		FreeHandles(h);
+		Com_Error(ERR_DROP, "IPC: Message with invalid header");
+	}
+	result -= internal_hdr.h.descriptor_data_bytes;
+
+	char* desc_ptr = &recv_buffer[0];
+	char* desc_end = desc_ptr + internal_hdr.h.descriptor_data_bytes;
+	size_t handle_index = 0;
+	while (desc_ptr < desc_end) {
+		int tag = 0xff & *desc_ptr++;
+		if (tag == NACL_DESC_TYPE_END_TAG)
+			break;
+		if (tag != NACL_DESC_SHM && tag != NACL_DESC_TRANSFERABLE_DATA_SOCKET && tag != NACL_DESC_HOST_IO) {
+			FreeHandles(h);
+			Com_Error(ERR_DROP, "IPC: Unrecognized descriptor tag in message: %02x", tag);
+		}
+
+		// Ignore flags
+		if (desc_end - desc_ptr < (int)sizeof(uint32_t)) {
+			FreeHandles(h);
+			Com_Error(ERR_DROP, "IPC: Descriptor flags missing from message");
+		}
+		desc_ptr += sizeof(uint32_t);
+
+		uint64_t size = 0;
+		int32_t flags = 0;
+		if (tag == NACL_DESC_SHM) {
+			if (desc_end - desc_ptr < (int)sizeof(uint64_t)) {
+				FreeHandles(h);
+				Com_Error(ERR_DROP, "IPC: Shared memory size missing from message");
+			}
+			memcpy(&size, desc_ptr, sizeof(uint64_t));
+			desc_ptr += sizeof(uint64_t);
+		} else if (tag == NACL_DESC_HOST_IO) {
+			if (desc_end - desc_ptr < (int)sizeof(int32_t)) {
+				FreeHandles(h);
+				Com_Error(ERR_DROP, "IPC: Host file mode missing from message");
+			}
+			memcpy(&flags, desc_ptr, sizeof(int32_t));
+			desc_ptr += sizeof(int32_t);
+		}
+
+		size_t i = handle_index++;
+		out.GetHandles().emplace_back();
+		out.GetHandles().back().handle = h[i];
+		out.GetHandles().back().type = tag;
+		h[i] = NACL_INVALID_HANDLE;
+		if (tag == NACL_DESC_SHM)
+			out.GetHandles().back().size = size;
+		else if (tag == NACL_DESC_HOST_IO)
+			out.GetHandles().back().flags = flags;
+	}
+
+	out.GetData().assign(&desc_end[0], &desc_end[result]);
+	return out;
+#endif
+}
+
+std::pair<Socket, Socket> Socket::CreatePair()
+{
+	NaClHandle handles[2];
+	if (NaClSocketPair(handles) != 0) {
+		char error[256];
+		NaClGetLastErrorString(error, sizeof(error));
+		Com_Error(ERR_DROP, "IPC: Failed to create socket pair: %s", error);
+	}
+	Socket a, b;
+	a.handle = handles[0];
+	b.handle = handles[1];
+	return std::make_pair(std::move(a), std::move(b));
+}
+
+static void* MapSharedMemory(OSHandleType handle, size_t size)
+{
+	// We don't use NaClMap here because it only supports MAP_FIXED
+#ifdef _WIN32
+	void *base = MapViewOfFile(handle, FILE_MAP_ALL_ACCESS, 0, 0, size);
+#else
+	void *base = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, handle, 0);
+	if (base == MAP_FAILED)
+		base = nullptr;
+#endif
+
+	if (base == nullptr) {
+		char error[256];
+		NaClGetLastErrorString(error, sizeof(error));
+		Com_Error(ERR_DROP, "IPC: Failed to map shared memory object of size %lu: %s", size, error);
+	}
+
+	return base;
+}
+
+void SharedMemory::Close()
+{
+	if (handle != INVALID_HANDLE) {
+#ifdef _WIN32
+		UnmapViewOfFile(base);
+#else
+		munmap(base, size);
+#endif
+		NaClClose(handle);
+	}
+	handle = INVALID_HANDLE;
+}
+
+Desc SharedMemory::GetDesc() const
+{
+#ifdef __native_client__
+	return handle;
+#else
+	Desc out;
+	out.type = NACL_DESC_SHM;
+	out.handle = handle;
+	out.size = size;
+	return out;
+#endif
+}
+SharedMemory SharedMemory::FromDesc(const Desc& desc)
+{
+	SharedMemory out;
+#ifdef __native_client__
+	struct stat st;
+	if (fstat(desc, &st) == -1) {
+		char error[256];
+		NaClGetLastErrorString(error, sizeof(error));
+		Com_Error(ERR_DROP, "IPC: Failed to stat shared memory handle: %s", error);
+	}
+	out.size = st.st_size;
+	out.handle = desc
+#else
+	out.size = desc.size;
+	out.handle = desc.handle;
+#endif
+	out.base = MapSharedMemory(out.handle, out.size);
+	return out;
+}
+
+SharedMemory SharedMemory::Create(size_t size)
+{
+	// Round size up to page size, otherwise the syscall will fail in NaCl
+	size = (size + NACL_MAP_PAGESIZE - 1) & ~(NACL_MAP_PAGESIZE - 1);
+
+	OSHandleType handle = NaClCreateMemoryObject(size, 0);
+	if (handle == INVALID_HANDLE) {
+		char error[256];
+		NaClGetLastErrorString(error, sizeof(error));
+		Com_Error(ERR_DROP, "IPC: Failed to create shared memory object of size %lu: %s", size, error);
+	}
+
+	SharedMemory out;
+	out.handle = handle;
+	out.base = MapSharedMemory(handle, size);
+	out.size = size;
+	return out;
+}
+
+} // namespace IPC
--- src/common/IPC.h
+++ src/common/IPC.h
@@ -1,34 +1,30 @@
 /*
 ===========================================================================
+Daemon BSD Source Code
+Copyright (c) 2013-2014, Daemon Developers
+All rights reserved.
 
-Daemon GPL Source Code
-Copyright (C) 1999-2010 id Software LLC, a ZeniMax Media company.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
-This file is part of the Daemon GPL Source Code (Daemon Source Code).
-
-Daemon Source Code is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Daemon Source Code is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Daemon Source Code.  If not, see <http://www.gnu.org/licenses/>.
-
-In addition, the Daemon Source Code is also subject to certain additional terms.
-You should have received a copy of these additional terms immediately following the
-terms and conditions of the GNU General Public License which accompanied the Daemon
-Source Code.  If not, please request a copy in writing from id Software at the address
-below.
-
-If you have questions concerning this license or the applicable additional terms, you
-may contact in writing id Software LLC, c/o ZeniMax Media Inc., Suite 120, Rockville,
-Maryland 20850 USA.
-
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ===========================================================================
 */
 
@@ -46,60 +42,216 @@ Maryland 20850 USA.
 #include "../engine/qcommon/q_shared.h"
 #include "../engine/qcommon/qcommon.h"
 
+namespace IPC {
+
+// Maximum number of bytes that can be sent in a message
+const size_t MSG_MAX_BYTES = 128 << 10;
+
+// Maximum number of handles that can be sent in a message
+const size_t MSG_MAX_HANDLES = 8;
+
+// Operating system handle type
+#ifdef _WIN32
+// HANDLE is defined as void* in windows.h
+// Note that the NaCl code uses -1 instead of 0 to represent invalid handles
+typedef void* OSHandleType;
+const OSHandleType INVALID_HANDLE = reinterpret_cast<void*>(-1);
+#else
+typedef int OSHandleType;
+const OSHandleType INVALID_HANDLE = -1;
+#endif
+
+// IPC descriptor which can be sent over a socket. You should treat this as an
+// opaque type and not access any of the fields directly.
+#ifdef __native_client__
+typedef int Desc;
+#else
+struct Desc {
+	OSHandleType handle;
+	int type;
+	union {
+		uint64_t size;
+		int32_t flags;
+	};
+};
+#endif
+void CloseDesc(const Desc& desc);
+
+// Conversion between Desc and handles
+enum FileOpenMode {
+	MODE_READ,
+	MODE_WRITE,
+	MODE_RW,
+	MODE_WRITE_APPEND,
+	MODE_RW_APPEND
+};
+Desc DescFromFile(OSHandleType handle, FileOpenMode mode);
+OSHandleType DescToHandle(const Desc& desc);
+
+// Message-based socket through which data and handles can be passed.
+class Reader;
+class Writer;
+class Socket {
+public:
+	Socket()
+		: handle(INVALID_HANDLE) {}
+	Socket(Socket&& other) NACL_NOEXCEPT
+		: handle(other.handle)
+	{
+		other.handle = INVALID_HANDLE;
+	}
+	Socket& operator=(Socket&& other) NACL_NOEXCEPT
+	{
+		std::swap(handle, other.handle);
+		return *this;
+	}
+	~Socket()
+	{
+		Close();
+	}
+	explicit operator bool() const
+	{
+		return handle != INVALID_HANDLE;
+	}
+
+	void Close();
+
+	Desc GetDesc() const;
+	static Socket FromDesc(const Desc& desc);
+
+	void SendMsg(const Writer& writer) const;
+	Reader RecvMsg() const;
+
+	static std::pair<Socket, Socket> CreatePair();
+
+private:
+	OSHandleType handle;
+};
+
+// Shared memory area, can be sent over a socket
+class SharedMemory {
+public:
+	SharedMemory()
+		: handle(INVALID_HANDLE) {}
+	SharedMemory(SharedMemory&& other) NACL_NOEXCEPT
+		: handle(other.handle), base(other.base), size(other.size)
+	{
+		other.handle = INVALID_HANDLE;
+	}
+	SharedMemory& operator=(SharedMemory&& other) NACL_NOEXCEPT
+	{
+		std::swap(handle, other.handle);
+		std::swap(base, other.base);
+		std::swap(size, other.size);
+		return *this;
+	}
+	~SharedMemory()
+	{
+		Close();
+	}
+	explicit operator bool() const
+	{
+		return handle != INVALID_HANDLE;
+	}
+
+	void Close();
+
+	Desc GetDesc() const;
+	static SharedMemory FromDesc(const Desc& desc);
+
+	static SharedMemory Create(size_t size);
+
+	void* GetBase() const
+	{
+		return base;
+	}
+	size_t GetSize() const
+	{
+		return size;
+	}
+
+private:
+	OSHandleType handle;
+	void* base;
+	size_t size;
+};
+
+// Base type for serialization traits.
+template<typename T, typename = void> struct SerializeTraits {};
+
+// Class to generate messages
 class Writer {
 public:
-	void Write(const void* p, size_t len)
+	void WriteData(const void* p, size_t len)
 	{
 		data.insert(data.end(), static_cast<const char*>(p), static_cast<const char*>(p) + len);
 	}
 	void WriteSize(size_t size)
 	{
 		if (size > std::numeric_limits<uint32_t>::max())
-			Com_Error(ERR_DROP, "Size out of range in IPC message");
-		uint32_t realSize = size;
-		Write(&realSize, sizeof(uint32_t));
+			Com_Error(ERR_DROP, "IPC: Size out of range in message");
+		Write<uint32_t>(size);
 	}
-	void WriteHandle(NaCl::IPCHandle& h)
+	template<typename T, typename Arg> void Write(Arg&& value)
 	{
-		handles.push_back(&h);
+		SerializeTraits<T>::Write(*this, std::forward<Arg>(value));
+	}
+	void WriteHandle(const Desc& h)
+	{
+		handles.push_back(h);
 	}
 
-	void Reset()
-	{
-		data.clear();
-		handles.clear();
-	}
 	const std::vector<char>& GetData() const
 	{
 		return data;
 	}
-	const std::vector<NaCl::IPCHandle*>& GetHandles() const
+	const std::vector<Desc>& GetHandles() const
 	{
 		return handles;
 	}
 
 private:
 	std::vector<char> data;
-	std::vector<NaCl::IPCHandle*> handles;
+	std::vector<Desc> handles;
 };
 
+// Class to read messages
 class Reader {
 public:
-	void Read(void* p, size_t len)
+	Reader()
+		: pos(0), handles_pos(0) {}
+	Reader(Reader&& other)
+		: data(std::move(other.data)), handles(std::move(other.handles)), pos(other.pos), handles_pos(other.handles_pos) {}
+	Reader& operator=(Reader&& other)
+	{
+		std::swap(data, other.data);
+		std::swap(handles, other.handles);
+		std::swap(pos, other.pos);
+		std::swap(handles_pos, other.handles_pos);
+		return *this;
+	}
+	~Reader()
+	{
+		// Close any handles that weren't read
+		for (size_t i = handles_pos; i < handles.size(); i++)
+			CloseDesc(handles[i]);
+	}
+
+	void ReadData(void* p, size_t len)
 	{
 		if (pos + len <= data.size()) {
 			memcpy(p, &data[pos], len);
 			pos += len;
 		} else
-			Com_Error(ERR_DROP, "IPC message too short");
+			Com_Error(ERR_DROP, "IPC: Unexpected end of message");
 	}
 	template<typename T> size_t ReadSize()
 	{
 		// Check for multiplication overflows when reading a size
 		uint32_t size;
-		Read(&size, sizeof(uint32_t));
+		ReadData(&size, sizeof(uint32_t));
 		if (size > std::numeric_limits<uint32_t>::max() / sizeof(T))
-			Com_Error(ERR_DROP, "Size out of range in IPC message");
+			Com_Error(ERR_DROP, "IPC: Size out of range in message");
 		return size;
 	}
 	const void* ReadInline(size_t len)
@@ -109,51 +261,47 @@ public:
 			pos += len;
 			return out;
 		} else
-			Com_Error(ERR_DROP, "IPC message too short");
+			Com_Error(ERR_DROP, "IPC: Unexpected end of message");
 	}
-	NaCl::IPCHandle ReadHandle()
+	template<typename T> decltype(SerializeTraits<T>::Read(std::declval<Reader&>())) Read()
+	{
+		return SerializeTraits<T>::Read(*this);
+	}
+	Desc ReadHandle()
 	{
 		if (handles_pos <= handles.size())
-			return std::move(handles[handles_pos++]);
+			return handles[handles_pos++];
 		else
-			Com_Error(ERR_DROP, "IPC message too short");
+			Com_Error(ERR_DROP, "IPC: Unexpected end of message");
 	}
 
-	void Reset()
-	{
-		pos = 0;
-		handles_pos = 0;
-	}
 	std::vector<char>& GetData()
 	{
 		return data;
 	}
-	std::vector<NaCl::IPCHandle>& GetHandles()
+	std::vector<Desc>& GetHandles()
 	{
 		return handles;
 	}
 
 private:
 	std::vector<char> data;
-	std::vector<NaCl::IPCHandle> handles;
+	std::vector<Desc> handles;
 	size_t pos;
 	size_t handles_pos;
 };
-
-// Base type for serialization traits.
-template<typename T, typename = void> struct SerializeTraits {};
 
 // Simple implementation for POD types
 template<typename T>
 struct SerializeTraits<T, typename std::enable_if<std::is_pod<T>::value>::type> {
 	static void Write(Writer& stream, const T& value)
 	{
-		stream.Write(std::addressof(value), sizeof(value));
+		stream.WriteData(std::addressof(value), sizeof(value));
 	}
 	static T Read(Reader& stream)
 	{
 		T value;
-		stream.Read(std::addressof(value), sizeof(value));
+		stream.ReadData(std::addressof(value), sizeof(value));
 		return value;
 	}
 };
@@ -164,13 +312,13 @@ struct SerializeTraits<std::vector<T>, typename std::enable_if<std::is_pod<T>::v
 	static void Write(Writer& stream, const std::vector<T>& value)
 	{
 		stream.WriteSize(value.size());
-		stream.Write(value.data(), value.size() * sizeof(T));
+		stream.WriteData(value.data(), value.size() * sizeof(T));
 	}
 	static std::vector<T> Read(Reader& stream)
 	{
 		std::vector<T> value;
 		value.resize(stream.ReadSize<T>());
-		stream.Read(value.data(), value.size() * sizeof(T));
+		stream.ReadData(value.data(), value.size() * sizeof(T));
 		return value;
 	}
 };
@@ -197,22 +345,20 @@ template<> struct SerializeTraits<std::string> {
 	static void Write(Writer& stream, Str::StringRef value)
 	{
 		stream.WriteSize(value.size());
-		stream.Write(value.data(), value.size());
+		stream.WriteData(value.c_str(), value.size() + 1);
 	}
 	static Str::StringRef Read(Reader& stream)
 	{
 		size_t size = stream.ReadSize<char>();
 		const char* p = static_cast<const char*>(stream.ReadInline(size + 1));
 		if (p[size] != '\0')
-			Com_Error(ERR_DROP, "String in IPC message is not NUL-terminated");
+			Com_Error(ERR_DROP, "IPC: String in message is not NUL-terminated");
 		return p;
 	}
 };
 
 // IPC message, which automatically serializes and deserializes objects
-template<uint32_t Major, uint32_t Minor, typename... T> class Message {
-	typedef std::tuple<T...> TupleType;
-
+template<uint32_t Major, uint32_t Minor, typename... T> class Message: public Writer {
 	template<size_t Index, typename Tuple> static void FillTuple(Tuple&, Reader&) {}
 	template<size_t Index, typename Type0, typename... Types, typename Tuple> static void FillTuple(Tuple& tuple, Reader& stream)
 	{
@@ -220,24 +366,22 @@ template<uint32_t Major, uint32_t Minor, typename... T> class Message {
 		FillTuple<Index + 1, Types...>(tuple, stream);
 	}
 
-	template<size_t Index> static void SerializeImpl(Writer&) {}
+	template<size_t Index> void SerializeImpl() {}
 	template<size_t Index, typename Arg0, typename... Args> void SerializeImpl(Arg0&& arg0, Args&&... args)
 	{
-		SerializeTraits<typename std::tuple_element<Index, TupleType>::type>::Write(stream, std::forward<Arg0>(arg0));
+		SerializeTraits<typename std::tuple_element<Index, std::tuple<T...>>::type>::Write(*this, std::forward<Arg0>(arg0));
 		SerializeImpl<Index + 1>(std::forward<Args>(args)...);
 	}
 
 public:
-	// Stream containing the serialized message data
-	Writer stream;
-
-	// Construct a message for sending
+	// Construct a message for sending. The message object contains a Writer which
+	// has the actual message data and can be passed to SendMsg().
 	template<typename... Args> Message(Args&&... args)
 	{
 		static_assert(sizeof...(Args) == sizeof...(T), "Incorrect number of arguments for IPC::Message::Serialize");
-		SerializeTraits<uint32_t>::Write(stream, Major);
-		SerializeTraits<uint32_t>::Write(stream, Minor);
-		SerializeImpl<0>(stream, std::forward<Args>(args)...);
+		SerializeTraits<uint32_t>::Write(*this, Major);
+		SerializeTraits<uint32_t>::Write(*this, Minor);
+		SerializeImpl<0>(std::forward<Args>(args)...);
 	}
 
 	// Deserialize a message and call the given function with the message arguments
@@ -248,5 +392,7 @@ public:
 		Util::apply(std::forward<Func>(func), std::move(tuple));
 	}
 };
+
+} // namespace IPC
 
 #endif // COMMON_IPC_H_
