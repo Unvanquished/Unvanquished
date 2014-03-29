83d24722b3b14cf92dcf05c60ff74db17da3537c
Theodor <theodoridisgr@gmail.com>
2014-03-29 19:32:22 +0200

    The audio_file classes (wav, ogg, opus) represent audio files.
    - audio_file is an abstract base class that holds the audio data and info.
    
    - each derived class loads the data in its constructor.
    
    - currently the old code is used for the loading.

---
+++ src/engine/audio/audio_file.h
@@ -0,0 +1,61 @@
+/*
+===========================================================================
+Daemon BSD Source Code
+Copyright (c) 2014, Theodoros Theodoridis <theodoridisgr@gmail.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Daemon developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL DAEMON DEVELOPERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+===========================================================================
+*/
+
+//TODO fix spacing
+#ifndef AUDIO_FILE_H
+#define AUDIO_FILE_H
+
+#include <vector>
+using std::vector;
+
+namespace Audio {
+
+class audio_file {
+ public:
+
+  virtual ~audio_file() = 0 ;
+
+  void* get_audio_data();
+
+  int get_sample_rate() const;
+  int get_bit_depth() const;
+  int get_number_of_channels() const;
+  int get_number_of_samples() const;
+
+ protected:
+  int sample_rate; //was rate
+  int bit_depth; //was width
+  int number_of_channels;
+  int number_of_samples; //was samples
+  vector<char> audio_data;
+};
+
+}  // namespace Audio
+#endif
---
+++ src/engine/audio/wav_file.cpp
@@ -0,0 +1,53 @@
+/*
+===========================================================================
+Daemon BSD Source Code
+Copyright (c) 2014, Theodoros Theodoridis <theodoridisgr@gmail.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Daemon developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL DAEMON DEVELOPERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+===========================================================================
+*/
+
+
+#include "wav_file.h"
+#include "snd_codec.h"
+#include "../qcommon/qcommon.h"
+#include <iterator>
+
+using Audio::wav_file;
+
+wav_file::wav_file(string filename) {
+
+  snd_info_t info;
+  char *data = static_cast<char *>(S_WAV_CodecLoad(filename.data(), &info));
+
+  std::copy(data, data + info.size, std::back_inserter(audio_data));
+
+  sample_rate = info.rate;
+  bit_depth = info.width * 8;
+  number_of_channels = info.channels;
+  number_of_samples = info.samples;
+
+  Hunk_FreeTempMemory(data);
+}
+
