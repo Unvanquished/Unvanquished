7631f7dd6cd466cbf1e27a12187bed5fa639819a
Amanieu d'Antras <amanieu@gmail.com>
2014-04-21 15:04:11 +0200

    Fix includes in new codec code

--- src/engine/audio/OpusCodec.cpp
+++ src/engine/audio/OpusCodec.cpp
@@ -29,7 +29,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include "SoundCodec.h"
-#include "../framework/FileSystem.h"
+#include "../../common/FileSystem.h"
 #include "AudioPrivate.h"
 #include <memory>
 #include <vector>
