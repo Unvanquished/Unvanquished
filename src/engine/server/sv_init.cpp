8ae3376ec77744738a01198f5f63027b1395c8e2
Darren Salt <devspam@moreofthesa.me.uk>
2014-05-06 03:23:15 +0100

    Set vm_game to 2 (TYPE_NATIVE_EXE) by default.
    Revert Debian changes which set this by other means.

--- src/engine/server/sv_init.cpp
+++ src/engine/server/sv_init.cpp
@@ -802,7 +802,7 @@ void SV_Init( void )
 	// ET://someserver.com
 	sv_fullmsg = Cvar_Get( "sv_fullmsg", "Server is full.", 0 );
 
-	vm_game = Cvar_Get( "vm_game", "0", 0 );
+	vm_game = Cvar_Get( "vm_game", "2", 0 ); // TYPE_NATIVE_EXE
 
 	svs.serverLoad = -1;
 }
