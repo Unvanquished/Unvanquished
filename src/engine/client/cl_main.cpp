f18d38d5b0ccc914c5e7d532c1bd8824a0355a1c
Corentin Wallez <corentin@wallez.net>
2014-05-29 04:08:05 +0200

    Remove unlikely to be used #if 0'ed code

--- src/engine/client/cl_main.cpp
+++ src/engine/client/cl_main.cpp
@@ -670,20 +670,6 @@ void CL_CaptureVoip( void )
 				Com_DPrintf( "VoIP: Send %d frames, %d bytes, %f power\n",
 				             speexFrames, wpos, clc.voipPower );
 
-#if 0
-				static FILE *encio = NULL;
-
-				if ( encio == NULL ) { encio = fopen( "voip-outgoing-encoded.bin", "wb" ); }
-
-				if ( encio != NULL ) { fwrite( clc.voipOutgoingData, wpos, 1, encio ); fflush( encio ); }
-
-				static FILE *decio = NULL;
-
-				if ( decio == NULL ) { decio = fopen( "voip-outgoing-decoded.bin", "wb" ); }
-
-				if ( decio != NULL ) { fwrite( sampbuffer, speexFrames * clc.speexFrameSize * 2, 1, decio ); fflush( decio ); }
-
-#endif
 			}
 		}
 	}
