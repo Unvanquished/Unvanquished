Unvanquished/Unvanquished

commit 80285befd96ed9ca223d4986a85881cbd624d59c

Add a skip mask to all traces, which allows to selectively ignore content types.
This commit breaks VM/Engine backwards compatibility.

src/gamelogic/cgame/cg_local.h
@@ -1797,11 +1797,12 @@ void CG_ModelDoor( centity_t *cent );
 void CG_BuildSolidList( void );
 int  CG_PointContents( const vec3_t point, int passEntityNum );
 void CG_Trace( trace_t *result, const vec3_t start, const vec3_t mins, const vec3_t maxs,
-               const vec3_t end, int skipNumber, int mask );
+               const vec3_t end, int skipNumber, int mask, int skipmask );
 void CG_CapTrace( trace_t *result, const vec3_t start, const vec3_t mins, const vec3_t maxs,
-                  const vec3_t end, int skipNumber, int mask );
+                  const vec3_t end, int skipNumber, int mask, int skipmask );
 void CG_BiSphereTrace( trace_t *result, const vec3_t start, const vec3_t end,
-                       const float startRadius, const float endRadius, int skipNumber, int mask );
+                       const float startRadius, const float endRadius, int skipNumber, int mask,
+                       int skipmask );
 void CG_PredictPlayerState( void );
 
 //

src/gamelogic/cgame/cg_predict.c
@@ -92,7 +92,7 @@ CG_ClipMoveToEntities
 */
 static void CG_ClipMoveToEntities( const vec3_t start, const vec3_t mins,
                                    const vec3_t maxs, const vec3_t end, int skipNumber,
-                                   int mask, trace_t *tr, traceType_t collisionType )
+                                   int mask, int skipmask, trace_t *tr, traceType_t collisionType )
 {
 	int           i, j, x, zd, zu;
 	trace_t       trace;
@@ -179,20 +179,20 @@ static void CG_ClipMoveToEntities( const vec3_t start, const vec3_t mins,
 		switch ( collisionType )
 		{
 		case TT_CAPSULE:
-			trap_CM_TransformedCapsuleTrace( &trace, start, end,
-			                                 mins, maxs, cmodel,  mask, origin, angles );
+			trap_CM_TransformedCapsuleTrace( &trace, start, end, mins, maxs, cmodel, mask, skipmask,
+			                                 origin, angles );
 			break;
 
 		case TT_AABB:
-			trap_CM_TransformedBoxTrace( &trace, start, end,
-			                             mins, maxs, cmodel,  mask, origin, angles );
+			trap_CM_TransformedBoxTrace( &trace, start, end, mins, maxs, cmodel, mask, skipmask,
+			                             origin, angles );
 			break;
 
 		case TT_BISPHERE:
 			assert( maxs != NULL );
 			assert( mins != NULL );
-			trap_CM_TransformedBiSphereTrace( &trace, start, end,
-			                                  mins[ 0 ], maxs[ 0 ], cmodel, mask, origin );
+			trap_CM_TransformedBiSphereTrace( &trace, start, end, mins[ 0 ], maxs[ 0 ], cmodel,
+			                                  mask, skipmask, origin );
 			break;
 
 		default: // Shouldn't Happen
@@ -232,15 +232,15 @@ static void CG_ClipMoveToEntities( const vec3_t start, const vec3_t mins,
 CG_Trace
 ================
 */
-void  CG_Trace( trace_t *result, const vec3_t start, const vec3_t mins, const vec3_t maxs, const vec3_t end,
-                int skipNumber, int mask )
+void CG_Trace( trace_t *result, const vec3_t start, const vec3_t mins, const vec3_t maxs,
+               const vec3_t end, int skipNumber, int mask, int skipmask )
 {
 	trace_t t;
 
-	trap_CM_BoxTrace( &t, start, end, mins, maxs, 0, mask );
+	trap_CM_BoxTrace( &t, start, end, mins, maxs, 0, mask, skipmask );
 	t.entityNum = t.fraction != 1.0 ? ENTITYNUM_WORLD : ENTITYNUM_NONE;
 	// check all other solid models
-	CG_ClipMoveToEntities( start, mins, maxs, end, skipNumber, mask, &t, TT_AABB );
+	CG_ClipMoveToEntities( start, mins, maxs, end, skipNumber, mask, skipmask, &t, TT_AABB );
 
 	*result = t;
 }
@@ -250,15 +250,15 @@ void  CG_Trace( trace_t *result, const vec3_t start, const vec3_t mins, const ve
 CG_CapTrace
 ================
 */
-void  CG_CapTrace( trace_t *result, const vec3_t start, const vec3_t mins, const vec3_t maxs, const vec3_t end,
-                   int skipNumber, int mask )
+void  CG_CapTrace( trace_t *result, const vec3_t start, const vec3_t mins, const vec3_t maxs,
+                   const vec3_t end, int skipNumber, int mask, int skipmask )
 {
 	trace_t t;
 
-	trap_CM_CapsuleTrace( &t, start, end, mins, maxs, 0, mask );
+	trap_CM_CapsuleTrace( &t, start, end, mins, maxs, 0, mask, skipmask );
 	t.entityNum = t.fraction != 1.0 ? ENTITYNUM_WORLD : ENTITYNUM_NONE;
 	// check all other solid models
-	CG_ClipMoveToEntities( start, mins, maxs, end, skipNumber, mask, &t, TT_CAPSULE );
+	CG_ClipMoveToEntities( start, mins, maxs, end, skipNumber, mask, skipmask, &t, TT_CAPSULE );
 
 	*result = t;
 }
@@ -269,7 +269,8 @@ CG_BiSphereTrace
 ================
 */
 void CG_BiSphereTrace( trace_t *result, const vec3_t start, const vec3_t end,
-                       const float startRadius, const float endRadius, int skipNumber, int mask )
+                       const float startRadius, const float endRadius, int skipNumber, int mask,
+                       int skipmask )
 {
 	trace_t t;
 	vec3_t  mins, maxs;
@@ -277,10 +278,10 @@ void CG_BiSphereTrace( trace_t *result, const vec3_t start, const vec3_t end,
 	mins[ 0 ] = startRadius;
 	maxs[ 0 ] = endRadius;
 
-	trap_CM_BiSphereTrace( &t, start, end, startRadius, endRadius, 0, mask );
+	trap_CM_BiSphereTrace( &t, start, end, startRadius, endRadius, 0, mask, skipmask );
 	t.entityNum = t.fraction != 1.0 ? ENTITYNUM_WORLD : ENTITYNUM_NONE;
 	// check all other solid models
-	CG_ClipMoveToEntities( start, mins, maxs, end, skipNumber, mask, &t, TT_BISPHERE );
+	CG_ClipMoveToEntities( start, mins, maxs, end, skipNumber, mask, skipmask, &t, TT_BISPHERE );
 
 	*result = t;
 }
@@ -445,7 +446,7 @@ static void CG_TouchTriggerPrediction( void )
 		}
 
 		trap_CM_BoxTrace( &trace, cg.predictedPlayerState.origin, cg.predictedPlayerState.origin,
-		                  cg_pmove.mins, cg_pmove.maxs, cmodel, -1 );
+		                  cg_pmove.mins, cg_pmove.maxs, cmodel, MASK_ALL, 0 );
 
 		if ( !trace.startsolid )
 		{

src/gamelogic/game/g_cm_world.cpp
@@ -221,7 +221,7 @@ qboolean G_CM_EntityContact( const vec3_t mins, const vec3_t maxs, const gentity
 	angles = gEnt->r.currentAngles;
 
 	ch = G_CM_ClipHandleForEntity( gEnt );
-	CM_TransformedBoxTrace( &trace, vec3_origin, vec3_origin, mins, maxs, ch, -1, origin, angles, type );
+	CM_TransformedBoxTrace( &trace, vec3_origin, vec3_origin, mins, maxs, ch, -1, 0, origin, angles, type );
 
 	return trace.startsolid;
 }
@@ -718,8 +718,8 @@ G_CM_ClipToEntity
 
 ====================
 */
-void G_CM_ClipToEntity( trace_t *trace, const vec3_t start, const vec3_t mins, const vec3_t maxs, const vec3_t end, int entityNum,
-                      int contentmask, traceType_t type )
+void G_CM_ClipToEntity( trace_t *trace, const vec3_t start, const vec3_t mins, const vec3_t maxs,
+                        const vec3_t end, int entityNum, int contentmask, traceType_t type )
 {
 	gentity_t *touch;
 	clipHandle_t   clipHandle;
@@ -748,8 +748,8 @@ void G_CM_ClipToEntity( trace_t *trace, const vec3_t start, const vec3_t mins, c
 		angles = vec3_origin; // boxes don't rotate
 	}
 
-	CM_TransformedBoxTrace( trace, ( float * ) start, ( float * ) end,
-	                        ( float * ) mins, ( float * ) maxs, clipHandle, contentmask, origin, angles, type );
+	CM_TransformedBoxTrace( trace, ( float * ) start, ( float * ) end, ( float * ) mins,
+	                        ( float * ) maxs, clipHandle, contentmask, 0, origin, angles, type );
 
 	if ( trace->fraction < 1 )
 	{
@@ -838,8 +838,8 @@ void G_CM_ClipMoveToEntities( moveclip_t *clip )
 			angles = vec3_origin; // boxes don't rotate
 		}
 
-		CM_TransformedBoxTrace( &trace, clip->start, clip->end,
-		                        clip->mins, clip->maxs, clipHandle, clip->contentmask, origin, angles, clip->collisionType );
+		CM_TransformedBoxTrace( &trace, clip->start, clip->end, clip->mins, clip->maxs, clipHandle,
+		                        clip->contentmask, 0, origin, angles, clip->collisionType );
 
 		if ( trace.allsolid )
 		{
@@ -874,8 +874,9 @@ Moves the given mins/maxs volume through the world from start to end.
 passEntityNum and entities owned by passEntityNum are explicitly not checked.
 ==================
 */
-void G_CM_Trace( trace_t *results, const vec3_t start, const vec3_t mins2, const vec3_t maxs2, const vec3_t end, int passEntityNum,
-               int contentmask, traceType_t type )
+void G_CM_Trace( trace_t *results, const vec3_t start, const vec3_t mins2, const vec3_t maxs2,
+                 const vec3_t end, int passEntityNum, int contentmask, int skipmask,
+                 traceType_t type )
 {
 	moveclip_t clip;
 	int        i;
@@ -900,10 +901,10 @@ void G_CM_Trace( trace_t *results, const vec3_t start, const vec3_t mins2, const
 	memset( &clip, 0, sizeof( moveclip_t ) );
 
 	// clip to world
-	CM_BoxTrace( &clip.trace, start, end, mins, maxs, 0, contentmask, type );
+	CM_BoxTrace( &clip.trace, start, end, mins, maxs, 0, contentmask, skipmask, type );
 	clip.trace.entityNum = clip.trace.fraction != 1.0 ? ENTITYNUM_WORLD : ENTITYNUM_NONE;
 
-	if ( clip.trace.fraction == 0 || passEntityNum == -2 )
+	if ( clip.trace.fraction == 0 )
 	{
 		*results = clip.trace;
 		return; // blocked immediately by the world

src/gamelogic/game/g_spawn_mover.cpp
@@ -67,11 +67,13 @@ gentity_t *G_TestEntityPosition( gentity_t *ent )
 
 	if ( ent->client )
 	{
-		trap_Trace( &tr, ent->client->ps.origin, ent->r.mins, ent->r.maxs, ent->client->ps.origin, ent->s.number, ent->clipmask );
+		trap_Trace( &tr, ent->client->ps.origin, ent->r.mins, ent->r.maxs, ent->client->ps.origin,
+		            ent->s.number, ent->clipmask, 0 );
 	}
 	else
 	{
-		trap_Trace( &tr, ent->s.pos.trBase, ent->r.mins, ent->r.maxs, ent->s.pos.trBase, ent->s.number, ent->clipmask );
+		trap_Trace( &tr, ent->s.pos.trBase, ent->r.mins, ent->r.maxs, ent->s.pos.trBase,
+		            ent->s.number, ent->clipmask, 0 );
 	}
 
 	if ( tr.startsolid )

src/common/cm/cm_local.h
@@ -228,6 +228,7 @@ typedef struct
 	vec3_t      bounds[ 2 ]; // enclosing box of start and end surrounding by size
 	vec3_t      modelOrigin; // origin of the model tracing through
 	int         contents; // ored contents of the model tracing through
+	int         skipContents; // ored contents that shall be ignored
 	qboolean    isPoint; // optimized case
 	trace_t     trace; // returned from trace call
 	sphere_t    sphere; // sphere for oriendted capsule collision

src/common/cm/cm_public.h
@@ -51,18 +51,19 @@ char         *CM_EntityString( void );
 int          CM_PointContents( const vec3_t p, clipHandle_t model );
 int          CM_TransformedPointContents( const vec3_t p, clipHandle_t model, const vec3_t origin, const vec3_t angles );
 
-void         CM_BoxTrace( trace_t *results, const vec3_t start, const vec3_t end,
-                          vec3_t mins, vec3_t maxs, clipHandle_t model, int brushmask, traceType_t type );
+void         CM_BoxTrace( trace_t *results, const vec3_t start, const vec3_t end, vec3_t mins,
+                          vec3_t maxs, clipHandle_t model, int brushmask, int skipmask,
+                          traceType_t type );
 void         CM_TransformedBoxTrace( trace_t *results, const vec3_t start, const vec3_t end,
-                                     const vec3_t mins, const vec3_t maxs,
-                                     clipHandle_t model, int brushmask, const vec3_t origin, const vec3_t angles, traceType_t type );
+                                     const vec3_t mins, const vec3_t maxs, clipHandle_t model,
+                                     int brushmask, int skipmask, const vec3_t origin,
+                                     const vec3_t angles, traceType_t type );
 void         CM_BiSphereTrace( trace_t *results, const vec3_t start,
                                const vec3_t end, float startRad, float endRad,
-                               clipHandle_t model, int mask );
-void         CM_TransformedBiSphereTrace( trace_t *results, const vec3_t start,
-    const vec3_t end, float startRad, float endRad,
-    clipHandle_t model, int mask,
-    const vec3_t origin );
+                               clipHandle_t model, int mask, int skipmask );
+void         CM_TransformedBiSphereTrace( trace_t *results, const vec3_t start, const vec3_t end,
+                                          float startRad, float endRad, clipHandle_t model,
+                                          int mask, int skipmask, const vec3_t origin );
 
 float CM_DistanceToModel( const vec3_t loc, clipHandle_t model );
 

src/common/cm/cm_trace.cpp
@@ -396,6 +396,11 @@ void CM_TestInLeaf( traceWork_t *tw, cLeaf_t *leaf )
 			continue;
 		}
 
+		if ( b->contents & tw->skipContents )
+		{
+			continue;
+		}
+
 		CM_TestBoxInBrush( tw, b );
 
 		if ( tw->trace.allsolid )
@@ -422,6 +427,11 @@ void CM_TestInLeaf( traceWork_t *tw, cLeaf_t *leaf )
 		surface->checkcount = cm.checkcount;
 
 		if ( !( surface->contents & tw->contents ) )
+		{
+			continue;
+		}
+
+		if ( surface->contents & tw->skipContents )
 		{
 			continue;
 		}
@@ -1449,6 +1459,11 @@ void CM_TraceThroughLeaf( traceWork_t *tw, cLeaf_t *leaf )
 			continue;
 		}
 
+		if ( b->contents & tw->skipContents )
+		{
+			continue;
+		}
+
 		b->collided = qfalse;
 
 		if ( !CM_BoundsIntersect( tw->bounds[ 0 ], tw->bounds[ 1 ], b->bounds[ 0 ], b->bounds[ 1 ] ) )
@@ -1487,6 +1502,11 @@ void CM_TraceThroughLeaf( traceWork_t *tw, cLeaf_t *leaf )
 			continue;
 		}
 
+		if ( surface->contents & tw->skipContents )
+		{
+			continue;
+		}
+
 		if ( !CM_BoundsIntersect( tw->bounds[ 0 ], tw->bounds[ 1 ], surface->sc->bounds[ 0 ], surface->sc->bounds[ 1 ] ) )
 		{
 			continue;
@@ -1520,6 +1540,11 @@ void CM_TraceThroughLeaf( traceWork_t *tw, cLeaf_t *leaf )
 				continue;
 			}
 
+			if ( b->contents & tw->skipContents )
+			{
+				continue;
+			}
+
 			CM_ProximityToBrush( tw, b );
 
 			if ( !tw->trace.lateralFraction )
@@ -1538,6 +1563,11 @@ void CM_TraceThroughLeaf( traceWork_t *tw, cLeaf_t *leaf )
 			}
 
 			if ( !( surface->contents & tw->contents ) )
+			{
+				continue;
+			}
+
+			if ( surface->contents & tw->skipContents )
 			{
 				continue;
 			}
@@ -2031,9 +2061,9 @@ static void CM_TraceThroughTree( traceWork_t *tw, int num, float p1f, float p2f,
 CM_Trace
 ==================
 */
-static void CM_Trace( trace_t *results, const vec3_t start,
-                      const vec3_t end, vec3_t mins, vec3_t maxs,
-                      clipHandle_t model, const vec3_t origin, int brushmask, traceType_t type, sphere_t *sphere )
+static void CM_Trace( trace_t *results, const vec3_t start, const vec3_t end, vec3_t mins,
+                      vec3_t maxs, clipHandle_t model, const vec3_t origin, int brushmask,
+                      int skipmask, traceType_t type, sphere_t *sphere )
 {
 	int         i;
 	traceWork_t tw;
@@ -2072,6 +2102,7 @@ static void CM_Trace( trace_t *results, const vec3_t start,
 
 	// set basic parms
 	tw.contents = brushmask;
+	tw.skipContents = skipmask;
 
 	// adjust so that mins and maxs are always symetric, which
 	// avoids some complications with plane expanding of rotated
@@ -2298,10 +2329,10 @@ static void CM_Trace( trace_t *results, const vec3_t start,
 CM_BoxTrace
 ==================
 */
-void CM_BoxTrace( trace_t *results, const vec3_t start, const vec3_t end,
-                  vec3_t mins, vec3_t maxs, clipHandle_t model, int brushmask, traceType_t type )
+void CM_BoxTrace( trace_t *results, const vec3_t start, const vec3_t end, vec3_t mins, vec3_t maxs,
+                  clipHandle_t model, int brushmask, int skipmask, traceType_t type )
 {
-	CM_Trace( results, start, end, mins, maxs, model, vec3_origin, brushmask, type, NULL );
+	CM_Trace( results, start, end, mins, maxs, model, vec3_origin, brushmask, skipmask, type, NULL );
 }
 
 /*
@@ -2313,8 +2344,9 @@ rotating entities
 ==================
 */
 void CM_TransformedBoxTrace( trace_t *results, const vec3_t start, const vec3_t end,
-                             const vec3_t mins, const vec3_t maxs,
-                             clipHandle_t model, int brushmask, const vec3_t origin, const vec3_t angles, traceType_t type )
+                             const vec3_t mins, const vec3_t maxs, clipHandle_t model,
+                             int brushmask, int skipmask, const vec3_t origin, const vec3_t angles,
+                             traceType_t type )
 {
 	trace_t  trace;
 	vec3_t   start_l, end_l;
@@ -2393,7 +2425,8 @@ void CM_TransformedBoxTrace( trace_t *results, const vec3_t start, const vec3_t 
 	}
 
 	// sweep the box through the model
-	CM_Trace( &trace, start_l, end_l, symetricSize[ 0 ], symetricSize[ 1 ], model, origin, brushmask, type, &sphere );
+	CM_Trace( &trace, start_l, end_l, symetricSize[ 0 ], symetricSize[ 1 ], model, origin,
+			  brushmask, skipmask, type, &sphere );
 
 	// if the bmodel was rotated and there was a collision
 	if ( rotated && trace.fraction != 1.0 )
@@ -2417,8 +2450,8 @@ void CM_TransformedBoxTrace( trace_t *results, const vec3_t start, const vec3_t 
 CM_BiSphereTrace
 ==================
 */
-void CM_BiSphereTrace( trace_t *results, const vec3_t start,
-                       const vec3_t end, float startRad, float endRad, clipHandle_t model, int mask )
+void CM_BiSphereTrace( trace_t *results, const vec3_t start, const vec3_t end, float startRad,
+                       float endRad, clipHandle_t model, int mask, int skipmask )
 {
 	int         i;
 	traceWork_t tw;
@@ -2448,6 +2481,7 @@ void CM_BiSphereTrace( trace_t *results, const vec3_t start,
 
 	// set basic parms
 	tw.contents = mask;
+	tw.skipContents = skipmask;
 
 	VectorCopy( start, tw.start );
 	VectorCopy( end, tw.end );
@@ -2522,9 +2556,9 @@ Handles offseting and rotation of the end points for moving and
 rotating entities
 ==================
 */
-void CM_TransformedBiSphereTrace( trace_t *results, const vec3_t start,
-                                  const vec3_t end, float startRad, float endRad,
-                                  clipHandle_t model, int mask, const vec3_t origin )
+void CM_TransformedBiSphereTrace( trace_t *results, const vec3_t start, const vec3_t end,
+                                  float startRad, float endRad, clipHandle_t model, int mask,
+                                  int skipmask, const vec3_t origin )
 {
 	trace_t trace;
 	vec3_t  start_l, end_l;
@@ -2533,7 +2567,7 @@ void CM_TransformedBiSphereTrace( trace_t *results, const vec3_t start,
 	VectorSubtract( start, origin, start_l );
 	VectorSubtract( end, origin, end_l );
 
-	CM_BiSphereTrace( &trace, start_l, end_l, startRad, endRad, model, mask );
+	CM_BiSphereTrace( &trace, start_l, end_l, startRad, endRad, model, mask, skipmask );
 
 	// re-calculate the end position of the trace because the trace.endpos
 	// calculated by CM_BiSphereTrace could be rotated and have an offset

src/engine/botlib/bot_nav_edit.cpp
@@ -55,7 +55,8 @@ bool GetPointPointedTo( NavData_t *nav, rVec &p )
 	AngleVectors( cl.snap.ps.viewangles, forward, NULL, NULL );
 	VectorMA( cl.snap.ps.origin, 8096, forward, end );
 
-	CM_BoxTrace( &trace, cl.snap.ps.origin, end, NULL, NULL, 0, CONTENTS_SOLID | CONTENTS_PLAYERCLIP, TT_AABB );
+	CM_BoxTrace( &trace, cl.snap.ps.origin, end, NULL, NULL, 0,
+	             CONTENTS_SOLID | CONTENTS_PLAYERCLIP, 0, TT_AABB );
 
 	pos = qVec( trace.endpos );
 	if ( dtStatusFailed( nav->query->findNearestPoly( pos, extents, &nav->filter, &nearRef, p ) ) )

commit 18f34253f243ee30979fa1a77347ee6d4444a7fe

Tentatively extend trace (skip-)masks to entities on both client and server side.
Spectators noclip through doors again (broken in 80285be).

src/gamelogic/cgame/cg_local.h
@@ -697,6 +697,11 @@ typedef struct centity_s
 	qboolean              valid;
 	qboolean              oldValid;
 	struct centity_s      *nextLocation;
+
+	// Content flags derived from the entity state
+	// HACK: This is not an exact copy of the content flags the server sees.
+	// If this is desired, it needs to be made part of entityState_t instead.
+	int                   contents;
 } centity_t;
 
 //======================================================================

src/gamelogic/cgame/cg_predict.c
@@ -42,6 +42,8 @@ CG_BuildSolidList
 When a new cg.snap has been set, this function builds a sublist
 of the entities that are actually solid, to make for more
 efficient collision detection
+
+It also adds content flags to allow for more specific traces in synchronized code.
 ====================
 */
 void CG_BuildSolidList( void )
@@ -77,6 +79,20 @@ void CG_BuildSolidList( void )
 
 		if ( cent->nextState.solid && ent->eType != ET_MISSILE )
 		{
+			cent->contents |= CONTENTS_SOLID;
+
+			// retreive some content flags from the entity type
+			switch ( ent->eType )
+			{
+				case ET_MOVER:
+				case ET_MODELDOOR:
+					cent->contents |= CONTENTS_MOVER;
+					break;
+
+				default:
+					break;
+			}
+
 			cg_solidEntities[ cg_numSolidEntities ] = cent;
 			cg_numSolidEntities++;
 			continue;
@@ -126,6 +142,16 @@ static void CG_ClipMoveToEntities( const vec3_t start, const vec3_t mins,
 		ent = &cent->currentState;
 
 		if ( ent->number == skipNumber )
+		{
+			continue;
+		}
+
+		if ( !( cent->contents & mask ) )
+		{
+			continue;
+		}
+
+		if ( cent->contents & skipmask )
 		{
 			continue;
 		}

src/gamelogic/game/g_cm_world.cpp
@@ -710,6 +710,7 @@ typedef struct
 	trace_t     trace;
 	int         passEntityNum;
 	int         contentmask;
+	int         skipmask;
 	traceType_t collisionType;
 } moveclip_t;
 
@@ -828,6 +829,11 @@ void G_CM_ClipMoveToEntities( moveclip_t *clip )
 			continue;
 		}
 
+		if ( clip->skipmask & touch->r.contents )
+		{
+			continue;
+		}
+
 		// might intersect, so do an exact clip
 		clipHandle = G_CM_ClipHandleForEntity( touch );
 
@@ -899,6 +905,8 @@ void G_CM_Trace( trace_t *results, const vec3_t start, const vec3_t mins2, const
 	memset( &clip, 0, sizeof( moveclip_t ) );
 
 	// clip to world
+	// -------------
+
 	CM_BoxTrace( &clip.trace, start, end, mins, maxs, 0, contentmask, skipmask, type );
 	clip.trace.entityNum = clip.trace.fraction != 1.0 ? ENTITYNUM_WORLD : ENTITYNUM_NONE;
 
@@ -908,7 +916,11 @@ void G_CM_Trace( trace_t *results, const vec3_t start, const vec3_t mins2, const
 		return; // blocked immediately by the world
 	}
 
+	// clip to entities
+	// ----------------
+
 	clip.contentmask = contentmask;
+	clip.skipmask = skipmask;
 	clip.start = start;
 //  VectorCopy( clip.trace.endpos, clip.end );
 	VectorCopy( end, clip.end );

src/gamelogic/game/g_spawn_mover.cpp
@@ -1281,6 +1281,7 @@ void InitMover( gentity_t *ent )
 
 	ent->moverState = MOVER_POS1;
 	ent->s.eType = ET_MOVER;
+	ent->r.contents |= CONTENTS_MOVER;
 	VectorCopy( ent->restingPosition, ent->r.currentOrigin );
 	trap_LinkEntity( ent );
 
@@ -1347,6 +1348,7 @@ void InitRotator( gentity_t *ent )
 
 	ent->moverState = ROTATOR_POS1;
 	ent->s.eType = ET_MOVER;
+	ent->r.contents |= CONTENTS_MOVER;
 	VectorCopy( ent->restingPosition, ent->r.currentAngles );
 	trap_LinkEntity( ent );
 
@@ -1801,6 +1803,7 @@ void SP_func_door_model( gentity_t *self )
 
 	self->moverState = MODEL_POS1;
 	self->s.eType = ET_MODELDOOR;
+	self->r.contents |= CONTENTS_MOVER; // TODO: Check if this is the right thing to add the flag to
 	VectorCopy( self->s.origin, self->s.pos.trBase );
 	self->s.pos.trType = TR_STATIONARY;
 	self->s.pos.trTime = 0;
@@ -2394,6 +2397,9 @@ void SP_func_static( gentity_t *self )
 	VectorCopy( self->s.origin, self->s.pos.trBase );
 	VectorCopy( self->s.origin, self->r.currentOrigin );
 
+	// HACK: Not really a mover, so unset the content flag
+	self->r.contents &= ~CONTENTS_MOVER;
+
 	// check if this func_static has a colorgrading texture
 	if( self->model[0] == '*' &&
 	    G_SpawnString( "gradingTexture", "", &gradingTexture ) ) {
@@ -2593,6 +2599,7 @@ void SP_func_spawn( gentity_t *self )
 {
   //ent->r.svFlags = SVF_USE_CURRENT_ORIGIN;
   self->s.eType = ET_MOVER;
+  self->r.contents |= CONTENTS_MOVER;
   self->moverState = MOVER_POS1;
   VectorCopy( self->s.origin, self->restingPosition );
 
@@ -2662,6 +2669,7 @@ void SP_func_destructable( gentity_t *self )
 
   //ent->r.svFlags = SVF_USE_CURRENT_ORIGIN;
   self->s.eType = ET_MOVER;
+  self->r.contents |= CONTENTS_MOVER;
   self->moverState = MOVER_POS1;
   VectorCopy( self->s.origin, self->restingPosition );
 