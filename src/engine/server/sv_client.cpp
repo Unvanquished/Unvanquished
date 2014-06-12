5cb50800fa7df69e4625c602a38b59cd3b91d906
Corentin Wallez <corentin@wallez.net>
2014-06-12 03:52:19 +0200

    Remove unused (^C^V from Urban Terror?) defines

--- src/engine/server/sv_client.cpp
+++ src/engine/server/sv_client.cpp
@@ -1450,32 +1450,6 @@ Also called by bot code
 ==================
 */
 
-// The value below is how many extra characters we reserve for every instance of '$' in a
-// ut_radio, say, or similar client command.  Some jump maps have very long $location
-// strings.  On these maps, it may be possible to crash the server if a carefully-crafted
-// client command is sent.  The constant below may require further tweaking.  For example,
-// a text of "$location" would have a total computed length of 25, because "$location" has
-// 9 characters, and we increment that by 16 for the '$'.
-#define STRLEN_INCREMENT_PER_DOLLAR_VAR 16
-
-// Don't allow more than this many dollared-strings (e.g. $location) in a client command
-// such as ut_radio and say.  Keep this value low for safety, in case some things like
-// $location expand to very large strings in some maps.  There is really no reason to have
-// more than 6 dollar vars (such as $weapon or $location) in things you tell other people.
-#define MAX_DOLLAR_VARS 6
-
-// When a radio text (as in "ut_radio 1 1 text") is sent, weird things start to happen
-// when the text gets to be greater than 118 in length.  When the text is really large the
-// server will crash.  There is an in-between gray zone above 118, but I don't really want
-// to go there.  This is the maximum length of radio text that can be sent, taking into
-// account increments due to presence of '$'.
-#define MAX_RADIO_STRLEN 118
-
-// Don't allow more than this text length in a command such as say.  I pulled this
-// value out of my ass because I don't really know exactly when problems start to happen.
-// This value takes into account increments due to the presence of '$'.
-#define MAX_SAY_STRLEN 256
-
 void SV_ExecuteClientCommand( client_t *cl, const char *s, qboolean clientOK, qboolean premaprestart )
 {
 	ucmd_t   *u;
