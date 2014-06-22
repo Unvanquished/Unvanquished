Unvanquished/Unvanquished

commit 9c99291caa68225190bf97e56babcafde195c18d

Don't add or remove momentum when the main structure gets built or deconstructed.
For destroying main structures, a fixed value is assumed (20 BP).
For other structures, always use the build point price as the buildable's value.

This fixes #325.

src/gamelogic/game/g_combat.cpp
@@ -213,7 +213,9 @@ void G_RewardAttackers( gentity_t *self )
 	{
 		ownTeam   = (team_t) self->buildableTeam;
 		maxHealth = BG_Buildable( self->s.modelindex )->health;
-		value     = BG_Buildable( self->s.modelindex )->value;
+		value     = BG_IsMainStructure( &self->s )
+		            ? MAIN_STRUCTURE_MOMENTUM_VALUE
+		            : BG_Buildable( self->s.modelindex )->buildPoints;
 
 		// Give partial credits for buildables in construction
 		if ( !self->spawned )
@@ -280,7 +282,7 @@ void G_RewardAttackers( gentity_t *self )
 			G_AddMomentumToScore( player, reward );
 
 			// Add momentum
-			G_AddMomentumForDestroyingStep( self, player, share );
+			G_AddMomentumForDestroyingStep( self, player, reward );
 		}
 		else
 		{
@@ -291,7 +293,7 @@ void G_RewardAttackers( gentity_t *self )
 			G_AddCreditToClient( player->client, ( short )reward, qtrue );
 
 			// Add momentum
-			G_AddMomentumForKillingStep( self, player, share );
+			G_AddMomentumForKillingStep( self, player, reward );
 		}
 	}
 

src/gamelogic/game/g_public.h
@@ -163,8 +163,8 @@ float             G_AddMomentumGenericStep( team_t team, float amount );
 float             G_PredictMomentumForBuilding( gentity_t *buildable );
 float             G_AddMomentumForBuilding( gentity_t *buildable );
 float             G_RemoveMomentumForDecon( gentity_t *buildable, gentity_t *deconner );
-float             G_AddMomentumForKillingStep( gentity_t *victim, gentity_t *attacker, float share );
-float             G_AddMomentumForDestroyingStep( gentity_t *buildable, gentity_t *attacker, float share );
+float             G_AddMomentumForKillingStep( gentity_t *victim, gentity_t *attacker, float amount );
+float             G_AddMomentumForDestroyingStep( gentity_t *buildable, gentity_t *attacker, float amount );
 void              G_AddMomentumEnd( void );
 
 // g_main.c

main/configs/buildables/acid_tube.attr.cfg
@@ -15,9 +15,6 @@ minNormal         0.0
 allowInvertNormal
 needsCreep
 
-// Value
-reward            8
-
 // Vitality
 health            125
 regen             10

main/configs/buildables/arm.attr.cfg
@@ -14,9 +14,6 @@ buildWeapon       human
 minNormal         0.95
 powerConsumption  10
 
-// Value
-reward            10
-
 // Vitality
 health            420
 regen             10

main/configs/buildables/barricade.attr.cfg
@@ -14,9 +14,6 @@ buildWeapon       alien
 minNormal         0.707
 needsCreep
 
-// Value
-reward            8
-
 // Vitality
 health            300
 regen             15

main/configs/buildables/booster.attr.cfg
@@ -14,9 +14,6 @@ buildWeapon       alien
 minNormal         0.707
 needsCreep
 
-// Value
-reward            12
-
 // Vitality
 health            150
 regen             8

main/configs/buildables/drill.attr.cfg
@@ -14,9 +14,6 @@ buildWeapon       human
 minNormal         0.95
 powerConsumption  10
 
-// Value
-reward            4
-
 // Vitality
 health            200
 regen             5

main/configs/buildables/eggpod.attr.cfg
@@ -14,9 +14,6 @@ buildWeapon       alien
 minNormal         0.5
 allowInvertNormal
 
-// Value
-reward            10
-
 // Vitality
 health            250
 regen             8

commit 3063722a87410122ca91262ef7b22c94bef37aae

Fix momentum reward size for killing players.
This was broken in 9c99291.

src/gamelogic/game/g_combat.cpp
@@ -293,7 +293,7 @@ void G_RewardAttackers( gentity_t *self )
 			G_AddCreditToClient( player->client, ( short )reward, qtrue );
 
 			// Add momentum
-			G_AddMomentumForKillingStep( self, player, reward );
+			G_AddMomentumForKillingStep( self, player, share );
 		}
 	}
 

src/gamelogic/game/g_momentum.cpp
@@ -460,8 +460,9 @@ float G_AddMomentumForDestroyingStep( gentity_t *buildable, gentity_t *attacker,
  *
  * G_AddMomentumEnd has to be called after all G_AddMomentum*Step steps are done.
  */
-float G_AddMomentumForKillingStep( gentity_t *victim, gentity_t *attacker, float amount )
+float G_AddMomentumForKillingStep( gentity_t *victim, gentity_t *attacker, float share )
 {
+	float  value;
 	team_t team;
 
 	// sanity check victim
@@ -476,9 +477,10 @@ float G_AddMomentumForKillingStep( gentity_t *victim, gentity_t *attacker, float
 		return 0.0f;
 	}
 
-	team = (team_t) attacker->client->pers.team;
+	value = BG_GetValueOfPlayer( &victim->client->ps ) * MOMENTUM_PER_CREDIT * share;
+	team  = (team_t) attacker->client->pers.team;
 
-	return AddMomentum( CONF_KILLING, team, amount, attacker, qtrue );
+	return AddMomentum( CONF_KILLING, team, value, attacker, qtrue );
 }
 
 /**

src/gamelogic/game/g_public.h
@@ -182,7 +182,7 @@ float             G_AddMomentumGenericStep( team_t team, float amount );
 float             G_PredictMomentumForBuilding( gentity_t *buildable );
 float             G_AddMomentumForBuilding( gentity_t *buildable );
 float             G_RemoveMomentumForDecon( gentity_t *buildable, gentity_t *deconner );
-float             G_AddMomentumForKillingStep( gentity_t *victim, gentity_t *attacker, float amount );
+float             G_AddMomentumForKillingStep( gentity_t *victim, gentity_t *attacker, float share );
 float             G_AddMomentumForDestroyingStep( gentity_t *buildable, gentity_t *attacker, float amount );
 void              G_AddMomentumEnd( void );
 