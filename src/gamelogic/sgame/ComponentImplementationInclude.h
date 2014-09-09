e4275c8345d481ea9fbc5405446c5b1b4529a3cc
Viech <viech@unvanquished.net>
2014-09-09 19:47:22 +0200

    Modify wording of license statements of new files.
    Use "Unvanquished GPL Source" and "Daemon BSD Source" respectively (and the
    former for the new files). Correct the copyrightable years (2014 only so far).

--- src/gamelogic/sgame/ComponentImplementationInclude.h
+++ src/gamelogic/sgame/ComponentImplementationInclude.h
@@ -1,31 +1,31 @@
 /*
 ===========================================================================
 
-Daemon GPL Source Code
-Copyright (C) 2012-2013 Unvanquished Developers
+Unvanquished GPL Source Code
+Copyright (C) 2014 Unvanquished Developers
 
-This file is part of the Daemon GPL Source Code (Daemon Source Code).
+This file is part of the Unvanquished GPL Source Code (Unvanquished Source Code).
 
-Daemon Source Code is free software: you can redistribute it and/or modify
+Unvanquished Source Code is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Daemon Source Code is distributed in the hope that it will be useful,
+Unvanquished Source Code is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Daemon Source Code.  If not, see <http://www.gnu.org/licenses/>.
+along with Unvanquished Source Code.  If not, see <http://www.gnu.org/licenses/>.
 
 ===========================================================================
 */
 
-#ifndef GAME_COMPONENT_IMPLEMENTATION_INCLUDE_H_
-#define GAME_COMPONENT_IMPLEMENTATION_INCLUDE_H_
+#ifndef SGAME_COMPONENT_IMPLEMENTATION_INCLUDE_H_
+#define SGAME_COMPONENT_IMPLEMENTATION_INCLUDE_H_
 
 #include "Components.h"
 #include "IgnitableComponent.h"
 
-#endif //GAME_COMPONENT_IMPLEMENTATION_INCLUDE_H_
+#endif //SGAME_COMPONENT_IMPLEMENTATION_INCLUDE_H_
