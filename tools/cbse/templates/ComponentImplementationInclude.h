64f7ace6809b074f6cba900673a82a48adc72c82
Viech <viech@unvanquished.net>
2014-09-11 22:15:11 +0200

    Add option to generate an include helper for the component implementation headers.

---
+++ tools/cbse/templates/ComponentImplementationInclude.h
@@ -0,0 +1,11 @@
+// THIS FILE IS AUTO GENERATED, EDIT AT YOUR OWN RISK
+//%L
+#ifndef COMPONENT_IMPLEMENTATION_INCLUDE_H_
+#define COMPONENT_IMPLEMENTATION_INCLUDE_H_
+//%L
+{% for component in components %}
+#include "{{component.get_type_name()}}.h"
+{% endfor %}
+//%L
+#endif // COMPONENT_IMPLEMENTATION_INCLUDE_H_
+//%L
