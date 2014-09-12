7e6e521e7a69bc9aad12760089c61160f0af790c
Corentin Wallez <corentin@wallez.net>
2014-09-12 02:12:28 +0200

    Split headers in two to fix a cyclic dependency

--- tools/cbse/templates/Components.h
+++ tools/cbse/templates/Components.h
@@ -164,46 +164,4 @@ class Entity {
     //% L
 {% endfor %}
 
-//% L
-
-#include "ComponentImplementationInclude.h"
-
-//% L
-// Entity definitions
-//% L
-
-{% for entity in entities %}
-
-    //% L
-    // Definition of {{entity.get_type_name()}}
-    //% L
-
-    class {{entity.get_type_name()}}: public Entity {
-            //* The vtables for each entities are statically defined
-            static const MessageHandler messageHandlers[];
-            static const int componentOffsets[];
-
-            //% L
-
-        public:
-            //* Default constructor
-            {{entity.get_type_name()}}(
-                {% for (i, attrib) in enumerate(general.common_entity_attributes) %}
-                    {% if i != 0 %}, {% endif %} {{attrib.get_declaration()}}
-                {% endfor %}
-            );
-            virtual ~{{entity.get_type_name()}}();
-
-            //% L
-
-            //* The components
-            {% for component in entity.get_components() %}
-                {{component.get_type_name()}} {{component.get_variable_name()}};
-            {% endfor %}
-    };
-    //% L
-{% endfor %}
-
-//% LL
-
 #endif //COMPONENTS_H_
