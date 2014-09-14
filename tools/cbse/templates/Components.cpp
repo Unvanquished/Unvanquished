d8302d036e21007045deddf322b2313f34fc091d
Viech <viech@unvanquished.net>
2014-09-14 15:53:05 +0200

    Improve formatting in Components.cpp template file a bit.

--- tools/cbse/templates/Components.cpp
+++ tools/cbse/templates/Components.cpp
@@ -11,8 +11,14 @@
 // /////////// //
 //%L
 // Base entity constructor.
-Entity::Entity(const MessageHandler *messageHandlers, const int* componentOffsets{% for attrib in general.common_entity_attributes %}, {{attrib.get_declaration()}}{% endfor %})
-	: messageHandlers(messageHandlers), componentOffsets(componentOffsets){% for attrib in general.common_entity_attributes %}, {{attrib.get_initializer()}}{% endfor %}
+Entity::Entity(const MessageHandler *messageHandlers, const int* componentOffsets
+{%- for attrib in general.common_entity_attributes -%}
+	, {{attrib.get_declaration()}}
+{%- endfor -%})
+	: messageHandlers(messageHandlers), componentOffsets(componentOffsets)
+{%- for attrib in general.common_entity_attributes -%}
+	, {{attrib.get_initializer()}}
+{%- endfor %}
 {}
 //%L
 // Base entity deconstructor.
@@ -35,12 +41,12 @@ bool Entity::SendMessage(int msg, const void* data) {
 //%L
 {% for message in messages %}
 bool Entity::{{message.name}}({{message.get_function_args()}}) {
-	{% if message.get_num_args() == 0 %}
+{% if message.get_num_args() == 0 %}
 	return SendMessage({{message.get_enum_name()}}, nullptr);
-	{% else %}
+{% else %}
 	{{message.get_tuple_type()}} data({{message.get_args_names()}});
 	return SendMessage({{message.get_enum_name()}}, &data);
-	{% endif %}
+{% endif %}
 }
 //%L
 {% endfor %}
@@ -52,13 +58,13 @@ bool Entity::{{message.name}}({{message.get_function_args()}}) {
 // {{entity.get_type_name()}}'s component offset vtable.
 //* TODO: doesn't handle component inheritance?
 const int {{entity.get_type_name()}}::componentOffsets[] = {
-	{% for component in components %}
-	{% if component in entity.get_components() %}
+{% for component in components %}
+{% if component in entity.get_components() %}
 	myoffsetof({{entity.get_type_name()}}, {{component.get_variable_name()}}),
-	{% else %}
+{% else %}
 	0,
-	{% endif %}
-	{% endfor %}
+{% endif %}
+{% endfor %}
 };
 //%L
 {% for message in entity.get_messages_to_handle() %}
@@ -66,42 +72,55 @@ const int {{entity.get_type_name()}}::componentOffsets[] = {
 void {{entity.get_message_handler_name(message)}}(Entity* _entity, const void* {% if message.get_num_args() > 0 %} _data{% endif %}) {
 	//* Cast the entity to the correct type (receive an Entity*)
 	{{entity.get_type_name()}}* entity = ({{entity.get_type_name()}}*) _entity;
-	//%L
-	{% if message.get_num_args() == 0 %}
+{% if message.get_num_args() == 0 %}
+{% for component in entity.get_components() %}
+{% if message in component.get_messages_to_handle() %}
 	//* No argument for the message, just call the handlers of all the components
-	{% for component in entity.get_components() %}
-	{% if message in component.get_messages_to_handle() %}
 	entity->{{component.get_variable_name()}}.{{message.get_handler_name()}}();
-	{% endif %}
-	{% endfor %}
-	{% else %}
+{% endif %}
+{% endfor %}
+{% else %}
 	//* Cast the message content to the right type (receive a const void*)
 	const {{message.get_tuple_type()}}* data = (const {{message.get_tuple_type()}}*) _data;
-	{% for component in entity.get_components() %}
-	{% if message in component.get_messages_to_handle() %}
+{% for component in entity.get_components() %}
+{% if message in component.get_messages_to_handle() %}
 	entity->{{component.get_variable_name()}}.{{message.get_handler_name()}}({{message.get_unpacked_tuple_args('*data')}});
-	{% endif %}
-	{% endfor %}
-	{% endif %}
+{% endif %}
+{% endfor %}
+{% endif %}
 }
 //%L
 {% endfor%}
 // {{entity.get_type_name()}}'s message dispatcher vtable.
 const MessageHandler {{entity.get_type_name()}}::messageHandlers[] = {
-	{% for message in messages %}
-	{% if message in entity.get_messages_to_handle() %}
+{% for message in messages %}
+{% if message in entity.get_messages_to_handle() %}
 	{{entity.get_message_handler_name(message)}},
-	{% else %}
+{% else %}
 	nullptr,
-	{% endif %}
-	{% endfor%}
+{% endif %}
+{% endfor%}
 };
 //%L
 // {{entity.get_type_name()}}'s constructor.
-{{entity.get_type_name()}}::{{entity.get_type_name()}}({% for (i, attrib) in enumerate(general.common_entity_attributes) %}{% if i != 0 %}, {% endif %} {{attrib.get_declaration()}}{% endfor %})
-	: Entity(messageHandlers, componentOffsets{% for attrib in general.common_entity_attributes %}, {{attrib.get_name()}}{% endfor %})
-	{% for component in entity.get_components() %}
-	, {{component.get_variable_name()}}(*this{% for param in component.get_param_names() %}, {{entity.get_params()[component.name][param]}}{% endfor %}{% for required in component.get_required_components() %}, *{{required.get_variable_name()}}{% endfor %}){% endfor %}
+{{entity.get_type_name()}}::{{entity.get_type_name()}}(
+{%- for (i, attrib) in enumerate(general.common_entity_attributes) -%}
+{%- if i != 0 -%}, {%- endif -%}
+	{{attrib.get_declaration()}}
+{%- endfor -%})
+	: Entity(messageHandlers, componentOffsets
+{%- for attrib in general.common_entity_attributes -%}
+	, {{attrib.get_name()}}
+{%- endfor -%})
+{%- for component in entity.get_components() %}
+	, {{component.get_variable_name()}}(*this
+{%- for param in component.get_param_names() -%}
+	, {{entity.get_params()[component.name][param]}}
+{%- endfor -%}
+{%- for required in component.get_required_components() -%}
+	, *{{required.get_variable_name()}}
+{%- endfor -%})
+{%- endfor %}
 {}
 //%L
 // {{entity.get_type_name()}}'s deconstructor.
@@ -112,3 +131,4 @@ const MessageHandler {{entity.get_type_name()}}::messageHandlers[] = {
 //%L
 #undef myoffsetof
 
+//* vi:ts=4:ai:filetype=jinja:
