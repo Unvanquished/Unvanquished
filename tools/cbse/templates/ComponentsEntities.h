1f418e13c11a30b53608ba7402df9e007418d727
Corentin Wallez <corentin@wallez.net>
2014-09-18 18:57:15 +0200

    Add back indentation of the templates
    This does not break the indentation of the source files.
    A preprocess step looks at the template block nesting level of each line
    and remove the corresponding indentation, before rendering the template.
    It also removes the need for the //% style commands.
    
    One small problem is that it doesn't handle the case where we have a
    template block opening and closing on the same line. (there are none for
    now)

--- tools/cbse/templates/ComponentsEntities.h
+++ tools/cbse/templates/ComponentsEntities.h
@@ -1,53 +1,41 @@
 // THIS FILE IS AUTO GENERATED, EDIT AT YOUR OWN RISK
 
-//% L
-
 #ifndef COMPONENTS_ENTITIES_H_
 #define COMPONENTS_ENTITIES_H_
-
-//% L
 
 #include "{{files['declaration']}}"
 #include "{{files['includehelper']}}"
 
-//% L
 // Entity definitions
-//% L
 
 {% for entity in entities %}
 
-    //% L
     // Definition of {{entity.get_type_name()}}
-    //% L
 
     class {{entity.get_type_name()}}: public Entity {
+        private:
             //* The vtables for each entities are statically defined
             static const MessageHandler messageHandlers[];
             static const int componentOffsets[];
 
-            //% L
-
         public:
             //* Default constructor
             {{entity.get_type_name()}}(
-                {% for (i, attrib) in enumerate(general.common_entity_attributes) %}
-                    {% if i != 0 %}, {% endif %} {{attrib.get_declaration()}}
-                {% endfor %}
+                {%- for (i, attrib) in enumerate(general.common_entity_attributes) -%}
+                    {%- if i != 0 -%}
+                        ,
+                    {%- endif -%}
+                    {{attrib.get_declaration()}}
+                {%- endfor -%}
             );
             virtual ~{{entity.get_type_name()}}();
-
-            //% L
 
             //* The components
             {% for component in entity.get_components() %}
                 {{component.get_type_name()}} {{component.get_variable_name()}};
             {% endfor %}
     };
-    //% L
+
 {% endfor %}
 
-//% LL
-
 #endif // COMPONENTS_ENTITIES_H_
-
-//% L
