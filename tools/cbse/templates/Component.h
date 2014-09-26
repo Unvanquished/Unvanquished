b0219fb69ea34efc53a2dd6786501402e3ae8d89
Viech <viech@unvanquished.net>
2014-09-26 22:07:04 +0200

    Make components know each other by default.

--- tools/cbse/templates/Component.h
+++ tools/cbse/templates/Component.h
@@ -4,7 +4,10 @@
 #ifndef {{component.get_name().upper()}}_COMPONENT_H_
 #define {{component.get_name().upper()}}_COMPONENT_H_
 
+//* Make sure the backend is included, even though this might have happened before.
 #include "../{{files['backend']}}"
+//* Make components know each other.
+#include "../{{files['components']}}"
 
 class {{component.get_type_name()}}: public {{component.get_base_type_name()}} {
 //* TODO: Uncomment after changing message handler visibility to protected.
