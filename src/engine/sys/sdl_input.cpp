7eeb8e4af96eae75a730cdba0307e559102fb4e4
Corentin Wallez <corentin@wallez.net>
2014-12-08 06:26:32 +0100

    Fix commit removing SDL1, SDL2 was always true and removes the cls.glconfig.isfullscreen, thanks ZTM!

--- src/engine/sys/sdl_input.cpp
+++ src/engine/sys/sdl_input.cpp
@@ -1554,12 +1554,12 @@ void IN_Frame( void )
 	// If not DISCONNECTED (main menu) or ACTIVE (in game), we're loading
 	loading = ( cls.state != CA_DISCONNECTED && cls.state != CA_ACTIVE );
 
-	if ( !cls.glconfig.isFullscreen && cls.keyCatchers & KEYCATCH_CONSOLE )
+	if ( cls.keyCatchers & KEYCATCH_CONSOLE )
 	{
 		// Console is down in windowed mode
 		IN_DeactivateMouse( qfalse );
 	}
-	else if ( !cls.glconfig.isFullscreen && loading )
+	else if ( loading )
 	{
 		// Loading in windowed mode
 		IN_DeactivateMouse( qtrue );
