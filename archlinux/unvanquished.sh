ffb8b440188bbe6483e470967f97eab04a445eb5
Viech <viech@unvanquished.net>
2015-03-03 19:48:59 +0100

    Archlinux: Don't handle unv:// URLs in starter script.

--- archlinux/unvanquished.sh
+++ archlinux/unvanquished.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
 # launcher script for unvanquished client
-# handles unv:// URI's as first parameter securely
 
-if echo "$1" | grep -q '^unv://'; then
-	uri=`echo "$1" | grep -o '^unv://[^[:space:]+;]*'`
-
-	if [ -p ~/.Unvanquished/main/pipe ]; then
-		echo "connect $uri" > ~/.Unvanquished/main/pipe
-	else
-		"$0" +connect $uri
-	fi
-else
-	exec /usr/lib/unvanquished/daemon -libpath /usr/lib/unvanquished -pakpath /usr/share/unvanquished/pkg "$@"
-fi
+exec /usr/lib/unvanquished/daemon -libpath /usr/lib/unvanquished -pakpath /usr/share/unvanquished/pkg "$@"
