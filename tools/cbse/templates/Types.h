edb21f65b2e43dc8d16b842e56ca0e08cbf64385
Viech <viech@unvanquished.net>
2015-04-08 20:12:01 +0200

    Add missing template.

---
+++ tools/cbse/templates/Types.h
@@ -0,0 +1,14 @@
+//* TODO: Add license from input file.
+// TODO: Insert license statement.
+
+#ifndef COMPONENTS_TYPES_H_
+#define COMPONENTS_TYPES_H_
+
+// TODO: Add definitions, forward declarations and includes that provide all
+//       the types used in the entities definition file (and thus the CBSE
+//       backend). Make sure none of the includes in this file includes any
+//       header that is part of the CBSE system.
+
+#endif // COMPONENTS_TYPES_H_
+
+//* vi:ai:ts=4
