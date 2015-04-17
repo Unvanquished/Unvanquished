Unvanquished/Unvanquished

commit 8ef53654efbf8b0b3572f033a156ca3ff5365383

Replace G_Heal and every direct access to gentity_t.health on clients and buildables.
This also fixes #614.

src/gamelogic/sgame/sg_active.cpp
@@ -84,7 +84,9 @@ void P_DamageFeedback( gentity_t *player )
 	if ( ( level.time > player->pain_debounce_time ) && !( player->flags & FL_GODMODE ) )
 	{
 		player->pain_debounce_time = level.time + 700;
-		G_AddEvent( player, EV_PAIN, player->health > 255 ? 255 : player->health );
+		int transmittedHalth = (int)std::ceil(player->entity->Get<HealthComponent>()->Health());
+		transmittedHalth = Math::Clamp(transmittedHalth, 0, 255);
+		G_AddEvent( player, EV_PAIN, transmittedHalth );
 		client->ps.damageEvent++;
 	}
 
@@ -126,7 +128,7 @@ void P_WorldEffects( gentity_t *ent )
 			// drown!
 			ent->client->airOutTime += 1000;
 
-			if ( ent->health > 0 )
+			if ( G_Alive( ent ) )
 			{
 				// take more damage the longer underwater
 				ent->damage += 2;
@@ -137,7 +139,7 @@ void P_WorldEffects( gentity_t *ent )
 				}
 
 				// play a gurp sound instead of a general pain sound
-				if ( ent->health <= ent->damage )
+				if ( ent->entity->Get<HealthComponent>()->Health() <= (float)ent->damage )
 				{
 					G_Sound( ent, CHAN_VOICE, G_SoundIndex( "*drown.wav" ) );
 				}
@@ -170,8 +172,7 @@ void P_WorldEffects( gentity_t *ent )
 	if ( waterlevel &&
 	     ( ent->watertype & ( CONTENTS_LAVA | CONTENTS_SLIME ) ) )
 	{
-		if ( ent->health > 0 &&
-		     ent->pain_debounce_time <= level.time )
+		if ( G_Alive( ent ) && ent->pain_debounce_time <= level.time )
 		{
 			if ( ent->watertype & CONTENTS_LAVA )
 			{
@@ -722,6 +723,7 @@ bool ClientInactivityTimer( gentity_t *ent, bool active )
 	return true;
 }
 
+// TODO: Move to MedikitComponent.
 static void G_ReplenishHumanHealth( gentity_t *self )
 {
 	gclient_t *client;
@@ -746,7 +748,7 @@ static void G_ReplenishHumanHealth( gentity_t *self )
 	}
 
 	// stop if client is fully healed
-	if ( self->health >= client->ps.stats[ STAT_MAX_HEALTH ] )
+	if ( self->entity->Get<HealthComponent>()->FullHealth() )
 	{
 		client->medKitHealthToRestore = 0;
 		client->ps.stats[ STAT_STATE ] &= ~SS_HEALING_4X;
@@ -779,7 +781,8 @@ static void G_ReplenishHumanHealth( gentity_t *self )
 	}
 
 	// heal
-	client->medKitHealthToRestore -= G_Heal( self, 1 );
+	self->entity->Heal(1.0f, nullptr);
+	client->medKitHealthToRestore --;
 }
 
 static void BeaconAutoTag( gentity_t *self, int timePassed )
@@ -1606,7 +1609,7 @@ static int FindAlienHealthSource( gentity_t *self )
 	for ( ent = nullptr; ( ent = G_IterateEntities( ent, nullptr, true, 0, nullptr ) ); )
 	{
 		if ( !G_OnSameTeam( self, ent ) ) continue;
-		if ( ent->health <= 0 )           continue;
+		if ( G_Dead( ent ) )              continue;
 
 		distance = Distance( ent->s.origin, self->s.origin );
 
@@ -1669,6 +1672,7 @@ static int FindAlienHealthSource( gentity_t *self )
 }
 
 // TODO: Synchronize
+// TODO: Move to HealthRegenComponent.
 static void G_ReplenishAlienHealth( gentity_t *self )
 {
 	gclient_t *client;
@@ -1679,8 +1683,8 @@ static void G_ReplenishAlienHealth( gentity_t *self )
 	client = self->client;
 
 	// Check if client is an alien and has the healing ability
-	if ( !client || client->pers.team != TEAM_ALIENS ||
-	     self->health <= 0 || level.surrenderTeam == client->pers.team )
+	if ( !client || client->pers.team != TEAM_ALIENS || G_Dead( self )
+	     || level.surrenderTeam == client->pers.team )
 	{
 		return;
 	}
@@ -1732,7 +1736,7 @@ static void G_ReplenishAlienHealth( gentity_t *self )
 		// If recovery interval is less than frametime, compensate by healing more
 		count = 1 + ( level.time - self->nextRegenTime ) / interval;
 
-		G_Heal( self, count );
+		self->entity->Heal((float)count, nullptr);
 
 		self->nextRegenTime = level.time + count * interval;
 	}
@@ -2225,9 +2229,7 @@ void ClientThink_real( gentity_t *self )
 
 	if ( self->suicideTime > 0 && self->suicideTime < level.time )
 	{
-		client->ps.stats[ STAT_HEALTH ] = self->health = 0;
-		G_PlayerDie( self, self, self, MOD_SUICIDE );
-
+		self->entity->Kill(nullptr, MOD_SUICIDE);
 		self->suicideTime = 0;
 	}
 }
@@ -2349,14 +2351,8 @@ void ClientEndFrame( gentity_t *ent )
 		ent->client->ps.eFlags &= ~EF_CONNECTION;
 	}
 
-	if( ent->client->ps.stats[ STAT_HEALTH ] != ent->health )
-	{
-		ent->client->ps.stats[ STAT_HEALTH ] = ent->health; // FIXME: get rid of ent->health...
-		ent->client->pers.infoChangeTime = level.time;
-	}
-
 	// respawn if dead
-	if ( ent->client->ps.stats[ STAT_HEALTH ] <= 0 && level.time >= ent->client->respawnTime )
+	if ( G_Dead( ent ) && level.time >= ent->client->respawnTime )
 	{
 		respawn( ent );
 	}

src/gamelogic/sgame/sg_main.cpp
@@ -1410,7 +1410,7 @@ void G_CountSpawns()
 
 	for ( i = MAX_CLIENTS, ent = g_entities + i; i < level.num_entities; i++, ent++ )
 	{
-		if ( !ent->inuse || ent->s.eType != ET_BUILDABLE || ent->health <= 0 )
+		if ( !ent->inuse || ent->s.eType != ET_BUILDABLE || G_Dead( ent ) )
 		{
 			continue;
 			// is it really useful? Seriously?
@@ -1730,7 +1730,7 @@ void BeginIntermission()
 		}
 
 		// respawn if dead
-		if ( client->health <= 0 )
+		if ( G_Dead( client ) )
 		{
 			respawn( client );
 		}

src/gamelogic/sgame/sg_public.h
@@ -317,13 +317,13 @@ bool          G_LineOfSight( const gentity_t *from, const gentity_t *to, int mas
 bool          G_LineOfSight( const gentity_t *from, const gentity_t *to );
 bool          G_LineOfFire( const gentity_t *from, const gentity_t *to );
 bool          G_LineOfSight( const vec3_t point1, const vec3_t point2 );
-int               G_Heal( gentity_t *self, int amount );
 bool              G_IsPlayableTeam( team_t team );
 bool              G_IsPlayableTeam( int team );
 team_t            G_IterateTeams( team_t team );
 team_t            G_Enemy( team_t team );
 float             G_Distance( gentity_t *ent1, gentity_t *ent2 );
-bool              G_Alive( gentity_t *ent );
+bool              G_Alive(gentity_t *ent);
+bool              G_Dead(gentity_t *ent);
 
 // sg_weapon.c
 void              G_ForceWeaponChange( gentity_t *ent, weapon_t weapon );

src/gamelogic/sgame/Beacon.cpp
@@ -25,6 +25,7 @@ along with Daemon.  If not, see <http://www.gnu.org/licenses/>.
 // handle the server-side beacon-related stuff
 
 #include "sg_local.h"
+#include "CBSE.h"
 
 // entityState_t   | cbeacon_t    | description
 // ----------------+--------------+-------------
@@ -559,7 +560,7 @@ namespace Beacon //this should eventually become a class
 		switch( ent->s.eType )
 		{
 			case ET_BUILDABLE:
-				if( ent->health <= 0 )
+				if( G_Dead( ent ) )
 					return false;
 				if( ent->buildableTeam == team )
 					return false;
@@ -693,7 +694,7 @@ namespace Beacon //this should eventually become a class
 			case ET_BUILDABLE:
 				targetTeam = ent->buildableTeam;
 				data       = ent->s.modelindex;
-				dead       = ( ent->health <= 0 );
+				dead       = G_Dead( ent );
 				player     = false;
 				BG_BuildableBoundingBox( ent->s.modelindex, mins, maxs );
 				break;

src/gamelogic/sgame/components/ArmorComponent.h
@@ -33,13 +33,14 @@ class ArmorComponent: public ArmorComponentBase {
 		// Handwritten Members //
 		// /////////////////// //
 
+		float GetLocationalDamageMod(float angle, float height);
+		float GetNonLocationalDamageMod();
+
 	private:
 		// /////////////////// //
 		// Handwritten Members //
 		// /////////////////// //
 
-		float GetLocationalDamageMod(float angle, float height);
-		float GetNonLocationalDamageMod();
 };
 
 #endif // ARMOR_COMPONENT_H_

src/gamelogic/sgame/components/HealthComponent.cpp
@@ -7,6 +7,7 @@ HealthComponent::HealthComponent(Entity& entity, float maxHealth)
 	: HealthComponentBase(entity, maxHealth), health(maxHealth)
 {}
 
+// TODO: Handle rewards array.
 HealthComponent& HealthComponent::operator=(const HealthComponent& other) {
 	health = (other.health / other.maxHealth) * maxHealth;
 	return *this;
@@ -14,55 +15,29 @@ HealthComponent& HealthComponent::operator=(const HealthComponent& other) {
 
 void HealthComponent::HandlePrepareNetCode() {
 	int transmittedHealth = Math::Clamp((int)std::ceil(health), -999, 999);
+	gclient_t *client = entity.oldEnt->client;
 
-	if (entity.oldEnt->client) {
-		entity.oldEnt->client->ps.stats[STAT_HEALTH] = transmittedHealth;
+	if (client) {
+		if (client->ps.stats[STAT_HEALTH] != transmittedHealth) {
+			client->pers.infoChangeTime = level.time;
+		}
+		client->ps.stats[STAT_HEALTH] = transmittedHealth;
+		client->ps.stats[STAT_MAX_HEALTH] = (int)std::ceil(maxHealth);
 	} else if (entity.oldEnt->s.eType == ET_BUILDABLE) {
-		entity.oldEnt->s.generic1 = transmittedHealth;
+		entity.oldEnt->s.generic1 = std::max(transmittedHealth, 0);
 	}
 }
 
-// TODO: Move damage account array to HealthComponent.
 void HealthComponent::HandleHeal(float amount, gentity_t* source) {
 	if (amount <= 0.0f) return;
 	if (health <= 0.0f) return;
-	if (health == maxHealth) return;
+	if (health >= maxHealth) return;
 
-	// Get total damage account and remember relevant clients.
-	float totalCredits = 0.0f;
-	std::vector<int> relevantClients;
-	for (int clientNum = 0; clientNum < MAX_CLIENTS; clientNum++) {
-		if (entity.oldEnt->credits[clientNum].value > 0.0f) {
-			relevantClients.push_back(clientNum);
-			totalCredits += entity.oldEnt->credits[clientNum].value;
-		}
-	}
+	// Only heal up to maximum health.
+	amount = std::min(amount, maxHealth - health);
 
-	// Calculate account scale factor.
-	float scaleAccounts;
-	if (amount < totalCredits) {
-		scaleAccounts = (totalCredits - amount) / totalCredits;
-	} else {
-		// Clear the account array.
-		scaleAccounts = 0.0f;
-	}
-
-	// Scale down or clear damage accounts.
-	for (int clientNum : relevantClients) {
-		entity.oldEnt->credits[clientNum].value *= scaleAccounts;
-	}
-
-	health = std::min(health + amount, maxHealth);
-}
-
-// TODO: Replace this with a call to a proper event factory.
-static void SpawnHitNotification(gentity_t *attacker)
-{
-	if (!attacker->client) return;
-
-	gentity_t *event = G_NewTempEntity(attacker->s.origin, EV_HIT);
-	event->r.svFlags = SVF_SINGLECLIENT;
-	event->r.singleClient = attacker->client->ps.clientNum;
+	health += amount;
+	ScaleDamageAccounts(amount);
 }
 
 #define DAMAGE_TO_KNOCKBACK 5
@@ -248,15 +223,64 @@ void HealthComponent::HandleKill(gentity_t* source, meansOfDeath_t meansOfDeath)
 	              meansOfDeath);
 }
 
+void HealthComponent::SetHealth(float health) {
+	Math::Clamp(health, FLT_EPSILON, maxHealth);
 
-void HealthComponent::SetMaxHealth(float maxHealth) {
+	healthLogger.Debug("Changing health: %3.1f → %3.1f.", HealthComponent::health, health);
+
+	ScaleDamageAccounts(health - HealthComponent::health);
+	HealthComponent::health = health;
+}
+
+void HealthComponent::SetMaxHealth(float maxHealth, bool scaleHealth) {
 	assert(maxHealth > 0.0f);
 
-	float scale = HealthComponent::maxHealth / maxHealth;
-
-	healthLogger.Debug("Changing maximum health: %3.1f → %3.1f, current health: %3.1f → %3.1f.",
-	                   HealthComponent::maxHealth, maxHealth, health, health * scale);
+	healthLogger.Debug("Changing maximum health: %3.1f → %3.1f.", HealthComponent::maxHealth, maxHealth);
 
 	HealthComponent::maxHealth = maxHealth;
-	health *= scale;
+	if (scaleHealth) SetHealth(health * (HealthComponent::maxHealth / maxHealth));
+}
+
+// TODO: Move credits array to HealthComponent.
+void HealthComponent::ScaleDamageAccounts(float healthRestored) {
+	if (healthRestored <= 0.0f) return;
+
+	// Get total damage account and remember relevant clients.
+	float totalAccreditedDamage = 0.0f;
+	std::vector<Entity*> relevantClients;
+	ForEntities<ClientComponent>([&](Entity& other, ClientComponent& client) {
+		float clientDamage = entity.oldEnt->credits[other.oldEnt->s.number].value;
+		if (clientDamage > 0.0f) {
+			totalAccreditedDamage += clientDamage;
+			relevantClients.push_back(&other);
+		}
+	});
+
+	// Calculate account scale factor.
+	float scale;
+	if (healthRestored < totalAccreditedDamage) {
+		scale = (totalAccreditedDamage - healthRestored) / totalAccreditedDamage;
+
+		//healthLogger.Debug("Scaling damage accounts by %.2f.", scale);
+	} else {
+		// Clear all accounts.
+		scale = 0.0f;
+
+		healthLogger.Debug("Clearing damage accounts.");
+	}
+
+	// Scale down or clear damage accounts.
+	for (Entity* other : relevantClients) {
+		entity.oldEnt->credits[other->oldEnt->s.number].value *= scale;
+	}
+}
+
+// TODO: Replace this with a call to a proper event factory.
+void HealthComponent::SpawnHitNotification(gentity_t *attacker)
+{
+	if (!attacker->client) return;
+
+	gentity_t *event = G_NewTempEntity(attacker->s.origin, EV_HIT);
+	event->r.svFlags = SVF_SINGLECLIENT;
+	event->r.singleClient = attacker->client->ps.clientNum;
 }

src/gamelogic/sgame/components/HealthComponent.h
@@ -57,7 +57,8 @@ class HealthComponent: public HealthComponentBase {
 		// Handwritten Members //
 		// /////////////////// //
 
-		void SetMaxHealth(float maxHealth);
+		void SetHealth(float health);
+		void SetMaxHealth(float maxHealth, bool scaleHealth = false);
 
 		/**
 		 * @brief Copies state from another HealthComponent.
@@ -65,13 +66,26 @@ class HealthComponent: public HealthComponentBase {
 		 */
 		HealthComponent& operator=(const HealthComponent& other);
 
+		bool Alive() const { return health > 0.0f; }
+		bool FullHealth() const { return health >= maxHealth; }
+		float Health() const { return health; }
+		float MaxHealth() const { return maxHealth; }
+		float HealthFraction() const { return Math::Clamp(health / maxHealth, 0.0f, 1.0f); }
+
 	private:
 		// /////////////////// //
 		// Handwritten Members //
 		// /////////////////// //
 
 		float health;
+
+		/**
+		 * @brief Whenever health is restored, this makes sure the damage accounts are scaled down
+		 *        so that they can't exceed the missing health.
+		 */
+		void ScaleDamageAccounts(float healthRestored);
+
+		void SpawnHitNotification(gentity_t *attacker);
 };
 
 #endif // HEALTH_COMPONENT_H_
-

src/gamelogic/sgame/sg_bot.cpp
@@ -22,6 +22,7 @@
 
 #include "sg_bot_parse.h"
 #include "sg_bot_util.h"
+#include "CBSE.h"
 
 static botMemory_t g_botMind[MAX_CLIENTS];
 static AITreeList_t treeList;
@@ -374,7 +375,8 @@ void G_BotThink( gentity_t *self )
 	BotFindDamagedFriendlyStructure( self );
 
 	//use medkit when hp is low
-	if ( self->health < BOT_USEMEDKIT_HP && BG_InventoryContainsUpgrade( UP_MEDKIT, self->client->ps.stats ) )
+	if ( self->entity->Get<HealthComponent>()->Health() < BOT_USEMEDKIT_HP &&
+	     BG_InventoryContainsUpgrade( UP_MEDKIT, self->client->ps.stats ) )
 	{
 		BG_ActivateUpgrade( UP_MEDKIT, self->client->ps.stats );
 	}

commit 74cafb093657f314d51e60db25ba8615751f2e0c

Export entity netcode in ClientThink_real before Pmove.
This fixes some weird bugs like missing UI and unusable weapons for aliens
introduced in 8ef5365.

src/gamelogic/sgame/sg_active.cpp
@@ -2042,6 +2042,9 @@ void ClientThink_real( gentity_t *self )
 	// moved from after Pmove -- potentially the cause of future triggering bugs
 	G_TouchTriggers( self );
 
+	// Do this before Pmove because it is shared code and accesses networked fields.
+	G_PrepareEntityNetCode();
+
 	Pmove( &pm );
 
 	G_UnlaggedDetectCollisions( self );

src/gamelogic/sgame/sg_main.cpp
@@ -2968,12 +2968,7 @@ void G_RunFrame( int levelTime )
 	G_UpdateZaps( msec );
 	Beacon::Frame( );
 
-	ent = &g_entities[0];
-	for ( i = 0; i < level.num_entities; i++, ent++ ) {
-		if (ent->entity) {
-			ent->entity->PrepareNetCode();
-		}
-	}
+	G_PrepareEntityNetCode();
 
 	// log gameplay statistics
 	G_LogGameplayStats( LOG_GAMEPLAY_STATS_BODY );
@@ -2992,4 +2987,14 @@ void G_RunFrame( int levelTime )
 
 	trap_BotUpdateObstacles();
 	level.frameMsec = trap_Milliseconds();
+}
+
+void G_PrepareEntityNetCode() {
+	// TODO: Allow ForEntities with empty template arguments.
+	gentity_t *oldEnt = &g_entities[0];
+	for (int i = 0; i < level.num_entities; i++, oldEnt++) {
+		if (oldEnt->entity) {
+			oldEnt->entity->PrepareNetCode();
+		}
+	}
 }

src/gamelogic/sgame/sg_public.h
@@ -233,6 +233,7 @@ void              G_ShutdownGame( int restart );
 vmCvar_t          *G_FindCvar( const char *name );
 void              G_CheckPmoveParamChanges();
 void              G_SendClientPmoveParams(int client);
+void              G_PrepareEntityNetCode();
 
 // sg_maprotation.c
 void              G_PrintRotations();