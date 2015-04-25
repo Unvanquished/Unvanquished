32039c76b76b2e92356c817e11b99b9e767cf54f
Matthias Bentrup <matthias.bentrup@googlemail.com>
2015-04-25 16:33:56 +0200

    Pass index -1 for the global colorgrading configstring.

--- src/gamelogic/sgame/sg_spawn.cpp
+++ src/gamelogic/sgame/sg_spawn.cpp
@@ -1055,11 +1055,11 @@ void SP_worldspawn()
 	trap_SetConfigstring( CS_MESSAGE, s );  // map specific message
 
 	if(G_SpawnString( "gradingTexture", "", &s ))
-		trap_SetConfigstring( CS_GRADING_TEXTURES, va( "%i %f %s", 0, 0.0f, s ) );
+		trap_SetConfigstring( CS_GRADING_TEXTURES, va( "%i %f %s", -1, 0.0f, s ) );
 
 	if(G_SpawnString( "colorGrade", "", &s )) {
 		Com_Printf("^3Warning: ^7\"colorGrade\" deprecated. Please use \"gradingTexture\"");
-		trap_SetConfigstring( CS_GRADING_TEXTURES, va( "%i %f %s", 0, 0.0f, s ) );
+		trap_SetConfigstring( CS_GRADING_TEXTURES, va( "%i %f %s", -1, 0.0f, s ) );
 	}
 
 	if(G_SpawnString( "reverbIntensity", "", &s ))
