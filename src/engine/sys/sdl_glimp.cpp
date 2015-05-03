88755cf45491ee91176f9a8c549404f6d89441e9
Corentin Wallez <corentin@wallez.net>
2015-05-03 10:44:13 -0700

    Increase the number of reserved vertex uniforms to avoid software rendering on Mac

--- src/engine/sys/sdl_glimp.cpp
+++ src/engine/sys/sdl_glimp.cpp
@@ -1069,7 +1069,7 @@ static void GLimp_InitExtensions()
 	glGetIntegerv( GL_MAX_VERTEX_UNIFORM_COMPONENTS_ARB, &glConfig2.maxVertexUniforms );
 	glGetIntegerv( GL_MAX_VERTEX_ATTRIBS_ARB, &glConfig2.maxVertexAttribs );
 
-	int reservedComponents = 26 * 10; // approximation how many uniforms we have besides the bone matrices
+	int reservedComponents = 36 * 10; // approximation how many uniforms we have besides the bone matrices
 	glConfig2.maxVertexSkinningBones = Maths::clamp( ( glConfig2.maxVertexUniforms - reservedComponents ) / 16, 0, MAX_BONES );
 	glConfig2.vboVertexSkinningAvailable = r_vboVertexSkinning->integer && ( ( glConfig2.maxVertexSkinningBones >= 12 ) ? true : false );
 
