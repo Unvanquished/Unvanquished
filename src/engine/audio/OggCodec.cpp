6b42bffb257e06c7e2073d205d8c493a2ad2969a
gm <gmisocpp@gmail.com>
2015-06-01 22:15:49 +1200

    Free memory if realloc fails as noticed by Amanieu.

--- src/engine/audio/OggCodec.cpp
+++ src/engine/audio/OggCodec.cpp
@@ -168,9 +168,13 @@ AudioData LoadOggCodec(Str::StringRef filename)
 			else
 				bufferCapacity *= 2;
 			bufferRemaining = bufferCapacity - bufferUsed;
+			char* oldBuffer = buffer;
 			buffer = (char*)realloc(buffer, bufferCapacity);
 			if (!buffer)
+			{
+				free(oldBuffer);
 				throw std::bad_alloc();
+			}
 		}
 		bytesRead = ov_read(vorbisFile.get(), &buffer[bufferUsed], bufferRemaining, 0, sampleWidth, 1, &bitStream);
 		if (bytesRead <= 0)
