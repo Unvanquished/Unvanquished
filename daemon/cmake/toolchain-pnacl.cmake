cd29851284424353e1f66c2651fbfae7288ddc3a
TimePath <andrew.hardaker1995@gmail.com>
2015-08-02 17:01:50 +1000

    Speedup nacl build

--- daemon/cmake/toolchain-pnacl.cmake
+++ daemon/cmake/toolchain-pnacl.cmake
@@ -27,9 +27,9 @@
 set(PLATFORM_PREFIX ${DEPS_DIR}/pnacl/bin)
 set(PLATFORM_TRIPLET "pnacl")
 if (MSVC)
-  set(PNACL_BIN_EXT ".bat")
+    set(PNACL_BIN_EXT ".bat")
 else()
-  set(PNACL_BIN_EXT "")
+    set(PNACL_BIN_EXT "")
 endif()
 set(PLATFORM_EXE_SUFFIX ".pexe")
 
@@ -50,15 +50,24 @@ set(CMAKE_C_USE_RESPONSE_FILE_FOR_INCLUDES 1)
 set(CMAKE_CXX_USE_RESPONSE_FILE_FOR_INCLUDES 1)
 set(CMAKE_C_RESPONSE_FILE_LINK_FLAG "@")
 set(CMAKE_CXX_RESPONSE_FILE_LINK_FLAG "@")
-set(CMAKE_C_CREATE_STATIC_LIBRARY "<CMAKE_AR> rc <TARGET> <LINK_FLAGS> <OBJECTS>")
-set(CMAKE_CXX_CREATE_STATIC_LIBRARY "<CMAKE_AR> rc <TARGET> <LINK_FLAGS> <OBJECTS>")
 
-if (NOT CMAKE_C_COMPILE_OBJECT)
-  set(CMAKE_C_COMPILE_OBJECT "<CMAKE_C_COMPILER> <DEFINES> <FLAGS> -o <OBJECT> -c <SOURCE>")
+if (NOT CMAKE_HOST_WIN32)
+    set(PythonInterp_FIND_VERSION 2)
+    find_package(PythonInterp 2)
+    if (NOT PYTHONINTERP_FOUND)
+        message(FATAL_ERROR "Please set the PNACLPYTHON environment variable to your Python2 executable")
+    endif()
+    set(PNACLPYTHON "PNACLPYTHON=${PYTHON_EXECUTABLE}")
 endif()
-if (NOT CMAKE_CXX_COMPILE_OBJECT)
-  set(CMAKE_CXX_COMPILE_OBJECT "<CMAKE_CXX_COMPILER> <DEFINES> <FLAGS> -o <OBJECT> -c <SOURCE>")
-endif()
+
+set(CMAKE_C_CREATE_STATIC_LIBRARY "env ${PNACLPYTHON} <CMAKE_AR> rc <TARGET> <LINK_FLAGS> <OBJECTS>")
+set(CMAKE_CXX_CREATE_STATIC_LIBRARY "env ${PNACLPYTHON} <CMAKE_AR> rc <TARGET> <LINK_FLAGS> <OBJECTS>")
+
+set(CMAKE_C_COMPILE_OBJECT "env ${PNACLPYTHON} <CMAKE_C_COMPILER> <DEFINES> <FLAGS> -o <OBJECT> -c <SOURCE>")
+set(CMAKE_CXX_COMPILE_OBJECT "env ${PNACLPYTHON} <CMAKE_CXX_COMPILER> <DEFINES> <FLAGS> -o <OBJECT> -c <SOURCE>")
+
+set(CMAKE_C_LINK_EXECUTABLE "env ${PNACLPYTHON} <CMAKE_C_COMPILER> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+set(CMAKE_CXX_LINK_EXECUTABLE "env ${PNACLPYTHON} <CMAKE_CXX_COMPILER> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
@@ -71,56 +80,59 @@ set(CMAKE_C_FLAGS "")
 set(CMAKE_CXX_FLAGS "")
 
 function(pnacl_finalize target)
-  add_custom_command(TARGET ${target} POST_BUILD
-    COMMENT "Finalising ${target}"
-    COMMAND
-      "${PLATFORM_PREFIX}/${PLATFORM_TRIPLET}-finalize${PNACL_BIN_EXT}"
-      "$<TARGET_FILE:${target}>"
-  )
+    add_custom_command(TARGET ${target} POST_BUILD
+        COMMENT "Finalising ${target}"
+        COMMAND
+            env ${PNACLPYTHON}
+            "${PLATFORM_PREFIX}/${PLATFORM_TRIPLET}-finalize${PNACL_BIN_EXT}"
+            "$<TARGET_FILE:${target}>"
+    )
 endfunction()
 
 set(NACL_TRANSLATE_OPTIONS
-  --allow-llvm-bitcode-input # FIXME: finalize as part of the build process
-  --pnacl-allow-exceptions
-  $<$<CONFIG:None>:-O3>
-  $<$<CONFIG:Release>:-O3>
-  $<$<CONFIG:Debug>:-O0>
-  $<$<CONFIG:RelWithDebInfo>:-O2>
-  $<$<CONFIG:MinSizeRel>:-O2>
+    --allow-llvm-bitcode-input # FIXME: finalize as part of the build process
+    --pnacl-allow-exceptions
+    $<$<CONFIG:None>:-O3>
+    $<$<CONFIG:Release>:-O3>
+    $<$<CONFIG:Debug>:-O0>
+    $<$<CONFIG:RelWithDebInfo>:-O2>
+    $<$<CONFIG:MinSizeRel>:-O2>
 )
 
 function(pnacl_translate target arch suffix)
-  cmake_policy(PUSH)
-    if (POLICY CMP0026)
-      cmake_policy(SET CMP0026 OLD)
-    endif()
-    get_target_property(FILE ${target} LOCATION)
-  cmake_policy(POP)
-  get_filename_component(DIRNAME ${FILE} PATH)
-  get_filename_component(BASENAME ${FILE} NAME_WE)
-  add_custom_command(
-    OUTPUT ${DIRNAME}/${BASENAME}${suffix}.nexe
-    COMMENT "Translating ${target} (${arch})"
-    DEPENDS ${FILE}
-    COMMAND
-      "${PLATFORM_PREFIX}/${PLATFORM_TRIPLET}-translate${PNACL_BIN_EXT}"
-      ${NACL_TRANSLATE_OPTIONS}
-      -arch ${arch}
-      ${FILE}
-      -o ${DIRNAME}/${BASENAME}${suffix}.nexe
-  )
-  add_custom_command(
-    OUTPUT ${DIRNAME}/${BASENAME}${suffix}-stripped.nexe
-    COMMENT "Stripping ${target} (${arch})"
-    DEPENDS ${DIRNAME}/${BASENAME}${suffix}.nexe
-    COMMAND
-      "${PLATFORM_PREFIX}/${PLATFORM_TRIPLET}-strip${PNACL_BIN_EXT}"
-      -s
-      ${DIRNAME}/${BASENAME}${suffix}.nexe
-      -o ${DIRNAME}/${BASENAME}${suffix}-stripped.nexe
-  )
-  add_custom_target(${target}${suffix} ALL
-      DEPENDS ${DIRNAME}/${BASENAME}${suffix}-stripped.nexe
-  )
-  add_dependencies(${target}${suffix} ${target})
+    cmake_policy(PUSH)
+        if (POLICY CMP0026)
+            cmake_policy(SET CMP0026 OLD)
+        endif()
+        get_target_property(FILE ${target} LOCATION)
+    cmake_policy(POP)
+    get_filename_component(DIRNAME ${FILE} PATH)
+    get_filename_component(BASENAME ${FILE} NAME_WE)
+    add_custom_command(
+        OUTPUT ${DIRNAME}/${BASENAME}${suffix}.nexe
+        COMMENT "Translating ${target} (${arch})"
+        DEPENDS ${FILE}
+        COMMAND
+            env ${PNACLPYTHON}
+            "${PLATFORM_PREFIX}/${PLATFORM_TRIPLET}-translate${PNACL_BIN_EXT}"
+            ${NACL_TRANSLATE_OPTIONS}
+            -arch ${arch}
+            ${FILE}
+            -o ${DIRNAME}/${BASENAME}${suffix}.nexe
+    )
+    add_custom_command(
+        OUTPUT ${DIRNAME}/${BASENAME}${suffix}-stripped.nexe
+        COMMENT "Stripping ${target} (${arch})"
+        DEPENDS ${DIRNAME}/${BASENAME}${suffix}.nexe
+        COMMAND
+            env ${PNACLPYTHON}
+            "${PLATFORM_PREFIX}/${PLATFORM_TRIPLET}-strip${PNACL_BIN_EXT}"
+            -s
+            ${DIRNAME}/${BASENAME}${suffix}.nexe
+            -o ${DIRNAME}/${BASENAME}${suffix}-stripped.nexe
+    )
+    add_custom_target(${target}${suffix} ALL
+        DEPENDS ${DIRNAME}/${BASENAME}${suffix}-stripped.nexe
+    )
+    add_dependencies(${target}${suffix} ${target})
 endfunction()
