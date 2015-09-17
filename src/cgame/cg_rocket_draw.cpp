9860ce4f60cb285a8970df7ffc8c826df13e29a5
dolcetriade <vcelestialragev@gmail.com>
2015-09-16 22:23:07 -0700

    Make mine delta look better. Convert <predictedMineEfficiency/>

--- src/cgame/cg_rocket_draw.cpp
+++ src/cgame/cg_rocket_draw.cpp
@@ -2075,6 +2075,99 @@ private:
 	Rocket::Core::String owner;
 };
 
+class PredictedMineEfficiencyElement : public HudElement
+{
+public:
+	PredictedMineEfficiencyElement( const Rocket::Core::String& tag ) :
+			HudElement( tag, ELEMENT_BOTH, false ),
+			shouldBeVisible( true ) { }
+
+	void OnPropertyChange( const Rocket::Core::PropertyNameList& changed_properties )
+	{
+		HudElement::OnPropertyChange( changed_properties );
+		if ( display.Empty() && changed_properties.find( "display" ) != changed_properties.end() )
+		{
+			display = GetProperty<Rocket::Core::String>( "display" );
+		}
+	}
+
+	void DoOnUpdate()
+	{
+		playerState_t  *ps = &cg.snap->ps;
+		buildable_t   buildable = ( buildable_t )( ps->stats[ STAT_BUILDABLE ] & SB_BUILDABLE_MASK );
+
+		// If display hasn't been set yet explicitly, assume display is block
+		if ( display.Empty() )
+		{
+			display = "block";
+		}
+
+		if ( buildable != BA_H_DRILL && buildable != BA_A_LEECH )
+		{
+			if ( IsVisible() && shouldBeVisible )
+			{
+				SetProperty( "display", "none" );
+				SetInnerRML( "" );
+				shouldBeVisible = false;
+				// Pick impossible value
+				lastDelta = -999;
+			}
+		}
+		else
+		{
+			if ( !IsVisible() && !shouldBeVisible )
+			{
+				SetProperty( "display", display );
+				shouldBeVisible = true;
+			}
+		}
+	}
+
+	void DoOnRender()
+	{
+		if ( shouldBeVisible )
+		{
+			playerState_t  *ps = &cg.snap->ps;
+			buildable_t   buildable = ( buildable_t )( ps->stats[ STAT_BUILDABLE ] & SB_BUILDABLE_MASK );
+			const char *msg = nullptr;
+			char color;
+			int  delta = ps->stats[ STAT_PREDICTION ];
+
+			if ( lastDelta != delta )
+			{
+				if ( delta < 0 )
+				{
+					color = COLOR_RED;
+					// Error sign
+					msg = va( "<span class='material-icon error'>&#xE000;</span> You are losing efficiency. Build the %ss further apart for more efficiency.", BG_Buildable( buildable )->humanName );
+				}
+				else if ( delta < 10 )
+				{
+					color = COLOR_ORANGE;
+					// Warning sign
+					msg = va( "<span class='material-icon warning'>&#xE002;</span> Minimal efficency gain. Build the %ss further apart for more efficiency.", BG_Buildable( buildable )->humanName );
+				}
+				else if ( delta < 50 )
+				{
+					color = COLOR_YELLOW;
+					msg = va( "<span class='material-icon warning'>&#xE002;</span> Average efficency gain. Build the %ss further apart for more efficiency.", BG_Buildable( buildable )->humanName );
+				}
+				else
+				{
+					color = COLOR_GREEN;
+				}
+
+				SetInnerRML( va("EFFICIENCY: %s%s%s", CG_Rocket_QuakeToRML( va( "^%c%+d%%", color, delta ) ), msg ? "<br/>" : "", msg ? msg : "" ) );
+				lastDelta = delta;
+			}
+		}
+	}
+private:
+	bool shouldBeVisible;
+	Rocket::Core::String display;
+	int lastDelta;
+};
+
 void CG_Rocket_DrawPlayerHealth()
 {
 	static int lastHealth = 0;
@@ -3168,48 +3261,6 @@ static void CG_Rocket_DrawNumSpawns()
 	Rocket_SetInnerRML( s, 0 );
 }
 
-void CG_Rocket_DrawPredictedRGSRate()
-{
-	playerState_t  *ps = &cg.snap->ps;
-	buildable_t   buildable = ( buildable_t )( ps->stats[ STAT_BUILDABLE ] & SB_BUILDABLE_MASK );
-	const char *msg = nullptr;
-	char color;
-	int  delta = ps->stats[ STAT_PREDICTION ];
-
-	if ( buildable != BA_H_DRILL && buildable != BA_A_LEECH )
-	{
-		Rocket_SetInnerRML( "", 0 );
-		return;
-	}
-
-	if ( delta < 0 )
-	{
-		color = COLOR_RED;
-		// Error sign
-		msg = va( "<span class='material-icon error'>&#xE000;</span> You are losing efficiency. Build the %ss further apart for more efficiency.", BG_Buildable( buildable )->humanName );
-	}
-
-	else if ( delta < 10 )
-	{
-		color = COLOR_ORANGE;
-		// Warning sign
-		msg = va( "<span class='material-icon warning'>&#xE002;</span> Minimal efficency gain. Build the %ss further apart for more efficiency.", BG_Buildable( buildable )->humanName );
-	}
-
-	else if ( delta < 50 )
-	{
-		color = COLOR_YELLOW;
-		msg = va( "<span class='material-icon warning'>&#xE002;</span> Average efficency gain. Build the %ss further apart for more efficiency.", BG_Buildable( buildable )->humanName );
-	}
-
-	else
-	{
-		color = COLOR_GREEN;
-	}
-
-	Rocket_SetInnerRML( va("%s%s%s", msg ? msg : "", msg ? "<br/>" : "", CG_Rocket_QuakeToRML( va( "^%c%+d%%", color, delta ) ) ), 0 );
-}
-
 static void CG_Rocket_DrawWarmup()
 {
 	int   sec = 0;
@@ -3413,7 +3464,6 @@ static const elementRenderCmd_t elementRenderCmdList[] =
 	{ "momentum_bar", &CG_Rocket_DrawPlayerMomentumBar, ELEMENT_BOTH },
 	{ "motd", &CG_Rocket_DrawMOTD, ELEMENT_ALL },
 	{ "numSpawns", &CG_Rocket_DrawNumSpawns, ELEMENT_DEAD },
-	{ "predictedMineEfficiency", &CG_Rocket_DrawPredictedRGSRate, ELEMENT_BOTH },
 	{ "progress_value", &CG_Rocket_DrawProgressValue, ELEMENT_ALL },
 	{ "spawnPos", &CG_Rocket_DrawSpawnQueuePosition, ELEMENT_DEAD },
 	{ "stamina_bolt", &CG_Rocket_DrawStaminaBolt, ELEMENT_HUMANS },
@@ -3483,4 +3533,5 @@ void CG_Rocket_RegisterElements()
 	REGISTER_ELEMENT( "beacon_info", BeaconInfoElement )
 	REGISTER_ELEMENT( "beacon_name", BeaconNameElement )
 	REGISTER_ELEMENT( "beacon_owner", BeaconOwnerElement )
+	REGISTER_ELEMENT( "predictedMineEfficiency", PredictedMineEfficiencyElement )
 }
