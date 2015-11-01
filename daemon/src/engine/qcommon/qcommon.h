cf547dff027ae154ef15c2c810c439d67e7859e4
Corentin Wallez <corentin@wallez.net>
2015-11-01 22:10:41 +0100

    Reorder clc

--- daemon/src/engine/qcommon/qcommon.h
+++ daemon/src/engine/qcommon/qcommon.h
@@ -332,8 +332,8 @@ enum clc_ops_e
   clc_move, // [usercmd_t]
   clc_moveNoDelta, // [usercmd_t]
   clc_clientCommand, // [string] message
-  clc_EOF,
   clc_voip, // not wrapped in USE_VOIP, so this value is reserved.
+  clc_EOF,
 };
 
 /*
