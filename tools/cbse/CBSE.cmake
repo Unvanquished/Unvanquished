187b9a0fce65b43a0cc51ab323efeccc2c61cb80
dolcetriade <vcelestialragev@gmail.com>
2015-11-15 13:44:33 -0800

    Update CBSE readme instructions on CMake dep install instructions

--- tools/cbse/CBSE.cmake
+++ tools/cbse/CBSE.cmake
@@ -52,7 +52,8 @@ function(CBSE target definition output)
         COMMAND ${PYTHON_EXECUTABLE} -c "import jinja2, yaml, collections, argparse, sys, os.path, re"
         RESULT_VARIABLE RET)
     if (NOT RET EQUAL 0)
-        message(FATAL_ERROR "Missing dependences for CBSE generation. Use pip -r install src/utils/cbse/requirements.txt to install")
+        message(FATAL_ERROR "Missing dependences for CBSE generation. Please ensure you have python ≥ 2, python-yaml, and python-jinja installed.
+                             Use pip -r install src/utils/cbse/requirements.txt to install")
     endif()
     set(GENERATED_CBSE ${output}/backend/CBSEBackend.cpp
                        ${output}/backend/CBSEBackend.h
