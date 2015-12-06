5adf5da42b549e14ea2e6c8de13f2b7f3765dd84
dolcetriade <vcelestialragev@gmail.com>
2015-12-06 00:23:34 -0800

    Version bump to Alpha 46

--- download-pk3.sh
+++ download-pk3.sh
@@ -16,7 +16,7 @@ set -e
 export LANG=C
 
 # Version of Unvanquished for which this script is built
-VERSION=0.45
+VERSION=0.46
 
 # Download from here
 CDN_URL="http://cdn.unvanquished.net/$VERSION/pkg/"
