283c18884df61350c0e380bd9c518c5401ebc558
perturbed <dog964@xemaps.com>
2015-12-29 05:29:00 -0600

    Hack to stop undefined symbol error when using SDL2 + VS2015

---
+++ daemon/src/engine/sys/sdl_compat.cpp
@@ -0,0 +1,47 @@
+/*
+===========================================================================
+Daemon BSD Source Code
+Copyright (c) 2013-2015, Daemon Developers
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Daemon developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL DAEMON DEVELOPERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+===========================================================================
+*/
+#if _MSC_VER >= 1900
+/* In versions of Visual Studio before VS2015, the streams in stdio.h were defined like
+
+    _CRTIMP FILE * __cdecl __iob_func(void);
+    #define stdin  (&__iob_func()[0])
+    #define stdout (&__iob_func()[1])
+    #define stderr (&__iob_func()[2])
+
+   In VS2015 __iob_func is no longer defined. It is referenced since SDL2 prints a message to 
+   stderr in an extremely unlikely situation. Mostly, we just want the link error to go 
+   away by defining something named __iob_func.
+*/
+extern "C" {
+    FILE* __iob_func(void) {
+        return stderr - 2;
+    }
+}
+#endif  // _MSC_VER >= 1900
