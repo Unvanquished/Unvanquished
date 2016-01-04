17681ed682b31f95c0ef7544bffd307cfb2f43e7
Corentin Wallez <corentin@wallez.net>
2016-01-04 10:50:37 -0500

    Implement IsDebuggerAttached

--- daemon/src.cmake
+++ daemon/src.cmake
@@ -17,6 +17,7 @@ set(COMMONLIST
     ${COMMON_DIR}/Compiler.h
     ${COMMON_DIR}/Cvar.cpp
     ${COMMON_DIR}/Cvar.h
+    ${COMMON_DIR}/Debugger.cpp
     ${COMMON_DIR}/DisjointSets.h
     ${COMMON_DIR}/Endian.h
     ${COMMON_DIR}/FileSystem.cpp
