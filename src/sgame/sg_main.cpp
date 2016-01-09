a49665e46d66be3f96deaf88efcaadb6600ca7a5
dolcetriade <vcelestialragev@gmail.com>
2016-01-09 14:22:40 -0800

    Shutdown lua in sgame in g_shutdown

--- src/sgame/sg_main.cpp
+++ src/sgame/sg_main.cpp
@@ -1012,6 +1012,8 @@ void G_ShutdownGame( int restart )
 	G_ShutdownMapRotations();
 	BG_UnloadAllConfigs();
 
+	Unv::SGame::Lua::Shutdown();
+
 	level.restarted = false;
 	level.surrenderTeam = TEAM_NONE;
 	trap_SetConfigstring( CS_WINNER, "" );
