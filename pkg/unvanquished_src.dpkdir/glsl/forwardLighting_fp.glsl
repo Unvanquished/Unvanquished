aebcbfc8553331c050a775162fffa8b69168dc3c
Matthias Bentrup <matthias.bentrup@googlemail.com>
2016-02-01 16:26:00 +0100

    Add tiled light-index renderer.

--- pkg/unvanquished_src.dpkdir/glsl/forwardLighting_fp.glsl
+++ pkg/unvanquished_src.dpkdir/glsl/forwardLighting_fp.glsl
@@ -80,7 +80,6 @@ uniform float       u_ShadowBlur;
 uniform mat4		u_ViewMatrix;
 
 uniform float		u_DepthScale;
-uniform vec2		u_SpecularExponent;
 
 varying vec3		var_Position;
 varying vec4		var_TexDiffuse;
