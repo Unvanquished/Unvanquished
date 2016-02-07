93cb1d3cab4a30db1d4711f0b0d4d6d108d55c0f
dolcetriade <vcelestialragev@gmail.com>
2016-02-07 14:19:59 -0800

    MingW32 only knows about windows.h

--- daemon/src/common/Debugger.cpp
+++ daemon/src/common/Debugger.cpp
@@ -30,7 +30,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #if defined(_WIN32)
 
-    #include "Windows.h"
+    #include "windows.h"
 
     namespace Sys {
         bool IsDebuggerAttached() {
