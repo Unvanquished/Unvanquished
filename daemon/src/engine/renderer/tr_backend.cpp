d9747ab7313c516e2e5868c77b2b895f3c4045b9
cmf028 <cmf028@users.noreply.github.com>
2016-03-08 20:37:22 -0600

    Minor cleanups/fixes to tiled lighting

--- daemon/src/engine/renderer/tr_backend.cpp
+++ daemon/src/engine/renderer/tr_backend.cpp
@@ -2824,7 +2824,7 @@ void RB_RenderPostDepth()
 
 		GL_Viewport( 0, 0, tr.lighttileRenderImage->width, tr.lighttileRenderImage->height );
 		tess.numIndexes = 0;
-		tess.numVertexes = tr.depthtile2RenderImage->width * tr.depthtile2RenderImage->height;
+		tess.numVertexes = tr.lighttileVBO->vertexesNum;
 
 		GL_VertexAttribsState( ATTR_POSITION | ATTR_TEXCOORD );
 		glEnable( GL_POINT_SPRITE );
