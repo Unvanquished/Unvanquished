2683c1cb43cd179374c252a326e09bc18baac2a3
Viech <viech@unvanquished.net>
2016-06-21 20:01:37 +0200

    Add a !!plain tag for specifying constant parameters.

--- tools/cbse/test.sh
+++ tools/cbse/test.sh
@@ -1,4 +1,4 @@
 mkdir -p test
-./CBSE.py -s def.yaml -o test
+./CBSE.py -s def.yaml -o test || exit
 cp test/components/skeletons/* test/components/
 c++ --std=c++11 -c test/backend/CBSEBackend.cpp -o /dev/null
