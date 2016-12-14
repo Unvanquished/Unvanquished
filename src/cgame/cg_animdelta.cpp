762e3aa03951f9db36758e7611fda034f130bf76
dolcetriade <vcelestialragev@gmail.com>
2016-12-13 21:46:19 -0800

    cg_players: Refactor handling of delta animations.
    Create the concept of a SkeletonModifier that can perform arbitrary
    modifications of a skeleton. Use this to allow handling hand deltas
    for human models.

---
+++ src/cgame/cg_animdelta.cpp
@@ -0,0 +1,119 @@
+/*
+===========================================================================
+Copyright (C) 1999-2005 Id Software, Inc.
+Copyright (C) 2000-2009 Darklegion Development
+
+This file is part of Daemon.
+
+Daemon is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+Daemon is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Daemon; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+===========================================================================
+*/
+
+#include "cg_animdelta.h"
+
+namespace
+{
+
+int LoadDeltaAnimation( weapon_t weapon, const char* modelName, bool iqm )
+{
+	int handle = 0;
+	if ( iqm )
+	{
+		handle = trap_R_RegisterAnimation( va( "models/players/%s/%s.iqm:%s_delta", modelName, modelName, BG_Weapon( weapon )->name ) );
+	}
+	else
+	{
+		handle = trap_R_RegisterAnimation( va( "models/players/%s/%s_delta.md5anim", modelName, BG_Weapon( weapon )->name ) );
+	}
+	return handle;
+}
+
+}  // namespace
+
+bool AnimDelta::ParseConfiguration(clientInfo_t* ci, const char* token2, const char** data_p)
+{
+	if ( Q_stricmp( token2, "handBones" ) ) return false;
+	char* token = COM_Parse2( data_p );
+	if ( !token || token[0] != '{' )
+	{
+		Log::Notice( "^1ERROR^7: Expected '{' but found '%s' in %s's character.cfg", token, ci->modelName );
+		return false;
+	}
+	while ( 1 )
+	{
+		token = COM_Parse( data_p );
+		if ( !token || token[0] == '}' ) break;
+		int index = trap_R_BoneIndex( ci->bodyModel, token );
+		if ( index < 0 )
+		{
+			Log::Warn("AnimDelta: Error finding bone '%s' in %s", token, ci->modelName );
+		}
+		boneIndicies_.push_back( index );
+	}
+	return true;
+}
+
+bool AnimDelta::LoadData(clientInfo_t* ci)
+{
+	char newModelName[ MAX_QPATH ];
+	// special handling for human_(naked|light|medium)
+	if ( !Q_stricmp( ci->modelName, "human_naked"   ) ||
+		!Q_stricmp( ci->modelName, "human_light"   ) ||
+		!Q_stricmp( ci->modelName, "human_medium" ) )
+	{
+		Q_strncpyz( newModelName, "human_nobsuit_common", sizeof( newModelName ) );
+	}
+	else
+	{
+		Q_strncpyz( newModelName, ci->modelName, sizeof( newModelName ) );
+	}
+
+	refSkeleton_t base;
+	refSkeleton_t delta;
+	for ( int i = WP_NONE + 1; i < WP_NUM_WEAPONS; ++i )
+	{
+		int handle = LoadDeltaAnimation( static_cast<weapon_t>( i ), newModelName, ci->iqm );
+		if ( !handle ) continue;
+		Log::Debug("Loaded delta for %s %s", newModelName, BG_Weapon( i )->humanName);
+		trap_R_BuildSkeleton( &delta, handle, 1, 1, 0, false );
+		// Derive the delta from the base stand animation.
+		trap_R_BuildSkeleton( &base, ci->animations[ TORSO_STAND ].handle, 1, 1, 0, false );
+		auto ret = deltas_.insert( std::make_pair( i, std::vector<delta_t>( boneIndicies_.size() ) ) );
+		auto& weaponDeltas = ret.first->second;
+		for ( size_t j = 0; j < boneIndicies_.size(); ++j )
+		{
+			VectorSubtract( delta.bones[ boneIndicies_[ j ] ].t.trans, base.bones[ boneIndicies_[ j ] ].t.trans, weaponDeltas[ j ].delta );
+			QuatInverse( base.bones[ boneIndicies_[ j ] ].t.rot );
+			QuatMultiply( base.bones[ boneIndicies_[ j ] ].t.rot, delta.bones[ boneIndicies_[ j ] ].t.rot, weaponDeltas[ j ].rot );
+		}
+	}
+	return true;
+}
+
+void AnimDelta::Apply(const entityState_t* es, refSkeleton_t* skeleton)
+{
+	if ( ( es->torsoAnim & ~ANIM_TOGGLEBIT ) < TORSO_ATTACK ) return;
+	auto it = deltas_.find( es->weapon );
+	if ( it == deltas_.end() ) return;
+	for ( size_t i = 0; i < it->second.size(); ++i )
+	{
+		VectorAdd( it->second[ i ].delta, skeleton->bones[ boneIndicies_[ i ] ].t.trans, skeleton->bones[ boneIndicies_[ i ] ].t.trans );
+		QuatMultiply2( skeleton->bones[ boneIndicies_[ i ] ].t.rot, it->second[ i ].rot );
+	}
+}
+
+
+
+
---
+++ src/cgame/cg_skeleton_modifier.h
@@ -0,0 +1,36 @@
+/*
+===========================================================================
+
+Daemon GPL Source Code
+Copyright (C) 2016 Unvanquished Developers
+
+Daemon is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Daemon is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+===========================================================================
+*/
+#ifndef CG_SKELETON_MODIFIER_H
+#define CG_SKELETON_MODIFIER_H
+
+// Forward declare due to circular dependency.
+struct clientInfo_t;
+
+class SkeletonModifier
+{
+public:
+	virtual ~SkeletonModifier() {}
+	virtual bool ParseConfiguration( clientInfo_t*, const char*, const char**) { return false; }
+	virtual bool LoadData( clientInfo_t* ) { return true; }
+	virtual void Apply(const entityState_t*, refSkeleton_t*) {}
+};
+#endif  // CG_SKELETON_MODIFIER_H
