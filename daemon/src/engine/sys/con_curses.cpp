Unvanquished/Unvanquished

commit e5c96b6d8d96dc35c9c3ff5fd39197c7587aa6b4

s/#define/static const/

daemon/src/engine/sys/con_curses.cpp
@@ -37,10 +37,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define TITLE         "^2[ ^3" CLIENT_WINDOW_TITLE " Console ^2]"
 #endif
 #define PROMPT        "^3-> "
-#define INPUT_SCROLL  15
-#define LOG_SCROLL    5
-#define MAX_LOG_LINES 1024
-#define LOG_BUF_SIZE  65536
+static const int INPUT_SCROLL  = 15;
+static const int LOG_SCROLL    = 5;
+static const int MAX_LOG_LINES = 1024;
+static const int LOG_BUF_SIZE  = 65536;
 
 // TTY Console prototypes
 void CON_Shutdown_TTY();
@@ -66,7 +66,7 @@ static bool     forceRedraw = false;
 static int      stderr_fd;
 #endif
 
-#define LOG_LINES      ( LINES - 3 )
+static const int LOG_LINES = ( LINES - 3 );
 
 static const int CURSES_DEFAULT_COLOR = 32;
 

daemon/src/common/IPC/Primitives.cpp
@@ -81,8 +81,8 @@ enum NaClDescTypeTag {
   NACL_DESC_CUSTOM,
   NACL_DESC_NULL
 };
-#define NACL_DESC_TYPE_MAX      (NACL_DESC_NULL + 1)
-#define NACL_DESC_TYPE_END_TAG  (0xff)
+static const int NACL_DESC_TYPE_MAX      = (NACL_DESC_NULL + 1);
+static const int NACL_DESC_TYPE_END_TAG  = (0xff);
 
 struct NaClInternalRealHeader {
   uint32_t  xfer_protocol_version;
@@ -95,7 +95,7 @@ struct NaClInternalHeader {
                 - sizeof(struct NaClInternalRealHeader)];
 };
 
-#define NACL_HANDLE_TRANSFER_PROTOCOL 0xd3c0de01
+static const uint32_t NACL_HANDLE_TRANSFER_PROTOCOL = 0xd3c0de01;
 // End of imported definitions
 
 namespace IPC {

daemon/src/common/cm/cm_load.cpp
@@ -38,11 +38,11 @@ Maryland 20850 USA.
 
 // to allow boxes to be treated as brush models, we allocate
 // some extra indexes along with those needed by the map
-#define BOX_LEAF_BRUSHES 1 // ydnar
-#define BOX_BRUSHES      1
-#define BOX_SIDES        6
-#define BOX_LEAFS        2
-#define BOX_PLANES       12
+static const int BOX_LEAF_BRUSHES = 1; // ydnar
+static const int BOX_BRUSHES      = 1;
+static const int BOX_SIDES        = 6;
+static const int BOX_LEAFS        = 2;
+static const int BOX_PLANES       = 12;
 
 #define LL( x ) x = LittleLong( x )
 
@@ -508,7 +508,7 @@ void CMod_LoadBrushSides(const byte *const cmod_base, lump_t *l)
 	}
 }
 
-#define CM_EDGE_VERTEX_EPSILON 0.1f
+static const float CM_EDGE_VERTEX_EPSILON = 0.1f;
 
 /*
 =================
@@ -735,7 +735,7 @@ void CMod_LoadEntityString(const byte *const cmod_base, lump_t *l)
 CMod_LoadVisibility
 =================
 */
-#define VIS_HEADER 8
+static const int VIS_HEADER = 8;
 void CMod_LoadVisibility(const byte *const cmod_base, lump_t *l)
 {
 	int len = l->filelen;
@@ -764,8 +764,8 @@ void CMod_LoadVisibility(const byte *const cmod_base, lump_t *l)
 CMod_LoadSurfaces
 =================
 */
-#define MAX_PATCH_SIZE  64
-#define MAX_PATCH_VERTS ( MAX_PATCH_SIZE * MAX_PATCH_SIZE )
+static const int MAX_PATCH_SIZE  = 64;
+static const int MAX_PATCH_VERTS = ( MAX_PATCH_SIZE * MAX_PATCH_SIZE );
 void CMod_LoadSurfaces(const byte *const cmod_base, lump_t *surfs, lump_t *verts, lump_t *indexesLump)
 {
 	drawVert_t    *dv, *dv_p;

daemon/src/common/cm/cm_patch.cpp
@@ -308,7 +308,7 @@ static void CM_SubdivideGridColumns( cGrid_t *grid )
 CM_ComparePoints
 ======================
 */
-#define POINT_EPSILON 0.1
+static const float POINT_EPSILON = 0.1;
 static bool CM_ComparePoints( float *a, float *b )
 {
 	float d;

daemon/src/common/cm/cm_plane.cpp
@@ -34,7 +34,7 @@ Maryland 20850 USA.
 
 #include "cm_local.h"
 
-#define PLANE_HASHES 8192
+static const int PLANE_HASHES = 8192;
 static cPlane_t *planeHashTable[ PLANE_HASHES ];
 
 int      numPlanes;
@@ -102,8 +102,8 @@ bool CM_PlaneFromPoints( vec4_t plane, const vec3_t a, const vec3_t b, const vec
 	return true;
 }
 
-#define NORMAL_EPSILON    0.0001
-#define DIST_EPSILON      0.02
+static const double NORMAL_EPSILON    = 0.0001;
+static const double DIST_EPSILON      = 0.02;
 
 /*
 ==================

daemon/src/common/cm/cm_trace.cpp
@@ -593,7 +593,7 @@ void CM_TestBoundingBoxInCapsule( traceWork_t *tw, clipHandle_t model )
 CM_PositionTest
 ==================
 */
-#define MAX_POSITION_LEAFS 1024
+static const int MAX_POSITION_LEAFS = 1024;
 void CM_PositionTest( traceWork_t *tw )
 {
 	int        leafs[ MAX_POSITION_LEAFS ];
@@ -1583,7 +1583,7 @@ void CM_TraceThroughLeaf( traceWork_t *tw, cLeaf_t *leaf )
 	}
 }
 
-#define RADIUS_EPSILON 1.0f
+static const float RADIUS_EPSILON = 1.0f;
 
 /*
 ================

daemon/src/engine/botlib/bot_nav_edit.cpp
@@ -40,7 +40,7 @@ Maryland 20850 USA.
 #include "bot_navdraw.h"
 #include "nav.h"
 
-#define DEFAULT_CONNECTION_SIZE 50
+static const int DEFAULT_CONNECTION_SIZE = 50;
 static int connectionSize = DEFAULT_CONNECTION_SIZE;
 
 bool GetPointPointedTo( NavData_t *nav, rVec &p )

daemon/src/engine/client/cin_ogm.cpp
@@ -58,7 +58,7 @@ theora:
 
 #include "client.h"
 
-#define OGG_BUFFER_SIZE 8 * 1024 //4096
+static const int OGG_BUFFER_SIZE = 8 * 1024; //4096
 
 struct cin_ogm_t
 {
@@ -172,10 +172,10 @@ static int loadPagesToStreams()
 	return r;
 }
 
-#define SIZEOF_RAWBUFF    4 * 1024
+static const int SIZEOF_RAWBUFF = 4 * 1024;
 
-#define MIN_AUDIO_PRELOAD 400 // in ms
-#define MAX_AUDIO_PRELOAD 500 // in ms
+static const int MIN_AUDIO_PRELOAD = 400; // in ms
+static const int MAX_AUDIO_PRELOAD = 500; // in ms
 
 /*
 

commit c07a40bfbd610e723dc3b86f8426c3409ed04749

Fix bug with curses console that can cause text not to display, due to a quantity that is not a compile-time constant being a const global variable (regression in e5c96b6d).

daemon/src/engine/sys/con_curses.cpp
@@ -66,7 +66,9 @@ static bool     forceRedraw = false;
 static int      stderr_fd;
 #endif
 
-static const int LOG_LINES = ( LINES - 3 );
+static int NumLogLines() {
+	return LINES - 3;
+}
 
 static const int CURSES_DEFAULT_COLOR = 32;
 
@@ -230,12 +232,12 @@ static void CON_Redraw()
 	{
 		lastline++;
 	}
-	scrollline = lastline - LOG_LINES;
+	scrollline = lastline - NumLogLines();
 	if ( scrollline < 0 )
 	{
 		scrollline = 0;
 	}
-	pnoutrefresh( logwin, scrollline, 0, 1, 0, LOG_LINES, COLS );
+	pnoutrefresh( logwin, scrollline, 0, 1, 0, NumLogLines(), COLS );
 
 	// Create the input field
 	inputwin = newwin( 1, COLS - Color::StrlenNocolor( PROMPT ) - 8, LINES - 1, Color::StrlenNocolor( PROMPT ) + 8 );
@@ -505,18 +507,18 @@ char *CON_Input()
 				continue;
 
 			case KEY_NPAGE:
-				if ( lastline > scrollline + LOG_LINES )
+				if ( lastline > scrollline + NumLogLines() )
 				{
 					scrollline += LOG_SCROLL;
 
-					if ( scrollline + LOG_LINES > lastline )
+					if ( scrollline + NumLogLines() > lastline )
 					{
-						scrollline = lastline - LOG_LINES;
+						scrollline = lastline - NumLogLines();
 					}
 
-					pnoutrefresh( logwin, scrollline, 0, 1, 0, LOG_LINES, COLS );
+					pnoutrefresh( logwin, scrollline, 0, 1, 0, NumLogLines(), COLS );
 				}
-				if (scrollline >= lastline - LOG_LINES) {
+				if (scrollline >= lastline - NumLogLines()) {
 					CON_SetColor( stdscr, Color::Green );
 					for (int i = COLS - 7; i < COLS - 1; i++)
 						mvaddch(LINES - 2, i, ACS_HLINE);
@@ -534,9 +536,9 @@ char *CON_Input()
 						scrollline = 0;
 					}
 
-					pnoutrefresh( logwin, scrollline, 0, 1, 0, LOG_LINES, COLS );
+					pnoutrefresh( logwin, scrollline, 0, 1, 0, NumLogLines(), COLS );
 				}
-				if (scrollline < lastline - LOG_LINES) {
+				if (scrollline < lastline - NumLogLines()) {
 					CON_SetColor( stdscr, Color::Red );
 					mvaddstr(LINES - 2, COLS - 7, "(more)");
 				}
@@ -668,7 +670,7 @@ CON_Print
 void CON_Print( const char *msg )
 {
 	int      col;
-	bool scroll = ( lastline > scrollline && lastline <= scrollline + LOG_LINES );
+	bool scroll = ( lastline > scrollline && lastline <= scrollline + NumLogLines() );
 
 	if ( !curses_on )
 	{
@@ -687,14 +689,14 @@ void CON_Print( const char *msg )
 
 	if ( scroll )
 	{
-		scrollline = lastline - LOG_LINES;
+		scrollline = lastline - NumLogLines();
 
 		if ( scrollline < 0 )
 		{
 			scrollline = 0;
 		}
 
-		pnoutrefresh( logwin, scrollline, 0, 1, 0, LOG_LINES, COLS );
+		pnoutrefresh( logwin, scrollline, 0, 1, 0, NumLogLines(), COLS );
 	}
 
 	// Add the message to the log buffer