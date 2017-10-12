bfaaf3031793025754ba7a48621474841300a211
Thomas Debesse <dev@illwieckz.net>
2017-10-12 19:18:17 +0200

    headers: replace Daemon reference to Unvanquished

--- src/sgame/sg_typedef.h
+++ src/sgame/sg_typedef.h
@@ -1,23 +1,23 @@
 /*
 ===========================================================================
 
-Daemon GPL Source Code
+Unvanquished GPL Source Code
 Copyright (C) 2012-2013 Unvanquished Developers
 
-This file is part of the Daemon GPL Source Code (Daemon Source Code).
+This file is part of the Unvanquished GPL Source Code (Unvanquished Source Code).
 
-Daemon Source Code is free software: you can redistribute it and/or modify
+Unvanquished Source Code is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Daemon Source Code is distributed in the hope that it will be useful,
+Unvanquished Source Code is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Daemon Source Code.  If not, see <http://www.gnu.org/licenses/>.
+along with Unvanquished Source Code.  If not, see <http://www.gnu.org/licenses/>.
 
 ===========================================================================
 */
