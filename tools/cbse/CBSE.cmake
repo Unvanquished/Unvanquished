a87cb8dd21ab3be914dad33284f025d4a251a103
Tim <t4im@users.noreply.github.com>
2018-11-07 18:42:41 +0100

    Add CBSE-templates to cmake DEPENDS, to force regeneration on template change

--- tools/cbse/CBSE.cmake
+++ tools/cbse/CBSE.cmake
@@ -49,6 +49,10 @@ function(CBSE target definition output)
         OUTPUT ${GENERATED_CBSE}
         COMMENT "Generating CBSE entities for ${definition}"
         DEPENDS ${definition}
+                ${CMAKE_SOURCE_DIR}/src/utils/cbse/templates/Backend.cpp
+                ${CMAKE_SOURCE_DIR}/src/utils/cbse/templates/Backend.h
+                ${CMAKE_SOURCE_DIR}/src/utils/cbse/templates/Component.cpp
+                ${CMAKE_SOURCE_DIR}/src/utils/cbse/templates/Component.h
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/src/utils/cbse
         COMMAND
                 ${PYTHON_EXECUTABLE}
