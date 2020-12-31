41da8349854c19d6ecec51cfb6a27469b2cf28fc
Antoine Fontaine <antoine.fontaine@epfl.ch>
2020-12-31 16:36:06 +0100

    Replace Q_clamp by Math::Clamp
    it's not very useful, so we may as well update the code that's still
    using it and drop it

--- src/cgame/cg_buildable.cpp
+++ src/cgame/cg_buildable.cpp
@@ -2326,7 +2326,7 @@ void CG_Buildable( centity_t *cent )
 			// The roll of Bone_barrel is the mgturret's barrel roll.
 			if ( es->modelindex == BA_H_MGTURRET )
 			{
-				roll  = Q_clamp( ( 1.0f / MGTURRET_ATTACK_PERIOD ) *
+				roll = Math::Clamp( ( 1.0f / MGTURRET_ATTACK_PERIOD ) *
 				        120.0f * ( cg.time - cent->muzzleFlashTime ), 0.0f, 120.0f );
 
 				QuatFromAngles( rotation, 0, 0, roll );
