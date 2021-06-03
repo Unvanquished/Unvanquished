daf3eb4d6828360d11600f7976e74fb7c95ece9a
slipher <slipher@protonmail.com>
2021-06-02 23:15:10 -0500

    Include all headers in sources list
    It's helpful for IDE indexing

--- src.cmake
+++ src.cmake
@@ -34,10 +34,13 @@ set(GAMESHAREDLIST
     ${GAMELOGIC_DIR}/shared/bg_voice.cpp
     ${GAMELOGIC_DIR}/shared/parse.cpp
     ${GAMELOGIC_DIR}/shared/parse.h
+    ${GAMELOGIC_DIR}/shared/Clustering.h
 )
 
 set(CGAMELIST
     ${GAMELOGIC_DIR}/cgame/cg_animation.cpp
+    ${GAMELOGIC_DIR}/cgame/cg_animdelta.cpp
+    ${GAMELOGIC_DIR}/cgame/cg_animdelta.h
     ${GAMELOGIC_DIR}/cgame/cg_animmapobj.cpp
     ${GAMELOGIC_DIR}/cgame/cg_api.cpp
     ${GAMELOGIC_DIR}/cgame/cg_attachment.cpp
@@ -51,6 +54,7 @@ set(CGAMELIST
     ${GAMELOGIC_DIR}/cgame/cg_gameinfo.cpp
     ${GAMELOGIC_DIR}/cgame/cg_key_name.cpp
     ${GAMELOGIC_DIR}/cgame/cg_key_name.h
+    ${GAMELOGIC_DIR}/cgame/cg_local.h
     ${GAMELOGIC_DIR}/cgame/cg_main.cpp
     ${GAMELOGIC_DIR}/cgame/cg_marks.cpp
     ${GAMELOGIC_DIR}/cgame/cg_minimap.cpp
@@ -68,6 +72,7 @@ set(CGAMELIST
     ${GAMELOGIC_DIR}/cgame/cg_segmented_skeleton.cpp
     ${GAMELOGIC_DIR}/cgame/cg_segmented_skeleton.h
     ${GAMELOGIC_DIR}/cgame/cg_servercmds.cpp
+    ${GAMELOGIC_DIR}/cgame/cg_skeleton_modifier.h
     ${GAMELOGIC_DIR}/cgame/cg_snapshot.cpp
     ${GAMELOGIC_DIR}/cgame/cg_trails.cpp
     ${GAMELOGIC_DIR}/cgame/cg_tutorial.cpp
@@ -75,7 +80,6 @@ set(CGAMELIST
     ${GAMELOGIC_DIR}/cgame/cg_utils.cpp
     ${GAMELOGIC_DIR}/cgame/cg_view.cpp
     ${GAMELOGIC_DIR}/cgame/cg_weapons.cpp
-    ${GAMELOGIC_DIR}/cgame/cg_animdelta.cpp
     ${GAMELOGIC_DIR}/cgame/Filter.h
     ${GAMELOGIC_DIR}/cgame/CombatFeedback.cpp
 
@@ -133,12 +137,17 @@ set(SGAMELIST
     ${GAMELOGIC_DIR}/sgame/Entities.h
     ${GAMELOGIC_DIR}/sgame/sg_active.cpp
     ${GAMELOGIC_DIR}/sgame/sg_admin.cpp
+    ${GAMELOGIC_DIR}/sgame/sg_admin.h
     ${GAMELOGIC_DIR}/sgame/sg_api.cpp
     ${GAMELOGIC_DIR}/sgame/sg_bot.cpp
+    ${GAMELOGIC_DIR}/sgame/sg_bot.h
     ${GAMELOGIC_DIR}/sgame/sg_bot_ai.cpp
+    ${GAMELOGIC_DIR}/sgame/sg_bot_ai.h
     ${GAMELOGIC_DIR}/sgame/sg_bot_nav.cpp
     ${GAMELOGIC_DIR}/sgame/sg_bot_parse.cpp
+    ${GAMELOGIC_DIR}/sgame/sg_bot_parse.h
     ${GAMELOGIC_DIR}/sgame/sg_bot_util.cpp
+    ${GAMELOGIC_DIR}/sgame/sg_bot_util.h
     ${GAMELOGIC_DIR}/sgame/sg_buildable.cpp
     ${GAMELOGIC_DIR}/sgame/sg_buildpoints.cpp
     ${GAMELOGIC_DIR}/sgame/sg_client.cpp
@@ -178,7 +187,9 @@ set(SGAMELIST
     ${GAMELOGIC_DIR}/sgame/sg_typedef.h
     ${GAMELOGIC_DIR}/sgame/sg_utils.cpp
     ${GAMELOGIC_DIR}/sgame/sg_weapon.cpp
+    ${GAMELOGIC_DIR}/sgame/CBSE.h
     ${GAMELOGIC_DIR}/sgame/CombatFeedback.cpp
+    ${GAMELOGIC_DIR}/sgame/CustomSurfaceFlags.h
 
     ${GAMELOGIC_DIR}/sgame/botlib/bot_api.h
     ${GAMELOGIC_DIR}/sgame/botlib/bot_debug.cpp
@@ -194,39 +205,73 @@ set(SGAMELIST
     ${GAMELOGIC_DIR}/sgame/botlib/nav.h
 
     ${GAMELOGIC_DIR}/sgame/components/AcidTubeComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/AcidTubeComponent.h
     ${GAMELOGIC_DIR}/sgame/components/AlienBuildableComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/AlienBuildableComponent.h
     ${GAMELOGIC_DIR}/sgame/components/AlienClassComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/AlienClassComponent.h
     ${GAMELOGIC_DIR}/sgame/components/ArmorComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/ArmorComponent.h
     ${GAMELOGIC_DIR}/sgame/components/ArmouryComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/ArmouryComponent.h
     ${GAMELOGIC_DIR}/sgame/components/BarricadeComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/BarricadeComponent.h
     ${GAMELOGIC_DIR}/sgame/components/BoosterComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/BoosterComponent.h
     ${GAMELOGIC_DIR}/sgame/components/BuildableComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/BuildableComponent.h
     ${GAMELOGIC_DIR}/sgame/components/ClientComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/ClientComponent.h
     ${GAMELOGIC_DIR}/sgame/components/DeferredFreeingComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/DeferredFreeingComponent.h
     ${GAMELOGIC_DIR}/sgame/components/DrillComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/DrillComponent.h
     ${GAMELOGIC_DIR}/sgame/components/EggComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/EggComponent.h
     ${GAMELOGIC_DIR}/sgame/components/HealthComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/HealthComponent.h
     ${GAMELOGIC_DIR}/sgame/components/HiveComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/HiveComponent.h
     ${GAMELOGIC_DIR}/sgame/components/HumanBuildableComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/HumanBuildableComponent.h
     ${GAMELOGIC_DIR}/sgame/components/HumanClassComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/HumanClassComponent.h
     ${GAMELOGIC_DIR}/sgame/components/IgnitableComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/IgnitableComponent.h
     ${GAMELOGIC_DIR}/sgame/components/KnockbackComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/KnockbackComponent.h
     ${GAMELOGIC_DIR}/sgame/components/LeechComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/LeechComponent.h
     ${GAMELOGIC_DIR}/sgame/components/MainBuildableComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/MainBuildableComponent.h
     ${GAMELOGIC_DIR}/sgame/components/MedipadComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/MedipadComponent.h
     ${GAMELOGIC_DIR}/sgame/components/MGTurretComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/MGTurretComponent.h
     ${GAMELOGIC_DIR}/sgame/components/MiningComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/MiningComponent.h
     ${GAMELOGIC_DIR}/sgame/components/OvermindComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/OvermindComponent.h
     ${GAMELOGIC_DIR}/sgame/components/ReactorComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/ReactorComponent.h
     ${GAMELOGIC_DIR}/sgame/components/RocketpodComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/RocketpodComponent.h
     ${GAMELOGIC_DIR}/sgame/components/SpawnerComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/SpawnerComponent.h
     ${GAMELOGIC_DIR}/sgame/components/SpectatorComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/SpectatorComponent.h
     ${GAMELOGIC_DIR}/sgame/components/SpikerComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/SpikerComponent.h
     ${GAMELOGIC_DIR}/sgame/components/TeamComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/TeamComponent.h
     ${GAMELOGIC_DIR}/sgame/components/TelenodeComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/TelenodeComponent.h
     ${GAMELOGIC_DIR}/sgame/components/ThinkingComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/ThinkingComponent.h
     ${GAMELOGIC_DIR}/sgame/components/TrapperComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/TrapperComponent.h
     ${GAMELOGIC_DIR}/sgame/components/TurretComponent.cpp
+    ${GAMELOGIC_DIR}/sgame/components/TurretComponent.h
 
     ${ENGINE_DIR}/server/sg_api.h
     ${ENGINE_DIR}/server/sg_msgdef.h
