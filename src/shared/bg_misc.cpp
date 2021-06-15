Unvanquished/Unvanquished

commit 88850e97b36e1e029d57a9d97592007955703906

split .missile.cfg files into .attr.cfg and .model.cfg files

src/shared/bg_misc.cpp
@@ -826,8 +826,8 @@ void BG_InitMissileAttributes()
 		ma->number = md->number;
 
 		// for simplicity, read both from a single file
-		BG_ParseMissileAttributeFile( va( "configs/missiles/%s.missile.cfg", ma->name ), ma );
-		BG_ParseMissileDisplayFile(   va( "configs/missiles/%s.missile.cfg", ma->name ), ma );
+		BG_ParseMissileAttributeFile( va( "configs/missiles/%s.attr.cfg", ma->name ), ma );
+		BG_ParseMissileDisplayFile(   va( "configs/missiles/%s.model.cfg", ma->name ), ma );
 	}
 }
 

commit f7a32da8917e923add6914e48a264ddd72e9ebbb

Fall back to old paths of missile configs
88850e97 broke compatibility with the 0.52 asset packages.

src/shared/bg_misc.cpp
@@ -825,9 +825,17 @@ void BG_InitMissileAttributes()
 		ma->name   = md->name;
 		ma->number = md->number;
 
-		// for simplicity, read both from a single file
-		BG_ParseMissileAttributeFile( va( "configs/missiles/%s.attr.cfg", ma->name ), ma );
-		BG_ParseMissileDisplayFile(   va( "configs/missiles/%s.model.cfg", ma->name ), ma );
+		if ( FS::PakPath::FileExists( va( "configs/missiles/%s.attr.cfg", ma->name ) ) )
+		{
+			BG_ParseMissileAttributeFile( va( "configs/missiles/%s.attr.cfg", ma->name ), ma );
+			BG_ParseMissileDisplayFile(   va( "configs/missiles/%s.model.cfg", ma->name ), ma );
+		}
+		else
+		{
+			// TODO(0.53): Remove this fallback
+			BG_ParseMissileAttributeFile( va( "configs/missiles/%s.missile.cfg", ma->name ), ma );
+			BG_ParseMissileDisplayFile(   va( "configs/missiles/%s.missile.cfg", ma->name ), ma );
+		}
 	}
 }
 