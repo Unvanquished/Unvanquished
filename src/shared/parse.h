21c13d660eb3671d1314a0df3d8423fb4e2b0e00
Antoine Fontaine <antoine.fontaine@epfl.ch>
2021-06-27 14:50:20 +0200

    Add Parse_WordListSplitter

--- src/shared/parse.h
+++ src/shared/parse.h
@@ -54,4 +54,48 @@ int Parse_FreeSourceHandle(int handle);
 bool Parse_ReadTokenHandle(int handle, pc_token_t *pc_token);
 int Parse_SourceFileAndLine(int handle, char (&filename)[MAX_QPATH], int *line);
 
+/*
+===============
+Parse_WordListSplitter
+
+Tokenize a list like "a, b, c" to "a", "b", "c".
+
+This is a partial implementation of an input interator: it can only go forward,
+and you don't know how many elements you will be able to read until you've read
+them all.
+
+Exemple usage:
+	for (Parse_WordListSplitter iterator(string); *iterator; ++iterator)
+	{
+		printf("C str: »%s«\n", *parser);
+	}
+
+This parser is quite tolerant: you can use one delimiter or another,
+or a variant of that: "a b,c ,d,,e    f" is equivalent to "a, b, c, d, e, f".
+
+The const char * it returns for each element (operator*) are not valid once
+the destructor is called.
+===============
+*/
+// We could have a template here to specify the delimiter.
+// But compilation times are large enough as is.
+class Parse_WordListSplitter {
+public:
+	Parse_WordListSplitter(const std::string &);
+	Parse_WordListSplitter(const char *);
+	~Parse_WordListSplitter();
+
+	const char *operator*() const;
+
+	Parse_WordListSplitter& operator++(); // only ++i is implemented. there is no i++.
+
+private:
+	char *_string; // the string
+	const size_t _len;   // size of the string
+	size_t _start; // start of this chunk
+	size_t _stop;  // stop of this chunk
+	size_t find_first_matching(size_t start = 0);
+	size_t find_first_not_matching(size_t start = 0);
+};
+
 #endif // SHARED_PARSE_H_
