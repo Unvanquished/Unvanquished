3bde6b0d007c2239ae1412103565808134e30fa7
slipher <slipher@protonmail.com>
2021-07-08 07:52:47 -0500

    Enable missile config bad token warnings

--- src/shared/bg_parse.cpp
+++ src/shared/bg_parse.cpp
@@ -2134,10 +2134,10 @@ void BG_ParseMissileAttributeFile( const char *filename, missileAttributes_t *ma
 		{
 			BG_ParseConfigVar( var, &text, filename );
 		}*/
-		/*else
+		else
 		{
 			Log::Warn( "%s: unknown token '%s'", filename, token );
-		}*/
+		}
 	}
 
 	if      ( !( defined & DAMAGE ) )         { token = "damage"; }
@@ -2413,10 +2413,10 @@ void BG_ParseMissileDisplayFile( const char *filename, missileAttributes_t *ma )
 #endif
 			defined |= IMPACT_FLESH_SND;
 		}
-		/*else
+		else
 		{
 			Log::Warn( "%s: unknown token '%s'", filename, token );
-		}*/
+		}
 	}
 
 	// check sprite data set for completeness
