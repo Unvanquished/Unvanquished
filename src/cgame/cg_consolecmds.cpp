5b70b03dcd5d5f9bb3c7456e889bba00da936321
Antoine Fontaine <antoine.fontaine@epfl.ch>
2021-07-28 11:19:53 +0200

    Add autocompletion for devteam

--- src/cgame/cg_consolecmds.cpp
+++ src/cgame/cg_consolecmds.cpp
@@ -457,6 +457,7 @@ static const struct
 	{ "destroy",          0,                       0                },
 	{ "destroyTestPS",    CG_DestroyTestPS_f,      0                },
 	{ "destroyTestTS",    CG_DestroyTestTS_f,      0                },
+	{ "devteam",          0,                       0                },
 	{ "follow",           0,                       CG_CompleteName  },
 	{ "follownext",       0,                       0                },
 	{ "followprev",       0,                       0                },
