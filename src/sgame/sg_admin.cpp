24a2b91281538013fd62892f5824e9678f15840d
slipher <slipher@protonmail.com>
2021-09-30 07:18:47 -0500

    Fix print_tr argument translation flags
    E.g. $2t$ instead of $2$ means the 2nd argument for the format string
    should be passed through the translator function.

--- src/sgame/sg_admin.cpp
+++ src/sgame/sg_admin.cpp
@@ -3028,15 +3028,15 @@ bool G_admin_adjustban( gentity_t *ent )
 	               ban->expires ? ban->expires - time : 0, ban->guid, ban->name, ban->reason,
 	               ban->ip.str ) );
 	AP( va( "print_tr %s %d %s %s %s %s %s %s %s %s %s", QQ( N_("^3adjustban:^* ban #$1$ for $2$^* has been updated by $3$^* "
-	        "$4t$$5$$6$$7t$$8$$9t$$10$") ),
+	        "$4$$5t$$6$$7t$$8$$9t$$10$") ),
 	        bnum,
 	        Quote( ban->name ),
 	        G_quoted_admin_name( ent ),
 	        ( mask ) ? Quote( va( "netmask: /%d%s", mask, ( length >= 0 || *reason ) ? ", " : "" ) ) : "",
-		( length >= 0 ) ? QQ( "duration: " ) : "",
+		( length >= 0 ) ? QQ( N_( "duration: " ) ) : "",
 		Quote( seconds ), duration,
 		( length >= 0 && *reason ) ? ", " : "",
-		( *reason ) ? QQ("reason: ") : "",
+		( *reason ) ? QQ( N_( "reason: ") ) : "",
 		Quote( reason ) ) );
 
 	if ( ent )
@@ -3293,7 +3293,7 @@ bool G_admin_changemap( gentity_t *ent )
 	AP( va( "print_tr %s %s %s %s %s %s", QQ( N_("^3changemap:^* map '$1$' started by $2$^* $3t$$4$$5$") ),
 		Quote( map ),
 	        G_quoted_admin_name( ent ),
-	        ( layout[ 0 ] ) ? QQ( "(forcing layout '") : "" ,
+	        ( layout[ 0 ] ) ? QQ( N_( "(forcing layout '") ) : "" ,
 			( layout[ 0 ] ) ? Quote( layout ) : "",
 			( layout[ 0 ] ) ? QQ( "')" ) : "" ) );
 	return true;
@@ -4401,7 +4401,7 @@ bool G_admin_restart( gentity_t *ent )
 	trap_SendConsoleCommand( "map_restart" );
 	G_MapLog_Result( 'R' );
 
-	AP( va( "print_tr %s %s %s %s %s %s %s %s", QQ( N_("^3restart:^* map restarted by $1$ $2$$3t$$4$$5$$6t$$7$") ),
+	AP( va( "print_tr %s %s %s %s %s %s %s %s", QQ( N_("^3restart:^* map restarted by $1$ $2t$$3$$4$$5t$$6$$7$") ),
 	        G_quoted_admin_name( ent ),
 	        ( layout[ 0 ] ) ? QQ( N_( "^7(forcing layout '" ) ) : "",
 			( layout[ 0 ] ) ? Quote( layout ) : "",
