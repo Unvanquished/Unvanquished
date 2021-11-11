Unvanquished/Unvanquished

commit 1085aa6246269e9512dba178fefa48844683336c

Use G_Team more

src/sgame/sg_combat.cpp
@@ -1012,15 +1012,8 @@ void G_LogDestruction( gentity_t *self, gentity_t *actor, int mod )
 		default:
 			if ( actor->client )
 			{
-				if ( actor->client->pers.team ==
-				     BG_Buildable( self->s.modelindex )->team )
-				{
-					fate = BF_TEAMKILL;
-				}
-				else
-				{
-					fate = BF_DESTROY;
-				}
+				fate = G_OnSameTeam(self, actor) ?
+					BF_TEAMKILL : BF_DESTROY;
 			}
 			else
 			{
@@ -1047,10 +1040,9 @@ void G_LogDestruction( gentity_t *self, gentity_t *actor, int mod )
 	             mod == MOD_DECONSTRUCT ? "deconstructed" : "destroyed",
 	             actor->client ? actor->client->pers.netname : "<world>" );
 
-	if ( actor->client && actor->client->pers.team ==
-	     BG_Buildable( self->s.modelindex )->team )
+	if ( G_OnSameTeam( self, actor ) )
 	{
-		G_TeamCommand( (team_t) actor->client->pers.team,
+		G_TeamCommand( G_Team( actor ),
 		               va( "print_tr %s %s %s", mod == MOD_DECONSTRUCT ? QQ( N_("$1$ ^3DECONSTRUCTED^* by $2$\n") ) :
 						   QQ( N_("$1$ ^3DESTROYED^* by $2$\n") ),
 		                   Quote( BG_Buildable( self->s.modelindex )->humanName ),

src/sgame/sg_admin.cpp
@@ -5163,12 +5163,10 @@ bool G_admin_buildlog( gentity_t *ent )
 	int        time;
 	int        start = MAX_CLIENTS + level.buildId - level.numBuildLogs;
 	int        i = 0, j;
-	int        team;
-	bool   admin;
+	bool       admin = !ent || G_admin_permission( ent, "buildlog_admin" );
+	int        team = admin ? TEAM_NONE : G_Team( ent );
 	buildLog_t *log;
 
-	admin = !ent || G_admin_permission( ent, "buildlog_admin" );
-	team = admin ? TEAM_NONE : ent->client->pers.team;
 
 	if ( !admin && team == TEAM_NONE )
 	{

src/sgame/sg_weapon.cpp
@@ -1335,7 +1335,7 @@ static void FindZapChainTargets( zap_t *zap )
 		if ( ( ( enemy->client &&
 		         enemy->client->pers.team == TEAM_HUMANS ) ||
 		       ( enemy->s.eType == entityType_t::ET_BUILDABLE &&
-		         BG_Buildable( enemy->s.modelindex )->team == TEAM_HUMANS ) ) &&
+		         G_Team( enemy ) == TEAM_HUMANS ) ) &&
 		     Entities::IsAlive( enemy ) &&
 		     distance <= LEVEL2_AREAZAP_CHAIN_RANGE )
 		{
@@ -1515,7 +1515,7 @@ static void FireAreaZap( gentity_t *ent )
 
 	if ( ( traceEnt->client && traceEnt->client->pers.team == TEAM_HUMANS ) ||
 	     ( traceEnt->s.eType == entityType_t::ET_BUILDABLE &&
-	       BG_Buildable( traceEnt->s.modelindex )->team == TEAM_HUMANS ) )
+	       G_Team( traceEnt ) == TEAM_HUMANS ) )
 	{
 		CreateNewZap( ent, traceEnt );
 	}

commit 8b4204d43ea6c0c87bdc83d18c8d9751552a3494

Fix crash introduced in 1085aa62
Fixes https://github.com/Unvanquished/Unvanquished/issues/1592

src/sgame/sg_combat.cpp
@@ -1040,7 +1040,7 @@ void G_LogDestruction( gentity_t *self, gentity_t *actor, int mod )
 	             mod == MOD_DECONSTRUCT ? "deconstructed" : "destroyed",
 	             actor->client ? actor->client->pers.netname : "<world>" );
 
-	if ( G_OnSameTeam( self, actor ) )
+	if ( actor->client && G_OnSameTeam( self, actor ) )
 	{
 		G_TeamCommand( G_Team( actor ),
 		               va( "print_tr %s %s %s", mod == MOD_DECONSTRUCT ? QQ( N_("$1$ ^3DECONSTRUCTED^* by $2$\n") ) :