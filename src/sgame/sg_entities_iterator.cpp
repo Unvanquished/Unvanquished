687929b9564c879671b2b20511eca95f94e99df1
Antoine Fontaine <antoine.fontaine@epfl.ch>
2021-11-16 00:22:10 +0100

    Introduce iterate_client_entities and other client iterate_*

--- src/sgame/sg_entities_iterator.cpp
+++ src/sgame/sg_entities_iterator.cpp
@@ -6,3 +6,13 @@ static bool entity_in_use(const gentity_t *ent) {
 	return ent->inuse;
 }
 const entity_iterator iterate_entities(entity_in_use);
+
+
+// clients
+
+static bool entity_is_bot(const gentity_t *ent) {
+	return ent->inuse && (ent->r.svFlags & SVF_BOT);
+}
+
+const client_entity_iterator iterate_client_entities(entity_in_use);
+const client_entity_iterator iterate_bot_entities(entity_is_bot);
