8f0c21c8372c0854e1a1c8162a85a7f622415e6c
Morel Bérenger <berengermorel76@gmail.com>
2021-11-26 17:40:59 +0100

    remove BotFindRandomPointOnMesh

--- src/sgame/sg_bot_util.h
+++ src/sgame/sg_bot_util.h
@@ -137,7 +137,7 @@ int   DistanceToGoal2DSquared( const gentity_t *self );
 float BotGetGoalRadius( const gentity_t *self );
 void  BotFindRandomPoint( int botClientNum, vec3_t point );
 bool  BotFindRandomPointInRadius( int botClientNum, const vec3_t origin, vec3_t point, float radius );
-void  BotFindRandomPointOnMesh( const gentity_t *self, vec3_t point );
+
 bool  BotPathIsWalkable( const gentity_t *self, botTarget_t target );
 
 //configurable constants
