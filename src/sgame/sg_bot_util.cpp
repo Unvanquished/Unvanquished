7e7fd5f5497bd31803b1e991518eff09952c6c58
Morel Bérenger <berengermorel76@gmail.com>
2021-11-27 16:02:09 +0100

    glm: botRouteTarget_t::setPos

--- src/sgame/sg_bot_util.cpp
+++ src/sgame/sg_bot_util.cpp
@@ -1036,7 +1036,7 @@ void BotTargetToRouteTarget( const gentity_t *self, botTarget_t target, botRoute
 
 	glm::vec3 mins, maxs;
 
-	VectorCopy( target.getPos(), routeTarget->pos );
+	routeTarget->setPos( target.getPos() );
 	BotTargetGetBoundingBox( target, mins, maxs, routeTarget );
 
 	routeTarget->polyExtents[ 0 ] = std::max( Q_fabs( mins[ 0 ] ), maxs[ 0 ] );
@@ -1066,7 +1066,7 @@ void BotTargetToRouteTarget( const gentity_t *self, botTarget_t target, botRoute
 		glm::vec3 end = targetPos + 600.f * invNormal;
 		trap_Trace( &trace, targetPos, mins, maxs, end, target.getTargetedEntity()->s.number,
 		            CONTENTS_SOLID, MASK_ENTITY );
-		VectorCopy( trace.endpos, routeTarget->pos );
+		routeTarget->setPos( VEC2GLM( trace.endpos ) );
 	}
 	
 
