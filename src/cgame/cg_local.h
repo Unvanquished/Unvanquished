3ed8f83b2d36f223e24dd8e6444e4bf227533d6a
slipher <slipher@protonmail.com>
2022-01-09 02:29:52 -0600

    Delete function Rocket_SetElementDimensions

--- src/cgame/cg_local.h
+++ src/cgame/cg_local.h
@@ -2310,7 +2310,6 @@ void Rocket_RegisterDataFormatter( const char *name );
 void Rocket_DataFormatterRawData( int handle, char *name, int nameLength, char *data, int dataLength );
 void Rocket_DataFormatterFormattedData( int handle, const char *data, bool parseQuake );
 void Rocket_GetElementTag( char *tag, int length );
-void Rocket_SetElementDimensions( float x, float y );
 void Rocket_RegisterElement( const char *tag );
 void Rocket_SetAttribute( const char *name, const char *id, const char *attribute, const char *value );
 void Rocket_GetAttribute( const char *name, const char *id, const char *attribute, char *out, int length );
