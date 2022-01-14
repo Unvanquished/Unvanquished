f66423de8657fd00d8e304c68f2ca01797d99d4a
Morel Bérenger <berengermorel76@gmail.com>
2022-01-14 14:38:54 +0100

    no longer rely on heal routine for poison resupply

--- src/sgame/sg_bot_ai.cpp
+++ src/sgame/sg_bot_ai.cpp
@@ -1100,17 +1100,16 @@ static AINodeStatus_t BotActionReachHealA( gentity_t *self )
 		return STATUS_FAILURE;
 	}
 
-	// retrieve creep size to have proper distance
-	//TODO: remove the booster exception, which is made so that
-	//bots can have poison. Instead, a real resupply API should be
-	//done, so that bots would not go grab poison by sheer luck.
-	//Would also allow humans to get ammos, jetpack "fuel" and advgoons
-	//regen all their barbs
+	// retrieve creep size or booster's range to have proper distance
 	int targetType = self->botMind->goal.getTargetedEntity()->s.modelindex;
-	float dist = 0;
+	float dist = -1; // minus one to make sure to step on creep
 	if ( targetType != BA_A_BOOSTER )
 	{
-		dist = BG_Buildable( targetType )->creepSize - 1.f; // minus one to make sure to step on creep
+		dist += BG_Buildable( targetType )->creepSize;
+	}
+	else
+	{
+		dist += REGEN_BOOSTER_RANGE;
 	}
 
 	if ( !GoalInRange( self, dist ) )
