b760578002010118d92dc26b2392b549ccd88576
Morel Bérenger <berengermorel76@gmail.com>
2022-07-12 23:34:55 +0200

    handle BT failures in atomic navigation actions
    Non atomic actions (fight, repair, heal, flee, buy, equip) are
    explicitly not handled because they could, and should, be implemented by
    the BT itself, with actions such as: aimAtGoal, changeGoal, fireWeapon,
    moveTo, moveToGoal, etc.

--- src/sgame/sg_bot_ai.cpp
+++ src/sgame/sg_bot_ai.cpp
@@ -637,8 +637,11 @@ AINodeStatus_t BotActionAimAtGoal( gentity_t *self, AIGenericNode_t* )
 
 AINodeStatus_t BotActionMoveToGoal( gentity_t *self, AIGenericNode_t* )
 {
-	BotMoveToGoal( self );
-	return STATUS_RUNNING;
+	if ( GoalInRange( self, BotGetGoalRadius( self ) ) )
+	{
+		return STATUS_SUCCESS;
+	}
+	return BotMoveToGoal( self ) ? STATUS_RUNNING : STATUS_FAILURE;
 }
 
 AINodeStatus_t BotActionMoveInDir( gentity_t *self, AIGenericNode_t *node )
@@ -919,13 +922,11 @@ AINodeStatus_t BotActionRoamInRadius( gentity_t *self, AIGenericNode_t *node )
 		self->botMind->currentNode = node;
 	}
 
-	if ( self->botMind->nav.directPathToGoal && GoalInRange( self, 70 ) )
+	if ( GoalInRange( self, BotGetGoalRadius( self ) ) )
 	{
 		return STATUS_SUCCESS;
 	}
-	BotMoveToGoal( self );
-
-	return STATUS_RUNNING;
+	return BotMoveToGoal( self ) ? STATUS_RUNNING : STATUS_FAILURE;
 }
 
 AINodeStatus_t BotActionRoam( gentity_t *self, AIGenericNode_t *node )
@@ -941,15 +942,11 @@ AINodeStatus_t BotActionRoam( gentity_t *self, AIGenericNode_t *node )
 		self->botMind->currentNode = node;
 	}
 
-	if ( self->botMind->nav.directPathToGoal && GoalInRange( self, 70 ) )
+	if ( GoalInRange( self, BotGetGoalRadius( self ) ) )
 	{
 		return STATUS_SUCCESS;
 	}
-	else
-	{
-		BotMoveToGoal( self );
-	}
-	return STATUS_RUNNING;
+	return BotMoveToGoal( self ) ? STATUS_RUNNING : STATUS_FAILURE;
 }
 
 static botTarget_t BotGetMoveToTarget( gentity_t *self, AIEntity_t e )
@@ -989,19 +986,16 @@ AINodeStatus_t BotActionMoveTo( gentity_t *self, AIGenericNode_t *node )
 		return STATUS_FAILURE;
 	}
 
-	BotMoveToGoal( self );
-
 	if ( radius == 0 )
 	{
 		radius = BotGetGoalRadius( self );
 	}
 
-	if ( DistanceToGoal2DSquared( self ) <= Square( radius ) && self->botMind->nav.directPathToGoal )
+	if ( GoalInRange( self, radius ) )
 	{
 		return STATUS_SUCCESS;
 	}
-
-	return STATUS_RUNNING;
+	return BotMoveToGoal( self ) ? STATUS_RUNNING : STATUS_FAILURE;
 }
 
 AINodeStatus_t BotActionRush( gentity_t *self, AIGenericNode_t *node )
@@ -1024,11 +1018,11 @@ AINodeStatus_t BotActionRush( gentity_t *self, AIGenericNode_t *node )
 		return STATUS_FAILURE;
 	}
 
-	if ( !GoalInRange( self, 100 ) )
+	if ( GoalInRange( self, BotGetGoalRadius( self ) ) )
 	{
-		BotMoveToGoal( self );
+		return STATUS_SUCCESS;
 	}
-	return STATUS_RUNNING;
+	return BotMoveToGoal( self ) ? STATUS_RUNNING : STATUS_FAILURE;
 }
 
 AINodeStatus_t BotActionHealA( gentity_t *self, AIGenericNode_t *node );
