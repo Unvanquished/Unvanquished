Unvanquished/Unvanquished

commit ddd45feeb7791d4c52da16b6f09250552b650d6a

fix target_push when angle is used instead of target

src/sgame/sg_spawn_shared.cpp
@@ -69,6 +69,10 @@ void think_aimAtTarget( gentity_t *self )
 	VectorAdd( self->r.absmin, self->r.absmax, origin );
 	VectorScale( origin, 0.5f, origin );
 
+	if ( self->s.origin2[0] != 0.f || self->s.origin2[1] != 0.f || self->s.origin2[2] != 0.f )
+	{
+		return;
+	}
 	pickedTarget = G_PickRandomTargetFor( self );
 
 	if ( !pickedTarget )

commit 8de3091d1adf447b87b571b8edc71cc4a09bdcbf

fix pushers now working correctly when having a target
This is a regression introduced in ddd45feeb7791d4c52da16b6f09250552b650d6a

src/sgame/sg_spawn_shared.cpp
@@ -69,7 +69,17 @@ void think_aimAtTarget( gentity_t *self )
 	VectorAdd( self->r.absmin, self->r.absmax, origin );
 	VectorScale( origin, 0.5f, origin );
 
-	if ( self->s.origin2[0] != 0.f || self->s.origin2[1] != 0.f || self->s.origin2[2] != 0.f )
+	bool hasTarget = false;
+	for ( char* target : self->targets )
+	{
+		if ( target && target[0] )
+		{
+			hasTarget = true;
+			break;
+		}
+	}
+
+	if ( !hasTarget )
 	{
 		return;
 	}