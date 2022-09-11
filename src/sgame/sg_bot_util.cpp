Unvanquished/Unvanquished

commit ab1968e9a330777bc43b12c35cdd2d11ec4a355c

BotEvolveToClass: exit early if we are not actually trying to evolve
Benchmarking suggests this function takes 1% of the CPU time in
daemonded. If we don't need to evolve at all, we can avoid doing
moderately expensive collision detection traces to see if we can fit.

src/sgame/sg_bot_util.cpp
@@ -1915,13 +1915,18 @@ bool BotEvolveToClass( gentity_t *ent, class_t newClass )
 	int clientNum;
 	int i;
 	vec3_t infestOrigin;
-	class_t currentClass = ent->client->pers.classSelection;
+	class_t currentClass = static_cast<class_t>( ent->client->ps.stats[ STAT_CLASS ] );
 	evolveInfo_t evolveInfo;
 	int entityList[ MAX_GENTITIES ];
 	vec3_t range = { AS_OVER_RT3, AS_OVER_RT3, AS_OVER_RT3 };
 	vec3_t mins, maxs;
 	int num;
 	gentity_t *other;
+
+	if ( currentClass == newClass )
+	{
+		return true;
+	}
 
 	if ( Entities::IsDead( ent ) )
 	{

commit 5f87f1fb71d09781ff0d03db9f88a7eee466df22

Bugfix: Fix bot action evolve and refactor bot evolve to allow for same-class evolve
* Closes https://github.com/Unvanquished/Unvanquished/issues/2181

* Fixes the bug where alien bots wouldn't fight until they got hurt,
  introduced in ab1968e9a3 "BotEvolveToClass: exit early if we are not actually trying to evolve"

src/sgame/sg_bot_util.cpp
@@ -509,10 +509,7 @@ bool BotCanEvolveToClass( const gentity_t *self, class_t newClass )
 		return false;
 	}
 
-	// TODO: one might be willing to allow switching to same cost classes,
-	// notably, from dretch to advanced granger when base in on fire.
-
-	return cl->canBuyNow() && info.evolveCost > 0 // no devolving
+	return cl->canBuyNow()
 		&& self->client->ps.persistant[PERS_CREDIT] >= info.evolveCost;
 }
 
@@ -542,11 +539,16 @@ float PercentAmmoRemaining( weapon_t weapon, playerState_t const* ps )
 	}
 }
 
-AINodeStatus_t BotActionEvolve ( gentity_t *self, AIGenericNode_t* )
+AINodeStatus_t BotActionEvolve( gentity_t *self, AIGenericNode_t* )
 {
+	class_t currentClass = static_cast<class_t>( self->client->ps.stats[ STAT_CLASS ] );
+
 	for ( auto const& cl : classes )
 	{
-		if ( BotEvolveToClass( self, cl.item ) )
+		evolveInfo_t info = BG_ClassEvolveInfoFromTo( currentClass, cl.item );
+
+		if ( info.evolveCost > 0 // no devolving or evolving to the same
+				&& BotEvolveToClass( self, cl.item ) )
 		{
 			return STATUS_SUCCESS;
 		}

src/shared/bg_misc.cpp
@@ -446,7 +446,7 @@ evolveInfo_t BG_ClassEvolveInfoFromTo( const int from, const int to )
 	evolveCost = toCost - fromCost;
 
 	isDevolving = evolveCost <= 0;
-	// exception for peolpe evolving to dretch
+	// exception for people evolving to dretch
 	if ( ( from == PCL_ALIEN_BUILDER0 || from == PCL_ALIEN_BUILDER0_UPG ) && to == PCL_ALIEN_LEVEL0 ) {
 		isDevolving = false;
 	}