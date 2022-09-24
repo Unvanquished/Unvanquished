6bb653cf611111dec4e5da869b6d875ae89c5981
DolceTriade <vcelestialragev@gmail.com>
2022-09-24 02:25:02 -0700

    advmantis: Allow ckit to stop corrosion

--- src/sgame/components/CorrodibleComponent.cpp
+++ src/sgame/components/CorrodibleComponent.cpp
@@ -2,11 +2,12 @@
 
 constexpr int CORRODE_TIME = 30000;
 constexpr float CORRODE_DAMAGE = 5.0f;
+constexpr int CORRODE_HEAL_RATE = 10000;
 
 CorrodibleComponent::CorrodibleComponent(Entity& entity, ThinkingComponent& r_ThinkingComponent)
 	: CorrodibleComponentBase(entity, r_ThinkingComponent),
 	  corrode_(false),
-	  corrodeTime_(0) {
+	  corrodeEndTime_(0) {
 	REGISTER_THINKER(Damage, ThinkingComponent::SCHEDULER_AVERAGE, 500);
 }
 
@@ -21,13 +22,19 @@ void CorrodibleComponent::HandlePrepareNetCode() {
 void CorrodibleComponent::HandleCorrode(bool c) {
 	corrode_ = c;
 	if (c) {
-		corrodeTime_ = level.time;
+		corrodeEndTime_ = level.time + CORRODE_TIME;
 	}
+}
+
+void CorrodibleComponent::HandleHeal(float amount, gentity_t* source) {
+	// Make sure a player explicitly heals.
+	if (!corrode_ || !source || !source->client) return;
+	corrodeEndTime_ -= CORRODE_HEAL_RATE;
 }
 
 void CorrodibleComponent::Damage(int timeDelta) {
 	if (!corrode_) return;
-	if (corrodeTime_ + CORRODE_TIME < level.time) {
+	if (corrodeEndTime_ < level.time) {
 		HandleCorrode(false);
 		return;
 	}
