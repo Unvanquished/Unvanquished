Unvanquished/Unvanquished

commit 84f7dda399ef9c6576ad6c7aa53ae7e6ff5b3fbc

prevent trying to give momentum to a non-playable team

src/sgame/sg_cmds.cpp
@@ -593,7 +593,7 @@ void Cmd_Give_f( gentity_t *ent )
 	char     *name;
 	bool give_all = false;
 	float    amount;
-	team_t team = static_cast<team_t>( ent->client->pers.team );
+	team_t team = G_Team( ent );
 
 	if ( trap_Argc() < 2 )
 	{
@@ -644,13 +644,16 @@ void Cmd_Give_f( gentity_t *ent )
 		{
 			++end;
 			team = BG_PlayableTeamFromString( end );
-		}
-		if ( team == team_t::TEAM_NONE )
-		{
-			team = static_cast<team_t>( ent->client->pers.team );
+			if ( team != TEAM_NONE )
+			{
+				team = G_Team( ent );
+			}
 		}
 
-		G_AddMomentumGeneric( team, amount );
+		if ( team != TEAM_NONE )
+		{
+			G_AddMomentumGeneric( team, amount );
+		}
 	}
 
 	if ( team != TEAM_NONE && Q_strnicmp( name, "bp", strlen("bp") ) == 0 )

commit a14b56d5e961334a29006c53594487a25ae6568a

fix condition inversion
This prevented "/give momentum [amount] [team]" to work, while it was
still working without any team specified.
Regression introduced by 84f7dda399ef9c6576ad6c7aa53ae7e6ff5b3fbc

src/sgame/sg_cmds.cpp
@@ -644,7 +644,7 @@ static void Cmd_Give_f( gentity_t *ent )
 		{
 			++end;
 			team = BG_PlayableTeamFromString( end );
-			if ( team != TEAM_NONE )
+			if ( team == TEAM_NONE )
 			{
 				team = G_Team( ent );
 			}