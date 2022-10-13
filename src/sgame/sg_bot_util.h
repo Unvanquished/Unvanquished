0531ac3bf7cbe1b244795e38c015450bcf169408
Antoine Fontaine <antoine.fontaine@epfl.ch>
2022-10-13 18:59:25 +0200

    Revert "Partial revert of #1532"
    This reverts commit 0072e4c15592dbfa43bedc94f6171ac462cc6588.

--- src/sgame/sg_bot_util.h
+++ src/sgame/sg_bot_util.h
@@ -47,6 +47,9 @@ gentity_t* BotFindBuilding( gentity_t *self, int buildingType, int range );
 bool   BotTeamateHasWeapon( gentity_t *self, int weapon );
 void       BotSearchForEnemy( gentity_t *self );
 void       BotPain( gentity_t *self, gentity_t *attacker, int damage );
+botEntityAndDistance_t BotGetClosestBuildingAmongTypes(
+		gentity_t *self, const std::initializer_list<buildable_t> buildables );
+const gentity_t *BotGetHealTarget( gentity_t *self );
 
 // aiming
 glm::vec3 BotGetIdealAimLocation( gentity_t *self, const botTarget_t &target );
