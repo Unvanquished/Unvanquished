Unvanquished/Unvanquished

commit 0a2d03ef4b480b6bd2f2c0d3b8e3a668393a5687

glm: migrate sg_spawn_gfx.cpp

src/sgame/sg_spawn_gfx.cpp
@@ -35,6 +35,7 @@ Maryland 20850 USA.
 #include "sg_local.h"
 #include "sg_spawn.h"
 
+#include <glm/geometric.hpp>
 /*
 ======================================================================
 
@@ -103,42 +104,35 @@ findEmptySpot
 Finds an empty spot radius units from origin
 ==============
 */
-static void findEmptySpot( vec3_t origin, float radius, vec3_t spot )
+static void findEmptySpot( glm::vec3 const& origin, float radius, glm::vec3& spot )
 {
-	int     i, j, k;
-	vec3_t  delta, test, total;
-	trace_t trace;
-
-	VectorClear( total );
-
+	glm::vec3 total;
 	//54(!) traces to test for empty spots
-	for ( i = -1; i <= 1; i++ )
+	for ( int i = -1; i <= 1; i++ )
 	{
-		for ( j = -1; j <= 1; j++ )
+		for ( int j = -1; j <= 1; j++ )
 		{
-			for ( k = -1; k <= 1; k++ )
+			for ( int k = -1; k <= 1; k++ )
 			{
-				VectorSet( delta, ( i * radius ),
-				           ( j * radius ),
-				           ( k * radius ) );
+				glm::vec3 delta( i, j, k );
+				delta *= radius;
 
-				VectorAdd( origin, delta, test );
+				glm::vec3 test = origin + delta;
 
-				trap_Trace( &trace, test, nullptr, nullptr, test, ENTITYNUM_NONE, MASK_SOLID, 0 );
+				trace_t trace;
+				trap_Trace( &trace, &test[0], nullptr, nullptr, &test[0], ENTITYNUM_NONE, MASK_SOLID, 0 );
 
 				if ( !trace.allsolid )
 				{
-					trap_Trace( &trace, test, nullptr, nullptr, origin, ENTITYNUM_NONE, MASK_SOLID, 0 );
-					VectorScale( delta, trace.fraction, delta );
-					VectorAdd( total, delta, total );
+					trap_Trace( &trace, &test[0], nullptr, nullptr, &origin[0], ENTITYNUM_NONE, MASK_SOLID, 0 );
+					delta *= trace.fraction;
+					total += delta;
 				}
 			}
 		}
 	}
 
-	VectorNormalize( total );
-	VectorScale( total, radius, total );
-	VectorAdd( origin, total, spot );
+	spot = origin + glm::normalize( total ) * radius;
 }
 
 void SP_gfx_light_flare( gentity_t *self )
@@ -149,7 +143,9 @@ void SP_gfx_light_flare( gentity_t *self )
 
 	//try to find a spot near to the flare which is empty. This
 	//is used to facilitate visibility testing
-	findEmptySpot( self->s.origin, 8.0f, self->s.angles2 );
+	glm::vec3 angles2 = VEC2GLM( self->s.angles2 );
+	findEmptySpot( VEC2GLM( self->s.origin ), 8.0f, angles2 );
+	VectorCopy( &angles2[0], self->s.angles2 );
 
 	self->act = gfx_light_flare_toggle;
 

commit c9b08713e9a65c20fc2040fe3b68e8e0d6bf8912

Fix unitialised value warning
In member function ‘constexpr glm::vec<3, T, Q>& glm::vec<3, T, Q>::operator+=(const glm::vec<3, U, Q>&) [with U = float; T = float; glm::qualifier Q = glm::packed_highp]’,
    inlined from ‘void findEmptySpot(const vec3&, float, glm::vec3&)’ at /home/afontain/unv/Unvanquished/src/sgame/sg_spawn_gfx.cpp:129:15,
    inlined from ‘void SP_gfx_light_flare(gentity_t*)’ at /home/afontain/unv/Unvanquished/src/sgame/sg_spawn_gfx.cpp:147:15:
/home/afontain/unv/Unvanquished/libs/glm/glm/./ext/../detail/type_vec3.inl:248:25: warning: ‘total.glm::vec<3, float, glm::packed_highp>::<anonymous>.glm::vec<3, float,
 glm::packed_highp>::<unnamed union>::x’ is used uninitialized [-Wuninitialized]                                                                                          248 |                 this->x += static_cast<T>(v.x);
      |                 ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~
/home/afontain/unv/Unvanquished/src/sgame/sg_spawn_gfx.cpp: In function ‘void SP_gfx_light_flare(gentity_t*)’:
/home/afontain/unv/Unvanquished/src/sgame/sg_spawn_gfx.cpp:109:19: note: ‘total’ declared here
  109 |         glm::vec3 total;
      |                   ^~~~~

This regression was introduced in 0a2d03e

src/sgame/sg_spawn_gfx.cpp
@@ -106,7 +106,7 @@ Finds an empty spot radius units from origin
 */
 static void findEmptySpot( glm::vec3 const& origin, float radius, glm::vec3& spot )
 {
-	glm::vec3 total;
+	glm::vec3 total = {};
 	//54(!) traces to test for empty spots
 	for ( int i = -1; i <= 1; i++ )
 	{