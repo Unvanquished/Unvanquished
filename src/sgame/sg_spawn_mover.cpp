e443cd5d702192bf22af293f0e5a11d7d4ce79ba
Antoine Fontaine <antoine.fontaine@epfl.ch>
2023-01-09 20:17:12 +0100

    Fix bot door regression.
    See the code comment for the rationale

--- src/sgame/sg_spawn_mover.cpp
+++ src/sgame/sg_spawn_mover.cpp
@@ -558,7 +558,7 @@ bool IsDoor( const gentity_t *ent )
 		if ( !Q_stricmp( fn, ent->classname ) )
 		{
 			return true;
-			}
+		}
 	}
 	return false;
 }
@@ -600,8 +600,10 @@ void BotHandleDoor( gentity_t *ent, moverState_t moverState )
 {
 	switch ( moverState )
 	{
+		// See comment below for MOVER_POS2
 		case MOVER_POS1:
 		case ROTATOR_POS1:
+		case ROTATOR_POS2:
 			if ( IsDoor( ent ) )
 			{
 				G_BotRemoveObstacle( ent->num() );
@@ -612,8 +614,17 @@ void BotHandleDoor( gentity_t *ent, moverState_t moverState )
 			}
 			break;
 
+		// We add regular opened doors as obstacles even if the doors are not automatic to avoid bots
+		// walking through the door pane, when it's opened (it's
+		// considered an obstacle because the door can't open further).
+		// See https://github.com/Unvanquished/Unvanquished/issues/2358
+		//
+		// We can't do that for rotating doors as this would break
+		// round "iris" doors, the axis-aligned bounding box would
+		// disable too much navmesh, making passing through the door
+		// difficult.
+		// https://github.com/Unvanquished/Unvanquished/pull/2360#issuecomment-1376145592
 		case MOVER_POS2:
-		case ROTATOR_POS2:
 			if ( IsDoor( ent ) )
 			{
 				G_BotRemoveObstacle( ent->num() );
