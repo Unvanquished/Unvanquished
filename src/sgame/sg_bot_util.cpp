d328712af885e8f12dec17cc38ecaeef20bcb27f
Antoine Fontaine <antoine.fontaine@epfl.ch>
2023-02-10 12:13:44 +0100

    Bot: remove aimShake and aimSlowness which were never implemented

--- src/sgame/sg_bot_util.cpp
+++ src/sgame/sg_bot_util.cpp
@@ -2432,9 +2432,6 @@ void BotSetSkillLevel( gentity_t *self, int skill )
 	ASSERT( skill >= MIN_SKILL && skill <= MAX_SKILL );
 
 	self->botMind->botSkill.level = skill;
-	// TODO: different aim for different teams
-	self->botMind->botSkill.aimSlowness = ( float ) skill / 10;
-	self->botMind->botSkill.aimShake = 10 - skill;
 
 	std::pair<std::string, skillSet_t>
 		pair = BotDetermineSkills(self, skill);
