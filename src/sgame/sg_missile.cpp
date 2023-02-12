28564c1ae487f6fc001a8b7d1d5aa52da7c295a9
Morel Bérenger <berengermorel76@gmail.com>
2023-02-12 20:30:20 +0100

    missiles deal damages to entities, too

--- src/sgame/sg_missile.cpp
+++ src/sgame/sg_missile.cpp
@@ -379,7 +379,7 @@ static void MissileImpact( gentity_t *ent, trace_t *trace )
 	// Deal impact damage.
 	if ( !( impactFlags & MIF_NO_DAMAGE ) )
 	{
-		if ( ent->damage && Entities::IsAlive( hitEnt ) )
+		if ( ent->damage && ( Entities::IsAlive( hitEnt ) || ( hitEnt && hitEnt->s.eType == entityType_t::ET_MOVER ) ) )
 		{
 			vec3_t dir;
 
