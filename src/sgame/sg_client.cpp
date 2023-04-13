c7dcc8d6ed6863aa31d2204acaf4e2bd5a117ffb
Thomas Debesse <dev@illwieckz.net>
2023-04-13 22:50:07 +0200

    sg_client: use cp_tr to make a string translatable

--- src/sgame/sg_client.cpp
+++ src/sgame/sg_client.cpp
@@ -1151,7 +1151,8 @@ const char *ClientConnect( int clientNum, bool firstTime )
 			if ( !G_ClientIsLagging( level.clients + i ) )
 			{
 				G_LogPrintf( "Duplicate GUID: %i", i );
-				trap_SendServerCommand( i, "cp \"Your GUID is not secure\"" );
+				trap_SendServerCommand( i, "cp_tr "
+					QQ( N_("Your GUID is not secure") ) );
 				G_LogPrintf( "FailedConnecting: %i [%s] (%s) \"%s^*\" \"%s^*\"",
 				             clientNum, client->pers.ip.str[0] ? client->pers.ip.str : "127.0.0.1", client->pers.guid,
 				             client->pers.netname,
