505f26d346e22bd3bf4ee638676d11909b2ac9a1
Morel Bérenger <berengermorel76@gmail.com>
2023-04-15 02:58:26 +0200

    PM_AddTouchEnt, PM_ClipVelocity constness

--- src/shared/bg_local.h
+++ src/shared/bg_local.h
@@ -52,8 +52,5 @@ extern  pmove_t *pm;
 #define pm_waterfriction     (1.125f)
 #define pm_spectatorfriction (5.0f)
 
-void            PM_ClipVelocity( const vec3_t in, const vec3_t normal, vec3_t out );
-void            PM_AddTouchEnt( int entityNum );
-
 //==================================================================
 #endif /* BG_LOCAL_H_ */
