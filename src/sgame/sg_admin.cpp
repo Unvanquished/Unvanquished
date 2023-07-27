c846cf0b82081098307aa8e988b8a873063f645a
Morel Bérenger <berengermorel76@gmail.com>
2023-07-27 23:34:43 +0200

    sg_admin.cpp add missing include

--- src/sgame/sg_admin.cpp
+++ src/sgame/sg_admin.cpp
@@ -33,6 +33,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
+#include <bitset>
+
 #include "sg_local.h"
 
 #include <glm/gtx/norm.hpp>
