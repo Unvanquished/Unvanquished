c0a02cecadc375930cde2f02c6cc08e4efc7f2c3
slipher <slipher@protonmail.com>
2023-08-18 14:35:12 -0500

    Nit: use BOT_DEFAULT_BEHAVIOR constant

--- src/sgame/sg_bot.cpp
+++ src/sgame/sg_bot.cpp
@@ -240,7 +240,7 @@ bool G_BotSetBehavior( botMemory_t *botMind, Str::StringRef behavior )
 	if ( !botMind->behaviorTree )
 	{
 		Log::Warn( "Problem when loading behavior tree %s, trying default", behavior );
-		botMind->behaviorTree = ReadBehaviorTree( "default", &treeList );
+		botMind->behaviorTree = ReadBehaviorTree( BOT_DEFAULT_BEHAVIOR, &treeList );
 
 		if ( !botMind->behaviorTree )
 		{
