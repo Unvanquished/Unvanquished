Unvanquished/Unvanquished

commit c4d3db81b4e86a108621615e311229cfc74125cd

Don't give spectators WP_ALEVEL0
This fixes the alien HUD being inappropriately drawn at various times,
e.g. for spectators after a map_restart.

Also remove the onSpawn event type for BSP entities. This seems to be a
remnant of Q3; you can't spawn from a map entity in Unvanquished.

src/sgame/sg_client.cpp
@@ -1782,27 +1782,6 @@ void ClientSpawn( gentity_t *ent, gentity_t *spawn, const vec3_t origin, const v
 	{
 		MoveClientToIntermission( ent );
 	}
-	else
-	{
-		// fire the targets of the spawn point
-		if ( !spawn && spawnPoint )
-		{
-			G_EventFireEntity( spawnPoint, ent, ON_SPAWN );
-		}
-
-		// select the highest weapon number available, after any
-		// spawn given items have fired
-		client->ps.weapon = 1;
-
-		for ( i = WP_NUM_WEAPONS - 1; i > WP_NONE; i-- )
-		{
-			if ( BG_InventoryContainsWeapon( i, client->ps.stats ) )
-			{
-				client->ps.weapon = i;
-				break;
-			}
-		}
-	}
 
 	// run a client frame to drop exactly to the floor,
 	// initialize animations and other things

src/sgame/sg_entities.cpp
@@ -527,7 +527,6 @@ static const entityCallEventDescription_t gentityEventDescriptions[] =
 		{ "onFree",      ON_FREE      },
 		{ "onReach",     ON_REACH     },
 		{ "onReset",     ON_RESET     },
-		{ "onSpawn",     ON_SPAWN     },
 		{ "onTouch",     ON_TOUCH     },
 		{ "onUse",       ON_USE       },
 		{ "target",      ON_DEFAULT   },

src/sgame/sg_entities.h
@@ -190,8 +190,6 @@ enum gentityCallEvent_t
 	ON_ENABLE,
 	ON_DISABLE,
 
-	ON_SPAWN
-
 };
 
 enum initEntityStyle_t

commit a87b3ce7f6e158d2d8d2f27ba4f7a2d5ee6bd67f

Fix alien attacks not working
Regression in c4d3db81b4e86a108621615e311229cfc74125cd "Don't give
spectators WP_ALEVEL0"

src/sgame/sg_client.cpp
@@ -1753,6 +1753,10 @@ void ClientSpawn( gentity_t *ent, gentity_t *spawn, const vec3_t origin, const v
 		{
 			G_ForceWeaponChange( ent, weapon );
 		}
+		else
+		{
+			client->ps.weapon = weapon;
+		}
 
 		client->ps.weaponstate = WEAPON_READY;
 	}