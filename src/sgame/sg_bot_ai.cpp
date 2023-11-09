dc8decc9b20ab62c5100bdc4800bee62bd68d961
Sweet <Sweet@Sweet>
2023-11-09 16:06:07 +0100

    Revert "make bot action buildNowChosenBuildable fail when not IBE_NONE"
    This reverts commit 7b2552f02bb595aa15113e2e1317ff9ef73d59c2.

--- src/sgame/sg_bot_ai.cpp
+++ src/sgame/sg_bot_ai.cpp
@@ -1289,13 +1289,7 @@ static bool build( gentity_t *self, buildable_t toBuild )
 		G_ForceWeaponChange( self, WP_HBUILD );
 	}
 
-	if ( SB_BUILDABLE_TO_IBE( self->client->ps.stats[ STAT_BUILDABLE ] ) != IBE_NONE )
-	{
-		return false;
-	}
-
-	self->client->ps.stats[ STAT_BUILDABLE ] &= ~SB_BUILDABLE_MASK;
-	self->client->ps.stats[ STAT_BUILDABLE ] |= toBuild;
+	self->client->ps.stats[ STAT_BUILDABLE ] = toBuild;
 	if ( self->client->ps.stats[ STAT_MISC ] == 0 )
 	{
 		BotFireWeapon( WPM_PRIMARY, &self->botMind->cmdBuffer );
