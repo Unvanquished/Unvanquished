5ed8a8553cf34441422a619470ccbbaa91cd3ee5
RedSky <redsky.git@gmail.com>
2024-01-29 19:56:20 +0100

    Refactor most of menu logic to CircleMenu object
    A shallow Object Oriented abstraction of circle menus.
    Concrete implementation derive from CircleMenu prototype.
    They must handle data acquisition, displaying info and option selection.
    In turn, objects inherit from prototype all mechanisms for building menu
    and all major event handlers.
    
    All these mechanisms centralised in CircleMenu object's methods, but can
    be easily overwritten to handle exceptional behaviour.

---
+++ pkg/unvanquished_src.dpkdir/ui/lua/circle_menu.lua
@@ -0,0 +1,112 @@
+CircleMenu = {
+	-- Default values
+	menuRadiusEm = 10,	-- radius on which buttons are placed
+	hintsRadiusEm = 6.3,	-- radius on which hints are placed
+}
+
+CircleMenu.__index = CircleMenu
+
+function CircleMenu:New(handle, idPrefix) 
+	local instance = { rt = {} }
+	setmetatable(instance, self)
+	instance.idPrefix = idPrefix
+	instance.handle = handle
+
+	return instance
+end
+
+function CircleMenu:Bind(document)
+	self.document = document
+	self.menuElement = document:GetElementById(self.idPrefix .. 'Menu')
+	self.keyHintsElement = document:GetElementById(self.idPrefix .. 'KeyboardHints')
+	self.backdropElement = document:GetElementById(self.idPrefix .. 'Backdrop')
+end
+
+function CircleMenu:BuildMenu()
+	local menu_rml, hints_rml
+	menu_rml= string.format(
+		'<button class="cancelButton" onmouseover="%s:Highlight(0)" onclick="document:Hide()"><span>X</span></button>', 
+		self.handle)
+	hints_rml = ''
+	local tau = 2 * math.pi / #self.items
+	for i=1,#self.items do
+		local x, y = NormalCartesian(tau * (i - 1))
+		menu_rml = menu_rml .. string.format(
+			'<div style="position: absolute; left: %.1fem; top: %.1fem;">%s</div>',
+			x * self.menuRadiusEm, y * self.menuRadiusEm, self:RMLButton(i)
+		)
+		hints_rml = hints_rml .. string.format(
+			'<div style="position: absolute; left: %.1fem; top: %.1fem;"><p class="key">%d</p></div>',
+			x * self.hintsRadiusEm, y * self.hintsRadiusEm, i % 10
+		)
+	end
+	self.menuElement.inner_rml = menu_rml
+	self.keyHintsElement.inner_rml = hints_rml
+end
+
+function CircleMenu:Build(document)
+	self:Bind(document)
+	self:PopulateItems()
+	self:Highlight(nil) -- clear info
+	self:BuildMenu()
+end
+
+
+-- EVENT HANDLERS
+function CircleMenu:Hover(index)
+	self:Highlight(index)
+end
+
+function CircleMenu:Click(index, event)
+	self:Select(index)
+end
+
+function CircleMenu:BackdropCapture(event)
+	local index = CirclemenuMouseToIndex(self.document, event, #self.items)
+	-- Cancel button is first
+	self:Highlight(index)
+	self.menuElement.child_nodes[index+1].first_child:Focus()
+end
+
+function CircleMenu:BackdropClick(event)
+	local index = CirclemenuMouseToIndex(self.document, event, #self.items)
+	-- Cancel button is first
+	self.menuElement.child_nodes[index+1].first_child:Click()
+end
+
+function CircleMenu:HandleKey(event)
+	local key = event.parameters["key_identifier"]
+	local index
+	if key == rmlui.key_identifier["0"] then
+		index = 10
+	elseif key >= rmlui.key_identifier["1"] and key <= rmlui.key_identifier["9"] then
+		index = key - rmlui.key_identifier["1"] + 1
+	else
+		detectEscape(event, self.document)
+		return
+	end
+	if index <= #self.items then
+		-- event:StopImmediatePropagation()
+		event:StopPropagation()
+		self.menuElement.child_nodes[index+1].first_child:Click()
+	end
+end
+
+
+-- PLACEHOLDER METHODS
+
+function CircleMenu:PopulateItems()
+	-- Placeholder method for obtaining data to create menu items
+end
+
+function CircleMenu:RMLButton(index)
+	-- Placeholder method for buidling RML of a button
+end
+
+function CircleMenu:Select(index)
+	-- Placeholder method for picking an option
+end
+
+function CircleMenu:Highlight(index)
+	-- Placeholder method for highlighting a option
+end
