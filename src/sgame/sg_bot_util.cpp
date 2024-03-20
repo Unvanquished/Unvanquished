630ab3634fadc3ddcd92d9008e830e8baf90e161
Sweet <Sweet@Sweet>
2024-03-20 16:16:58 +0100

    add bot action extinguishFire, add extinguish_fire.bt, use them

--- src/sgame/sg_bot_util.cpp
+++ src/sgame/sg_bot_util.cpp
@@ -857,6 +857,7 @@ void BotFindClosestBuildings( gentity_t *self )
 void BotFindDamagedFriendlyStructure( gentity_t *self )
 {
 	float minDistSqr;
+	team_t team = G_Team( self );
 
 	gentity_t *target;
 	self->botMind->closestDamagedBuilding.ent = nullptr;
@@ -883,7 +884,12 @@ void BotFindDamagedFriendlyStructure( gentity_t *self )
 			continue;
 		}
 
-		if ( Entities::HasFullHealth(target) )
+		if ( team == TEAM_HUMANS && Entities::HasFullHealth(target) )
+		{
+			continue;
+		}
+
+		if ( team == TEAM_ALIENS && !G_IsOnFire( target ) )
 		{
 			continue;
 		}
