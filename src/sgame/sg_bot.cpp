699243d861876d1a434d6df1b79f4c4e9d58c6e4
slipher <slipher@protonmail.com>
2024-04-08 19:06:18 -0500

    Reset botMemory_t fields on some state transitions
    Ensure some fields are properly reset on behavior tree change, e.g.
    goal, lastNavconTime, myTimer, and (needed if the bot is dead)
    blackboardTransient. Resetting myTimer fixes a bug pointed out by Sweet:
    if a bot switches to our default behavior tree from any other one, it
    instantly commits suicide (rather than waiting 20 seconds as intended).
    
    Ensure some fields are reset on respawn, e.g. exhausted and
    ones related to stuck state.
    
    Ensure userSpecifiedClientNum and userSpecifiedLocation are cleared if a
    bot connects in some client slot a second time.
    
    Don't reset enemyQueue on behavior change as its operation does not
    depend on the behavior.
    
    Reorganize the botMemory_t struct based on the lifetimes of its fields.

--- src/sgame/sg_bot.cpp
+++ src/sgame/sg_bot.cpp
@@ -219,10 +219,9 @@ void G_BotChangeBehavior( int clientNum, Str::StringRef behavior )
 
 bool G_BotSetBehavior( botMemory_t *botMind, Str::StringRef behavior )
 {
-	botMind->runningNodes.clear();
-	botMind->currentNode = nullptr;
-	botMind->clearNav();
-	BotResetEnemyQueue( &botMind->enemyQueue );
+	G_Bot_ResetBehaviorState( *botMind );
+	botMind->blackboardTransient = 0;
+	botMind->myTimer = level.time;
 
 	botMind->behaviorTree = ReadBehaviorTree( behavior.c_str(), &treeList );
 
@@ -245,6 +244,7 @@ bool G_BotSetDefaults( int clientNum, team_t team, Str::StringRef behavior )
 	botMemory_t *botMind;
 	gentity_t *self = &g_entities[ clientNum ];
 	botMind = self->botMind = &g_botMind[clientNum];
+	ResetStruct( *botMind );
 
 	if ( !G_BotSetBehavior( botMind, behavior ) )
 	{
@@ -453,6 +453,8 @@ void G_BotThink( gentity_t *self )
 	while ( trap_BotGetServerCommand( self->num(), buf, sizeof( buf ) ) );
 
 	BotSearchForEnemy( self );
+
+	// Populate transient caches
 	BotFindClosestBuildings( self );
 	BotFindDamagedFriendlyStructure( self );
 	BotCalculateStuckTime( self );
@@ -528,6 +530,7 @@ void G_BotSpectatorThink( gentity_t *self )
 	}
 
 	self->botMind->spawnTime = level.time;
+	self->botMind->myTimer = level.time;
 
 	if ( self->client->ps.pm_flags & PMF_QUEUED )
 	{
@@ -553,15 +556,17 @@ void G_BotSpectatorThink( gentity_t *self )
 		return;
 	}
 
-	// reset stuff
-	self->botMind->goal.clear();
-	self->botMind->bestEnemy.ent = nullptr;
-	BotResetEnemyQueue( &self->botMind->enemyQueue );
-	self->botMind->currentNode = nullptr;
-	self->botMind->clearNav();
+	G_Bot_ResetBehaviorState( *self->botMind );
+
+	// Reset non-time-dependent alive state
+	self->botMind->lastThink = -999999;
+	self->botMind->stuckTime = 0;
+	self->botMind->stuckPosition = {1.0e12f, 1.0e12f, 1.0e12f};
 	self->botMind->futureAimTime = 0;
 	self->botMind->futureAimTimeInterval = 0;
-	self->botMind->runningNodes.clear();
+	BotResetEnemyQueue( &self->botMind->enemyQueue );
+	self->botMind->enemyLastSeen = -999999;
+	self->botMind->exhausted = false;
 
 	//FIXME: duplicate of sg_cmds.cpp:883 function "void Cmd_Team_f( gentity_t * )"
 	if ( g_doWarmup.Get() && ( ( level.warmupTime - level.time ) / 1000 ) > 0 )
@@ -790,6 +795,18 @@ void botMemory_t::doSprint( int jumpCost, int stamina, usercmd_t& cmd )
 	}
 
 	exhausted = exhausted && stamina <= jumpCost * 2;
+}
+
+// TODO: also reset state stored in BT nodes
+void G_Bot_ResetBehaviorState( botMemory_t &memory )
+{
+	memory.currentNode = nullptr;
+	memory.runningNodes.clear();
+	memory.goal.clear();
+	memory.clearNav();
+	memory.lastNavconTime = 0;
+	memory.lastNavconDistance = 0;
+	memory.hasOffmeshGoal = false;
 }
 
 // assumes bot is a bot, otherwise will crash.
