ffa4059cef3974b92a64b8e763ddab63d9da4158
slipher <slipher@protonmail.com>
2024-06-03 15:39:07 -0500

    Move sgame trap call declarations to engine
    Most trap_ functions wrapping sgame->engine IPC messages are in
    daemon/src/shared/server/sg_api.cpp. Move the declarations of these
    functions to the engine repo's sg_api.h, instead of
    sgame/sg_trapcalls.h.

--- src/sgame/sg_trapcalls.h
+++ src/sgame/sg_trapcalls.h
@@ -28,13 +28,18 @@ along with Unvanquished Source Code.  If not, see <http://www.gnu.org/licenses/>
 #include <glm/vec3.hpp>
 
 #include "shared/CommonProxies.h"
+#include "shared/server/sg_api.h"
 
 struct gentity_t;
 
-void             trap_LocateGameData( int numGEntities, int sizeofGEntity_t, int sizeofGClient );
-void             trap_DropClient( int clientNum, const char *reason );
-void             trap_SendServerCommand( int clientNum, const char *text );
-void             trap_SetConfigstring( int num, const char *string );
+// Actually involves a trap call. (But does stuff within the sgame VM too; hence it being
+// implemented in the sgame rather than daemon/src/shared/).
+void             trap_AdjustAreaPortalState( gentity_t *ent, bool open );
+
+/*
+ * All the rest of these are fake trap calls: none of them talk to the engine.
+ */
+
 void             trap_LinkEntity( gentity_t *ent );
 void             trap_UnlinkEntity( gentity_t *ent );
 int              trap_EntitiesInBox( const vec3_t mins, const vec3_t maxs, int *list, int maxcount );
@@ -44,23 +49,5 @@ void             trap_Trace( trace_t *results, const vec3_t start, const vec3_t 
 void             trap_SetBrushModel( gentity_t *ent, const char *name );
 bool         trap_InPVS( const vec3_t p1, const vec3_t p2 );
 bool         trap_InPVSIgnorePortals( const vec3_t p1, const vec3_t p2 );
-void             trap_SetConfigstringRestrictions( int num, const clientList_t *clientList );
-void             trap_GetConfigstring( int num, char *buffer, int bufferSize );
-void             trap_SetUserinfo( int num, const char *buffer );
-void             trap_GetUserinfo( int num, char *buffer, int bufferSize );
-void             trap_GetServerinfo( char *buffer, int bufferSize );
-void             trap_AdjustAreaPortalState( gentity_t *ent, bool open );
-int              trap_BotAllocateClient();
-void             trap_BotFreeClient( int clientNum );
-void             trap_GetUsercmd( int clientNum, usercmd_t *cmd );
-bool         trap_GetEntityToken( char *buffer, int bufferSize );
-int              trap_BotGetServerCommand( int clientNum, char *message, int size );
-
-int              trap_RSA_GenerateMessage( const char *public_key, char *cleartext, char *encrypted );
-
-void             trap_GenFingerprint( const char *pubkey, int size, char *buffer, int bufsize );
-void             trap_GetPlayerPubkey( int clientNum, char *pubkey, int size );
-
-void             trap_GetTimeString( char *buffer, int size, const char *format, const qtime_t *tm );
 
 #endif // SG_TRAPCALLS_H_
