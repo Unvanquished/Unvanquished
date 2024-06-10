Unvanquished/Unvanquished

commit 47702c121585f1cd39d8b12a3192b2e4766f0e31

Optimize finding room for class change a bit
Do some earlier returns and omit some redundant tests (checking
startsolid is not necessary in a position test).

src/sgame/sg_cmds.cpp
@@ -1748,7 +1748,13 @@ static bool FindRoomForClassChangeVertically(
 	VectorCopy( newOrigin, temp );
 	temp[ 2 ] += nudgeHeight;
 	trap_Trace( &tr, newOrigin, toMins, toMaxs, temp, ent->num(), MASK_PLAYERSOLID, 0 );
-	temp[ 2 ] = newOrigin[2] + nudgeHeight * tr.fraction;
+
+	if ( tr.fraction == 0.0f )
+	{
+		// it's not helping, we are probably too close to something horizontally
+		return false;
+	}
+	temp[ 2 ] = tr.endpos[ 2 ];
 
 	// trace down to the ground so that we can evolve on slopes
 	trap_Trace( &tr, temp, toMins, toMaxs, newOrigin, ent->num(), MASK_PLAYERSOLID, 0 );
@@ -1756,7 +1762,7 @@ static bool FindRoomForClassChangeVertically(
 
 	// make REALLY sure
 	trap_Trace( &tr, newOrigin, toMins, toMaxs, newOrigin, ent->num(), MASK_PLAYERSOLID, 0 );
-	return !tr.startsolid && tr.fraction == 1.0f;
+	return tr.fraction == 1.0f;
 }
 
 static bool FindRoomForClassChangeLaterally(
@@ -1794,20 +1800,25 @@ static bool FindRoomForClassChangeLaterally(
 		trap_Trace( &trace, start_point, toMins, toMaxs,
 				origin, ent->num(),
 				MASK_PLAYERSOLID, 0 );
+		if ( trace.fraction == 0.0f )
+		{
+			continue;
+		}
+
+		float new_distance = Distance( trace.endpos, origin );
+		if ( new_distance >= best_distance )
+			continue; // not interesting
 
 		vec3_t first_trace_end;
 		VectorCopy(trace.endpos, first_trace_end);
-		// make REALLY sure
+		// verify that the endpoint is unblocked - it might be blocked if the first trace
+		// has startsolid
 		trap_Trace( &trace, first_trace_end, toMins, toMaxs,
 				first_trace_end, ent->num(),
 				MASK_PLAYERSOLID, 0 );
 
-		if ( trace.startsolid || trace.fraction < 1.0f )
+		if ( trace.fraction < 1.0f )
 			continue; // collision
-
-		float new_distance = Distance(trace.endpos, origin);
-		if ( new_distance > best_distance )
-			continue; // not interesting
 
 		// new best!
 		found = true;

commit 6dcc91c1d9eee294a0e2eb5ddf461c86431072d7

Fix evolve room-finding when flush with wall
Fixes a regression in 47702c121585f1cd39d8b12a3192b2e4766f0e31 which
made it so that the game can't automatically find room to evolve if you
are exactly against an (axis-aligned) wall.

The room-finding algorithm was moving away from the origin only by the
exact minimum difference between bounding box sizes (thus dubiously
relying on the exactness of a floating-point calculation). I assumed
that if the trace goes 0 distance, it must be stuck and we can assume
failure for the given position. But it turns out the trace often goes 0
distance but is NOT stuck in an obstacle because it was positioned such
that the new bounding box will be flush with the wall if the old one is.

I added 0.5 qu to the distance to avoid this problem. Also the change
from (fraction == 0.0f) to (trace.allsolid) would fix the problem on its
own, but it's better not to rely on exact distances.

Thanks to freem for identifying the blameful commit.

src/sgame/sg_cmds.cpp
@@ -1751,7 +1751,7 @@ static bool FindRoomForClassChangeVertically(
 	temp[ 2 ] += nudgeHeight;
 	trap_Trace( &tr, newOrigin, toMins, toMaxs, temp, ent->num(), MASK_PLAYERSOLID, 0 );
 
-	if ( tr.fraction == 0.0f )
+	if ( tr.allsolid )
 	{
 		// it's not helping, we are probably too close to something horizontally
 		return false;
@@ -1783,7 +1783,7 @@ static bool FindRoomForClassChangeLaterally(
 	// each 4 directions, but this is close enough.
 	// This is correct assuming the bbox is centered on the
 	// origin on x and y.
-	float distance = std::min(max_x, max_y);
+	float distance = std::min(max_x, max_y) + 0.5f;
 
 	vec3_t origin;
 	origin[0] = ent->client->ps.origin[0];
@@ -1802,7 +1802,7 @@ static bool FindRoomForClassChangeLaterally(
 		trap_Trace( &trace, start_point, toMins, toMaxs,
 				origin, ent->num(),
 				MASK_PLAYERSOLID, 0 );
-		if ( trace.fraction == 0.0f )
+		if ( trace.allsolid )
 		{
 			continue;
 		}