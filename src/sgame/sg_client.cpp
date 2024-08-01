Unvanquished/Unvanquished

commit d61c9d2237ddf2df04799f33e91f9477a3bc79d3

Remove decolor on/off constants

src/sgame/sg_client.cpp
@@ -955,10 +955,10 @@ const char *ClientUserinfoChanged( int clientNum, bool forceName )
 
 			if ( *oldname )
 			{
-				G_LogPrintf( "ClientRename: %i [%s] (%s) \"%s^7\" -> \"%s^7\" \"%c%s%c^7\"\n",
+				G_LogPrintf( "ClientRename: %i [%s] (%s) \"%s^7\" -> \"%s^7\" \"%s^7\"\n",
 				             clientNum, client->pers.ip.str, client->pers.guid,
 				             oldname, client->pers.netname,
-				             DECOLOR_OFF, client->pers.netname, DECOLOR_ON );
+				             client->pers.netname );
 			}
 		}
 
@@ -1234,10 +1234,10 @@ const char *ClientConnect( int clientNum, bool firstTime )
 		return userInfoError;
 	}
 
-	G_LogPrintf( "ClientConnect: %i [%s] (%s) \"%s^7\" \"%c%s%c^7\"\n",
+	G_LogPrintf( "ClientConnect: %i [%s] (%s) \"%s^7\" \"%s^7\"\n",
 	             clientNum, client->pers.ip.str[0] ? client->pers.ip.str : "127.0.0.1", client->pers.guid,
 	             client->pers.netname,
-	             DECOLOR_OFF, client->pers.netname, DECOLOR_ON );
+	             client->pers.netname );
 
 	country = Info_ValueForKey( userinfo, "geoip" );
 	Q_strncpyz( client->pers.country, country, sizeof( client->pers.country ) );
@@ -1328,10 +1328,10 @@ const char *ClientBotConnect( int clientNum, bool firstTime, team_t team )
 		G_BotSetDefaults( clientNum, team, client->sess.botSkill, client->sess.botTree );
 	}
 
-	G_LogPrintf( "ClientConnect: %i [%s] (%s) \"%s^7\" \"%c%s%c^7\" [BOT]\n",
+	G_LogPrintf( "ClientConnect: %i [%s] (%s) \"%s^7\" \"%s^7\" [BOT]\n",
 	             clientNum, client->pers.ip.str[0] ? client->pers.ip.str : "127.0.0.1", client->pers.guid,
 	             client->pers.netname,
-	             DECOLOR_OFF, client->pers.netname, DECOLOR_ON );
+	             client->pers.netname );
 
 	// don't do the "xxx connected" messages if they were caried over from previous level
 	if ( firstTime )

daemon/src/common/Color.cpp
@@ -122,21 +122,16 @@ char *StripColors( char *string )
 void StripColors( const char *in, char *out, int len )
 {
 	--len;
-	bool decolor = true;
 
 	for ( const auto& token : Parser( in ) )
 	{
-		if ( !decolor || token.Type() == Token::CHARACTER )
+		if ( token.Type() == Token::CHARACTER )
 		{
 			if ( len < token.Size() )
 			{
 				break;
 			}
-			if ( *token.Begin() == Constants::DECOLOR_OFF || *token.Begin() == Constants::DECOLOR_ON )
-			{
-				decolor = ( *token.Begin() == Constants::DECOLOR_ON );
-				continue;
-			}
+			
 			strncpy( out, token.Begin(), token.Size() );
 			out += token.Size();
 			len -= token.Size();

daemon/src/common/Color.h
@@ -352,8 +352,6 @@ namespace Constants {
 enum {
 	ESCAPE = '^',
 	NULL_COLOR = '*',
-	DECOLOR_OFF = '\16', // these two are used by the second overload of StripColors (and a couple other places)
-	DECOLOR_ON  = '\17', // will have to be checked whether they are really useful
 }; // enum
 } // namespace Constants
 

src/sgame/sg_definitions.h
@@ -43,9 +43,6 @@ along with Daemon Source Code.  If not, see <http://www.gnu.org/licenses/>.
 #define QUEUE_PLUS1(x)  ((( x ) + 1 ) % MAX_CLIENTS )
 #define QUEUE_MINUS1(x) ((( x ) + MAX_CLIENTS - 1 ) % MAX_CLIENTS )
 
-#define DECOLOR_OFF '\16'
-#define DECOLOR_ON  '\17'
-
 #define DAMAGE_RADIUS        0x00000001 // damage was indirect
 #define DAMAGE_NO_ARMOR      0x00000002 // armour does not protect from this damage
 #define DAMAGE_KNOCKBACK     0x00000004 // move the target in damage direction

commit 568918e529797864946197e388d90461fe93a48d

Include colored player names in the logfile
Regression in d61c9d2237ddf2df04799f33e91f9477a3bc79d3 found by Ishq
(the names were printed twice without color instead of one with and one
without).

src/sgame/sg_client.cpp
@@ -901,10 +901,10 @@ const char *ClientUserinfoChanged( int clientNum, bool forceName )
 
 			if ( *oldname )
 			{
-				G_LogPrintf( "ClientRename: %i [%s] (%s) \"%s^*\" -> \"%s^*\" \"%s^*\"",
-				             clientNum, client->pers.ip.str, client->pers.guid,
-				             oldname, client->pers.netname,
-				             client->pers.netname );
+				G_LogPrintfColored(
+					"ClientRename: %i [%s] (%s) \"%s^*\" -> \"%s\" \"%s^*\"",
+					clientNum, client->pers.ip.str, client->pers.guid, oldname,
+					Color::StripColors( client->pers.netname ), client->pers.netname );
 			}
 		}
 
@@ -1184,10 +1184,10 @@ const char *ClientConnect( int clientNum, bool firstTime )
 		return userInfoError;
 	}
 
-	G_LogPrintf( "ClientConnect: %i [%s] (%s) \"%s^*\" \"%s^*\"",
-	             clientNum, client->pers.ip.str[0] ? client->pers.ip.str : "127.0.0.1", client->pers.guid,
-	             client->pers.netname,
-	             client->pers.netname );
+	G_LogPrintfColored(
+		"ClientConnect: %i [%s] (%s) \"%s\" \"%s^*\"",
+		clientNum, client->pers.ip.str[0] ? client->pers.ip.str : "127.0.0.1", client->pers.guid,
+		Color::StripColors( client->pers.netname ), client->pers.netname );
 
 	G_SendClientPmoveParams(clientNum);
 
@@ -1256,10 +1256,10 @@ const char *ClientBotConnect( int clientNum, bool firstTime )
 
 	ent->r.svFlags |= SVF_BOT;
 
-	G_LogPrintf( "ClientConnect: %i [%s] (%s) \"%s^*\" \"%s^*\" [BOT]",
-	             clientNum, client->pers.ip.str[0] ? client->pers.ip.str : "127.0.0.1", client->pers.guid,
-	             client->pers.netname,
-	             client->pers.netname );
+	G_LogPrintfColored(
+		"ClientConnect: %i [%s] (%s) \"%s\" \"%s^*\" [BOT]",
+		clientNum, client->pers.ip.str[0] ? client->pers.ip.str : "127.0.0.1", client->pers.guid,
+		Color::StripColors( client->pers.netname ), client->pers.netname );
 
 	// don't do the "xxx connected" messages if they were caried over from previous level
 	if ( firstTime )

src/sgame/sg_main.cpp
@@ -1552,7 +1552,7 @@ void G_AdminMessage( gentity_t *ent, const char *msg )
 	             msg );
 }
 
-void G_LogPrintfImpl( const std::string &msg )
+void G_LogPrintfImpl( bool stripColors, const std::string &msg )
 {
 	gameEventLog->Notice( msg );
 
@@ -1561,7 +1561,7 @@ void G_LogPrintfImpl( const std::string &msg )
 		return;
 	}
 
-	char    string[ 1024 ], decolored[ 1024 ];
+	char    string[ 1024 ];
 	int     min, tens, sec;
 
 	sec = level.matchTime / 1000;
@@ -1573,10 +1573,9 @@ void G_LogPrintfImpl( const std::string &msg )
 
 	Com_sprintf( string, sizeof( string ), "%3i:%i%i ", min, tens, sec );
 
-	Q_strcat( string, sizeof( string ), msg.c_str() );
+	Q_strcat( string, sizeof( string ), ( stripColors? Color::StripColors( msg ) : msg ).c_str() );
 
-	Color::StripColors( string, decolored, sizeof( decolored ) );
-	trap_FS_Write( decolored, strlen( decolored ), level.logFile );
+	trap_FS_Write( string, strlen( string ), level.logFile );
     trap_FS_Write( "\n", 1, level.logFile );
 }
 

src/sgame/sg_public.h
@@ -216,7 +216,7 @@ void              CalculateRanks();
 void              FindIntermissionPoint();
 void              G_RunThink( gentity_t *ent );
 void              G_AdminMessage( gentity_t *ent, const char *string );
-void              G_LogPrintfImpl( const std::string &message );
+void              G_LogPrintfImpl( bool stripColors, const std::string &message );
 void              SendScoreboardMessageToAllClients();
 void              G_Vote( gentity_t *ent, team_t team, bool voting );
 void              G_ResetVote( team_t team );
@@ -245,7 +245,15 @@ template<typename... T>
 void G_LogPrintf( Str::StringRef fmt, const T&... args )
 {
 	std::string msg = Str::Format( fmt, args... );
-	G_LogPrintfImpl( msg );
+	G_LogPrintfImpl( true, msg );
+}
+
+// Like G_LogPrintf but colors are retained in the logfile.
+template<typename... T>
+void G_LogPrintfColored( Str::StringRef fmt, const T&... args )
+{
+	std::string msg = Str::Format( fmt, args... );
+	G_LogPrintfImpl( false, msg );
 }
 
 // sg_maprotation.c