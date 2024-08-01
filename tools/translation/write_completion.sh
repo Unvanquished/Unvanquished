950d07448dace527f639f4e26b180551f9b0843a
Thomas Debesse <dev@illwieckz.net>
2024-08-01 14:30:53 +0200

    tools/translation: add write_completion.sh script

--- tools/translation/write_completion.sh
+++ tools/translation/write_completion.sh
@@ -27,7 +27,36 @@
 set -u -e -o pipefail
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+repo_dir="$(realpath "${script_dir}/../..")"
+dpk_dir="${repo_dir}/pkg/unvanquished_src.dpkdir"
+pot_dir="${dpk_dir}/translation"
 
-"${script_dir}/generate_pot.sh"
-"${script_dir}/update_po.sh"
-"${script_dir}/write_completion.sh"
+cd "${pot_dir}"
+
+printf '' > 'completion.txt'
+
+# Only consider game translation for completeness.
+for po_file in $(find 'game' -name '*.po')
+do
+	lang="$(echo "${po_file}" | cut -f2 -d'/' | cut -f1 -d'.')"
+
+	po_entries="$(sed 'N;s/ ""\n"/ "/g;P;D;' "${po_file}" | grep -E '^msgid "|^msgstr "')"
+
+	count_source="$(echo "${po_entries}" | grep -c -E '^msgid "' || true)"
+	count_translation="$(echo "${po_entries}" | grep -c -E '^msgstr "' || true)"
+	count_empty_source="$(echo "${po_entries}" | grep -c -E '^msgid ""' || true)"
+	count_empty_translation="$(echo "${po_entries}" | grep -c -E '^msgstr ""' || true)"
+
+	total_source="$((${count_source} - ${count_empty_source}))"
+	total_translation="$((${count_translation} - ${count_empty_translation}))"
+
+	percentage="$((${total_translation} * 100 / ${total_source}))"
+
+	printf '%s\t%+3s%%,\tsources: %s (%s-%s),\ttranslations: %-4s (%s-%s)\n' \
+		"${lang}:" "${percentage}" \
+		"${total_source}" "${count_source}" "${count_empty_source}" \
+		"${total_translation}" "${count_translation}" "${count_empty_translation}"
+
+	printf '%s %s\n' "${percentage}" "${lang}" >> "completion.txt"
+
+done | column -t -s $'\t' -o ' '
