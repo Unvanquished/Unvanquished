Unvanquished/Unvanquished

commit ce7d25c3224ce4af5c77c3847ce829455e47681b

Rename DAEMON_CBSE_PYTHON_PATH -> CBSE_PYTHON_PATH
This CMake variable has no relation to Daemon

tools/cbse/CBSE.cmake
@@ -25,12 +25,12 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # Allow overriding the python path with a cached variable.
-set(DAEMON_CBSE_PYTHON_PATH "NOTFOUND" CACHE FILEPATH "Python executable for CBSE code generation")
-if (NOT DAEMON_CBSE_PYTHON_PATH OR NOT EXISTS ${DAEMON_CBSE_PYTHON_PATH})
+set(CBSE_PYTHON_PATH "NOTFOUND" CACHE FILEPATH "Python executable for CBSE code generation")
+if (NOT CBSE_PYTHON_PATH OR NOT EXISTS ${CBSE_PYTHON_PATH})
     find_package(Python REQUIRED)
-    set(DAEMON_CBSE_PYTHON_PATH "${Python_EXECUTABLE}" CACHE FILEPATH "Python executable for CBSE code generation" FORCE)
+    set(CBSE_PYTHON_PATH "${Python_EXECUTABLE}" CACHE FILEPATH "Python executable for CBSE code generation" FORCE)
 endif()
-message(STATUS "Using CBSE Python executable: ${DAEMON_CBSE_PYTHON_PATH}")
+message(STATUS "Using CBSE Python executable: ${CBSE_PYTHON_PATH}")
 
 function(maybe_add_dep target dep)
     if (TARGET ${target})
@@ -42,7 +42,7 @@ function(CBSE target definition output)
     # Check if python has all the dependencies
     # TODO: Execute pip directly here and install them
     execute_process(
-        COMMAND ${DAEMON_CBSE_PYTHON_PATH} -c "import jinja2, yaml, collections, argparse, sys, os.path, re"
+        COMMAND ${CBSE_PYTHON_PATH} -c "import jinja2, yaml, collections, argparse, sys, os.path, re"
         RESULT_VARIABLE RET)
     if (NOT RET EQUAL 0)
         message(FATAL_ERROR "Missing dependences for CBSE generation. Please ensure you have python ≥ 2, python-yaml, and python-jinja installed.
@@ -66,7 +66,7 @@ function(CBSE target definition output)
                 ${CMAKE_SOURCE_DIR}/tools/cbse/templates/Helper.h
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/tools/cbse
         COMMAND
-                ${DAEMON_CBSE_PYTHON_PATH}
+                ${CBSE_PYTHON_PATH}
                 ${CMAKE_SOURCE_DIR}/tools/cbse/CBSE.py
                 -s -o
                 "${output}"

azure-pipelines.yml
@@ -28,22 +28,22 @@ strategy:
     Linux GCC Native DLL:
       VM_IMAGE: 'ubuntu-20.04'
       INSTALL_DEPS: 'sudo apt-get update && sudo apt-get -y -q --no-install-recommends install libfreetype6-dev liblua5.2-dev python-yaml python-jinja2 ninja-build gcc-8 g++-8'
-      CMAKE_OPTIONS: '-G Ninja -DDAEMON_CBSE_PYTHON_PATH=/usr/bin/python2 -DCMAKE_C_COMPILER=gcc-8 -DCMAKE_CXX_COMPILER=g++-8 -DBUILD_GAME_NATIVE_DLL=1 -DBUILD_GAME_NATIVE_EXE=0 -DBUILD_GAME_NACL=0'
+      CMAKE_OPTIONS: '-G Ninja -DCBSE_PYTHON_PATH=/usr/bin/python2 -DCMAKE_C_COMPILER=gcc-8 -DCMAKE_CXX_COMPILER=g++-8 -DBUILD_GAME_NATIVE_DLL=1 -DBUILD_GAME_NATIVE_EXE=0 -DBUILD_GAME_NACL=0'
       BUILDER_OPTIONS: '-j$(nproc) -k 10'
     Linux Clang Native DLL:
       VM_IMAGE: 'ubuntu-20.04'
       INSTALL_DEPS: 'sudo apt-get update && sudo apt-get -y -q --no-install-recommends install libfreetype6-dev liblua5.2-dev python-yaml python-jinja2 ninja-build'
-      CMAKE_OPTIONS: '-G Ninja -DDAEMON_CBSE_PYTHON_PATH=/usr/bin/python2 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DBUILD_GAME_NATIVE_DLL=1 -DBUILD_GAME_NATIVE_EXE=0 -DBUILD_GAME_NACL=0'
+      CMAKE_OPTIONS: '-G Ninja -DCBSE_PYTHON_PATH=/usr/bin/python2 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DBUILD_GAME_NATIVE_DLL=1 -DBUILD_GAME_NATIVE_EXE=0 -DBUILD_GAME_NACL=0'
       BUILDER_OPTIONS: '-j$(nproc) -k 10'
     Linux NaCl:
       VM_IMAGE: 'ubuntu-20.04'
       INSTALL_DEPS: 'sudo apt-get update && sudo apt-get -y -q --no-install-recommends install python-yaml python-jinja2 ninja-build'
-      CMAKE_OPTIONS: '-G Ninja -DDAEMON_CBSE_PYTHON_PATH=/usr/bin/python2 -DBUILD_GAME_NATIVE_DLL=0 -DBUILD_GAME_NATIVE_EXE=0 -DBUILD_GAME_NACL=1'
+      CMAKE_OPTIONS: '-G Ninja -DCBSE_PYTHON_PATH=/usr/bin/python2 -DBUILD_GAME_NATIVE_DLL=0 -DBUILD_GAME_NATIVE_EXE=0 -DBUILD_GAME_NACL=1'
       BUILDER_OPTIONS: '-j$(nproc) -k 10'
     Mac:
       VM_IMAGE: 'macOS-12'
       INSTALL_DEPS: 'python3 -m pip install -r tools/cbse/requirements.txt'
-      CMAKE_OPTIONS: '-DDAEMON_CBSE_PYTHON_PATH=$(which python3) -DBUILD_GAME_NATIVE_DLL=1 -DBUILD_GAME_NATIVE_EXE=0 -DBUILD_GAME_NACL=0'
+      CMAKE_OPTIONS: '-DCBSE_PYTHON_PATH=$(which python3) -DBUILD_GAME_NATIVE_DLL=1 -DBUILD_GAME_NATIVE_EXE=0 -DBUILD_GAME_NACL=0'
       BUILDER_OPTIONS: '-j$(sysctl -n hw.logicalcpu)'
 
 pool:

commit 4b805a60376b36fdb16dd24a5c0e72cf4d38e93e

Don't do CBSE generation in the NaCl subproject
When building the NaCl sgame, don't generate the CBSE files in the NaCl
build. Instead depend on the top-level CMake project to generate them
and force a dependency between the nacl-vms target and the generated
files.

This fixes the regression in ce7d25c3224ce4af5c77c3847ce829455e47681b
(rename DAEMON_CBSE_PYTHON_PATH -> CBSE_PYTHON_PATH) that the Python
path was no longer passed to the subproject. Also it makes sure that two
instances of the CBSE generator would not try to run at the same time.

CMakeLists.txt
@@ -202,9 +202,16 @@ endif()
 
 if (BUILD_SGAME)
     include(tools/cbse/CBSE.cmake)
-    CBSE("sgame"
-         "${GAMELOGIC_DIR}/sgame/entities.yaml"
-         "${GAMELOGIC_DIR}/sgame/")
+
+    # Avoid generating CBSE in the NaCl subproject.
+    if (FORK EQUAL 2)
+        CBSEFileList("${GAMELOGIC_DIR}/sgame/" sgame_GENERATED_CBSE)
+    else()
+        CBSE("sgame"
+             "${GAMELOGIC_DIR}/sgame/entities.yaml"
+             "${GAMELOGIC_DIR}/sgame/")
+    endif()
+
     GAMEMODULE(NAME sgame
         DEFINITIONS
             BUILD_SGAME
@@ -219,6 +226,14 @@ if (BUILD_SGAME)
             srclibs-recast
             srclibs-fastlz
     )
+
+    if (BUILD_GAME_NACL AND NOT (FORK EQUAL 2))
+        include(ExternalProject)
+        ExternalProject_Add_Step(nacl-vms cbse-trigger
+                                 COMMENT "Finished CBSE generation, ready to build NaCl"
+                                 DEPENDS ${sgame_GENERATED_CBSE}
+                                 DEPENDERS build)
+    endif()
 endif()
 
 if (BUILD_CGAME)

tools/cbse/CBSE.cmake
@@ -32,6 +32,13 @@ if (NOT CBSE_PYTHON_PATH OR NOT EXISTS ${CBSE_PYTHON_PATH})
 endif()
 message(STATUS "Using CBSE Python executable: ${CBSE_PYTHON_PATH}")
 
+macro(CBSEFileList outputDir variable)
+    set(${variable} ${outputDir}/backend/CBSEBackend.cpp
+                    ${outputDir}/backend/CBSEBackend.h
+                    ${outputDir}/backend/CBSEComponents.h
+                    ${outputDir}/backend/CBSEEntities.h)
+endmacro()
+
 function(CBSE target definition output)
     # Check if python has all the dependencies
     execute_process(
@@ -41,10 +48,7 @@ function(CBSE target definition output)
         message(FATAL_ERROR "Missing dependences for CBSE generation. Please ensure you have python ≥ 2, python-yaml, and python-jinja installed.
                              Use python -m pip install -r tools/cbse/requirements.txt to install")
     endif()
-    set(GENERATED_CBSE ${output}/backend/CBSEBackend.cpp
-                       ${output}/backend/CBSEBackend.h
-                       ${output}/backend/CBSEComponents.h
-                       ${output}/backend/CBSEEntities.h)
+    CBSEFileList(${output} GENERATED_CBSE)
     add_custom_command(
         OUTPUT ${GENERATED_CBSE}
         COMMENT "Generating CBSE entities for ${definition}"