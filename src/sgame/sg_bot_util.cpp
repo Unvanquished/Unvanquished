Unvanquished/Unvanquished

commit 15301118931aa17ccee74b8973c184fe1dbd8a2a

Obey usercmd_t protocol to set bot view angles
Don't 'cheat' by modifying player state directly.

There is no functional difference here in how the bots behave. Just an
architectural fix.

src/sgame/sg_bot_util.cpp
@@ -1589,6 +1589,16 @@ static int BotGetAimTime( gentity_t *self )
 	return std::max( 1, int(time) );
 }
 
+// The aiming algorithm is some framerate-dependent nonsense. It does a
+// "linear interpolation" toward the desired position, but the starting direction
+// of this interpolation is the bot's *current* view direction, rather than the one
+// when it started aiming. So if the bot's position remains constant, the aim approaches
+// the desired position at a (faster than) exponential rate, with the exponent dependent
+// on the frame rate (higher sv_fps = faster aiming). The distance from the desired aim direction
+// can go up due to the bot moving, but it will still go down exponentially after.
+// Near the end of the aim interval aim errors are correctly almost instantly, e.g. if 90% of the
+// interval has passed, the diff goes down 90% in one frame. This is prevented from being too
+// powerful against moving targets by the fact that the aim position is quite outdated at that point.
 void BotAimAtEnemy( gentity_t *self )
 {
 	if ( self->botMind->futureAimTime < level.time )
@@ -1611,13 +1621,13 @@ void BotAimAtEnemy( gentity_t *self )
 	float frac = ( 1.0f - ( static_cast<float>( self->botMind->futureAimTime - level.time ) ) / self->botMind->futureAimTimeInterval );
 	glm::vec3 newAim = glm::mix( current, desired, frac );
 
-	VectorSet( self->client->ps.delta_angles, 0, 0, 0 );
 	glm::vec3 angles;
 	vectoangles( GLM4READ( newAim ), GLM4RW( angles ) );
 
 	for ( int i = 0; i < 3; i++ )
 	{
-		self->botMind->cmdBuffer.angles[ i ] = ANGLE2SHORT( angles[ i ] );
+		float angle = AngleSubtract( angles[ i ], SHORT2ANGLE( self->client->ps.delta_angles[ i ] ) );
+		self->botMind->cmdBuffer.angles[ i ] = ANGLE2SHORT( angle );
 	}
 }
 
@@ -1625,7 +1635,6 @@ void BotAimAtLocation( gentity_t *self, const glm::vec3 &target )
 {
 	glm::vec3 aimVec, aimAngles, viewBase;
 	int i;
-	usercmd_t *rAngles = &self->botMind->cmdBuffer;
 
 	if ( !self->client )
 	{
@@ -1637,19 +1646,11 @@ void BotAimAtLocation( gentity_t *self, const glm::vec3 &target )
 
 	vectoangles( GLM4READ( aimVec ), GLM4RW( aimAngles ) );
 
-	VectorSet( self->client->ps.delta_angles, 0.0f, 0.0f, 0.0f );
-
 	for ( i = 0; i < 3; i++ )
 	{
-		aimAngles[i] = ANGLE2SHORT( aimAngles[i] );
+		float angle = AngleSubtract( aimAngles[ i ], SHORT2ANGLE( self->client->ps.delta_angles[ i ] ) );
+		self->botMind->cmdBuffer.angles[ i ] = ANGLE2SHORT( angle );
 	}
-
-	//save bandwidth
-	//Meh. I doubt it saves much. Casting to short ints might have, though. (copypaste)
-	aimAngles = glm::floor( aimAngles + 0.5f );
-	rAngles->angles[0] = aimAngles[0];
-	rAngles->angles[1] = aimAngles[1];
-	rAngles->angles[2] = aimAngles[2];
 }
 
 void BotSlowAim( gentity_t *self, glm::vec3 &target, float slowAmount )

src/sgame/sg_bot_nav.cpp
@@ -951,7 +951,8 @@ void BotMoveUpward( gentity_t *self, glm::vec3 nextCorner )
 		{
 			self->botMind->cmdBuffer.forwardmove = 127;
 			self->botMind->cmdBuffer.rightmove = 0;
-			self->botMind->cmdBuffer.angles[ PITCH ] = ANGLE2SHORT( -60.f );
+			float pitch = AngleSubtract( -60.f, SHORT2ANGLE( self->client->ps.delta_angles[ PITCH ] ) );
+			self->botMind->cmdBuffer.angles[ PITCH ] = ANGLE2SHORT( pitch );
 			BotJump( self );
 			break;
 		}
@@ -975,7 +976,13 @@ void BotMoveUpward( gentity_t *self, glm::vec3 nextCorner )
 	if ( wpm != WPM_NONE )
 	{
 		usercmd_t &botCmdBuffer = self->botMind->cmdBuffer;
-		botCmdBuffer.angles[PITCH] = ANGLE2SHORT( -CalcAimPitch( self, nextCorner, magnitude ) - g_bot_upwardLeapAngleCorr.Get() );
+
+		float pitch = Math::Clamp(
+			-CalcAimPitch( self, nextCorner, magnitude ) - g_bot_upwardLeapAngleCorr.Get(),
+			-90.f, 90.f );
+		pitch = AngleSubtract( pitch, SHORT2ANGLE( self->client->ps.delta_angles[ PITCH ] ) );
+		self->botMind->cmdBuffer.angles[ PITCH ] = ANGLE2SHORT( pitch );
+
 		BotFireWeapon( wpm, &botCmdBuffer );
 	}
 }
@@ -1181,7 +1188,9 @@ bool BotMoveToGoal( gentity_t *self )
 		if ( magnitude )
 		{
 			glm::vec3 target = self->botMind->nav().tpos;
-			botCmdBuffer.angles[PITCH] = ANGLE2SHORT( -CalcAimPitch( self, target, magnitude ) / 3 );
+			float pitch = -CalcAimPitch( self, target, magnitude ) / 3;
+			pitch = AngleSubtract( pitch, SHORT2ANGLE( self->client->ps.delta_angles[ PITCH ] ) );
+			self->botMind->cmdBuffer.angles[ PITCH ] = ANGLE2SHORT( pitch );
 		}
 		BotFireWeapon( wpm, &botCmdBuffer );
 	}

commit c9a8207c3c1d1eac14632fa2fce0e759f2c89731

Bots: correct for delta_angles with goon pounce/barb
Fixes #3273, a regression in 15301118931aa17ccee74b8973c184fe1dbd8a2a.

Before PR #3057 pounce and barb aim also didn't correct for delta
angles, but that flaw was masked by the movement code frequently setting
the delta angles.

src/sgame/sg_bot_util.cpp
@@ -2140,7 +2140,10 @@ void BotFireWeaponAI( gentity_t *self )
 		case WP_ALEVEL3:
 			if ( self->botMind->skillSet[BOT_A_POUNCE_ON_ATTACK] && distance > LEVEL3_CLAW_RANGE && self->client->ps.weaponCharge < LEVEL3_POUNCE_TIME )
 			{
-				botCmdBuffer->angles[PITCH] = ANGLE2SHORT( -CalcPounceAimPitch( self, target ) ); //compute and apply correct aim pitch to hit target
+				//compute and apply correct aim pitch to hit target
+				float pitch = -CalcPounceAimPitch( self, target );
+				self->botMind->cmdBuffer.angles[ PITCH ] =
+					ANGLE2SHORT( pitch ) - self->client->ps.delta_angles[ PITCH ];
 				BotFireWeapon( WPM_SECONDARY, botCmdBuffer ); //goon pounce
 			}
 			else
@@ -2162,7 +2165,10 @@ void BotFireWeaponAI( gentity_t *self )
 
 			if ( outOfClawsRange && hasBarbs && barbIsSafe )
 			{
-				botCmdBuffer->angles[PITCH] = ANGLE2SHORT( -CalcBarbAimPitch( self, target ) ); //compute and apply correct aim pitch to hit target
+				//compute and apply correct aim pitch to hit target
+				float pitch = -CalcBarbAimPitch( self, target );
+				self->botMind->cmdBuffer.angles[ PITCH ] =
+					ANGLE2SHORT( pitch ) - self->client->ps.delta_angles[ PITCH ];
 
 				glm::vec3 delta = targetPos - VEC2GLM( self->s.origin );
 
@@ -2181,7 +2187,11 @@ void BotFireWeaponAI( gentity_t *self )
 			}
 			else if ( self->botMind->skillSet[BOT_A_POUNCE_ON_ATTACK] && distance > LEVEL3_CLAW_UPG_RANGE && self->client->ps.weaponCharge < LEVEL3_POUNCE_TIME_UPG )
 			{
-				botCmdBuffer->angles[PITCH] = ANGLE2SHORT( -CalcPounceAimPitch( self, target ) ); //compute and apply correct aim pitch to hit target
+				//compute and apply correct aim pitch to hit target
+				float pitch = -CalcPounceAimPitch( self, target );
+				self->botMind->cmdBuffer.angles[ PITCH ] =
+					ANGLE2SHORT( pitch ) - self->client->ps.delta_angles[ PITCH ];
+
 				BotFireWeapon( WPM_SECONDARY, botCmdBuffer ); //goon pounce
 			}
 			else