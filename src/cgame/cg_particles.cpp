Unvanquished/Unvanquished

commit c794469d99ba836ca883c66d42013d9e96e6ed00

cg_particles: fix a division by zero in CG_CalculateTimeFrac()

src/cgame/cg_particles.cpp
@@ -2123,9 +2123,8 @@ Calculate the fraction of time passed
 */
 static float CG_CalculateTimeFrac( int birth, int life, int delay )
 {
-	float frac = ( ( float ) cg.time - ( float )( birth + delay ) ) / ( float )( life - delay );
-
-	return Math::Clamp( frac, 0.0f, 1.0f );
+	int diff = life - delay;
+	return diff ? Math::Clamp( (float) (cg.time - birth + delay) / (float) diff, 0.0f, 1.0f ) : 1.0f;
 }
 
 /*

commit 7c3877ce38a0e59f3d808cb791cba07ff33bc952

Fix acid tube gas being too transparent
Regression in c794469d99ba836ca883c66d42013d9e96e6ed00, which changed
the denominator of the fraction in CG_CalculateTimeFrac from
(cg.time - birth - delay) to (cg.time - birth + delay). The acid cloud
was affected because the `alpha` field uses a delay.

src/cgame/cg_particles.cpp
@@ -2118,13 +2118,24 @@ static void CG_GarbageCollectParticleSystems()
 ===============
 CG_CalculateTimeFrac
 
-Calculate the fraction of time passed
+Calculate the fraction of time passed, in range [0, 1]
+Fraction is 0 for the first `delay` ms of life, then ramps linearly to 1 at the time of death
 ===============
 */
 static float CG_CalculateTimeFrac( int birth, int life, int delay )
 {
-	int diff = life - delay;
-	return diff ? Math::Clamp( (float) (cg.time - birth + delay) / (float) diff, 0.0f, 1.0f ) : 1.0f;
+	ASSERT_LE( cg.time, birth + life ); // particle should still be alive
+	ASSERT_GE( delay, 0 ); // enforced by parser
+
+	int rampTime = cg.time - ( birth + delay );
+
+	if ( rampTime <= 0 )
+	{
+		return 0.0f;
+	}
+
+	int adjustedLife = life - delay;
+	return float(rampTime) / float(adjustedLife);
 }
 
 /*