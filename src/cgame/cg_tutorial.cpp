09acde544f5ff6f5c00c37676fec7a0b7ae22ab5
slipher <slipher@protonmail.com>
2025-03-24 17:01:18 -0500

    Tutorial: improve wording of bot tactic menu line

--- src/cgame/cg_tutorial.cpp
+++ src/cgame/cg_tutorial.cpp
@@ -55,7 +55,7 @@ static bind_t bindings[] =
 	{ "weapnext",       N_( "Next Weapon" ),                           {} },
 	{ "message_public", N_( "Global chat" ),                           {} },
 	{ "message_team",   N_( "Team chat" ),                             {} },
-	{ "botTacticMenu",  N_( "Tactic bot menu" ),                       {} },
+	{ "botTacticMenu",  N_( "Bot tactic menu" ),                       {} },
 	{ OPEN_CONSOLE_CMD, N_( "Toggle Console" ),                        {} },
 	{ OPEN_MENU_CMD,    N_( "Toggle Menu" ),                           {} },
 	{ "itemact grenade", N_( "Throw a grenade" ),                      {} }
@@ -341,7 +341,7 @@ CG_HumanText
 */
 static void CG_HumanText( std::string& text, playerState_t *ps )
 {
-	text += va( _( "Press %s to show the tactic bot menu." ), CG_KeyNameForCommand( "botTacticMenu" ) );
+	text += va( _( "Press %s to show the bot tactic menu." ), CG_KeyNameForCommand( "botTacticMenu" ) );
 	text += '\n';
 	if ( !ps->ammo && !ps->clips && !BG_Weapon( ps->weapon )->infiniteAmmo )
 	{
@@ -594,7 +594,7 @@ const std::string& CG_TutorialText()
 			{
 				text += va( _( "Press %s to evolve." ), CG_KeyNameForCommand( "+activate" ) );
 				text += '\n';
-				text += va( _( "Press %s to show the tactic bot menu." ), CG_KeyNameForCommand( "botTacticMenu" ) );
+				text += va( _( "Press %s to show the bot tactic menu." ), CG_KeyNameForCommand( "botTacticMenu" ) );
 				text += '\n';
 			}
 		}
