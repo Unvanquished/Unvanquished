Unvanquished/Unvanquished

commit 3447e9365838fdbc0dd7df0e639b30589e67fec7

Migrate cvar g_password to new style
Turns out g_needpass is not supposed to be used for configuration. It's
automatically derived based on whether g_password is empty.

src/sgame/sg_client.cpp
@@ -1126,8 +1126,7 @@ const char *ClientConnect( int clientNum, bool firstTime )
 	// check for a password
 	value = Info_ValueForKey( userinfo, "password" );
 
-	if ( g_password.string[ 0 ] && Q_stricmp( g_password.string, "none" ) &&
-	     strcmp( g_password.string, value ) != 0 )
+	if ( g_needpass.Get() && g_password.Get() != value )
 	{
 		return "Invalid password";
 	}

src/sgame/sg_main.cpp
@@ -59,9 +59,10 @@ Cvar::Range<Cvar::Cvar<int>> g_gravity(
 
 Cvar::Cvar<int> g_timelimit("timelimit", "max game length in minutes", Cvar::SERVERINFO, 45);
 Cvar::Cvar<bool> g_dretchPunt("g_dretchPunt", "aliens can propel dretches by attacking them", Cvar::NONE, true);
-vmCvar_t           g_password;
-// int instead of bool for now to avoid changing the serverinfo format
-Cvar::Range<Cvar::Cvar<int>> g_needpass("g_needpass", "password required to join the server?", Cvar::SERVERINFO, 0, 0, 1);
+// This isn't a configuration variable to set whether a password is needed.
+// Rather, it's value is automatically set based on whether g_password is empty;
+// it's a hack to stick a value into the serverinfo string.
+Cvar::Range<Cvar::Cvar<int>> g_needpass("g_needpass", "FOR INTERNAL USE", Cvar::SERVERINFO, 0, 0, 1);
 vmCvar_t           g_maxclients;
 Cvar::Cvar<int> g_maxGameClients("g_maxGameClients", "max number of players (see also sv_maxclients)", Cvar::SERVERINFO, 0);
 Cvar::Cvar<float> g_speed("g_speed", "player movement speed multiplier", Cvar::NONE, 320);
@@ -308,9 +309,6 @@ static cvarTable_t gameCvarTable[] =
 	{ nullptr,                           "g_mapStartupMessage",           "",                                 0,                                               0, false },
 	{ nullptr,                           "g_mapConfigsLoaded",            "0",                                0,                                               0, false },
 	{ &g_maxclients,                  "sv_maxclients",                 "24",                               CVAR_SERVERINFO | CVAR_LATCH,                    0, false    },
-
-	// server: basic
-	{ &g_password,                    "g_password",                    "",                                 CVAR_USERINFO,                                   0, false },
 
 	// clients: limits
 	{ &g_inactivity,                  "g_inactivity",                  "0",                                0,                                               0, true  },
@@ -2280,31 +2278,12 @@ void G_CheckVote( team_t team )
 	G_ResetVote( team );
 }
 
-/*
-==================
-CheckCvars
-==================
-*/
-void CheckCvars()
+static void SetNeedpass(const std::string& password)
 {
-	static int lastPasswordModCount = -1;
-
-	if ( g_password.modificationCount != lastPasswordModCount )
-	{
-		lastPasswordModCount = g_password.modificationCount;
-
-		if ( *g_password.string && Q_stricmp( g_password.string, "none" ) )
-		{
-			g_needpass.Set(true);
-		}
-		else
-		{
-			g_needpass.Set(false);
-		}
-	}
-
-	level.frameMsec = trap_Milliseconds();
+	g_needpass.Set( !password.empty() && !Str::IsIEqual( password, "none" ) );
 }
+Cvar::Callback<Cvar::Cvar<std::string>> g_password("password", "password to join the server", Cvar::NONE, "", SetNeedpass);
+
 
 /*
 =============
@@ -2431,7 +2410,8 @@ void G_RunFrame( int levelTime )
 
 		// get any cvar changes
 		G_UpdateCvars();
-		CheckCvars();
+
+		level.frameMsec = trap_Milliseconds();
 
 		CheckExitRules();
 
@@ -2488,7 +2468,8 @@ void G_RunFrame( int levelTime )
 
 	// get any cvar changes
 	G_UpdateCvars();
-	CheckCvars();
+
+	level.frameMsec = trap_Milliseconds();
 
 	// now we are done spawning
 	level.spawning = false;

src/sgame/sg_bot.cpp
@@ -317,7 +317,7 @@ bool G_BotAdd( const char *name, team_t team, int skill, const char *behavior, b
 	//so we can connect if server is password protected
 	if ( g_needpass.Get() )
 	{
-		Info_SetValueForKey( userinfo, "password", g_password.string, false );
+		Info_SetValueForKey( userinfo, "password", g_password.Get().c_str(), false );
 	}
 
 	trap_SetUserinfo( clientNum, userinfo );

src/sgame/sg_extern.h
@@ -55,7 +55,7 @@ extern Cvar::Cvar<int> g_maxNameChanges;
 extern Cvar::Range<Cvar::Cvar<int>> g_showHelpOnConnection;
 extern Cvar::Cvar<int> g_timelimit;
 extern Cvar::Cvar<bool> g_dretchPunt;
-extern  vmCvar_t g_password;
+extern Cvar::Callback<Cvar::Cvar<std::string>> g_password;
 extern Cvar::Range<Cvar::Cvar<int>> g_needpass;
 extern  Cvar::Range<Cvar::Cvar<int>> g_gravity;
 extern Cvar::Cvar<float> g_speed;

commit 9de72b757476cbd3c3801d6c3a323c50c8d0612b

Fix password protection with local servers
Rename the server password variable from "password" to "g_password" so
that it no longer conflicts with the client password cvar. Exempt the
loopback client from password checking.

This fixes the warning about the cvar "password" being registered twice.

This fixes password protection in servers hosted in the client. It
didn't work due to the cvar registration conflict which prevented the
server from reading the cvar.

Note: the server creation UI doesn't need adjustment because it was
already using the cvar name "g_password". This was the wrong cvar name
but it wouldn't have worked before anyway.

Fixes regression from 3447e9365838fdbc0dd7df0e639b30589e67fec7
which accidentally renamed the cvar "g_password" -> "password".

src/sgame/sg_client.cpp
@@ -1127,7 +1127,7 @@ const char *ClientConnect( int clientNum, bool firstTime )
 	// check for a password
 	value = Info_ValueForKey( userinfo, "password" );
 
-	if ( g_needpass.Get() && g_password.Get() != value )
+	if ( !client->pers.localClient && g_needpass.Get() && g_password.Get() != value )
 	{
 		return "Invalid password";
 	}

src/sgame/sg_main.cpp
@@ -2075,7 +2075,7 @@ static void SetNeedpass(const std::string& password)
 {
 	g_needpass.Set( !password.empty() && !Str::IsIEqual( password, "none" ) );
 }
-Cvar::Callback<Cvar::Cvar<std::string>> g_password("password", "password to join the server", Cvar::NONE, "", SetNeedpass);
+Cvar::Callback<Cvar::Cvar<std::string>> g_password("g_password", "password to join the server", Cvar::NONE, "", SetNeedpass);
 
 
 /*