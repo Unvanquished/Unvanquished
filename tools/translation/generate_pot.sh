00912525cbce03b035efdb918d470c83e4c07950
dev@illwieckz.net <dev@illwieckz.net>
2026-06-06 15:26:57 +0200

    tools/translate: introduce pot_printer
    - open files as read-only and as UTF-8
    - make the pot printer reusable

--- tools/translation/generate_pot.sh
+++ tools/translation/generate_pot.sh
@@ -2,7 +2,7 @@
 
 # ===========================================================================
 #
-# Copyright (c) 2017-2024 Unvanquished Developers
+# Copyright (c) 2017-2026 Unvanquished Developers
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
---
+++ tools/translation/pot_printer.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import argparse
+import collections
+import datetime
+import sys
+
+def load(filename):
+    file = open(filename, "r", encoding="utf-8")
+    content = file.read()
+    file.close()
+    return content
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", action="store_true", help="sort strings")
+    parser.add_argument("-d", action="store_true", help="print debug information")
+    parser.add_argument("file", nargs="*")
+    argv = parser.parse_args()
+
+    translation_dict = collections.defaultdict(list)
+    errors = 0
+
+    files = argv.file
+
+    if not files:
+        files = sys.stdin
+
+    for filename in argv.file:
+        errors += process(translation_dict, load(filename), filename, argv.d)
+
+    datetime_string = datetime.datetime.now().astimezone().strftime("%Y-%m-%d %H:%M%z")
+
+    print(rf'''# SOME DESCRIPTIVE TITLE.
+# Copyright (C) YEAR THE PACKAGE'S COPYRIGHT HOLDER
+# This file is distributed under the same license as the PACKAGE package.
+# FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.
+#
+#, fuzzy
+msgid ""
+msgstr ""
+"POT-Creation-Date: {datetime_string}\n"
+"PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
+"Language: \n"
+"MIME-Version: 1.0\n"
+"Content-Type: text/plain; charset=UTF-8\n"
+"Content-Transfer-Encoding: 8bit\n"''')
+
+    if argv.s:
+        translation_dict = dict(sorted(translation_dict.items()))
+
+    for translatable_string, filenames in translation_dict.items():
+        translatable_string = translatable_string.replace("\"", "\\\"")
+        print()
+        print("#: " + " ".join(filenames))
+        print("msgid \"" + r"\\n\"\n\"".join(translatable_string.split("\n")) + "\"")
+        print("msgstr \"\"")
+
+    if errors > 0:
+        print(errors, "markup error(s) found", file=sys.stderr)
+        exit(1)
